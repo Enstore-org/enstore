593cc3635209a184ebb081fc627ff28ea44ce98a
Jon Bakken <bakken>
1998-03-05 06:52:35 +0000

    changes

--- modules/Devcodes.c
+++ modules/Devcodes.c
@@ -1,38 +1,80 @@
 #include <Python.h>
 
-static char Devcodes_Doc[] =  "Get major, minor device codes";
+#ifdef STDC_HEADERS
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+/* Since major is a function on SVR4, we can't use `ifndef major'.  */
+#ifdef MAJOR_IN_MKDEV
+#include <sys/mkdev.h>
+#define HAVE_MAJOR
+#endif
+
+#ifdef MAJOR_IN_SYSMACROS
+#include <sys/sysmacros.h>
+#define HAVE_MAJOR
+#endif
+
+#ifdef major                    /* Might be defined in sys/types.h.  */
+#define HAVE_MAJOR
+#endif
+
+#ifndef HAVE_MAJOR
+#define major(dev) (((dev) >> 8) & 0xff)
+#define minor(dev) ((dev) & 0xff)
+#define makedev(ma, mi) (((ma) << 8) | (mi))
+#endif
+#undef HAVE_MAJOR
+
+#include <sys/stat.h>
+
+
+static char Devcodes_Doc[] =  "Get major and  minor device codes";
 
 /*
    Method implementations
 */
 
-static char GetCodes_Doc[] = "Get major, minor device codes";
+static char GetCodes_Doc[] = "Get major and minor device codes";
 
 static PyObject*
 GetCodes(PyObject *self, PyObject *args)
 {
-char *filename;
+  char *filename;
+  int istatus;
+  int dmajor;
+  int dminor;
+  struct stat statbuf;
+
 /*
-	Get the arguements
+        Get the arguements
 */
-  PyArg_ParseTuple(args, "s", filename);
+  PyArg_ParseTuple(args, "s", &filename);
 /*
-	Get the tape stats from FTT
+        Get the tape stats from FTT
 */
-  return( Py_BuildValue ("{s:i,s:i}", 
-    "Major",3 ,
-    "Minor", 3
-     ) );
+  istatus = stat(filename, &statbuf);
+  dmajor = major(statbuf.st_dev);
+  dminor = minor(statbuf.st_dev);
+
+  return( Py_BuildValue ("{s:i,s:i}",
+                         "Major", dmajor,
+                         "Minor", dminor
+                         ) );
 }
 
 /*
-   Module Methods table. 
+   Module Methods table.
 
    There is one entry with four items for for each method in the module
 
    Entry 1 - the method name as used  in python
          2 - the c implementation function
-         3 - flags 
+         3 - flags
          4 - method documentation string
 */
 static PyMethodDef Devcodes_Methods[] = {
@@ -53,14 +95,14 @@ static PyMethodDef Devcodes_Methods[] = {
 
    Seond        - a list of the module methods
 
-   Third	- a doumentation string for the module
-  
+   Third        - a doumentation string for the module
+
    Fourth & Fifth - see Python/modsupport.c
 
 */
 void initDevcodes()
 {
-  (void) Py_InitModule4("Devcodes", Devcodes_Methods, Devcodes_Doc, 
+  (void) Py_InitModule4("Devcodes", Devcodes_Methods, Devcodes_Doc,
                                (PyObject*)NULL,PYTHON_API_VERSION);
 
 }
