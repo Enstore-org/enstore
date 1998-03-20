65483ee7d9756753710221fd9be7cb2e47e60336
Jon Bakken <bakken>
1998-03-20 03:28:23 +0000

    changes

--- test/nameserver.py
+++ test/nameserver.py
@@ -1,17 +1,16 @@
 import socket
 import sys
 import time
-import StringIO
 
 while 1:
     try:
-	socket.gethostbyaddr(socket.gethostname())
-	socket.gethostbyaddr("pcfar4.fnal.gov")
+        socket.gethostbyaddr(socket.gethostname())
+        socket.gethostbyaddr("pcfarm4.fnal.gov")
     except:
-	format = time.strftime("%c",time.localtime(time.time()))+" "+\
-		 str(sys.argv)+" "+\
-		 str(sys.exc_info()[0])+" "+\
-		 str(sys.exc_info()[1])+" "+\
-		 "continuing"
-	print format
+        format = time.strftime("%c",time.localtime(time.time()))+" "+\
+                 str(sys.argv)+" "+\
+                 str(sys.exc_info()[0])+" "+\
+                 str(sys.exc_info()[1])+" "+\
+                 "nameserver testing continuing"
+        print format
     time.sleep(1)
