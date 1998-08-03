d5ee9ab5f060d0570bcecebd6a6461b0d7e097a1
Ron Rechenmacher <ron>
1998-08-03 19:33:12 +0000

    chnage L to l for LINUX and better variable name, also option for absolute time stamp

--- modules/trace_delta.c
+++ modules/trace_delta.c
@@ -7,39 +7,44 @@ void
 main(  int	argc
      , char	**argv )
 {
-	double	x, y, delta, total;
+	double	x, sav, delta;
 	char	buf[200], buf2[200];
-	int	opt, opt_reverse=0;
-	double	nanoseconds=1.0;
-	int	tot_bytes=0;
+	int	opt, opt_reverse=0, opt_absolute=0;
 
 
     opt=1;
     for (opt=1; (opt<argc) && (strncmp(argv[opt],"-",1)==0); opt++)
     {   
-	if        (strcmp(argv[opt],"-r") == 0)
+	if      (strcmp(argv[opt],"-r") == 0)
 	{   opt_reverse = 1;
+	}
+	else if (strcmp(argv[opt],"-nr") == 0)
+	{   opt_reverse = 0;
+	}
+	else if (strcmp(argv[opt],"-a") == 0)
+	{   opt_absolute = 1;
 	}
     }
 
     while (fscanf(stdin,"%[^\n]\n",buf2) && !sscanf(buf2,"%lf%[^\n]\n",&x,buf))
 	fprintf( stdout, "%s\n", buf2 );
 
-    y = x; total = delta = 0;
-    fprintf( stdout, "%18.6lf %18.6lf%s\n", delta, x, buf );
-
-    while (fscanf( stdin,"%Lf%[^\n]\n", &x, buf ) == 2)
-    {   
-	if (opt_reverse)   delta = x - y;
-	else               delta = y - x;
-
-	total += delta;
-
-	if (nanoseconds != 1.0)
-	    fprintf( stdout, "%11.2fns ", nanoseconds*delta );
-
+    sav = x; delta = 0;
+    if (opt_absolute)
 	fprintf( stdout, "%18.6lf %18.6lf%s\n", delta, x, buf );
-        y = x;
+    else
+	fprintf( stdout, "%18.6lf %s\n", delta, buf );
+
+    while (fscanf( stdin,"%lf%[^\n]\n", &x, buf ) == 2)
+    {   
+	if (opt_reverse)   delta = x - sav;
+	else               delta = sav - x;
+
+	if (opt_absolute)
+	    fprintf( stdout, "%18.6lf %18.6lf%s\n", delta, x, buf );
+	else
+	    fprintf( stdout, "%18.6lf %s\n", delta, buf );
+        sav = x;
     }
 
 }
