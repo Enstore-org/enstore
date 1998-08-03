e45cb8aa7b25a37b6505ebaae8bf95e5a82efed4
Eileen Berman <berman>
1998-08-03 17:40:44 +0000

    change class name

--- src/client_defaults.py
+++ src/client_defaults.py
@@ -1,12 +1,5 @@
-class ClientDefaults:
+class GenericClient ():
 
     # check on alive status
     def alive(self):
         return self.send({'work':'alive'})
-
-    def start_backup(self):
-    	return self.send({'work':'start_backup'})
-
-    def stop_backup(self):
-    	return self.send({'work':'stop_backup'})	
-
