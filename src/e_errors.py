980e0193b0658ac413f795048143126402109ba9
Alexander Moibenko <moibenko>
1998-09-24 18:53:00 +0000

    enstore errors

---
+++ src/e_errors.py
@@ -0,0 +1,11 @@
+###############################################################################
+# src/$RCSfile$   $Revision$
+#
+import sys
+import types
+import pprint
+
+KEYERROR = 'KEYERROR'
+OK         = 'Ok'
+DOESNOTEXIST = 'DOESNOTEXIST'
+
