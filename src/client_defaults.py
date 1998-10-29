4507c5db02c9cace587dc8be0a8ced2a66535ba6
Eileen Berman <berman>
1998-10-29 22:30:07 +0000

    subsumed by generic_client.py

--- src/client_defaults.py
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# src/$RCSfile$   $Revision$
-#
-class GenericClient ():
-
-    # check on alive status
-    def alive(self, rcv_timeout=0, tries=0):
-        return self.send({'work':'alive'}, rcv_timeout, tries)
