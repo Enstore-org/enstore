1996e7f7e0346087dc65a5f67651fbbb6f39c905
Eileen Berman <berman>
1998-11-11 22:16:43 +0000

    use logname for print_id

--- src/volume_clerk.py
+++ src/volume_clerk.py
@@ -802,6 +802,10 @@ class VolumeClerk(VolumeClerkMethods,\
         #   get our port and host from the name server
         #   exit if the host is not this machine
         keys = self.csc.get("volume_clerk")
+        try:
+            self.print_id = keys['logname']
+        except:
+            pass
         Trace.init(keys["logname"])
         dispatching_worker.DispatchingWorker.__init__(self, (keys['hostip'],
 	                                              keys['port']))
