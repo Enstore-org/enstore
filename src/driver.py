11c01ee1db7e83e5271d8490b3ea85be0d12c82d
Ron Rechenmacher <ron>
1999-01-26 21:26:53 +0000

    comment ftt_stats for time being

--- src/driver.py
+++ src/driver.py
@@ -212,7 +212,7 @@ class  FTTDriver(GenericDriver) :
     def get_stats( self ) :
 	# Note: remaining_bytes is updated in write and
 	# fd_xfer (when opened for write)
-	ss = FTT.get_stats()
+	#ss = FTT.get_stats()
 	return {'remaining_bytes':self.remaining_bytes,
 		'wr_err':self.wr_err,
 		'rd_err':self.rd_err,
