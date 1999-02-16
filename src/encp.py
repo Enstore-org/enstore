3db46f365c042aa0bc0148faf22034897b7970c8
Alexander Moibenko <moibenko>
1999-02-16 15:36:59 +0000

    made errno analysis after  EXfer.fd_xfer less platform dependent

--- src/encp.py
+++ src/encp.py
@@ -466,7 +466,8 @@ def write_to_hsm(input, output, output_file_family='',
 		data_path_socket.close()
 		in_file.close()
 
-		if str(err_msg) =="(32, 'fd_xfer - write - Broken pipe')":
+		#if str(err_msg) =="(32, 'fd_xfer - write - Broken pipe')":
+		if err_msg[0] == errno.EPIPE:
 		    # could be network or could be mover closing socket...
 		    # try to get done_ticket
 		    try:
