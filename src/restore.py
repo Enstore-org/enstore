6d092c020c7e2dcb6a68c45d37483c3a834c487e
Chih Hao Huang <huangch>
1999-02-22 20:28:02 +0000

    reading mutiple journal files

--- src/restore.py
+++ src/restore.py
@@ -5,6 +5,8 @@
 import copy
 import db
 import regsub
+import os
+import generic_cs
 
 cursor_open = 0
 
@@ -16,57 +18,49 @@ class DbTable(db.DbTable):
 	# plus a "deletes" list containing the deleted keys
 
 	def __init__(self, dbname, logc='', indlst=[]):
-		self.journaling = 0
 		self.dbname = dbname
-		db.DbTable.__init__(self, dbname, logc, indlst)
+		db.DbTable.__init__(self, dbname, logc, indlst, 0)
+
+		# Now let's deal with all the journal files
+
 		jfile = self.dbHome+"/"+dbname+".jou"
+		cmd = "ls -c "+jfile+".*"
+		jfiles = os.popen(cmd).readlines()
+		jfiles.append(jfile+"\012")	# to be same as the rest
+
+		self.dict = {}
+		for jf in jfiles:
+			jf = jf[:-1]		# trim /012
+			try:
+				f = open(jf,"r")
+			except IOError:
+				print(ji+": not found")
+				sys.exit(0)
+			while 1:
+				l = f.readline()
+				if len(l) == 0: break
+				exec(l)
+			f.close()
+
+		# read it twice to get the deletion list
+
 		self.deletes = []
-		ji = open(jfile, 'r')
-		while 1:
-			l = ji.readline()
-			if len(l) == 0:
-				break
-			if l[0:3] == 'del':
-				k = regsub.split(l, "'")[1]
-				if not self.jou.has_key(k):
-					self.deletes.append(k)
-		ji.close()
-
-	def start_backup(self):
-		pass		# mask out start_backup()
-
-	def stop_backup(self):
-		pass		# mask out stop_backup()
-
-	def checkpoint(self):
-		pass		# mask out checkpoint()
-
-	# __setitem__() does not do automatic journaling
-
-	def __setitem__(self,key,value):
-		if 'db_flag' in value.keys(): del value['db_flag']
-		if self.journaling:
-			self.jou[key]=copy.deepcopy(value)
-		for name in self.inx.keys():
-			self.inx[name][value[name]]=key
-		if cursor_open==1:
-			self.cursor("update",key,value)
-			return
-		t=self.db.txn()
-		self.db[(key,t)]=value
-		t.commit()
-
-	# __delitem__() does not do automatic journaling
-
-	def __delitem__(self,key):
-		if self.journaling:
-			del self.jou[key]
-		t=self.db.txn()
-		del self.db[(key,t)]
-		t.commit()
-		for name in self.inx.keys():
-			del self.inx[name][(key,value[name])]
-
+		for jf in jfiles:
+			jf = jf[:-1]
+			try:
+				f = open(jf,"r")
+			except IOError:
+				print(ji+": not found")
+				sys.exit(0)
+			while 1:
+				l = f.readline()
+				if len(l) == 0: break
+				if l[0:3] == 'del':
+					k = regsub.split(l, "'")[1]
+					if not self.dict.has_key(k):
+						self.deletes.append(k)
+			f.close()
+			
 	# cross_check() cross check journal dictionary and database
 
 	def cross_check(self):
@@ -76,13 +70,13 @@ class DbTable(db.DbTable):
 		# check if the items in db has the same value of that
 		# in journal dictionary
 
-		for i in self.jou.keys():
+		for i in self.dict.keys():
 			if not self.has_key(i):
 				print 'M> key('+i+') is not in database'
 				error = error + 1
-			elif self.jou[i] != self.__getitem__(i):
+			elif self.dict[i] != self.__getitem__(i):
 				print 'C> database and journal disagree on key('+i+')'
-				print 'C>  journal['+i+'] =', self.jou[i]
+				print 'C>  journal['+i+'] =', self.dict[i]
 				print 'C> database['+i+'] =', self.__getitem__(i)
 				error = error + 1
 		# check if the deleted items are still in db
@@ -103,19 +97,19 @@ class DbTable(db.DbTable):
 		# check if the items in db has the same value of that
 		# in journal dictionary
 
-		for i in self.jou.keys():
+		for i in self.dict.keys():
 			if not self.has_key(i):
 				# add it
 				print 'M> key('+i+') is not in database'
-				self.__setitem__(i, self.jou[i])
+				self.__setitem__(i, self.dict[i])
 				error = error + 1
-				print 'F> database['+i+'] =', self.jou[i]
-			elif self.jou[i] != self.__getitem__(i):
+				print 'F> database['+i+'] =', self.dict[i]
+			elif self.dict[i] != self.__getitem__(i):
 				print 'C> database and journal disagree on key('+i+')'
-				print 'C>  journal['+i+'] =', self.jou[i]
+				print 'C>  journal['+i+'] =', self.dict[i]
 				print 'C> database['+i+'] =', self.__getitem__(i)
-				self.__setitem__(i, self.jou[i])
-				print 'F> database['+i+'] =', self.jou[i]
+				self.__setitem__(i, self.dict[i])
+				print 'F> database['+i+'] =', self.dict[i]
 				error = error + 1
 		# check if the deleted items are still in db
 
@@ -127,9 +121,3 @@ class DbTable(db.DbTable):
 				error = error + 1
 
 		return error
-
-	def journaling_on(self):
-		self.journaling = 1
-
-	def journaling_off(self):
-		self.journaling = 0
