a06e236a394b3afde6c46f4a81b228eb55b25567
Jon Bakken <bakken>
1999-03-10 22:11:20 +0000

    remove tabs, so lint can run properly.  There are too many mixed cases of tabs, spaces.. for lint to work properly

--- src/admin_clerk.py
+++ src/admin_clerk.py
@@ -34,100 +34,100 @@ class AdminClerkMethods(dispatching_worker.DispatchingWorker) :
             return
         joint=0
         if ticket['dbname']=="volume,file":
-	    dict=dictV
-	    joint=1
-	elif ticket['dbname']=="volume":
-	    dict=dictV
-	elif ticket['dbname']=="file"  :
-	    dict=dictF
-	else :
-	   ticket["status"] = (e_errors.DOESNOTEXIST, \
-			       "wrong name "+dbname+"for database table")
-	   self.reply_to_caller(ticket) 
-	   return
+            dict=dictV
+            joint=1
+        elif ticket['dbname']=="volume":
+            dict=dictV
+        elif ticket['dbname']=="file"  :
+            dict=dictF
+        else :
+           ticket["status"] = (e_errors.DOESNOTEXIST, \
+                               "wrong name "+dbname+"for database table")
+           self.reply_to_caller(ticket)
+           return
         criteria=self.modifyOpt(ticket['criteria'])
         del ticket['criteria']
         self.get_user_sockets(ticket)
         ticket["status"] = (e_errors.OK, None)
-	callback.write_tcp_socket(self.data_socket,ticket,
+        callback.write_tcp_socket(self.data_socket,ticket,
                                   "admin_clerk select, controlsocket")
-  	if len(criteria)==0 :
-     	   key=dict.next()
-      	   while key :
-		callback.write_tcp_buf(self.data_socket,repr(dict[key]),
+        if len(criteria)==0 :
+           key=dict.next()
+           while key :
+                callback.write_tcp_buf(self.data_socket,repr(dict[key]),
                                   "admin_clerk select, datasocket")
-	   	key=dict.next()
-  	else :
-    	   index=None
-    	   for k,val in criteria.items():
-		if dict.is_index(k):
-		   index=k
-		   break
-    	   if index != None:
-      		for main_crt in criteria[index]:
-         		for  key in dict.index(index,main_crt) :
-			   value=dict[key]
-			   if self.find_item(value,criteria,main_crt):
-			      if joint:
-				for fkey in dictF.index('external_label',\
-						value['external_label']):
-				   callback.write_tcp_buf(self.data_socket,\
-					repr(dictF[fkey]),"admin_clerk select,\
-					datasocket")
-			      else:
-			           callback.write_tcp_buf(self.data_socket,\
-				        repr(value),"admin_clerk select,\
-					datasocket")
-    	   else:
-      		key=dict.next()
-      		while key :
-        		value=dict[key]
-			if self.find_item(value,criteria):
-			  if joint:
-				for fkey in dictF.index('external_label',\
-						value['external_label']):
-				   callback.write_tcp_buf(self.data_socket,\
-					repr(dictF[fkey]),"admin_clerk select,\
-					datasocket")
-			  else:
-			  	callback.write_tcp_buf(self.data_socket,\
-					repr(value),"admin_clerk select, \
-					datasocket")
-			key=dict.next()
+                key=dict.next()
+        else :
+           index=None
+           for k,val in criteria.items():
+                if dict.is_index(k):
+                   index=k
+                   break
+           if index != None:
+                for main_crt in criteria[index]:
+                        for  key in dict.index(index,main_crt) :
+                           value=dict[key]
+                           if self.find_item(value,criteria,main_crt):
+                              if joint:
+                                for fkey in dictF.index('external_label',\
+                                                value['external_label']):
+                                   callback.write_tcp_buf(self.data_socket,\
+                                        repr(dictF[fkey]),"admin_clerk select,\
+                                        datasocket")
+                              else:
+                                   callback.write_tcp_buf(self.data_socket,\
+                                        repr(value),"admin_clerk select,\
+                                        datasocket")
+           else:
+                key=dict.next()
+                while key :
+                        value=dict[key]
+                        if self.find_item(value,criteria):
+                          if joint:
+                                for fkey in dictF.index('external_label',\
+                                                value['external_label']):
+                                   callback.write_tcp_buf(self.data_socket,\
+                                        repr(dictF[fkey]),"admin_clerk select,\
+                                        datasocket")
+                          else:
+                                callback.write_tcp_buf(self.data_socket,\
+                                        repr(value),"admin_clerk select, \
+                                        datasocket")
+                        key=dict.next()
         self.data_socket.close()
         callback.write_tcp_socket(self.control_socket,ticket,
                                   "admin_clerk select, controlsocket")
         self.control_socket.close()
-	return
+        return
    def modifyOpt(self,criteria):
-	if criteria.has_key('first_access'):
-	   criteria['first_access']=dbutil.timeOper(criteria['first_access'])
-	if criteria.has_key('last_access'):
-	   criteria['last_access']=dbutil.timeOper(criteria['last_access'])
-	if criteria.has_key('declared'):
-	   criteria['declared']=dbutil.timeOper(criteria['declared'])
-	if criteria.has_key('capacity'):
-	   criteria['capacity']=dbutil.numOper(criteria['capacity'])
-	if criteria.has_key('rem_bytes'):
-	   criteria['rem_bytes']=dbutil.numOper( criteria['rem_bytes'])
-	return criteria
+        if criteria.has_key('first_access'):
+           criteria['first_access']=dbutil.timeOper(criteria['first_access'])
+        if criteria.has_key('last_access'):
+           criteria['last_access']=dbutil.timeOper(criteria['last_access'])
+        if criteria.has_key('declared'):
+           criteria['declared']=dbutil.timeOper(criteria['declared'])
+        if criteria.has_key('capacity'):
+           criteria['capacity']=dbutil.numOper(criteria['capacity'])
+        if criteria.has_key('rem_bytes'):
+           criteria['rem_bytes']=dbutil.numOper( criteria['rem_bytes'])
+        return criteria
    def find_item(self,value,criteria,main_crt=""):
         found=1
-	for k,v in  criteria.items():
+        for k,v in  criteria.items():
            if k==main_crt:
-		continue
-	   operator=1
-	   val=value[k]
-	   if type(v)==type(dbutil.numOper()):
-		operator=0
-		if v.numcmp(val):
-		   found=0
-		   break
-           if operator : 		 	
-	        if val not in v:
-		   found=0
-		   break
-	return found
+                continue
+           operator=1
+           val=value[k]
+           if type(v)==type(dbutil.numOper()):
+                operator=0
+                if v.numcmp(val):
+                   found=0
+                   break
+           if operator :
+                if val not in v:
+                   found=0
+                   break
+        return found
     # get a port for the data transfer
     # tell the user I'm your volume clerk and here's your ticket
    def get_user_sockets(self, ticket) :
@@ -146,8 +146,8 @@ class AdminClerk(AdminClerkMethods, generic_server.GenericServer) :
     def __init__(self, csc=0, verbose=0, host=interface.default_host(), \
                  port=interface.default_port()):
         Trace.trace(10, '{__init__')
-	self.print_id = "ACS"
-	self.verbose = verbose
+        self.print_id = "ACS"
+        self.verbose = verbose
         # get the config server
         configuration_client.set_csc(self, csc, host, port, verbose)
         #   pretend that we are the test system
@@ -161,7 +161,7 @@ class AdminClerk(AdminClerkMethods, generic_server.GenericServer) :
             pass
         Trace.init(keys["logname"])
         dispatching_worker.DispatchingWorker.__init__(self, (keys['hostip'],
-	                                              keys['port']))
+                                                      keys['port']))
         # get a logger
         self.logc = log_client.LoggerClient(self.csc, keys["logname"], \
                                             'logserver', 0)
@@ -183,7 +183,7 @@ if __name__=="__main__":
     ac = AdminClerk(0, intf.verbose, intf.config_host, intf.config_port)
 
     indlst=['media_type','file_family','library']
-    dictV = db.DbTable("volume",ac.logc,indlst) 
+    dictV = db.DbTable("volume",ac.logc,indlst)
     indlst=['external_label']
     dictF = db.DbTable("file",ac.logc,indlst)
     while 1:
@@ -192,7 +192,7 @@ if __name__=="__main__":
             ac.logc.send(log_client.INFO, 1, "Admin Clerk (re)starting")
             ac.serve_forever()
         except:
-	    ac.serve_forever_error("admin clerk", ac.logc)
+            ac.serve_forever_error("admin clerk", ac.logc)
             continue
 
     Trace.trace(1,"Admin Clerk finished") # impossible
