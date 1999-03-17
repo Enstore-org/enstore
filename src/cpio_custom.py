8cdb4642c941c46655933db0dd26f84629a91c22
Charles Waldman <cgw>
1999-03-17 17:39:38 +0000

    fix typo in hex constants

--- src/cpio_custom.py
+++ src/cpio_custom.py
@@ -80,9 +80,10 @@ To extract:   cpio -idmv < archive
 
 """
 
-def hex_string_to_signed_int(s):
+def hex_string_to_signed_int32(s):
     x=string.atol(s,16)
-    if x > 0x7fffffffL: x = x - 0x10000000L
+    if x > 0x7fffffffL:
+        x = x - 0x100000000L
     return int(x)
 
 
@@ -278,7 +279,7 @@ def encrc( buffer ):
         # We have switched to 32 bit crcs.  This means that string.atoi now
         # causes an overflow when the crc value has the sign bit set.  The most
         # obvious solution of going to atol doesn't work. 
-        crc=hex_string_to_signed_int(buffer[data_offset:data_offset+8])
+        crc = hex_string_to_signed_int32(buffer[data_offset:data_offset+8]))
         return crc
 
 ###############################################################################
