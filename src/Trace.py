901787c0d33bd5b5449456e25031ebd96fd9ad89
Ron Rechenmacher <ron>
1999-03-23 23:45:19 +0000

    prepare for updating "trace"

---
+++ src/Trace.py
@@ -0,0 +1,9 @@
+#  This file (Trace.py) was created by Ron Rechenmacher <ron@fnal.gov> on
+#  Mar 23, 1999. "TERMS AND CONDITIONS" governing this file are in the README
+#  or COPYING file. If you do not have such a file, one can be obtained by
+#  contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
+#  $RCSfile$
+#  $Revision$
+#  $Date$
+
+from Ptrace import *
