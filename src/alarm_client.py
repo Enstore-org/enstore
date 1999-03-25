3fb72551b8551ceaf22116db035a6bcabf498bb8
Eileen Berman <berman>
1999-03-25 20:00:15 +0000

    first slightly tested version, supports server generated alarms only

--- src/alarm_client.py
+++ src/alarm_client.py
@@ -1,6 +1,7 @@
 # system imports
 #
 import sys
+import os
 
 # enstore imports
 import configuration_client
@@ -9,88 +10,79 @@ import generic_cs
 import udp_client
 import interface
 import Trace
+import e_errors
 
 class AlarmClient(generic_client.GenericClient):
 
-    def __init__(self, csc=0, verbose=0, host=interface.default_host(), \
-                 port=interface.default_port()):
+    def __init__(self, csc=0, verbose=0, \
+                 host=interface.default_host(), \
+                 port=interface.default_port(), \
+                 source="ALRMC"):
         # we always need to be talking to our configuration server
         Trace.trace(10,'{__init__')
-	self.print_id = "ALRMC"
+        self.print_id = "ALRMC"
+        self.pid = os.getpid()
+        self.uid = os.getuid()
+        self.source = source
         configuration_client.set_csc(self, csc, host, port, verbose)
         self.u = udp_client.UDPClient()
-	self.verbose = verbose
+        self.verbose = verbose
         ticket = self.csc.get("alarm_server")
-	try:
+        try:
             self.print_id = ticket['logname']
-        except:
+        except KeyError:
             pass
         Trace.trace(10,'}__init')
 
     def send (self, ticket, rcv_timeout=0, tries=0):
-        Trace.trace(12,"{send"+repr(ticket))
+        Trace.trace(12,"{send "+repr(ticket))
         # who's our alarm server that we should send the ticket to?
         vticket = self.csc.get("alarm_server")
         # send user ticket and return answer back
-        Trace.trace(12,"send addr="+repr((vticket['hostip'], vticket['port'])))
-        s = self.u.send(ticket, (vticket['hostip'], vticket['port']), rcv_timeout, tries )
-        Trace.trace(12,"}send"+repr(s))
+        Trace.trace(12,"send addr="+repr((vticket['hostip'], \
+                                           vticket['port'])))
+        s = self.u.send(ticket, (vticket['hostip'], vticket['port']), \
+                        rcv_timeout, tries )
+        Trace.trace(12,"}send "+repr(s))
         return s
 
-    def status(self, str, server):
-	Trace.trace(16,"{status "+repr(str))
-	# format the ticket to send to the alarm server
-	ticket = {"work" : "status", \
-	          "status" : str , \
-	          "server" : server }
-	s = self.send(ticket)
-	Trace.trace(16,"}status ")
+    def alarm(self, severity=e_errors.DEFAULT_SEVERITY, \
+              root_error=e_errors.DEFAULT_ROOT_ERROR,
+              alarm_info={}, rcv_timeout=0, tries=0):
+        Trace.trace(12,"{alarm ")
+        # format the ticket to send to the alarm server
+        ticket = {}
+        ticket['work'] = "post_alarm"
+        ticket['root_error'] = root_error
+        ticket['severity'] = severity
+        ticket['uid'] = self.uid
+        ticket['pid'] = self.pid
+        ticket['source'] = self.source
+	ticket.update(alarm_info)
+        s = self.send(ticket, rcv_timeout, tries )
 	return s
-	
-class AlarmClientInterface(interface.Interface):
+
+class AlarmClientInterface(generic_client.GenericClientInterface,\
+                           interface.Interface):
 
     def __init__(self):
         Trace.trace(10,'{alarmci.__init__')
         # fill in the defaults for the possible options
-        self.alive = 0
         self.alive_rcv_timeout = 0
         self.alive_retries = 0
-	self.verbose = 0
-	self.got_server_verbose = 0
-	self.dump = 0
-	self.status = ""
+        self.alarm = 0
+        self.severity = e_errors.DEFAULT_SEVERITY
+        self.root_error = e_errors.DEFAULT_ROOT_ERROR
+        generic_client.GenericClientInterface.__init__(self)
         interface.Interface.__init__(self)
 
-        # now parse the options
-        self.parse_options()
-        Trace.trace(10,'}alarmci.__init')
-
-    #  define our specific help
-    def parameters(self):
-        if 0: print self # quiet lint
-        return "server"
-
-    # parse the options like normal but see if we have a server
-    def parse_options(self):
-        interface.Interface.parse_options(self)
-        # see if we have a server
-        if len(self.args) < 1 :
-	    if self.status:
-	        self.missing_parameter(self.parameters())
-	        self.print_help()
-	        sys.exit(1)
-	    else:
-	        self.server = ""
-        else:
-            self.server = self.args[0]
+        Trace.trace(10,'}alarmci.__init__')
 
     # define the command line options that are valid
     def options(self):
         Trace.trace(16,"{}options")
-        return self.config_options()+self.alive_options() +\
-	       self.verbose_options() +\
-	       ["dump", "status="] +\
-               self.help_options()
+        return self.client_options() +\
+	       ["alarm", "severity=", "root_error="]
 
 
 if __name__ == "__main__" :
@@ -116,8 +108,8 @@ if __name__ == "__main__" :
         ticket = alc.dump(intf.alive_rcv_timeout, intf.alive_retries)
 	msg_id = generic_cs.CLIENT
 
-    elif not intf.status == "":
-        ticket = alc.status(intf.status, intf.server)
+    elif not intf.alarm == 0:
+        ticket = alc.alarm(intf.severity, intf.root_error)
 	msg_id = generic_cs.CLIENT
 
     else:
--- src/alarm_server.py
+++ src/alarm_server.py
@@ -1,14 +1,17 @@
 #
 # system import
 import sys
+import socket
 
 # enstore imports
 import log_client
 import configuration_client
 import dispatching_worker
 import interface
+import enstore_status
 import generic_server
 import Trace
+import alarm
 import e_errors
 
 def default_alive_rcv_timeout():
@@ -17,55 +20,156 @@ def default_alive_rcv_timeout():
 def default_alive_retries():
     return 2
 
+DEFAULT_FILE_NAME = "/enstore_alarms.txt"
+DEFAULT_PATROL_FILE_NAME = "/enstore_patrol.txt"
+
 class AlarmServerMethods(dispatching_worker.DispatchingWorker):
 
-    # dump everything we have
-    def dump(self, ticket):
-	Trace.trace(10,"{dump "+repr(ticket))
-        ticket["status"] = (e_errors.OK, None)
-	self.send_reply(ticket)
-	Trace.trace(10,"}dump")
+    # pull out alarm info from the ticket and raise the alarm
+    def post_alarm(self, ticket):
+        Trace.trace(12, "{post_alarm "+repr(ticket))
+        if ticket.has_key("severity"):
+            severity = ticket["severity"]
+            # remove this entry from the dictionary, so if won't be included
+            # as part of alarm_info
+            del ticket["severity"]
+        else:
+            severity = e_errors.DEFAULT_SEVERITY
+        if ticket.has_key("root_error"):
+            root_error = ticket["root_error"]
+            # remove this entry from the dictionary, so if won't be included
+            # as part of alarm_info
+            del ticket["root_error"]
+        else:
+            root_error = e_errors.DEFAULT_ROOT_ERROR
+        # remove this entry from the dictionary, so it won't be included
+        # as part of alarm_info
+        if ticket.has_key("work"):
+            del ticket["work"]
 
+        self.alarm(severity, root_error, ticket)
+
+        # send the reply to the client
+        ret_ticket = { 'status'   : (e_errors.OK, None) }
+        self.send_reply(ret_ticket)
+        Trace.trace(12, "}post_alarm")
+
+    # raise the alarm
+    def alarm(self, severity=e_errors.DEFAULT_SEVERITY,\
+              root_error=e_errors.DEFAULT_ROOT_ERROR, alarm_info={}):
+        Trace.trace(12,"{alarm ")
+        # find out where the alarm came from
+        try:
+            host = socket.gethostbyaddr(self.reply_address[0])[0]
+        except:
+            host = str(sys.exc_info()[1])
+        # get a new alarm
+        theAlarm = alarm.Alarm(host, severity, root_error, alarm_info)
+        # save it in memory for us now
+        self.alarms[theAlarm.timedate] = theAlarm
+        # write it to the persistent file
+        self.write_alarm_file(theAlarm)
+        # write it out to the patrol file
+        self.write_patrol_file()
+        Trace.trace(12,"}alarm ")
+
+    def write_alarm_file(self, alarm):
+        self.alarm_file.open()
+        self.alarm_file.write(alarm)
+        self.alarm_file.close()
+
+    def write_patrol_file(self):
+        if not self.alarms == {}:
+            keys = self.alarms.keys()
+            keys.sort()
+            self.patrol_file.open()
+            for key in keys:
+                self.patrol_file.write(self.alarms[key])
+            else:
+                self.patrol_file.close()
+
+    def get_log_path(self):
+        log = self.csc.get("logserver")
+        if log.has_key("log_file_path"):
+            return log["log_file_path"]
+        else:
+            return "."
+        
+    # read the persistent alarm file if it exists.  this reads in all the
+    # alarms that have not been resolved.
+    def get_alarm_file(self):
+        # the alarm file lives in the same directory as the log file
+        self.alarm_file = enstore_status.EnAlarmFile(self.get_log_path()+ \
+                                                     DEFAULT_FILE_NAME)
+        self.alarm_file.open('r')
+        self.alarms = self.alarm_file.read()
+        self.alarm_file.close()
+
+    # get the location of the file we will create for patrol and write any
+    # alarms to it
+    def get_patrol_file(self):
+        # the patrol file lives in the same directory as the log file
+        self.patrol_file = enstore_status.EnPatrolFile(self.get_log_path()+ \
+                                                      DEFAULT_PATROL_FILE_NAME)
+        self.write_patrol_file()
+
+    def handle_alarms(self):
+        pass
+
+                    
 class AlarmServer(AlarmServerMethods, generic_server.GenericServer):
 
     def __init__(self, csc=0, verbose=0, host=interface.default_host(), \
                  port=interface.default_port()):
-	Trace.trace(10, '{__init__')
-	self.print_id = "ALRMS"
-	self.verbose = verbose
+        Trace.trace(10, '{__init__')
+        self.alarms = {}
+        self.print_id = "ALRMS"
+        self.verbose = verbose
 
 	# get the config server
-	configuration_client.set_csc(self, csc, host, port, verbose)
-	keys = self.csc.get("alarm_server")
-	self.hostip = keys['hostip']
+        configuration_client.set_csc(self, csc, host, port, verbose)
+        keys = self.csc.get("alarm_server")
+        self.hostip = keys['hostip']
         Trace.init(keys["logname"])
-	try:
-	    self.print_id = keys['logname']
-	except:
-	    pass
-	dispatching_worker.DispatchingWorker.__init__(self, (keys['hostip'], \
+        try:
+            self.print_id = keys['logname']
+        except KeyError:
+            pass
+        dispatching_worker.DispatchingWorker.__init__(self, (keys['hostip'], \
 	                                              keys['port']))
         # get a logger
         self.logc = log_client.LoggerClient(self.csc, keys["logname"], \
                                             'logserver', 0)
-	Trace.trace(10, '}__init__')
+
+        # see if an alarm file exists. if it does, open it and read it in.
+        # these are the alarms that have not been dealt with.
+        self.get_alarm_file()
+
+        # get the patrol file name and location and write any current alarms
+        # out to it.
+        self.get_patrol_file()
+
+        # look through all the alarms and see if we need to do anything with
+        # them
+        self.handle_alarms()
+        Trace.trace(10, '}__init__')
 
 class AlarmServerInterface(interface.Interface):
 
     def __init__(self):
-	Trace.trace(10,'{alrmi.__init__')
-	# fill in the defaults for possible options
-	self.verbose = 0
-	interface.Interface.__init__(self)
+        Trace.trace(10,'{alrmi.__init__')
+        # fill in the defaults for possible options
+        self.verbose = 0
+        interface.Interface.__init__(self)
 
-	# now parse the options
-	self.parse_options()
-	Trace.trace(10,'}iqsi.__init__')
+        # now parse the options
+        self.parse_options()
+        Trace.trace(10,'}iqsi.__init__')
 
     # define the command line options that are valid
     def options(self):
-	Trace.trace(16, "{}options")
-	return self.config_options()+\
+        Trace.trace(16, "{}options")
+        return self.config_options()+\
 	       ["verbose="] +\
 	       self.help_options()
 
@@ -82,7 +186,7 @@ if __name__ == "__main__":
     while 1:
         try:
             Trace.trace(1,'Alarm Server (re)starting')
-            als.logc.send(log_client.INFO, 1, "Alarm Server (re)starting")
+            als.logc.send(e_errors.INFO, 1, "Alarm Server (re)starting")
             als.serve_forever()
         except:
 	    als.serve_forever_error("alarm_server", als.logc)
