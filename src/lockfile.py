972739fcbf1f7365e321fb13dba4538c1e8d16fb
Eileen Berman <berman>
1999-04-16 19:07:54 +0000

    remove beginning and ending traces

--- src/lockfile.py
+++ src/lockfile.py
@@ -11,21 +11,17 @@ import struct, fcntl, FCNTL
 import Trace
 
 def _lock(f, op):
-	Trace.trace(21,'{_lock')
 	dummy = fcntl.fcntl(f.fileno(), FCNTL.F_SETLKW,
 			    struct.pack('2h8l', op,
 					0, 0, 0, 0, 0, 0, 0, 0, 0))
-	Trace.trace(21,'}_lock '+repr(dummy))
+	Trace.trace(21,'_lock '+repr(dummy))
 	
 def writelock(f):
-	Trace.trace(20,'{}writelock')
 	_lock(f, FCNTL.F_WRLCK)
 
 def readlock(f):
-	Trace.trace(20,'{}readlock')
 	_lock(f, FCNTL.F_RDLCK)
 
 def unlock(f):
-	Trace.trace(20,'{}unlock')
 	_lock(f, FCNTL.F_UNLCK)
 
--- src/timeofday.py
+++ src/timeofday.py
@@ -5,8 +5,6 @@
 import time
 
 #enstore imports
-import Trace
 
 def tod() :
-    Trace.trace(20,'{}tod')
     return time.strftime("%c",time.localtime(time.time()))
