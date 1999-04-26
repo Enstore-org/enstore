537d9c7f833bd2f7a363ce11d07bbf94c73670b0
Charles Waldman <cgw>
1999-04-26 19:53:54 +0000

    add missing base (16) in atol conversion of crc value

--- src/cpio.py
+++ src/cpio.py
@@ -272,7 +272,7 @@ def encrc( buffer ):
         data_offset =data_offset + (4-(data_offset%4))%4
         # We have switched to 32 bit crcs.
         # We are using Python long integers to represent them so they don't go negative
-        crc = string.atol("0x"+buffer[data_offset:data_offset+8])
+        crc = string.atol(buffer[data_offset:data_offset+8],16)
         return crc
 
 ###############################################################################
