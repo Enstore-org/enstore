7a475a091c999c8ccfacc1030a1eea13ef41a6f2
Chih Hao Huang <huangch>
1999-06-02 20:12:31 +0000

    new face of restore.py

--- src/restore.py
+++ src/restore.py
@@ -17,16 +17,16 @@ class DbTable(db.DbTable):
 	# __init__() is almost the same as db.DbTable.__init__()
 	# plus a "deletes" list containing the deleted keys
 
-	def __init__(self, dbname, indlst=[]):
-		self.dbname = dbname
-		db.DbTable.__init__(self, dbname, indlst, 0)
+	def __init__(self, dbname, dbHome, jouHome, indlst=[]):
+		db.DbTable.__init__(self, dbname, dbHome, jouHome, indlst, 0)
 
 		# Now let's deal with all the journal files
 
-		jfile = self.dbHome+"/"+dbname+".jou"
-		cmd = "ls -c "+jfile+".* 2>/dev/null"
+		jfile = self.jouHome+"/"+dbname+".jou"
+		cmd = "ls -1 "+jfile+".* 2>/dev/null"
 		jfiles = os.popen(cmd).readlines()
-		jfiles.append(jfile+"\012")	# to be same as the rest
+		if jfiles:
+			jfiles.append(jfile+"\012")	# to be same as the rest
 
 		self.dict = {}
 		for jf in jfiles:
@@ -122,13 +122,102 @@ class DbTable(db.DbTable):
 
 		return error
 
+
 if __name__ == "__main__":		# main
 
-	for i in sys.argv[1:]:
-		d = DbTable(i, [])
+	import interface
+	import configuration_client
+	import socket
+
+	intf = interface.Interface()
+
+	# find dbHome and jouHome
+	try:
+		dbInfo = configuration_client.ConfigurationClient(
+			(intf.config_host, intf.config_port)).get(
+			database')
+        	dbHome = dbInfo['db_dir']
+        	try:  # backward compatible
+			jouHome = dbInfo['jou_dir']
+		except:
+			jouHome = dbHome
+	except:
+		dbHome = os.environ['ENSTORE_DIR']
+		jouHome = dbHome
+
+	# find backup host and path
+	backup_config = configuration_client.ConfigurationClient(
+		(interface.default_host(),
+		interface.default_port())).get('backup')
+
+	local_host = socket.gethostname()
+	try:
+		bckHost = backup_config['host']
+	except:
+		bckHost = local_host
+
+	bckHome = backup_config['dir']
+
+	print " dbHome = "+dbHome
+	print "jouHome = "+jouHome
+	print "bckHost = "+bckHost
+	print "bckHome = "+bckHome
+
+	# find the last backup
+	# so far, we only deal with the last backup
+	# In theory, if it is not good, we should go all the way back
+	# to the "last good backup"
+
+	cmd = "rsh "+bckHost+" ls -1t "+bckHome+" | head -1"
+	bckHome = os.popen(cmd).readline()
+
+	print "restore from "+bckHost+":"+bckHome
+
+	# databases
+	dbs = ['file', 'volume']
+
+	# go to dbHome
+	os.chdir(dbHome)
+
+	# save the current (bad?) database
+	for i in dbs:
+		cmd = "mv "+i+" "+i+".saved"
+		os.system(cmd)
+
+	# save all log.* files
+	cmd = "ls -1 log.*"
+	logs = os.popen(cmd).readlines()
+	for i in logs:
+		cmd = "mv "+i+" "+i+".saved"
+		os.system(cmd)
+
+	# get the database file from the backup
+
+	cmd = "rsh -n "+bckHost+" dd if="+bckHome+"/dbase.tar bs=20b | tar xvBfb - 20"
+	os.system(cmd)
+
+	# run db_recover to put the databases in consistent state
+
+	os.system("db_recover")
+
+	# get the journal files in place
+	# basically, we only need current journal file to restore the
+	# databases from the previous backup. Just to be paranoid,
+	# we still get some previous journal files ...
+
+	os.chdir(jouHome)
+
+	for i in dbs:
+		cmd = "rsh -n "+bckHost+" dd if="+bckHome+"/"+i+\
+			".tar bs=20b | tar xvBfb - 20 '"+i+".jou*'"
+		os.system(cmd)
+
+	for i in dbs:
+		d = DbTable(i, dbHome, jouHome, [])
 		print "Checking "+i+" ... "
 		err = d.cross_check()
 		if err:
-			print "Fixing "+i+" ... "
+			print "Fixing "+i+" ... ",
 			d.fix_db()
-
+		else:
+			print i+" is OK"
