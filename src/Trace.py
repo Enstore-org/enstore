73d2610b95080362495789577269badfbfc78cf2
Jon Bakken <bakken>
1999-07-09 22:46:18 +0000

    turn on profiling again

--- src/Trace.py
+++ src/Trace.py
@@ -76,5 +76,6 @@ def default_log_func( time, pid, name, args ):
 set_log_func( default_log_func )
 
 
-# let user turn this on manully... sys.setprofile(Ptrace.profile)
+# let user turn this on manully...
+sys.setprofile(Ptrace.profile)
 
