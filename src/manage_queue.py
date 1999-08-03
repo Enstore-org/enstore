0f5977ea32bc38b43cd7cf026f687f7f4934d706
Alexander Moibenko <moibenko>
1999-08-03 18:20:57 +0000

    check if ticket has ['vc']['external_label']

--- src/manage_queue.py
+++ src/manage_queue.py
@@ -165,7 +165,8 @@ class LM_Queue:
 	else:
 	    # no match has been found, return first for this volume
 	    for w in self.queue:
-		if w['vc']['external_label'] == v["external_label"]:
+		if (w['vc'].has_key(w'external_label') and
+                    ['vc']['external_label'] == v["external_label"]):
 		    w['at_the_top'] = w['at_the_top']+1
 		    w['status'] = v['status']
 		    break
