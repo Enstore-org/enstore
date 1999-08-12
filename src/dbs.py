e2a714d600dfe7a65dd1962111bfaffb5dc387f0
Eileen Berman <berman>
1999-08-12 16:04:34 +0000

    remove putting cmd to background as it goofs up restart

--- src/dbs.py
+++ src/dbs.py
@@ -24,11 +24,11 @@ import Trace
 # loop on rsh and dump rgang
 CMDa = "(F=~/\\\\\\`hostname\\\\\\`."
 CMDb = ";echo >>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1;. /usr/local/etc/setups.sh>>\\\\\\$F 2>&1; setup enstore>>\\\\\\$F 2>&1;"
-CMDc = ";echo >>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1;. /usr/local/etc/setups.sh>>\\\\\\$F 2>&1; setup enstore -r /devel/berman/enstore  -M ups -m enstore.table>>\\\\\\$F 2>&1;"
+CMDc = ";echo >>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1;. /usr/local/etc/setups.sh>>\\\\\\$F 2>&1; setup enstore efb>>\\\\\\$F 2>&1;"
 CMD1 = "%s%s%s"%(CMDa, "database", CMDb)
 #CMD1 = "%s%s%s"%(CMDa, "database", CMDc)
 # the tee is not robust - need to add code to check if we can write to tty (that is connected to console server)
-CMD2 = "|tee /dev/console>>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1) 1>&- 2>&- <&- &"
+CMD2 = "|tee /dev/console>>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1) 1>&- 2>&- <&-"
 
 def send_dbs_cmd(intf, farmlet, db):
     # build the command and send it to the correct node
