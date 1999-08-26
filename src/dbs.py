df090581acfea5057d9547ffdb476406680b94b2
Charles Waldman <cgw>
1999-08-26 17:22:15 +0000

    reorder code to avoid forward references (passes lint now)

--- src/dbs.py
+++ src/dbs.py
@@ -22,6 +22,7 @@ import Trace
 # entire enstore system.
 # Yes, all those blasted slashes are needed and I agree it is insane. We should
 # loop on rsh and dump rgang
+
 CMDa = "(F=~/\\\\\\`hostname\\\\\\`."
 CMDb = ";echo >>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1;. /usr/local/etc/setups.sh>>\\\\\\$F 2>&1; setup enstore>>\\\\\\$F 2>&1;"
 CMDc = ";echo >>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1;. /usr/local/etc/setups.sh>>\\\\\\$F 2>&1; setup enstore efb>>\\\\\\$F 2>&1;"
@@ -29,6 +30,124 @@ CMD1 = "%s%s%s"%(CMDa, "database", CMDb)
 #CMD1 = "%s%s%s"%(CMDa, "database", CMDc)
 # the tee is not robust - need to add code to check if we can write to tty (that is connected to console server)
 CMD2 = " 2>&1 |tee /dev/console>>\\\\\\$F 2>&1;date>>\\\\\\$F 2>&1) 1>&- 2>&- <&- &"
+
+
+
+class DBS(generic_client.GenericClient):
+    
+    def __init__(self, csc):
+	# mainly we want to get the config client and log client.
+	generic_client.GenericClient.__init__(self, csc, "DBS")
+
+# similar to db.DbTable, without automatic journaling and backup up.
+class DbTable(db.DbTable):
+
+	# __init__() is almost the same as db.DbTable.__init__()
+	# plus a "deletes" list containing the deleted keys
+
+	def __init__(self, dbname, dbHome, jouHome, indlst=[]):
+		db.DbTable.__init__(self, dbname, dbHome, jouHome, indlst, 0)
+
+		# Now let's deal with all the journal files
+
+		jfile = self.jouHome+"/"+dbname+".jou"
+		cmd = "ls -1 "+jfile+".* 2>/dev/null"
+		jfiles = os.popen(cmd).readlines()
+		jfiles.append(jfile+"\012")	# to be same as the rest
+		self.dict = {}
+		for jf in jfiles:
+			jf = jf[:-1]		# trim \012
+			try:
+				f = open(jf,"r")
+			except IOError:
+				print(jf+": not found")
+				sys.exit(0)
+			while 1:
+				l = f.readline()
+				if len(l) == 0: break
+				exec(l)
+			f.close()
+
+		# read it twice to get the deletion list
+
+		self.deletes = []
+		for jf in jfiles:
+			jf = jf[:-1]
+			try:
+				f = open(jf,"r")
+			except IOError:
+				print(jf+": not found")
+				sys.exit(0)
+			while 1:
+				l = f.readline()
+				if len(l) == 0: break
+				if l[0:3] == 'del':
+					k = string.split(l, "'")[1]
+					if not self.dict.has_key(k):
+						self.deletes.append(k)
+			f.close()
+			
+	# cross_check() cross check journal dictionary and database
+
+	def cross_check(self):
+
+		error = 0
+
+		# check if the items in db has the same value of that
+		# in journal dictionary
+
+		for i in self.dict.keys():
+			if not self.has_key(i):
+				print 'M> key('+i+') is not in database'
+				error = error + 1
+			elif `self.dict[i]` != `self.__getitem__(i)`:
+				print 'C> database and journal disagree on key('+i+')'
+				print 'C>  journal['+i+'] =', self.dict[i]
+				print 'C> database['+i+'] =', self.__getitem__(i)
+				error = error + 1
+		# check if the deleted items are still in db
+
+		for i in self.deletes:
+			if self.has_key(i):
+				print 'D> database['+i+'] should be deleted'
+				error = error + 1
+
+		return error
+
+	# fix_db() fix db according to journal dictionary
+
+	def fix_db(self):
+
+		error = 0
+
+		# check if the items in db has the same value of that
+		# in journal dictionary
+
+		for i in self.dict.keys():
+			if not self.has_key(i):
+				# add it
+				print 'M> key('+i+') is not in database'
+				self.__setitem__(i, self.dict[i])
+				error = error + 1
+				print 'F> database['+i+'] =', self.dict[i]
+			elif `self.dict[i]` != `self.__getitem__(i)`:
+				print 'C> database and journal disagree on key('+i+')'
+				print 'C>  journal['+i+'] =', self.dict[i]
+				print 'C> database['+i+'] =', self.__getitem__(i)
+				self.__setitem__(i, self.dict[i])
+				print 'F> database['+i+'] =', self.dict[i]
+				error = error + 1
+		# check if the deleted items are still in db
+
+		for i in self.deletes:
+			if self.has_key(i):
+				print 'D> database['+i+'] should be deleted'
+				self.__delitem__(i)
+				print 'F> delete database['+i+']'
+				error = error + 1
+
+		return error
+
 
 def send_dbs_cmd(intf, farmlet, db):
     # build the command and send it to the correct node
@@ -282,121 +401,6 @@ def do_work(intf):
 	    sys.exit(rtn)
 
     return rtn
-
-class DBS(generic_client.GenericClient):
-    
-    def __init__(self, csc):
-	# mainly we want to get the config client and log client.
-	generic_client.GenericClient.__init__(self, csc, "DBS")
-
-# similar to db.DbTable, without automatic journaling and backup up.
-class DbTable(db.DbTable):
-
-	# __init__() is almost the same as db.DbTable.__init__()
-	# plus a "deletes" list containing the deleted keys
-
-	def __init__(self, dbname, dbHome, jouHome, indlst=[]):
-		db.DbTable.__init__(self, dbname, dbHome, jouHome, indlst, 0)
-
-		# Now let's deal with all the journal files
-
-		jfile = self.jouHome+"/"+dbname+".jou"
-		cmd = "ls -1 "+jfile+".* 2>/dev/null"
-		jfiles = os.popen(cmd).readlines()
-		jfiles.append(jfile+"\012")	# to be same as the rest
-		self.dict = {}
-		for jf in jfiles:
-			jf = jf[:-1]		# trim \012
-			try:
-				f = open(jf,"r")
-			except IOError:
-				print(jf+": not found")
-				sys.exit(0)
-			while 1:
-				l = f.readline()
-				if len(l) == 0: break
-				exec(l)
-			f.close()
-
-		# read it twice to get the deletion list
-
-		self.deletes = []
-		for jf in jfiles:
-			jf = jf[:-1]
-			try:
-				f = open(jf,"r")
-			except IOError:
-				print(jf+": not found")
-				sys.exit(0)
-			while 1:
-				l = f.readline()
-				if len(l) == 0: break
-				if l[0:3] == 'del':
-					k = string.split(l, "'")[1]
-					if not self.dict.has_key(k):
-						self.deletes.append(k)
-			f.close()
-			
-	# cross_check() cross check journal dictionary and database
-
-	def cross_check(self):
-
-		error = 0
-
-		# check if the items in db has the same value of that
-		# in journal dictionary
-
-		for i in self.dict.keys():
-			if not self.has_key(i):
-				print 'M> key('+i+') is not in database'
-				error = error + 1
-			elif `self.dict[i]` != `self.__getitem__(i)`:
-				print 'C> database and journal disagree on key('+i+')'
-				print 'C>  journal['+i+'] =', self.dict[i]
-				print 'C> database['+i+'] =', self.__getitem__(i)
-				error = error + 1
-		# check if the deleted items are still in db
-
-		for i in self.deletes:
-			if self.has_key(i):
-				print 'D> database['+i+'] should be deleted'
-				error = error + 1
-
-		return error
-
-	# fix_db() fix db according to journal dictionary
-
-	def fix_db(self):
-
-		error = 0
-
-		# check if the items in db has the same value of that
-		# in journal dictionary
-
-		for i in self.dict.keys():
-			if not self.has_key(i):
-				# add it
-				print 'M> key('+i+') is not in database'
-				self.__setitem__(i, self.dict[i])
-				error = error + 1
-				print 'F> database['+i+'] =', self.dict[i]
-			elif `self.dict[i]` != `self.__getitem__(i)`:
-				print 'C> database and journal disagree on key('+i+')'
-				print 'C>  journal['+i+'] =', self.dict[i]
-				print 'C> database['+i+'] =', self.__getitem__(i)
-				self.__setitem__(i, self.dict[i])
-				print 'F> database['+i+'] =', self.dict[i]
-				error = error + 1
-		# check if the deleted items are still in db
-
-		for i in self.deletes:
-			if self.has_key(i):
-				print 'D> database['+i+'] should be deleted'
-				self.__delitem__(i)
-				print 'F> delete database['+i+']'
-				error = error + 1
-
-		return error
 
 class Interface(interface.Interface):
 
