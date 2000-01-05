627bdfbba07b0f13980bbd366b4a506036885670
Eileen Berman <berman>
2000-01-05 17:34:44 +0000

    constants for use in multiple files

---
+++ src/enstore_constants.py
@@ -0,0 +1,18 @@
+# this file contains constants used in several enstore modules
+
+FILE_PREFIX = "enplot_"
+BPD = "bpd"
+XFER = "xfer"
+MLAT = "mlat"
+MPH = "mph"
+LOG = ".log"
+
+MPH_FILE = "%s%s"%(FILE_PREFIX, MPH)
+MLAT_FILE = "%s%s"%(FILE_PREFIX, MLAT)
+BPD_FILE = "%s%s"%(FILE_PREFIX, BPD)
+XFER_FILE = "%s%s"%(FILE_PREFIX, XFER)
+XFERLOG_FILE = "%s%s"%(XFER_FILE, LOG)
+
+JPG = ".jpg"
+PS = ".ps"
+STAMP = "_stamp"
