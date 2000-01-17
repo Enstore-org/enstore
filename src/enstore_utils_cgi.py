a1976061f859425f3e6e45697c5a717eb3eb5866
Eileen Berman <berman>
2000-01-17 17:32:39 +0000

    read a line at a time

--- src/enstore_utils_cgi.py
+++ src/enstore_utils_cgi.py
@@ -94,14 +94,16 @@ def pgrep_html(pat, files, sensit):
 	# support both log files that are flat and those that are gzipped
 	if string.find(filename, ".gz") == -1:
 	    # not a gzipped file
-	    lines = open(file, 'r').readlines()
+	    fd = open(file, 'r')
 	else:
-	    lines = gzip.open(file, 'r').readlines()
-	for line in lines:
+	    fd = gzip.open(file, 'r')
+	line = fd.read()
+	while line:
 	    if patr.search(line) >=0:
 		# only print out the name of the file and not the directory path
 		print '[<B>%s</B>] %04d) %s<BR>' %(filename, lineno, line)
 	    lineno = lineno + 1
+	    line = fd.read()
 	print "<HR>"
 
 def agrep_html(pat1, pat2, files, sensit):
