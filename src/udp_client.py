213a69e09b617ce70f48a9942e834cf73ebfeb09
Charles Waldman <cgw>
2000-02-26 02:58:13 +0000

    remove empty_socket simplify logic in send new function "ident()" to detect forking and recompute ident string txn_id is now a counter, rather than time

--- src/udp_client.py
+++ src/udp_client.py
@@ -48,43 +48,17 @@ def get_client() :
         for port in range (port1, port2) : 
             success, sockt = try_a_port (host, port)
             if success :
-                Trace.trace(20,'get_client '+repr((host,port))+" "+repr(sockt)+" "+repr((hostname,ha,hi)))
                 return host, port, sockt
-        Trace.trace(6,'get_client sleeping for 10 - all ports used '+\
-                    repr((port1, port2)))
+        Trace.log(e_errors.INFO,'get_client sleeping for 10 - all ports from %s to %s used '%
+                  (port1, port2))
         time.sleep(10) # tried all ports, try later.
 
-def empty_socket( sock ):
-    xcount = 0         # number of exceptions on select
-    rcount = 0         # number of reads
-    xcountmax = 2      # retry count if select exception
-    rcountalert = 10   # complain if we read too much
-    while xcount < xcountmax:
-        r, w, x = cleanUDP.Select([sock],[],[sock],0)
-        Trace.trace(20,'empty_socket select r,w,x='+repr(r)+' '+repr(w)+' '+repr(x))
-        if r:
-            rcount = rcount+1
-            if rcount%rcountalert == 0:
-                Trace.trace(8,"empty_socket: r from select - count="+repr(rcount))
-		pass
-	    reply , server = sock.recvfrom(TRANSFER_MAX)
-	    Trace.trace(10,"empty_socket read from "+repr(server)+":"+repr(reply))
-	elif x or w :
-	    raise UDPError, "imposible to get these set w/out [r]"
-        else:
-            xcount = xcountmax # nothing to read - no more retries
-    return
-
-def send_socket( sock, message, address ):
-    return sock.sendto( message, address )
 
 def wait_rsp( sock, address, rcv_timeout ):
     # init return vals
-    reply=''
-    server=''
+    reply,server=None,None
 
-    r, w, x = cleanUDP.Select( [sock], [], [sock], rcv_timeout )
-    Trace.trace(20,'wait_rsp select r,w,x='+repr(r)+' '+repr(w)+' '+repr(x))
+    r, w, x, rcv_timeout = cleanUDP.Select( [sock], [], [sock], rcv_timeout)
     if r:
 	reply , server = sock.recvfrom( TRANSFER_MAX )
     elif x or w :
@@ -92,53 +66,29 @@ def wait_rsp( sock, address, rcv_timeout ):
 	Trace.log(e_errors.INFO, "UDPClient.send: exception on select after send to %s %s: %s %s"%
                   (address,x,exc,msg))
 	raise UDPError, "impossible to get these set w/out [r]"
-    return reply, server
-
-def protocolize( self, text ):
-    # make a new message number - response needs to match this number
-    # We use to increment by one, but that does not work with forking
-    # lcl_number = self.number + 1
-    # note: do str here; elsewhere causes problems (precision is 1ms -
-    # this should be OK)
-    lcl_number = "%.6f"%(time.time(),)
-
-    # CRC text
-    body = `(self.ident, lcl_number, text)`
-    crc = checksum.adler32(0L, body, len(body))
-
-    # stringify message and check if it is too long
-    message = `(body, crc)`
-
-    if len(message) > TRANSFER_MAX:
-        errmsg="send:message too big, size=%d, max=%d" %(len(message),TRANSFER_MAX)
-        print "DEBUG", errmsg #XXXXX
-	Trace.log(e_error.ERROR,errmsg)
-	raise errno.errorcode[errno.EMSGSIZE],errmsg
-    
-    return message, lcl_number
+    return reply, server, rcv_timeout
 
 
 
 class UDPClient:
 
-    def __init__(self, host="", port=0, socket=0):
-        if host == "":
-            host, port, self.socket = get_client()
+    def __init__(self, host=None, port=0, socket=0):
+        if not host:
+            self.host, self.port, self.socket = get_client()
         else:
             self.socket = socket
-        self.number = "0"
-        self.ident = "%s-%d-%f-%d" \
-                     % (host, port, time.time(), os.getpid() )
+        self.txn_counter = 0L
+        self.pid = None
         self.sendport = 7
         self.where_sent = {}
-	try:
-	    x = os.environ['ENSTORE_UDP_PP']
-            Trace.trace(20,'ENSTORE_UDP_PP found in environment'+repr(x))
-	    self.pp = 1
-	except:
-	    self.pp = 0
-        Trace.trace(10,'__init__ udpclient '+repr(self.ident))
 
+    def ident(self):
+        pid=os.getpid()  
+        if pid != self.pid:  #recompute ident each time we fork
+            self.pid = pid
+            self._ident = "%s-%d-%f-%d" % (self.host, self.port, time.time(), self.pid )
+        return self._ident 
+        
     def __del__(self):
         # tell file clerk we're done - this allows it to delete our unique id in
         # its dictionary - this keeps things cleaner & stops memory from growing
@@ -149,104 +99,83 @@ class UDPClient:
             except:
                 pass
 
-    # this (generally) is received/processed by dispatching worker
-    def send( self, text, address, rcv_timeout=0, tries=0 ):
-	if self.pp and text['work']!='idle_mover':
-	    x=sys.stdout;sys.stdout=sys.stderr
-	    Trace.log(e_errors.INFO, "\nreq/cmd to address: "+repr(address)+\
-                      " from: "+ repr(self.ident))
-	    Trace.log(e_errors.INFO, repr(text))
-	    sys.stdout=x
+    def _eval_reply(self, reply): #private to send
+        try:
+            number,  out, t  = eval(reply)   ##XXX
+            # catch any error and keep going. server needs to be robust
+        except:
+            exc,msg,tb=sys.exc_info()
+            logmsg="udpClient.eval_reply %s %s"%(exc, msg)
+            if exc == exceptions.SyntaxError: #msg size> max datagram size?
+                logmsg=logmsg+"Truncated message?"
+            Trace.log(e_errors.ERROR, logmsg)
+            raise exc, msg
+        return number, out, t
+
+
+    def protocolize( self, text ):
+
+        self.txn_counter = self.txn_counter + 1
+
+        # CRC text
+        body = `(self.ident(), self.txn_counter, text)`
+        crc = checksum.adler32(0L, body, len(body))
+
+        # stringify message and check if it is too long
+        message = `(body, crc)`
+
+        if len(message) > TRANSFER_MAX:
+            errmsg="send:message too big, size=%d, max=%d" %(len(message),TRANSFER_MAX)
+            Trace.log(e_errors.ERROR,errmsg)
+            raise errno.errorcode[errno.EMSGSIZE],errmsg
+
+        return message, self.txn_counter
+
+
+        
+    def send(self, data, dst, rcv_timeout=0, max_send=0):
+        """send msg to dst address, up to `max_send` times, each time
+        waiting `rcv_timeout' seconds for reply
+        A value of 0 for max_send means retry forever"""
 
 	if rcv_timeout:
-            if tries==0:
-                tries = 1      # if timeout!=0 and tries=0, then try just once
+            if max_send==0:
+                max_send = 1 # protect from nonsense inputs XXX should we do this?
 	else:
-	    rcv_timeout = 10   # default timeout - also no adjusting of tries
+	    rcv_timeout = 10   
 
-	message, self.number = protocolize( self, text )
-
+        msg, txn_id = self.protocolize(data)
         # keep track of where we are sending things so we can clean up later
-        self.where_sent[address] = (self.ident,text)
+        self.where_sent[dst] = (self.ident,msg)
+        
+        n_sent = 0
+        while max_send==0 or n_sent<max_send:
+            self.socket.sendto( msg, dst )
+            n_sent=n_sent+1
+            rcvd_txn_id=None
+            timeout=rcv_timeout
+            while rcvd_txn_id != txn_id: #look for reply while rejecting "stale" responses
+                reply, server, timeout = wait_rsp( self.socket, dst, timeout)
+                if not reply:  # receive timed out
+                    break #resend
+                rcvd_txn_id, out, t = self._eval_reply(reply)
+            else: # we got a good reply
+                return out
 
-        # make sure the socket is empty before we start
-	empty_socket( self.socket )
-        # send the udp message until we get a response that it was sent
-        number = "0"  # impossible "number"
-        ntries = 0  
-        while number != self.number:
-	    send_socket( self.socket, message, address )
-            ntries = ntries+1
-            
-            # check for a response	
+        ##if we got here, it's because we didn't receive a response to the message we sent.
+        raise errno.errorcode[errno.ETIMEDOUT]
 
-	    reply , server = wait_rsp( self.socket, address, rcv_timeout )
-
-	    if reply != "":
-		# OK, we have received something -- "try" it
-		try:
-		    number,  out, time  = eval(reply)   ##XXX
-		# did we read entire message (bigger than TRANSFER_MAX?)
-	        except exceptions.SyntaxError :
-                    exc,msg,tb=sys.exc_info()
-		    Trace.log(e_errors.ERROR,
-                              "send disaster: didn't read entire message: server=%s, reply=%s, error=%s %s"%
-                              (server,reply,exc,msg))
-                    raise exc,msg
-		# goofy test feature - need for client being echo service only
-		except exceptions.ValueError :
-		    Trace.trace(6,'send GOOFY TEST FEATURE')
-		    ident, number,  out, time  = eval(reply)
-                    Trace.trace(20,'goofy test:'+repr((ident,number,out,time)))
-                # catch any error and keep going. server needs to be robust
-                except:
-                    exc,msg,tb=sys.exc_info()
-                    Trace.log(e_errors.ERROR,
-                              "unexpected exception in udp_client.send: %s %s "%
-                              (exc,msg))
-		    raise exc, msg
-
-		# now (after receive), check...
-		if number != self.number :
-                    #T(repr(type(number))+" "+repr(type(self.number)))
-		    msg="UDPClient.send: stale_number=%s number=%s" %\
-			 (number,self.number)
-		    Trace.trace(21,'send stale='+repr(number)+' want='+repr(self.number))
-                    if 0:
-                        Trace.log(e_errors.INFO,
-                                  msg+" resending to "+repr(address)+message)
-
-	    elif tries!=0 and ntries>=tries:  # no reply after requested tries
-                Trace.trace(10,"send quiting,no reply after tries="+repr(ntries))
-		raise errno.errorcode[errno.ETIMEDOUT]
-            else:
-                Trace.trace(6,"send no reply after tries="+repr(ntries))
-
-	if self.pp and (not 'work' in out.keys() or out['work']!='nowork'):
-	    x=sys.stdout;sys.stdout=sys.stderr
-	    Trace.log(e_errors.INFO,"\nrsp - sent to: "+repr(self.ident))
-	    Trace.log(e_errors.INFO, repr(out))
-	    sys.stdout=x
-        return out
-
+        
     # send message without waiting for reply and resend
-    def send_no_wait(self, text, address) :
-	if self.pp:
-	    x=sys.stdout;sys.stdout=sys.stderr
-	    Trace.log(e_errors.INFO, "\nmsg/cmd to address: "+repr(address)+\
-                      " from: "+repr(self.ident))
-	    Trace.log(e_errors.INFO, repr(text))
-	    sys.stdout=x
+    def send_no_wait(self, data, address) :
+	message, txn_id = self.protocolize( data )
+	return self.socket.sendto( message, address )
 
-	message, self.number = protocolize( self, text )
-
-        # send the udp message
-	return send_socket( self.socket, message, address )
-
+        
 class UDPClientInterface(interface.Interface):
 
     def __init__(self):
-        self.msg = "All dogs have fleas, but cats make you sick!"
+        self.msg = "All dogs have fleas"
 	self.sendhost="localhost"
 	self.sendport=9998
         self.host, self.port, self.socket = get_client()
@@ -254,7 +183,6 @@ class UDPClientInterface(interface.Interface):
 
         # parse the options
         self.parse_options()
-        Trace.trace(10,'__init__ ci '+repr((self.host,self.port,self.socket)))
 
     # define the command line options that are valid
     def options(self):
@@ -271,19 +199,16 @@ if __name__ == "__main__" :
     # get a UDP client
     u = UDPClient(intf.host, intf.port, intf.socket)
 
-    #Trace.log(e_errors.INFO, repr(u.__dict__))
+    print "Sending message", intf.msg, "to", intf.sendhost, " with callback on ", intf.port
 
-    Trace.trace(10,"Sending:\n"+intf.msg+"\nto"+repr(intf.sendhost)+" "+\
-                repr(intf.sendport)+" with calback on "+\
-                repr(intf.port))
     back = u.send(intf.msg, (intf.sendhost, intf.sendport))
 
     if back != intf.msg :
-        Trace.log(e_errors.INFO,
-                  "Error: sent:\n"+intf.msg+"\nbut read:\n"+back)
-        status = status|1
+        print "Error: sent:",intf.msg+"but read:",back
+        status = 1
 
     else:
-	Trace.trace(10, "Read back:\n%s"%(back,))
+	print "Read back:", back
 
     sys.exit(status)
+    
