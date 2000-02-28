162b57e0fd99376bf7a4fdbb410cb99c21520e4d
Eileen Berman <berman>
2000-02-28 22:18:21 +0000

    only delete objects if created them

--- src/inquisitor.py
+++ src/inquisitor.py
@@ -1069,9 +1069,9 @@ class InquisitorMethods(dispatching_worker.DispatchingWorker):
             mlatfile.close()
             mlatfile.install(self.html_dir)
             mlatfile.cleanup(keep, pts_dir)
+	    del mphfile
+	    del mlatfile
 	del mountfile
-	del mphfile
-	del mlatfile
 
     # make the total transfers per unit of time and the bytes moved per day
     # plot
@@ -1114,9 +1114,9 @@ class InquisitorMethods(dispatching_worker.DispatchingWorker):
             # plotting needs the bpd data file
             bpdfile.cleanup(keep, pts_dir)
             xferfile.cleanup(keep, pts_dir)
+	    del bpdfile
+	    del xferfile
 	del encpfile
-	del bpdfile
-	del xferfile
 
 class Inquisitor(InquisitorMethods, generic_server.GenericServer):
 
