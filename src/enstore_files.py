ce283160734eabe1890e32da62136a1e6b0c2720
Charles Waldman <cgw>
2000-03-01 23:30:22 +0000

    fix name error on line 156

--- src/enstore_files.py
+++ src/enstore_files.py
@@ -153,7 +153,7 @@ class HTMLStatusFile(EnStatusFile, enstore_status.EnStatus):
 	self.refresh = refresh
 
     def set_alive_error_status(self, key):
-	self.text[key][enstore_status.STATUS][0] = "error"
+	self.text[key][enstore_constants.STATUS][0] = "error"
 
     # write the status info to the file
     def write(self):
