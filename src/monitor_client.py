f4ba2e73db68f316a2d61c63dfdfe562febf9a5b
Don Petravic <petravic>
2000-03-02 01:53:05 +0000

    new monitoring stuff

---
+++ src/monitor_client.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+###############################################################################
+# src/$RCSfile$   $Revision$
+#
+# system imports
+import sys
+import string
+import time
+import errno
+import pprint
+import socket
+
+# enstore imports
+import callback
+import interface
+import generic_client
+import backup_client
+import udp_client
+import Trace
+import e_errors
+import configuration_client
+import log_client
+
+MY_NAME = "MNTR_CLI"
+MY_SERVER = "monitor_server"
+
+class MonitorServerClient:
+
+    def __init__( self, servr_addr ):
+        self.u = udp_client.UDPClient()
+	self.servr_addr = servr_addr
+        Trace.trace(10,'__init__ u='+str(self.u))
+
+    # send the request to the monitor server
+    def send (self, ticket,  rcv_timeout=0, tries=0):
+        x = self.u.send( ticket, self.servr_addr, rcv_timeout, tries )
+        return x
+
+    # ping a server like ENCP would
+    def simulate_encp_transfer(self, remote_interface,
+                               block_size=65536, block_count=160 ):
+
+        c_hostip, c_port, c_socket = callback.get_callback(
+            use_multiple=0,verbose=0)
+        
+        ticket= { 'work'           : 'simulate_encp_transfer',
+                  'callback_addr' :    (c_hostip, c_port),
+                  'remote_interface' : remote_interface,
+                  'block_count' : block_count,
+                  'block_size' : block_size
+                  }
+        reply=self.send(ticket)
+        #simulate the control socket between encp and the mover
+        ms_socket, address = c_socket.accept()
+        c_socket.close()
+        returned_ticket = callback.read_tcp_obj(ms_socket)
+        ms_socket.close()
+        data_socket=callback.mover_callback_socket(returned_ticket)
+        data=data_socket.recv(1)
+        if not data:
+            raise "Server closed connection"
+        bytes_received=len(data)
+        t0=time.time()
+        while bytes_received<block_size*block_count:
+            data = data_socket.recv(block_size)
+            if not data: #socket is closed
+                raise "Server closed connection"
+            bytes_received=bytes_received+len(data)
+        ticket['elapsed']=time.time()-t0
+        return ticket
+    
+        
+            
+    
+class MonitorServerClientInterface(generic_client.GenericClientInterface):
+
+    def __init__(self, flag=1, opts=[]):
+        generic_client.GenericClientInterface.__init__(self)
+
+
+
+def get_all_ips():
+    """
+    inquire the configuration server, return a list
+    of every  IP address involved in Enstore  
+    """
+    
+    x = csc.u.send({"work":"reply_serverlist"},
+                   (intf.config_host, intf.config_port))
+    if x['status'][0] != 'ok': raise "error from config server"
+    server_dict = x['server_list']
+    ip_dict = {}
+    for k in server_dict.keys():
+        
+        details = csc.get(k)
+        if details.has_key('data_ip'):
+            ip = details['data_ip']     #check first if a mover
+        elif details.has_key('hostip'):
+            ip = details['hostip']      #other server
+        else:
+            continue
+        ip = socket.gethostbyname(ip)  #canonicalize int dot notation
+        ip_dict[ip] = 1                #dictionary will strike duplicates
+    return ip_dict.keys()              #keys of the dict is a tuple of IPs
+
+csc=None
+
+if __name__ == "__main__":
+    global csc
+
+    intf = MonitorServerClientInterface()
+    
+    Trace.init(MY_NAME)
+    Trace.trace( 6, 'msc called with args: %s'%(sys.argv,) )
+
+    csc = configuration_client.ConfigurationClient((intf.config_host,
+                                                    intf.config_port))
+    
+
+    logc=log_client.LoggerClient(csc, MY_NAME, 'log_server')
+
+
+    ip_list = get_all_ips()
+    ip_list = ["localhost"] #TEST FIXTURE
+    msport = 9999
+    for ip in ip_list:
+        msc = MonitorServerClient((ip, msport))
+        ret=msc.simulate_encp_transfer(ip)
+        pprint.pprint(ret)
+        Trace.log(e_errors.INFO, `ret`)
+        
+    
--- src/monitor_server.py
+++ src/monitor_server.py
@@ -23,21 +23,12 @@ MY_NAME = "Monitor_Server"
 
 class MonitorServer(dispatching_worker.DispatchingWorker):
 
-    def __init__(self):
-	self.print_id="CONFIG_DICT"
-        self.serverlist = {}
-
-
-class ConfigurationServer(ConfigurationDict, generic_server.GenericServer):
-
     def __init__(self, csc, configfile=interface.default_file()):
 	self.running = 0
 	self.print_id = MY_NAME
+        print "Monitor Server at %s %s" %(csc[0], csc[1])
         Trace.trace(10,
-            "Instantiating Configuration Server at %s %s using config file %s"
-            %(csc[0], csc[1], configfile))
-
-        # default socket initialization - ConfigurationDict handles requests
+            "Monitor Server at %s %s" %(csc[0], csc[1]))
         dispatching_worker.DispatchingWorker.__init__(self, csc)
 
 	self.running = 1
@@ -45,11 +36,31 @@ class ConfigurationServer(ConfigurationDict, generic_server.GenericServer):
         # always nice to let the user see what she has
         Trace.trace(10, repr(self.__dict__))
 
+    def simulate_encp_transfer(self, ticket):
+        ticket['status'] = ('ok', None)
+        self.reply_to_caller(ticket)
+        pid=self.fork()
+        if pid: #parent
+            return
+        
+        #simulate mover connecting on callback and a read_from_HSM transfer
+        localhost, localport, well_known_sock = callback.get_data_callback(
+            use_multiple=0,
+            fixed_ip=ticket['remote_interface'],verbose=0)
+        ticket['mover']={'callback_addr': (localhost,localport)}
+        callback.send_to_user_callback(ticket)
+        xfer_sock, address = well_known_sock.accept()
+        well_known_sock.close()
+        sendstr = "S"*ticket['block_size']
+        for x in xrange(ticket['block_count']):
+            xfer_sock.send(sendstr)
+        xfer_sock.close()
+            
 class MonitorServerInterface(generic_server.GenericServerInterface):
 
     def __init__(self):
       
-    generic_server.GenericServerInterface.__init__(self)
+        generic_server.GenericServerInterface.__init__(self)
 
     # define the command line options that are valid
     def options(self):
@@ -63,8 +74,9 @@ if __name__ == "__main__":
 
     # get the interface
     intf = MonitorServerInterface()
-
-    # get a monitor server
+    # get amonitor server:
+    intf.config_host = "" #listen on all interfaces on this machine
+    intf.config_port = 9999 #HACK -- should ger port from config server
     ms = MonitorServer((intf.config_host, intf.config_port))
 
     while 1:
