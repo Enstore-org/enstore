a67de0923eeecdcbe1f9706422cf152967f449b9
Charles Waldman <cgw>
2000-05-17 19:02:40 +0000

    setup sys.path for enstore

---
+++ src/setpath.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# $Id$
+    
+import sys
+import os
+
+def addpath(p):
+    p = os.path.normpath(p)
+    if p not in sys.path:
+        sys.path.insert(0,p)
+
+edir = os.environ.get('ENSTORE_DIR')
+
+if edir:
+    addpath(os.path.join(edir, 'src'))
+    addpath(os.path.join(edir, 'modules'))
+
+cdir = os.getcwd()
+addpath(os.path.join(cdir, '../modules'))
+
+
+if __name__=='__main__':
+    print sys.path
+    
