c4890237eb334490a6a2b788584a8e91e4b1b3d9
Charles Waldman <cgw>
2000-05-18 20:22:40 +0000

    verbose -> self.verbose

--- src/ftt_driver.py
+++ src/ftt_driver.py
@@ -13,7 +13,6 @@ KB=1024
 MB=KB*KB
 GB=MB*KB
 
-verbose=1
 
 class FTTDriver(driver.Driver):
 
@@ -24,6 +23,7 @@ class FTTDriver(driver.Driver):
         self._start_time = None
         self._rate = self._last_rate = 0
         self._fast_rate = 10*MB
+        self.verbose = 0
         
     def open(self, device=None, mode=None):
         if mode not in (0,1):
@@ -36,25 +36,25 @@ class FTTDriver(driver.Driver):
         self._last_rate = 0
         self._rate = 0
         self._bytes_transferred = 0
-        if verbose: print "ftt_open returns", self.ftt
+        if self.verbose: print "ftt_open returns", self.ftt
         for retry in xrange(10):
             if retry:
-                if verbose: print "retrying open"
+                if self.verbose: print "retrying open"
             try:
                 self.fd = self.ftt.open_dev()
                 break
             except ftt.FTTError, detail:
-                if verbose: print detail, detail.errno
+                if self.verbose: print detail, detail.errno
                 if detail.errno == ftt.EBUSY:
                     time.sleep(5)
                 else:
                     break
         self._rate = self._last_rate = self._bytes_transferred = 0
-        if verbose: print "ftt_open_dev returns", self.fd
+        if self.verbose: print "ftt_open_dev returns", self.fd
         return self.fd
 
     def reopen(self, device, mode):
-        if verbose: print "ftt reopen"
+        if self.verbose: print "ftt reopen"
         if not self.ftt:
             raise "XXX reopen, not open"
         if mode != self.mode:
@@ -64,7 +64,7 @@ class FTTDriver(driver.Driver):
         else:
             for retry in xrange(10):
                 if retry:
-                    if verbose: print "retrying open"
+                    if self.verbose: print "retrying open"
                 try:
                     self.fd = self.ftt.open_dev()
                     break
@@ -76,20 +76,20 @@ class FTTDriver(driver.Driver):
                         break
 
         self._rate = self._last_rate = self._bytes_transferred = 0
-        if verbose: print "reopen: ftt_open_dev returns", self.fd
+        if self.verbose: print "reopen: ftt_open_dev returns", self.fd
         return self.fd
 
     
     def rewind(self):
         r = self.ftt.rewind()
-        if verbose: print "ftt_rewind returns", r
+        if self.verbose: print "ftt_rewind returns", r
 
     def tell(self):
         if not self.ftt:
-            if verbose: print "tell: no ftt descriptor"
+            if self.verbose: print "tell: no ftt descriptor"
             return None
         file, block = self.ftt.get_position()
-        if verbose: print "ftt_get_position returns", file, block
+        if self.verbose: print "ftt_get_position returns", file, block
         return file
     
     def seek(self, target):
@@ -107,7 +107,7 @@ class FTTDriver(driver.Driver):
         if current == target:
             return 0
         else:
-            if verbose: print "seek: current = ", current, "target=",target
+            if self.verbose: print "seek: current = ", current, "target=",target
         if target>current:
             self.ftt.skip_fm(target-current)
         else:
@@ -115,7 +115,7 @@ class FTTDriver(driver.Driver):
             self.ftt.skip_fm(1)
 
         current = self.tell()
-        if verbose: print "seek2: current=", current, "target=", target
+        if self.verbose: print "seek2: current=", current, "target=", target
         if current != target:
             raise "XXX Positioning error", (current, target)
         
@@ -123,9 +123,9 @@ class FTTDriver(driver.Driver):
         return self.fd
 
     def flush(self):
-        if verbose: print "flushing", self.ftt
+        if self.verbose: print "flushing", self.ftt
         now=time.time()
-        if verbose:
+        if self.verbose:
             print "transferred %s bytes in %s seconds"%(
                 self._bytes_transferred, now-self._start_time),
             if now>self._start_time and self._bytes_transferred:
@@ -133,14 +133,14 @@ class FTTDriver(driver.Driver):
             else:
                 print
         r = self.ftt.close_dev()
-        if verbose: print "ftt_close_dev returns",r
+        if self.verbose: print "ftt_close_dev returns",r
         self.fd = -1
         return r
 
     def close(self):
-        if verbose: print "closing", self.ftt
+        if self.verbose: print "closing", self.ftt
         r = self.ftt.close()
-        if verbose: print "ftt_close returns",r
+        if self.verbose: print "ftt_close returns",r
         self.ftt = None
         self.fd = -1
         return r
@@ -192,7 +192,7 @@ class FTTDriver(driver.Driver):
         if blocksize is None:
             blocksize = mode[3]
         r = self.ftt.set_mode(density, compression, blocksize)
-        if verbose: print "ftt mode is", self.ftt.get_mode()
+        if self.verbose: print "ftt mode is", self.ftt.get_mode()
         return r
 
     def set_fast_rate(self, rate):
