900ba583aa91643fe8a041db6461df9694a9a99a
Charles Waldman <cgw>
2000-05-31 18:15:03 +0000

    cleaned up and simplified

--- src/null_wrapper.py
+++ src/null_wrapper.py
@@ -2,35 +2,10 @@
 # $Id$
 
 
-# create header
-def create_header(inode, mode, uid, gid, nlink, mtime, filesize,
-             major, minor, rmajor, rminor, filename):
+def headers(ticket):
+    return '', ''
 
-    return ""
+min_header_size = 0
 
-# generate the enstore cpio "trailers"
-def trailers(blocksize, siz, trailer):
-    return ""
-             
-class Wrapper :
-
-    def sw_mount( self, driver, info ):
-        ##print "NULL sw_mount"
-	return
-
-    # generate an enstore cpio archive: devices must be open and ready
-    def write_pre_data( self, driver, info ):
-        ##print "NULL write-pre-data"
-        return
-
-    def write_post_data( self, driver, crc ):
-        ##print "NULL write-post-data"
-        return
-
-    def read_pre_data( self, driver, info ):
-        ##print "NULL read-pre-data"
-        return
-
-    def read_post_data( self, driver, info ):
-        ##print "NULL read-post-data"
-        return
+def header_size(header_start):
+    return 0
