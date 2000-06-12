6059e489e00b104896ab0cc3e0f4f4187b208a58
Charles Waldman <cgw>
2000-06-12 20:40:51 +0000

    simplify init_done logic

--- src/generic_client.py
+++ src/generic_client.py
@@ -40,42 +40,55 @@ class GenericClient:
 
     def __init__(self, csc, name):
         global init_done
-        if not init_done:
-            # we only want to get these clients once per process
-            init_done = 1
-            import configuration_client
-            if csc and (type(csc) == types.TupleType):
-                self.csc = configuration_client.ConfigurationClient((csc[0],
-                                                                    csc[1]))
-            elif csc:
+        if init_done:
+            return
+        # we only want to get these clients once per process
+        init_done = 1
+        import configuration_client
+        if csc:
+            if type(csc) == type(()):
+                self.csc = configuration_client.ConfigurationClient(csc)
+            else:
                 # it is not a tuple of address and port, so we assume that it
                 # is a configuration client object
                 self.csc = csc
-	    else:
-		# it is a 0 (the default value from i.e. log_client)
-		def_addr = (os.environ['ENSTORE_CONFIG_HOST'],
-			    string.atoi(os.environ['ENSTORE_CONFIG_PORT']))
-                self.csc = configuration_client.ConfigurationClient( def_addr )
-		pass
-            # try to find the logname for this object in the config dict.  use
-            # the lowercase version of the name as the server key.  if this
-            # object is not defined in the config dict, then just use the
-            # passed in name.
-            self.log_name = self.get_name(name)
-            if not self.__dict__.get('is_logger', 0):
-                import log_client
-                self.logc = log_client.LoggerClient(self.csc, self.log_name,
-                                                    'log_server')
-            if not self.__dict__.get('is_alarm', 0):
-                import alarm_client
-                self.alarmc = alarm_client.AlarmClient(self.csc)
         else:
-            if csc and (not type(csc) == types.TupleType):
-                # we assume this is a configuration client object since it is
-                # not a tuple of the address and port.
-                self.csc = csc
+            # it is None or 0 (the default value from i.e. log_client)
+            def_addr = (os.environ['ENSTORE_CONFIG_HOST'],
+                        string.atoi(os.environ['ENSTORE_CONFIG_PORT']))
+            self.csc = configuration_client.ConfigurationClient( def_addr )
 
-    # return the name used for this client/server
+        # try to find the logname for this object in the config dict.  use
+        # the lowercase version of the name as the server key.  if this
+        # object is not defined in the config dict, then just use the
+        # passed in name.
+        self.log_name = self.get_name(name)
+        if not self.__dict__.get('is_logger', 0):
+            import log_client
+            self.logc = log_client.LoggerClient(self.csc, self.log_name,
+                                                'log_server')
+        if not self.__dict__.get('is_alarm', 0):
+            import alarm_client
+            self.alarmc = alarm_client.AlarmClient(self.csc)
+
+    def get_server_address(self, MY_SERVER):
+        ticket = self.csc.get(MY_SERVER)
+        try:
+            server_address = (ticket['hostip'], ticket['port'])
+            return server_address
+        except KeyError, detail:
+            sys.stderr.write("Unknown server %s (no %s in config)\n" % ( MY_SERVER, detail))
+            os._exit(1)
+
+    def send(self, ticket, rcv_timeout, tries):
+        try:
+            x = self.u.send(ticket, self.server_address, rcv_timeout, tries)
+        except errno.errorcode[errno.ETIMEDOUT]:
+            x = {'status' : (e_errors.TIMEDOUT, None)}
+        return x
+        
+                
+    # return the name used for this client/server #XXX what is this nonsense? cgw
     def get_name(self, name):
         return name
 
@@ -93,9 +106,8 @@ class GenericClient:
 	    Trace.trace(14,"alive - ERROR, alive timed out")
 	    x = {'status' : (e_errors.TIMEDOUT, None)}
         except KeyError, detail:
-            print "Unknown server", server
-            print "KeyError : ", detail
-            sys.exit(-1)
+            sys.stderr.write("Unknown server %s (no key %s)\n" % (server, detail))
+            os._exit(1)
         return x
 
 
@@ -116,24 +128,23 @@ class GenericClient:
     
     
     def handle_generic_commands(self, server, intf):
+        ret = None
         if intf.alive:
             ret = self.alive(server, intf.alive_rcv_timeout,intf.alive_retries)
-        elif intf.do_print:
+        if intf.do_print:
             ret = self.trace_levels(server, 'do_print', intf.do_print)
-        elif intf.dont_print:
+        if intf.dont_print:
             ret = self.trace_levels(server, 'dont_print', intf.dont_print)
-        elif intf.do_log:
+        if intf.do_log:
             ret = self.trace_levels(server, 'do_log', intf.do_log)
-        elif intf.dont_log:
+        if intf.dont_log:
             ret = self.trace_levels(server, 'dont_log', intf.dont_log)
-        elif intf.do_alarm:
+        if intf.do_alarm:
             ret = self.trace_levels(server, 'do_alarm', intf.do_alarm)
-        elif intf.dont_alarm:
+        if intf.dont_alarm:
             ret = self.trace_levels(server, 'dont_alarm', intf.dont_alarm)
-        else:
-            return None
-
         return ret
+
             
     # examine the final ticket to check for any errors
     def check_ticket(self, ticket):
