dbde0fb329b19cd24022328e7e20c1afbc391e19
Jon Bakken <bakken>
2000-06-26 21:00:27 +0000

    cgw: avoid returning negative bytecounts

--- src/net_driver.py
+++ src/net_driver.py
@@ -52,9 +52,13 @@ class NetDriver(driver.Driver):
         t0 = time.time()
         r =  strbuffer.buf_recv(self.fileno(), buf, offset, nbytes)
         
-        if r<0 and strbuffer.cvar.errno == errno.EAGAIN:
-            r=0
-            
+        if r < 0:
+            if strbuffer.cvar.errno in (errno.EAGAIN, errno.EINTR):
+                r=0
+            else:
+                msg = "net_driver: read(%d) returns %d, errno=%d" %(nbytes, r, strbuffer.cvar.errno)
+                Trace.log(e_errors.ERROR, msg)
+                raise driver.DriverError, msg
         if r > 0:
             now = time.time()
             self._last_rate = r/(now-t0)
@@ -62,15 +66,18 @@ class NetDriver(driver.Driver):
                 self._start_time = t0
             self._bytes_transferred = self._bytes_transferred + r
             self._rate = self._bytes_transferred/(now - self._start_time)
-        if r <= 0:
-            pass #raise exception?
         return r
                                   
     def write(self, buf, offset, nbytes):
         t0 = time.time()
         r = strbuffer.buf_send_dontwait(self.fileno(), buf, offset, nbytes)
-        if r<0 and strbuffer.cvar.errno == errno.EAGAIN:
-            r = 0
+        if r < 0:
+            if strbuffer.cvar.errno in (errno.EAGAIN, errno.EINTR):
+                r=0
+            else:
+                msg =  "net_driver: write(%d) returns %d, errno=%d" % (nbytes, r, strbuffer.cvar.errno)
+                Trace.log(e_errors.ERROR, msg)
+                raise driver.DriverError, msg
         if r > 0:
             now = time.time()
             self._last_rate = r/(now - t0)
