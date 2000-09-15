25934aa45ae78cff592cd51f7d4accb3388390b0
Charles Waldman <cgw>
2000-09-15 20:36:41 +0000

    add usage message

--- src/entv.py
+++ src/entv.py
@@ -23,18 +23,35 @@ for item, value in configdict.items():
         mover = item[:-6]
         movers.append(mover)
     movers.sort()
-    
-target_ip, target_port = sys.argv[1:]
-target_port = int(target_port)
-dst = (target_ip, target_port)
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
+s = None
+dst = None
 def send(msg):
-    print msg, dst
+    print "sending",   msg
     s.sendto(msg, dst)
 
-send("movers "+string.join(movers))
-time.sleep(3)
+def main():
+    global s, dst
+    
+    if len(sys.argv) != 3:
+        print "Usage: %s host port" % (sys.argv[0],)
+        print "  host and port refer to the host and port enstore_display is running on"
+        sys.exit(1)
+        
+    target_ip, target_port = sys.argv[1:]
+    target_port = int(target_port)
+    dst = (target_ip, target_port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#this gets us 15 minutes worth of update messages
-s.sendto("notify %s %s" % (target_ip, target_port), (event_relay_host, event_relay_port))
+    send("movers "+string.join(movers))
+    #give it a little time to draw the movers
+    time.sleep(3)
+
+    #this gets us 15 minutes worth of update messages
+    s.sendto("notify %s %s" % (target_ip, target_port), (event_relay_host, event_relay_port))
+
+if __name__ == "__main__":
+    main()
+    
+    
