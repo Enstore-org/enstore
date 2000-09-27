afebc6c9b7514005694f6f7d5c84746d9b315f9e
Charles Waldman <cgw>
2000-09-27 21:15:12 +0000

    get rid of unused stuff - the old multiple-interface implementation is gone

--- src/hostaddr.py
+++ src/hostaddr.py
@@ -58,80 +58,7 @@ def name_to_address(name):
         addr = name
     known_names[name] = addr
     return addr
-    
-def get_interface_file_name(verbose=0):
-    hostname, junk, junk = gethostinfo()
-    if '.' in hostname:
-        hostname=string.split(hostname,'.')[0]
-    filename = "%s.interface.conf"%(hostname,)
-    trydirs = []
-    for envvar in "ENSTORE_DIR", "ENCP_DIR":
-        if envvar in os.environ.keys():
-            trydirs.append(os.environ[envvar])
-            trydirs.append(os.path.join(os.environ[envvar], "etc"))
-    trydirs.append("/etc") # fallback
-    for trydir in trydirs:
-        trypath=os.path.join(trydir, filename)
-        if verbose: print "searching", trydir, "for", filename
-        if os.access(trypath, os.R_OK):
-            if verbose: print "found it"
-            return trypath
-    if verbose: print "not found"
-    return None
-        
-        
-multi_interface_table=None
-last_mtime = 0
 
-def get_multiple_interfaces(verbose=0):
-    global multi_interface_table
-    global last_mtime
-
-    filename = get_interface_file_name(verbose)
-
-    #see if we previously loaded the file, if so, check the mtime
-    if multi_interface_table:
-        if verbose: print "table already loaded, checking for file update"
-        if filename:
-            try:
-                mtime=os.stat(filename)[stat.ST_MTIME]
-                if mtime == last_mtime:
-                    if verbose: print "not changed"
-                    return multi_interface_table
-            except os.error: # file must have been deleted
-                if verbose: print "cannot stat", filename
-                multi_interface_table = None
-                last_mtime = 0
-        else:
-             multi_interface_table = None
-             last_mtime = 0
-            
-    table = []
-
-    if filename:
-        file = open(filename,'r')
-        last_mtime=os.stat(filename)[stat.ST_MTIME]
-        if verbose: print "reading", filename
-        ##this table will be  a list of tuples,  (IP#.  Relative B/W)
-        for line in file.readlines():
-            words = string.split(line)
-            if not words:
-                continue
-            if words[0][0]=='#':
-                continue
-            if len(words)==2:
-                ip,bw=words
-                table.append((ip,int(bw)))
-            else:
-                print "Configuration file error: %s %s" % (filename, line)
-    if not table:
-        if verbose: print "no multiple interface table"
-        junk, junk, ips = gethostinfo()
-        table = [(ips[0], 1)]
-    multi_interface_table = table
-    if verbose:
-        sys.stdout.flush()
-    return multi_interface_table
 
 ifconfig_command=None
 ifinfo={}
@@ -145,7 +72,8 @@ def find_ifconfig_command():
             ifconfig_command=tryit
             return ifconfig_command
     return None
-        
+
+
 def interface_name(ip):
     if not ifinfo or not ifinfo.has_key(ip):
         find_ifconfig_command()
@@ -181,20 +109,14 @@ def interface_name(ip):
     return ifinfo.get(ip)
     
     
-    
 if __name__ == "__main__":
     print gethostinfo(1)
     print gethostinfo(1)
-    print get_multiple_interfaces(1)
-    print get_multiple_interfaces(1)
     print address_to_name('127.0.0.1')
     print address_to_name('1.1.1.1')
     print address_to_name('131.225.84.156')
     print name_to_address('rip8.fnal.gov')
     print interface_name('127.0.0.1')
-    while 1:
-        print get_multiple_interfaces(1)
-        time.sleep(1)
     
 
 
