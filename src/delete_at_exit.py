f4f86fd0d69dea22c7b92fd37530776e6999d77c
Charles Waldman <cgw>
2000-09-29 23:05:56 +0000

    special-case /dev/null

--- src/delete_at_exit.py
+++ src/delete_at_exit.py
@@ -9,10 +9,14 @@ import sys
 _deletion_list = []
 
 def register(filename):
+    if filename == '/dev/null':
+        return
     if filename not in _deletion_list:
         _deletion_list.append(filename)
 
 def unregister(filename):
+    if filename == '/dev/null':
+        return
     if filename in _deletion_list:
         _deletion_list.remove(filename)
 
