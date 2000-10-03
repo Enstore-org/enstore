4ad24cfce401b7c4af2cc1f722d963f767eec802
Charles Waldman <cgw>
2000-10-03 21:51:10 +0000

    more reasonable OO

--- src/ratemeter.py
+++ src/ratemeter.py
@@ -25,105 +25,135 @@ def atol(s):
         s = s[:-1] #chop off any trailing "L"
     return string.atol(s)
 
-def next_minute(t=None):
-    if t is None:
-        t = time.time()
-    Y, M, D, h, m, s, wd, jd, dst = time.localtime(t)
-    m = (m+1)%60
-    if m==0:
-        h=(h+1)%24
-        if h==0:
-            d=d+1
-            wd=wd+1
-            jd=jd+1
-            ##I'm not going to worry about end-of-month.  Sue me!
-    t = time.mktime((Y, M, D, h, m, 0, wd, jd, dst))
-    return t
+I = complex(0,1)
 
-
-class Meter(Canvas):
+class Meter:
     angle_range = 160
     fancy_needle = 1
     avg_interval = 5
+
+    def __init__(self, panel, left, top, right, bottom, scale_max=2.5):
+        self.panel = panel
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.scale_max = scale_max
+        self.values = []
+        self.total = 0.0
+        self.needle = None
+        self.labels = []
+
+    def resize(self, left, top, right, bottom):
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.redraw_labels()
+        
+    def update(self, value):
+        self.total = self.total + value
+        self.values.insert(0,value)
+        while len(self.values)>self.avg_interval:
+            self.total = self.total - self.values.pop()
+        avg = self.total / self.avg_interval
+        self.draw_needle(avg)
+        
+    def draw_needle(self, value):
+        left, top, right, bottom = self.left, self.top, self.right, self.bottom
+        size = (right - left) / 2.
+        mid_x = (left + right) / 2.
+
+        bottom = bottom - 3 #adjustment
+        
+        if value>self.scale_max:
+            while value>self.scale_max:
+                self.scale_max = self.scale_max * 2
+            self.redraw_labels()
+        
+        start_angle = (180 - self.angle_range)/2
+        angle_range = self.angle_range
+        radius = 0.85 * size
+        ang = dtr(start_angle + angle_range * (1 - value/self.scale_max))
+        p0 = complex(mid_x, 0)
+        p1 = p0 + radius * exp(I*ang)
+        p2 = p0 + 3*exp(I*(ang-pi/2))
+        p3 = p0 + 3*exp(I*(ang+pi/2))
+        if self.needle:
+            self.panel.delete(self.needle)
+        if self.fancy_needle:
+            self.needle = self.panel.create_polygon(p1.real, bottom-p1.imag,
+                                               p2.real, bottom-p2.imag,
+                                               p3.real, bottom-p3.imag,
+                                               fill='red')
+        else:
+            self.needle1 = self.panel.create_line(p0.real, bottom-p0.imag,
+                                            p1.real, bottom-p1.imag,
+                                            width=3, fill='red')
+            
+        
+        
+    def redraw_labels(self):
+        left, top, right, bottom = self.left, self.top, self.right, self.bottom
+        mid_x = (left + right) / 2.
+        size = (right - left) / 2.
+        start_angle = (180 - self.angle_range) / 2.
+        angle_range = self.angle_range
+        for l in self.labels:
+            self.panel.delete(l)
+
+        self.labels.append(self.panel.create_rectangle(left, top-3, #adjustment
+                                                 right, bottom,
+                                                 fill='white'))
+
+        bottom = bottom - 3 #adjustment
+        radius = 0.85 * size
+        self.labels.append(self.panel.create_arc(mid_x - radius,  bottom - radius,
+                                           mid_x + radius, bottom + radius, 
+                                           start=start_angle, extent=angle_range, width=2, style='arc'))
+
+        self.labels.append(self.panel.create_oval(mid_x - 3, bottom - 3,
+                                            mid_x + 3, bottom + 3,
+                                            fill='red'))
+
+        p0 = complex(mid_x, 0)
+        r0 = 0.80 * size
+        r1 = 0.85 * size
+        r2 = 0.93 * size
+        for x in range(11):
+            label = "%.3g" % (x*self.scale_max/10.0)
+            ang = start_angle + angle_range * (1 - x/10.0)
+            p1 = p0 + r0 * exp(dtr(I*ang))
+            p2 = p0 + r1 * exp(dtr(I*ang))
+            p3 = p0 + r2 * exp(dtr(I*ang))
+            self.labels.append(self.panel.create_line(p1.real, bottom-p1.imag, p2.real, bottom-p2.imag))
+            self.labels.append(self.panel.create_text(p3.real, bottom-p3.imag, text=label))
+
+    
+class MeterPanel(Canvas):
+    bottom_margin = 20
     def __init__(self, master, system_name, **attributes):
-        Canvas.__init__(self, master)
+        width, height = 475, 150
+        Canvas.__init__(self, master, width=width, height=height)
         self.system_name = system_name
         title = "Ratemeter: "+system_name
         Tk.title(self.master, title)
         self.pack(expand=1, fill=BOTH)
-        self.width, self.height = 0,0
-        self.scale_max = 50.0
-        self.r = []
-        self.w = []
-        self.tr = 0.0
-        self.tw = 0.0
-        self.needle1 = None
-        self.needle2 = None
+        self.width, self.height = width, height
+        bottom = height - self.bottom_margin
+        meter_size = 0.9 * width / 2.
+        x0 = (width/2. - meter_size) / 2.
+        y0 = bottom - meter_size/2.
+        self.meter1 = Meter(self, x0, y0, x0+meter_size, bottom)
+        x0 = x0 + self.width/2.
+        self.meter2 = Meter(self, x0, y0, x0+meter_size, bottom)
         self.labels = []
         
-        self.bottom_margin = 20
-        
-    def update_needles(self, r, w): 
-        self.tr = self.tr + r
-        self.r.insert(0,r)
-        while len(self.r)>self.avg_interval:
-            self.tr = self.tr - self.r.pop()
-        self.tw = self.tw + w
-        self.w.insert(0,w)
-        while len(self.w)>self.avg_interval:
-            self.tw = self.tw - self.w.pop()
-        ar = self.tr / self.avg_interval
-        aw = self.tw / self.avg_interval
-
+    def update_meters(self, r, w): 
         self.maybe_resize()
-        self.draw_needles(ar, aw)
+        self.meter1.update(r)
+        self.meter2.update(w)
         self.update()
-
-    def draw_needles(self, r, w):
-
-        width, height = self.width, self.height
-        bottom = self.bottom_margin
-        
-        I = complex(0,1)
-        size  = 0.8 * self.width/4.0
-        start_angle = dtr(180-self.angle_range)/2
-        angle_range = dtr(self.angle_range)
-        
-        ang = start_angle + angle_range * (1 - r/self.scale_max)
-        p0 = complex(width/4.0, bottom)
-        p1 = p0 + size * exp(I*ang)
-        p2 = p0 + 3*exp(I*(ang-pi/2))
-        p3 = p0 + 3*exp(I*(ang+pi/2))
-        if self.needle1:
-            self.delete(self.needle1)
-        if self.fancy_needle:
-            self.needle1 = self.create_polygon(p1.real, height-p1.imag,
-                                               p2.real, height-p2.imag,
-                                               p3.real, height-p3.imag,
-                                               fill='red')
-        else:
-            self.needle1 = self.create_line(p0.real, height-p0.imag,
-                                            p1.real, height-p1.imag,
-                                            width=3, fill='red')
-            
-        ang = start_angle + angle_range * (1 - w/self.scale_max)
-        p0 = complex(3*width/4.0, bottom)
-        p1 = p0 + size * exp(I*ang)
-        p2 = p0 + 3*exp(I*(ang-pi/2))
-        p3 = p0 + 3*exp(I*(ang+pi/2))
-        if self.needle2:
-            self.delete(self.needle2)
-
-        if self.fancy_needle:
-            self.needle2 = self.create_polygon(p1.real, height-p1.imag,
-                                               p2.real, height-p2.imag,
-                                               p3.real, height-p3.imag,
-                                               fill='red')
-        else:
-            self.needle2 = self.create_line(p0.real, height-p0.imag,
-                                            p1.real, height-p1.imag,
-                                            width=3, fill='red')
-        
         
     def maybe_resize(self):
         width =  self.winfo_width()
@@ -133,7 +163,7 @@ class Meter(Canvas):
             return
 
         self.width, self.height = width, height
-        bottom = self.bottom_margin
+        bottom = height - self.bottom_margin
         
         for l in self.labels:
             self.delete(l)
@@ -146,49 +176,13 @@ class Meter(Canvas):
         self.labels.append(self.create_text(width/4, height-7, text="Read MB/S"))
         self.labels.append(self.create_text(3*width/4, height-7, text="Write MB/S"))
 
-        size = 0.9 * width / 4.0
-        self.labels.append(self.create_rectangle(width/4 - size, height - bottom - size,
-                                                 width/4 + size, height-bottom+3, fill='white'))
-
-        self.labels.append(self.create_rectangle(3*width/4 - size, height - bottom - size,
-                                                 3*width/4 + size, height-bottom+3, fill='white'))
-
-        size = 0.8 * width / 4.0
-        start_angle = (180-self.angle_range)/2
-        self.labels.append(self.create_arc(width/4 - size, height - bottom - size,
-                                           width/4 + size, height - bottom + size,
-                                           start=start_angle, extent=self.angle_range, width=2, style='arc'))
-        
-        self.labels.append(self.create_arc(3*width/4 - size, height - bottom - size,
-                                           3*width/4 + size, height - bottom + size,
-                                           start=start_angle, extent=self.angle_range, width=2, style='arc'))
-
-        self.labels.append(self.create_oval(width/4 - 3, height - bottom - 3,
-                                            width/4 + 3, height - bottom + 3,
-                                            fill='red'))
-
-        self.labels.append(self.create_oval(3*width/4 - 3, height - bottom - 3,
-                                            3*width/4 + 3, height - bottom + 3,
-                                            fill='red'))
-
-        I = complex(0,1)
-        start_angle = dtr(start_angle)
-        angle_range = dtr(self.angle_range)
-        for x in range(11):
-            label = "%.2g" % (x*self.scale_max/10.0)
-            ang = start_angle + angle_range * (1 - x/10.0)
-            p0 = complex(width/4.0, bottom)
-            p1 = p0 + (0.75*width/4.0) * exp(I*ang)
-            p2 = p0 + (0.8*width/4.0) * exp(I*ang)
-            p3 = p0 + (0.85*width/4.0) * exp(I*ang)
-            self.labels.append(self.create_line(p1.real, height-p1.imag, p2.real, height-p2.imag))
-            self.labels.append(self.create_text(p3.real, height-p3.imag, text=label))
-            p0 = complex(3*width/4.0, bottom)
-            p1 = p0 + (0.75*width/4.0) * exp(I*ang)
-            p2 = p0 + (0.8*width/4.0) * exp(I*ang)
-            p3 = p0 + (0.85*width/4.0) * exp(I*ang)
-            self.labels.append(self.create_line(p1.real, height-p1.imag, p2.real, height-p2.imag))
-            self.labels.append(self.create_text(p3.real, height-p3.imag, text=label))
+        meter_size = 0.9 * self.width/2.
+        x0 = 0.05 * self.width/2.
+        y0 = bottom - meter_size/2.
+        self.meter1.resize(x0, y0, x0+meter_size, bottom)
+        x0 = x0 + self.width/2.
+        self.meter2.resize(x0, y0, x0+meter_size, bottom)
+        self.update()
             
 class Ratemeter:
     interval = 1
@@ -200,11 +194,13 @@ class Ratemeter:
         self.output_dir = output_dir
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         hostname = os.uname()[1]
+        if hostname == 'sirius.net.home':
+            hostname = 'cgw-sirius.dyndns.org'
         self.sock.bind((hostname, 0))
         self.addr = self.sock.getsockname()
         self.subscribe_time = 0
         self.mover_msg = {} #key is mover, value is last (num, denom)
-        self.meter = Meter(master=None, system_name = system_name)
+        self.meter_panel = MeterPanel(master=None, system_name = system_name)
         
     def subscribe(self):
         self.sock.sendto("notify %s %s" % (self.addr),
@@ -214,9 +210,8 @@ class Ratemeter:
         now = time.time()
         self.start_time = now
         N = 1L
-        MB = 1024*1024
-        bytes_read = 0.0
-        bytes_written = 0.0
+        MB = 1024*1024.
+        bytes_read = bytes_written = 0.0
         while 1:
             now = time.time()
 
@@ -227,15 +222,13 @@ class Ratemeter:
             end_time = self.start_time + N * self.interval
             remaining = end_time - now
             if remaining <= 0:
-                self.meter.update_needles(bytes_read/MB, bytes_written/MB)
-
-                bytes_read = 0.0
-                bytes_written = 0.0
+                self.meter_panel.update_meters(bytes_read/MB, bytes_written/MB)
+                bytes_read = bytes_written = 0.0
                 N = N + 1
                 end_time = self.start_time + N * self.interval
                 remaining = end_time - now
 
-            r, w, x = select.select([self.sock], [], [], max(0,remaining))
+            r, w, x = select.select([self.sock], [], [], remaining)
             
             if not r:
                 continue
@@ -250,6 +243,7 @@ class Ratemeter:
             if not cmd:
                 continue
             cmd = string.strip(cmd)
+
             words = string.split(cmd)
             if not words:
                 continue
@@ -264,15 +258,13 @@ class Ratemeter:
             writing = num>0
             num = abs(num)
             denom = atol(words[3])
-            
             prev = self.mover_msg.get(mover)
             self.mover_msg[mover] = (num, denom)
             
             if not prev:
                 continue
-
             num_0, denom_0 = prev
-            if denom != denom_0 or num<num_0:
+            if num<num_0:
                 #consider this the beginning of a new transfer
                 continue
             bytes = num - num_0
@@ -280,7 +272,6 @@ class Ratemeter:
                 bytes_written = bytes_written + bytes
             else:
                 bytes_read = bytes_read + bytes
-
 
 if __name__ == "__main__":
     
