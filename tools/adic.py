3c2acd4cba20cbca7ffb22942aa87087afba7dd7
Jon Bakken <bakken>
2000-10-18 22:32:32 +0000

    convert for new amu

--- tools/adic.py
+++ tools/adic.py
@@ -14,23 +14,32 @@ while 1:
     if len(tok) < 6:
         continue
     
-    dat, tim, ampm, junk, seq = tok[:5]
-    rest = tok[5:]
+    dat, tim, seq, junk = tok[:4]
+    rest = tok[4:]
 
-    if rest[0]=='Command':
-        cmd_dict[seq] = rest[2:]
+    if len(rest)>2 and rest[2]=='command:':
+        cmd_dict[seq] = (rest[3:],dat,tim)
     
     if rest[0] in ['Positive', 'Negative']:
-        cmd = cmd_dict.get(seq)
+        (cmd,dat0,tim0) = cmd_dict.get(seq)
         if not cmd:
             continue
-        print string.join(cmd,' '), rest[0]
+        if dat == dat0:
+            day="%s %s->%s"%(dat,tim0,tim)
+        else:
+            day="%s %s->%s %s"%(dat0,tim0,dat,tim)
+        print string.join(cmd,' '), rest[0], day
         del cmd_dict[seq]
     
 
-""" Lines look like this.  The time is always wrong
+""" Lines look like this. (All times are now correct.)
 
 09-28-00 12:44:01 pm 0 0827     Command 0827: MONT of Volser ..........PRF022 to D22.......<01030>
+
+2000-10-18 17:06:35     0687 <01030> RQM requests command: KEEP from Drive D25........
+2000-10-18 17:06:35     0687 <01056> KEEP [D25.......] received, will be processed soon.
+2000-10-18 17:06:35     0687 <01094> The requested drive DC25010101 is empty (Archive catalog).
+2000-10-18 17:06:35     0687 <01041> Negative answer: KEEP 0687  RC: 1094.
 
 """
 
