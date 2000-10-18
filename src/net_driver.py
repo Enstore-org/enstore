c8206a2cfe7b8436511b4cddd157659bd7749df6
Charles Waldman <cgw>
2000-10-18 01:10:40 +0000

    don't set SO_SENDBUF and SO_RCVBUF, this seems to cause trouble when receiving data from d0test using gigabit ethernet

--- src/net_driver.py
+++ src/net_driver.py
@@ -24,16 +24,20 @@ class NetDriver(driver.Driver):
         
     def fdopen(self, sock):
         self.sock = sock
-        size = 1024*1024
-        for opt in (socket.SO_RCVBUF, socket.SO_SNDBUF):
-            try:
-                sock.setsockopt(socket.SOL_SOCKET, opt, size)
-                Trace.trace(10, "tcp buffer size %s %s" % (
-                    {socket.SO_RCVBUF:"SO_RCVBUF", socket.SO_SNDBUF:"SO_SNDBUF"}[opt],
-                    sock.getsockopt(socket.SOL_SOCKET, opt)))
-            except socket.error, msg:
-                Trace.log(e_errors.ERROR, "setting tcp buffer size:  %s %s %s" % (
-                    opt, size, msg))
+        size = 128 * 1024
+
+        #This is disabled because it seems to trigger a problem
+        # when recieving data from a gigabit ethernet interface
+        # on IRIX
+##        for opt in  (socket.SO_RCVBUF, socket.SO_SNDBUF):
+##            try:
+##                sock.setsockopt(socket.SOL_SOCKET, opt, size)
+##                Trace.trace(10, "tcp buffer size %s %s" % (
+##                    {socket.SO_RCVBUF:"SO_RCVBUF", socket.SO_SNDBUF:"SO_SNDBUF"}[opt],
+##                    sock.getsockopt(socket.SOL_SOCKET, opt)))
+##            except socket.error, msg:
+##                Trace.log(e_errors.ERROR, "setting tcp buffer size:  %s %s %s" % (
+##                    opt, size, msg))
 
         self._last_rate = 0
         self._rate = 0
