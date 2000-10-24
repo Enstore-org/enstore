2a93208280a0e6c6954afa3f2f9911d68c3759d8
Charles Waldman <cgw>
2000-10-24 18:42:41 +0000

    reenable noaccess

--- src/volume_clerk.py
+++ src/volume_clerk.py
@@ -1162,13 +1162,8 @@ class VolumeClerkMethods(dispatching_worker.DispatchingWorker):
 
     # flag that the current volume is marked as noaccess
     def set_system_noaccess(self, ticket):
-        Trace.log(e_errors.INFO, "Not setting %s NOACCESS" % (ticket["external_label"],))
-        ticket['status'] = (e_errors.OK, None)
-        self.reply_to_caller(ticket)
-        return
-##  original code:
-##    Trace.alarm(e_errors.WARNING, e_errors.NOACCESS,{"label":ticket["external_label"]}) 
-##    return self.set_system_inhibit(ticket, e_errors.NOACCESS)
+        Trace.alarm(e_errors.WARNING, e_errors.NOACCESS,{"label":ticket["external_label"]}) 
+        return self.set_system_inhibit(ticket, e_errors.NOACCESS)
 
     # flag that the current volume is marked as not allowed
     def set_system_notallowed(self, ticket):
