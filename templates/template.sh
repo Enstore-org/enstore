ca5af7d119817fe762e9a2e76d32a73dc3bf9a90
Charles Waldman <cgw>
2000-11-14 23:15:32 +0000

    goodbye

--- templates/template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -u  # force better programming and ability to use check for not set
-if [ "${1:-}" = "-x" ] ; then set -xv; shift; fi
-
-# bin/$RCSfile template.sh $  $Revision$
-# Description...
