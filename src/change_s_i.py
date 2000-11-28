3080e0bcf341b56cff3a78b6e7c1c64294eb395f
Jon Bakken <bakken>
2000-11-28 00:40:27 +0000

    change into database clean-up script - still primitive, not ready to use

--- src/change_s_i.py
+++ src/change_s_i.py
@@ -28,7 +28,23 @@ jouHome = dbInfo['jou_dir']
 print "dbHome", dbHome
 print "jouHome", jouHome
 
-dict = db.DbTable("volume", dbHome, jouHome, [])
+vcdict = db.DbTable("volume", dbHome, jouHome, ['library', 'volume_family'])
+fcdict = db.DbTable("file", dbHome, jouHome, ['external_label'])
+
+for k in vcdict.keys():
+    rec = vcdict[k]
+    lib = rec['library']
+    if lib in ('samm2'):
+        print k, rec['library'], rec['non_del_files']
+
+D=open('/home/enstore/DE','r')
+for b in D.readlines():
+    b=b[:-1]
+    try:
+        if fcdict[b]['deleted'] != 'yes':
+            print b, fcdict[b]['deleted'], fcdict[b]['external_label'],fcdict[b]['pnfs_name0']
+    except:
+        print 'Trouble', fcdict[b]
 
 #for k in dict.keys():
 #    rec =  dict[k]
@@ -40,25 +56,25 @@ dict = db.DbTable("volume", dbHome, jouHome, [])
 #    print k, rec['volume_family']
 #    dict[k] =  rec
 
-for k in dict.keys():
-    rec = dict[k]
-    fields = string.split(rec['volume_family'],'.')
-    size = len(fields)
-    if size == 3:
-        if fields[2] not in ('cpio_odc','null','none'):
-            print 'ERROR', k, size, fields
-    elif size > 3:
-        vf = string.join((fields[0],fields[1],fields[2]),'.')
-        rec['volume_family'] = vf
-        #dict[k] =  rec
-        print 'FUNNY:', k, size, fields, vf
-    elif size == 1:
-        print 'ONE:',k, size, fields
-    elif size == 2:
-        if fields[1] == 'none':
-            vf = string.join((fields[0],fields[1],'none'),'.')
-            rec['volume_family'] = vf
-            #dict[k] =  rec
-            print 'TWONONE:', k, size, fields, vf
-        else:
-            print 'TWO:',k, size, fields
+# for k in dict.keys():
+#     rec = dict[k]
+#     fields = string.split(rec['volume_family'],'.')
+#     size = len(fields)
+#     if size == 3:
+#         if fields[2] not in ('cpio_odc','null','none'):
+#             print 'ERROR', k, size, fields
+#     elif size > 3:
+#         vf = string.join((fields[0],fields[1],fields[2]),'.')
+#         rec['volume_family'] = vf
+#         #dict[k] =  rec
+#         print 'FUNNY:', k, size, fields, vf
+#     elif size == 1:
+#         print 'ONE:',k, size, fields
+#     elif size == 2:
+#         if fields[1] == 'none':
+#             vf = string.join((fields[0],fields[1],'none'),'.')
+#             rec['volume_family'] = vf
+#             #dict[k] =  rec
+#             print 'TWONONE:', k, size, fields, vf
+#         else:
+#             print 'TWO:',k, size, fields
