92b99e99601598fef1aff2645ec03f9cb869f6d6
Charles Waldman <cgw>
2000-12-13 20:08:57 +0000

    untabify

--- src/event_relay.py
+++ src/event_relay.py
@@ -18,19 +18,19 @@ def get_message_filter_dict(msg_tok):
     filter_d = {}
     # first see if there is a message type list 
     if len(msg_tok) > MINTOKLEN:
-	# yes there is
-	for tok in msg_tok[MINTOKLEN:]:
-	    if tok == ALL:
-		# client wants all messages
-		filter_d = None
-		break
-	    else:
-		# the value of the dictionary element does not matter
-		filter_d[tok] = 1
+        # yes there is
+        for tok in msg_tok[MINTOKLEN:]:
+            if tok == ALL:
+                # client wants all messages
+                filter_d = None
+                break
+            else:
+                # the value of the dictionary element does not matter
+                filter_d[tok] = 1
     else:
-	# the client wants all of the messages
-	filter_d = None
-		
+        # the client wants all of the messages
+        filter_d = None
+                
     return filter_d
         
 class Relay:
@@ -60,33 +60,33 @@ class Relay:
                 try:
                     ip = tok[1]
                     port = int(tok[2])
-		    # the rest of the message is the list of message types the
-		    # client is interested in.  if there is no list, the client
-		    # wants all message types
-		    filter_d = get_message_filter_dict(tok)
+                    # the rest of the message is the list of message types the
+                    # client is interested in.  if there is no list, the client
+                    # wants all message types
+                    filter_d = get_message_filter_dict(tok)
                     self.clients[(ip, port)] = (now, filter_d)
                 except:
                     print "cannot handle request", msg
 
-	    elif tok[0] == UNSUBSCRIBE:
-		try:
-		    ip = tok[1]
-		    port = int(tok[2])
-		    del self.clients[(ip, port)]
-		except:
-		    print "cannot handle request", msg
+            elif tok[0] == UNSUBSCRIBE:
+                try:
+                    ip = tok[1]
+                    port = int(tok[2])
+                    del self.clients[(ip, port)]
+                except:
+                    print "cannot handle request", msg
             else:
                 for addr, (t0, filter_d) in self.clients.items():
                     if now - t0 > self.client_timeout:
                         del self.clients[addr]
                     else:
-			# client wants the message if there is no filter or if
-			# the filter contains the message type in its dict.
-			if (not filter_d) or filter_d.has_key(tok[0]):
-			    try:
-				self.send_socket.sendto(msg, addr)
-			    except:
-				print "send failed", addr
+                        # client wants the message if there is no filter or if
+                        # the filter contains the message type in its dict.
+                        if (not filter_d) or filter_d.has_key(tok[0]):
+                            try:
+                                self.send_socket.sendto(msg, addr)
+                            except:
+                                print "send failed", addr
                             
                 
 if __name__ == '__main__':
