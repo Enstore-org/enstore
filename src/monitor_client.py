7a3b68fc865e54f1ad7d7cbaee4ac54945e660d1
Michael Zalokar <zalokar>
2000-12-14 20:02:15 +0000

    The enstore system failure should work with this fix.

--- src/monitor_client.py
+++ src/monitor_client.py
@@ -118,7 +118,8 @@ class MonitorServerClient(generic_client.GenericClient):
         r,w,ex = select.select([self.c_socket], [], [self.c_socket],
                                self.timeout)
         if not r :
-            print "passive open did not hear back from monitor server via TCP"
+            if not summary:
+                print "passive open did not hear back from monitor server via TCP"
             raise  errno.errorcode[errno.ETIMEDOUT]
 
         #simulate the control socket between encp and the mover
