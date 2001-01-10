7dfdf17a928231c0f2374b2b3a27c5adc13ba363
Charles Waldman <cgw>
2001-01-10 20:29:49 +0000

    oops - we were calling stat on the wrong file

--- src/atomic.py
+++ src/atomic.py
@@ -12,8 +12,23 @@ def _open1(pathname,mode=0666):
     delete_at_exit.register(pathname)
     fd = os.open(pathname, os.O_CREAT|os.O_EXCL|os.O_RDWR, mode)
     return fd
-        
-def _open2(pathname,mode=0666): #see "man open(2)"
+
+##  From man open(2)
+##       O_EXCL  When used with O_CREAT, if the file already exists
+##               it is an error and the open will fail.  O_EXCL  is
+##               broken on NFS file systems, programs which rely on
+##               it for performing locking  tasks  will  contain  a
+##               race   condition.   The  solution  for  performing
+##               atomic file locking using a lockfile is to  create
+##               a  unique file on the same fs (e.g., incorporating
+##               hostname and pid), use link(2) to make a  link  to
+##               the  lockfile.  If  link()  returns 0, the lock is
+##               successful.  Otherwise, use stat(2) on the  unique
+##               file  to  check if its link count has increased to
+##               2, in which case the lock is also successful.
+
+
+def _open2(pathname,mode=0666)
 
     tmpname = "%s-%s-%s" % (pathname, os.uname()[1], os.getpid())
     delete_at_exit.register(tmpname)
@@ -26,7 +41,7 @@ def _open2(pathname,mode=0666): #see "man open(2)"
         ok = 1
     except:
         try:
-            s = os.stat(pathname)
+            s = os.stat(tmpname)
             if s and s[stat.ST_NLINK]==2:
                 ok = 1
         except:
