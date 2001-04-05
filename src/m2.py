d467c9f90787687c789d777b685218d88f3cc042
Chih Hao Huang <huangch>
2001-04-05 21:25:39 +0000

    add error handling

--- src/m2.py
+++ src/m2.py
@@ -16,6 +16,10 @@ import getpass
 #	notify:	a list of email addresses to send the notification to
 #	comment:extra message that goes into notification
 #		This is useful to pass enstore mover information
+#
+#	return error message or None
+#
+#	Most of the errors are handled by m2prob
 
 def dump_code(device, path=None, sendto=None, notify=None, comment=None):
 
@@ -30,20 +34,24 @@ def dump_code(device, path=None, sendto=None, notify=None, comment=None):
 	# parse m2probe's output for file name and status
 	l = os.popen(cmd).readlines()
 	res = string.split(l[-1], "dumped to")
-	if len(res) != 2:	# something is wrong
-		return 1
-
 	status = string.join(l, '')
+	if len(res) != 2:	# something is wrong
+		return "code dumping failed:\n"+status
+
 	# get the file name
 	f = string.strip(res[-1])
 
 	from_add = getpass.getuser()+'@'+os.uname()[1]
 	subject = "M2 dump taken at "+time.ctime(time.time())
 
+	error_msg = None
+
 	# send it to some one?
 	if sendto:
 		mesg = "This is an automatically generated M2 dump by an enstore mover\n\n"+status
-		enmail.mail_bin(from_add, sendto, subject, f, mesg)
+		res = enmail.mail_bin(from_add, sendto, subject, f, mesg)
+		if res:
+			error_msg = 'On sending binary\n'+res
 
 	if notify:
 		mesg = "A M2 dump is taken by "+from_add+"\n\n"+status
@@ -55,6 +63,12 @@ def dump_code(device, path=None, sendto=None, notify=None, comment=None):
 			else:
 				to_addresses = sendto
 			mesg = mesg+"\n\nThe dump file has been sent to "+to_addresses
-		enmail.mail(from_add, sendto, subject, mesg)
+		res = enmail.mail(from_add, sendto, subject, mesg)
 
-	return(0)
+		if res:
+			if error_msg:
+				error_msg = error_msg+'\n\nOn sending notification:\n'+res
+			else:
+				error_msg = 'On sending notification:\n'+res
+
+	return error_msg
