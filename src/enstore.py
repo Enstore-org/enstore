59e73d4da60fe1c95216c031cb11738d9863258c
Eileen Berman <berman>
2001-06-04 19:58:27 +0000

    fix bug in call to get_valid_options

--- src/enstore.py
+++ src/enstore.py
@@ -266,7 +266,7 @@ class EnstoreInterface(UserOptions):
             print "\t%s"%(server,)
 
     def print_valid_options(self, server):
-        opts = self.get_valid_options()
+        opts = self.get_valid_options(server)
         print "\nERROR: Allowed options for %s are : "
         for opt in opts:
             print "\t%s"%(opt,)
