e8eaffb1a565347e039f56d7bb74d8507ffcb647
cvs2svn <UNKNOWN@fnal.gov>
2001-06-11 15:57:50 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_9_encp_SunOS+5'.
    Sprout from master 2001-06-07 15:48:25 UTC Michael Zalokar <zalokar> 'Added the "error" RESUBMITTING because NET_ERROR confused everyone.'
    Cherrypick from master 2001-04-11 20:19:38 UTC Eileen Berman <berman> 'change name logged under':
        etc/rip.conf
        src/db.py
        src/db_retrive_backup.py
        src/enstore_up_down.py
        src/espion.py
        src/event_relay.py
        src/event_relay_client.py
        src/file_clerk.py
        src/file_clerk_client.py
        src/pnfs.py
    Cherrypick from master 2000-12-26 15:49:04 UTC Jon Bakken <bakken> 'stopgap backup solution to prevent database hangs':
        modules/STK.c
        sbin/down-backup-up
        test/t_rand.c
        test/t_read.c
    Cherrypick from master 2001-06-05 14:38:41 UTC Michael Zalokar <zalokar> 'fix code error':
        src/enstore_functions.py
        src/inquisitor.py
    Cherrypick from master 2001-06-11 15:57:45 UTC Chih Hao Huang <huangch> 'bumping version to v2_9 because of encpCut':
        sbin/encpCut
        src/encp.py
    Cherrypick from master 2001-02-07 16:38:25 UTC Eileen Berman <berman> '.k5login file for rip cluster':
        etc/rip.k5login
    Cherrypick from master 2001-02-12 21:07:19 UTC Eileen Berman <berman> 'support FNAL.GOV realm too':
        etc/rip.root.k5login
    Cherrypick from master 2001-03-20 17:25:01 UTC Jon Bakken <bakken> 'add in stk rsh media changer':
        src/stk.py
    Cherrypick from master 2001-03-28 22:42:46 UTC Eileen Berman <berman> '.k5login file for user enstore on d0en and stken cluster':
        etc/d0en-stken.enstore.k5login
    Cherrypick from master 2001-05-17 15:16:26 UTC Alexander Moibenko <moibenko> 'check for the presence of volmap_name in remove_deleted_volume':
        src/volume_clerk.py
    Delete:
        src/option.py
        src/super_remove.py
        tools/pychecker/.cvsignore
        tools/pychecker/COPYRIGHT
        tools/pychecker/Config.py
        tools/pychecker/OP.py
        tools/pychecker/README
        tools/pychecker/Stack.py
        tools/pychecker/VERSION
        tools/pychecker/__init__.py
        tools/pychecker/checker.py
        tools/pychecker/printer.py
        tools/pychecker/warn.py

--- src/enstore_functions.py
+++ src/enstore_functions.py
@@ -2,7 +2,6 @@ import time
 import string
 import os
 import exceptions
-import socket
 
 import configuration_server
 import enstore_constants
@@ -12,18 +11,6 @@ import Trace
 import e_errors
 
 DEFAULTHTMLDIR = "."
-
-# this is done here to have central exception handling.  it cannot be
-# done in the erc read routine, because we cannot import Trace in that
-# module as Trace imports the erc module
-def read_erc(erc, fd=None):
-    try:
-    	msg = erc.read()
-    except socket.error, detail:
-	Trace.log (e_errors.ERROR, 
-		   "socket error - could not read from erc (%s)"%(detail,))
-	return None
-    return msg
 
 def get_config_dict():
     name = os.environ.get("ENSTORE_CONFIG_FILE", "")
