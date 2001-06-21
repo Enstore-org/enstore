2ec77f904f0f5fdb70b8f8cbbeef2177e8742f6d
Eileen Berman <berman>
2001-06-21 14:31:31 +0000

    support new plots

--- src/enstore_html.py
+++ src/enstore_html.py
@@ -63,7 +63,8 @@ PLOT_INFO = [[enstore_constants.MPH_FILE, "Mounts/Hour"],
 	     [enstore_constants.BPD_FILE, "Bytes/Day"], 
 	     [enstore_constants.XFERLOG_FILE, "Transfer Activity (log)"],
 	     [enstore_constants.XFER_FILE, "Transfer Activity"],
-             [enstore_constants.RATES, "Total Terabytes/Day (no null mvs)"]]
+             [enstore_constants.NULL_RATES, "Null Terabytes/Day"],
+	     [enstore_constants.REAL_RATES, "Real Terabytes/Day (no null mvs)"]]
 
 DEFAULT_LABEL = "UNKNOWN INQ PLOT"
 # these defaults mean, output all the lines on the main page
