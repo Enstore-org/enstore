49166ebb3ac4d9d0260de60ae8b6a25088e60e8a
Chih Hao Huang <huangch>
2001-08-23 22:09:19 +0000

    reset declared time stamp when being recycled

--- src/volume_clerk.py
+++ src/volume_clerk.py
@@ -365,6 +365,7 @@ class VolumeClerkMethods(dispatching_worker.DispatchingWorker):
         if recycle:
             record['external_label'] = vol
             record['remaining_bytes'] = record['capacity_bytes']
+            record['declared'] = time.time()
             record['eod_cookie'] = '0000_000000000_0000001'
             record['last_access'] = -1
             record['first_access'] = -1
