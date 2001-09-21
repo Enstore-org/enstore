cad0edb8293e48a5aa628bc467a84e98e077c502
Eileen Berman <berman>
2001-09-21 20:45:43 +0000

    check for too many pending reads in an lm

---
+++ src/set_lm_noread.py
@@ -0,0 +1,89 @@
+import os
+import sys
+
+import library_manager_client
+import volume_family
+import log_client
+import enstore_functions
+import interface
+import Trace
+import e_errors
+
+MY_NAME = "SET_LM_NOREAD"
+WRITE_LIMIT = 20
+READ_LIMIT = 0
+NOREAD = 'noread'
+UNLOCKED = 'unlocked'
+
+"""
+check a library manager to see if it has many read queues up for the entered file families.
+if so, and there are read jobs for these file families, set the lm to noread so that the
+writes can finish.  set the lm back to unlocked if the writes are done.
+
+"""
+
+def get_ff(vc):
+    ff = vc.get('file_family', None)
+    if not ff:
+	vf = vc.get('volume_family', None)
+	if vf:
+	    ff = volume_family.extract_file_family(vf)
+    return ff
+
+def parse_queue(queue, ff_d, ff_k):
+    for elem in queue:
+	ff = get_ff(elem['vc'])
+	if ff in ff_k:
+	    ff_d[ff] = ff_d[ff] + 1
+
+Trace.init(MY_NAME)
+config_host = interface.default_host()
+config_port = interface.default_port()
+csc = (config_host, config_port)
+
+if config_host and config_port:
+    logc = log_client.LoggerClient(csc, MY_NAME)
+    if len(sys.argv) < 3:
+	# there was no lib man or file family entered, exit
+	Trace.log(e_errors.WARNING, "No library manager specified or file family specified")
+    else:
+	lm = sys.argv[1]
+	ff_l = []
+	ff_d = {}
+	ff_d_r = {}
+	# get a list of all the file families to look for
+	for arg in sys.argv[2:]:
+	    ff_d[arg] = 0
+	    ff_d_r[arg] = 0
+
+	ff_k = ff_d.keys()
+	lmc = library_manager_client.LibraryManagerClient((config_host, config_port), lm)
+	ticket = lmc.get_lm_state()
+	if enstore_functions.is_ok(ticket):
+	    state = ticket['state']
+	    ticket = lmc.getworks_sorted()
+	    if enstore_functions.is_ok(ticket):
+		pq = ticket['pending_works']
+		adminq = pq['admin_queue']
+		writeq = pq['write_queue']
+		readq = pq['read_queue']
+
+		parse_queue(adminq, ff_d, ff_k)
+		parse_queue(writeq, ff_d, ff_k)
+		parse_queue(readq, ff_d_r, ff_k)
+
+		for ff in ff_k:
+		    if ff_d[ff] > WRITE_LIMIT and ff_d_r[ff] > READ_LIMIT:
+			if not state == NOREAD:
+			    Trace.log(e_errors.WARNING, 
+				    "Setting %s to noread, %s has %s writes and %s reads"%(lm, 
+										  ff, ff_d[ff],
+										  ff_d_r[ff]))
+			    lmc.change_lm_state(NOREAD)
+			# we do not have to check any more
+			break
+		else:
+		    # we did not change the state of the lm to noread.  
+		    # see if it is in noread, if so set it back to normal
+		    if state == NOREAD:
+			ticket = lmc.change_lm_state(UNLOCKED)
