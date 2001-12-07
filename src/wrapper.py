4e9d794e007564a6ccc9e36e225694bccd4f0b1e
Eileen Berman <berman>
2001-12-07 20:06:28 +0000

    change name of some keys for mover info

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,7 +1,9 @@
 # create the dictionary needed by the wrappers
+import string
+
 import cern_wrapper
 
-MOVER_WRAPPER_INFO = 'mover_info_wrapper'
+MOVER = 'mover'
 
 def create_wrapper_dict(ticket):
     # the wrapper section already contains much of the information that we need
@@ -9,13 +11,16 @@ def create_wrapper_dict(ticket):
     wrapper_d = ticket['wrapper']
 
     # values needed for the cern wrapper
-    ticket_mover = ticket[MOVER_WRAPPER_INFO]
+    ticket_mover = ticket[MOVER]
     wrapper_d[cern_wrapper.ENCPVERSION] = ticket['version']
     wrapper_d[cern_wrapper.BLOCKLEN] = None                                                   ####
-    wrapper_d[cern_wrapper.COMPRESSION] = ticket_mover[cern_wrapper.COMPRESSION]              ####
-    wrapper_d[cern_wrapper.MOVERNODE] = ticket_mover[cern_wrapper.MOVERNODE]                  ####
-    wrapper_d[cern_wrapper.DRIVEMFG] = ticket_mover[cern_wrapper.DRIVEMFG]                    ####
-    wrapper_d[cern_wrapper.DRIVEMODEL] = ticket_mover[cern_wrapper.DRIVEMODEL]                ####
-    wrapper_d[cern_wrapper.DRIVESERIALNUMBER] = ticket_mover[cern_wrapper.DRIVESERIALNUMBER]  ####
+    wrapper_d[cern_wrapper.COMPRESSION] = ticket_mover[cern_wrapper.COMPRESSION]
+    mnode = ticket_mover[cern_wrapper.MOVERNODE]
+    # the node name is the characters up to the first :
+    mnode = string.split(mnode, ":")
+    wrapper_d[cern_wrapper.MOVERNODE] = mnode[0]
+    wrapper_d[cern_wrapper.DRIVEMFG] = ticket_mover[cern_wrapper.DRIVEMFG]
+    wrapper_d[cern_wrapper.DRIVEMODEL] = ticket_mover[cern_wrapper.DRIVEMODEL]
+    wrapper_d[cern_wrapper.DRIVESERIALNUMBER] = ticket_mover[cern_wrapper.DRIVESERIALNUMBER]
     
     return wrapper_d
