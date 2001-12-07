d8b6d3f0e462400c3f1f80aa053f50a66a36ba52
Alexander Moibenko <moibenko>
2001-12-07 16:16:04 +0000

    fixed bug

--- src/mover.py
+++ src/mover.py
@@ -3441,9 +3441,9 @@ class DiskMover(Mover):
         ticket['mover']={}
         ticket['mover'].update(self.config)
         ticket['mover']['device'] = "%s:%s" % (self.config['host'], self.config['device'])
-
+        if ticket['fc']['external_label'] == None
+            del(ticket['fc']['external_label'])
         self.current_work_ticket = ticket
-        del(self.current_work_ticket['fc']['external_label'])
         self.run_in_thread('client_connect_thread', self.connect_client)
 
     def finish_transfer_setup(self):
