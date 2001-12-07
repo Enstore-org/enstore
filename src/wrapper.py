6a993bf6b7b84ac9dcc79bf3594a535afa61c512
Eileen Berman <berman>
2001-12-07 19:22:19 +0000

    change mover dict key name

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,13 +1,15 @@
 # create the dictionary needed by the wrappers
 import cern_wrapper
 
+MOVER_WRAPPER_INFO = 'mover_info_wrapper'
+
 def create_wrapper_dict(ticket):
     # the wrapper section already contains much of the information that we need
     # including all of the information for the cpio_odc_wrapper
     wrapper_d = ticket['wrapper']
 
     # values needed for the cern wrapper
-    ticket_mover = ticket['mover']
+    ticket_mover = ticket[MOVER_WRAPPER_INFO]
     wrapper_d[cern_wrapper.ENCPVERSION] = ticket['version']
     wrapper_d[cern_wrapper.BLOCKLEN] = None                                                   ####
     wrapper_d[cern_wrapper.COMPRESSION] = ticket_mover[cern_wrapper.COMPRESSION]              ####
@@ -15,6 +17,5 @@ def create_wrapper_dict(ticket):
     wrapper_d[cern_wrapper.DRIVEMFG] = ticket_mover[cern_wrapper.DRIVEMFG]                    ####
     wrapper_d[cern_wrapper.DRIVEMODEL] = ticket_mover[cern_wrapper.DRIVEMODEL]                ####
     wrapper_d[cern_wrapper.DRIVESERIALNUMBER] = ticket_mover[cern_wrapper.DRIVESERIALNUMBER]  ####
-    wrapper_d[cern_wrapper.CHECKSUM] = None                                                   ####
     
     return wrapper_d
