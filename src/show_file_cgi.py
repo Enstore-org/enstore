cbef0bf267cab0546aa6cd54a11a7e09320e8800
Chih Hao Huang <huangch>
2002-01-25 00:02:07 +0000

    just setup

--- src/show_file_cgi.py
+++ src/show_file_cgi.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import cgi
-import enstore_utils_cgi
 import os
 import string
 
@@ -12,12 +11,9 @@ print "<html><head><title>File "+bfid+"</title></head>"
 print "<body bgcolor=#ffffd0>"
 print '<pre>'
 
-# setup enstore environment
-config_host, config_port = enstore_utils_cgi.find_enstore()
-os.environ['ENSTORE_CONFIG_HOST'] = config_host
-os.environ['ENSTORE_CONFIG_PORT'] = config_port
 print '<h1><font color=#aa0000>', bfid, '</font></h1><p>'
-res = os.popen('enstore file --bfid '+bfid).readlines()
+# res = os.popen('enstore file --bfid '+bfid).readlines()
+res = os.popen('. /usr/local/etc/setups.sh; setup enstore; enstore file --bfid '+bfid).readlines()
 for i in res:
     print i,
 print "</pre></body></html>"
