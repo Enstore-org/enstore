42a89cce27832e510f67ef4a5a294a0a25fa49fb
Chih Hao Huang <huangch>
2002-01-31 23:40:25 +0000

    align format

--- src/inventory.py
+++ src/inventory.py
@@ -410,7 +410,7 @@ def print_volume_quotas_status(volume_quotas, authorized_tapes, output_file):
     vq_file.write("Date this listing was generated: %s\n" % \
                   time.asctime(time.localtime(time.time())))
     
-    vq_file.write("   %-15s %-15s %-11s %-12s %-6s %-9s %-10s %-12s %-7s %-12s %-12s %-13s %s\n" %
+    vq_file.write("   %-15s %-15s %-11s %-12s %-6s %-9s %-10s %-12s %-7s %12s %-12s %-13s %s\n" %
           ("Library", "Storage Group", "Req. Alloc.",
            "Auth. Alloc.", "Quota", "Allocated",
            "Blank Vols", "Written Vols", "Deleted Vols", "Space Used",
