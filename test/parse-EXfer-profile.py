9e7f008299e5092616fc0a987e53ec09a598807a
cvs2svn <UNKNOWN@fnal.gov>
2002-03-19 21:12:51 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_16_encp_IRIX+6_5'.
    Sprout from master 2002-03-19 21:12:49 UTC Chih Hao Huang <huangch> 'bumping version to v2_16 because of encpCut'
    Cherrypick from master 2001-09-04 14:06:18 UTC Eileen Berman <berman> 'headers for disk cache page':
        doc/WWW/disk_cache.gif
        doc/WWW/disk_cache.kil
        doc/WWW/info.gif
        doc/WWW/info.kil
        etc/disk_cache_sys.gif
        etc/disk_cache_sys.kil
        etc/en_patrol.gif
        etc/en_patrol.kil
        etc/enalarm
        etc/enget_patrol_file
        etc/enpatrol.cf
        etc/enpatrol.job
        etc/patrolHelp.html
        modules/Setup
    Cherrypick from master 2000-12-26 15:49:04 UTC Jon Bakken <bakken> 'stopgap backup solution to prevent database hangs':
        modules/STK.c
        sbin/down-backup-up
        src/enstore_schedule.py
        test/t_rand.c
        test/t_read.c
    Cherrypick from master 2001-12-24 17:54:32 UTC Jon Bakken <bakken> 'add step tickers for cdf':
        etc/cdf.step-tickers
        etc/dcache_user_guide.html
        etc/hosts.allow
        src/quickquota.py
    Cherrypick from master 2001-03-21 23:24:42 UTC Chih Hao Huang <huangch> 'an utility to retrive database files and journal files from backup':
        src/db_retrive_backup.py
        src/stk.py
    Cherrypick from master 2001-08-31 21:07:58 UTC Eileen Berman <berman> 'disk cache heading':
        etc/dcache.gif
        etc/dcache.kil
    Cherrypick from master 2002-03-04 19:41:36 UTC Eileen Berman <berman> 'keep cron error output files':
        sbin/save-cron-output.py
        test/parse-EXfer-profile.py
    Cherrypick from master 2001-02-07 16:38:25 UTC Eileen Berman <berman> '.k5login file for rip cluster':
        etc/rip.k5login
    Cherrypick from master 2001-02-12 21:07:19 UTC Eileen Berman <berman> 'support FNAL.GOV realm too':
        etc/rip.root.k5login
    Cherrypick from master 2001-03-28 22:42:46 UTC Eileen Berman <berman> '.k5login file for user enstore on d0en and stken cluster':
        etc/d0en-stken.enstore.k5login
    Cherrypick from master 2001-10-11 18:57:13 UTC Chih Hao Huang <huangch> 'take care of JOURNALS directory and correct order of backups':
        src/restore.py
    Cherrypick from master 2002-01-16 20:17:53 UTC Eileen Berman <berman> 'remove cambot link':
        etc/enstore_system.html
    Delete:
        ngop/.cvsignore
        ngop/.ngop/configxml/Cronjob.enstore.xml
        ngop/.ngop/configxml/Cronjob.xml
        ngop/.ngop/configxml/Hardware.enstore.xml
        ngop/.ngop/configxml/Hardware.xml
        ngop/.ngop/configxml/OSHealth.enstore.xml
        ngop/.ngop/configxml/OSHealth.xml
        ngop/.ngop/configxml/Ping.fermi.xml
        ngop/.ngop/configxml/bigASwitch.xml
        ngop/.ngop/configxml/defME.rules.xml
        ngop/.ngop/configxml/defSys.rules.xml
        ngop/.ngop/configxml/enstore.xml
        ngop/.ngop/configxml/known_status.xml
        ngop/.ngop/configxml/service_class.xml
        ngop/OsHealthLnxAgent.py
        ngop/basAgent.py
        ngop/checkSensor.py
        ngop/checkTime.py
        ngop/cronjobAgent.py
        ngop/cronjobAgent.start
        ngop/documentation.html
        ngop/enstore_ping.xml
        ngop/fermi_hosts.xml
        ngop/linuxHealth.xml
        ngop/local_cronjob.xml
        ngop/monitor.xml
        ngop/myAgentInfo.html
        ngop/ncs.xml
        ngop/newtable.html
        ngop/node_init.py
        ngop/srv_linuxHealth.xml
        tools/pychecker/.cvsignore
        tools/pychecker/COPYRIGHT
        tools/pychecker/CodeChecks.py
        tools/pychecker/Config.py
        tools/pychecker/OP.py
        tools/pychecker/OptionTypes.py
        tools/pychecker/Options.ad
        tools/pychecker/README
        tools/pychecker/Stack.py
        tools/pychecker/VERSION
        tools/pychecker/Warning.py
        tools/pychecker/__init__.py
        tools/pychecker/checker.py
        tools/pychecker/function.py
        tools/pychecker/msgs.py
        tools/pychecker/options.py
        tools/pychecker/printer.py
        tools/pychecker/python.py
        tools/pychecker/utils.py
        tools/pychecker/warn.py

--- test/parse-EXfer-profile.py
+++ test/parse-EXfer-profile.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 
 import string
-import sys
-import time
-
-# the next line causes the program to traceback and exit if it is not called properly
-thefile=sys.argv[1]
-begin=-1
-end=-1
-
 
 for kind in "rsel","read","wsel","write":
  print kind
@@ -29,6 +21,7 @@ for kind in "rsel","read","wsel","write":
     first  = "7:"
     second = "8:"
 
+ thefile="T"
  f = open(thefile,"r")
  o = open(thefile+kind,"w")
 
@@ -43,30 +36,17 @@ for kind in "rsel","read","wsel","write":
          ts = string.atol(tokens[2])%100
          tu = string.atol(tokens[4])*.000001
          start=ts+tu
-         if begin == -1:
-             begin=string.atol(tokens[2])+string.atol(tokens[4])*.000001
      elif tokens[0] == second:
          count = count + 1
          ts = string.atol(tokens[2])%100
          tu = string.atol(tokens[4])*.000001
          t=ts+tu
-         s=tokens[2]
-         u=tokens[4]
-         delta=(t-start)*1000000.
-         bytes=string.atol(tokens[5])
-         Mbytespersec=bytes/delta/1024./1024.*1000000
-         o.write("%i %f %f %f %f \n"%(count, t, delta, bytes,Mbytespersec))
+         o.write("%i %f %f \n"%(count, t, (t-start)*1000000.))
 
- if end==-1:
-     end=string.atol(s)+string.atol(u)*.000001
  o.close()
  f.close()
 
-print '%s from %s to %s'%(thefile,time.ctime(begin),time.ctime(end))
-print 'gnuplot'
-print ' set output "output.ps"'
-print ' set terminal postscript color solid'
-print ' set log y'
-print ' plot "%srsel" using 1:3, "%sread" using 1:3, "%swsel" using 1:3, "%swrite" using 1:3'%(thefile,thefile,thefile,thefile)
-print ' plot "%sread" using 1:5, "%swrite" using 1:5'%(thefile,thefile)
-print ' plot "%sread" using 4:5, "%swrite" using 4:5'%(thefile,thefile)
+# set output "output.ps"
+# set terminal postscript color solid
+# set log y
+# plot "Trsel" using 1:3, "Tread" using 1:3, "Twsel" using 1:3, "Twrite" using 1:3
