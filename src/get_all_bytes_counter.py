230b4eca924216941939d0fe7267a34615c0e205
Eileen Berman <berman>
2002-04-30 15:03:24 +0000

    ping before rcping

--- src/get_all_bytes_counter.py
+++ src/get_all_bytes_counter.py
@@ -8,11 +8,34 @@ import time
 import signal
 
 # since this is being run from a cron job on hppc, i do not want to import from
-# enstore.  we would normally use get_remote_file from enstore_functions and
+# enstore.  we would normally use get_remote_file & ping from enstore_functions and
 # VQFORMATED from enstore_constants.  instead define them here
 #import enstore_functions
 #import enstore_constants
 VQFORMATED = "VOLUME_QUOTAS_FORMATED"
+DEAD = 0
+ALIVE = 1
+
+def ping(node):
+    # ping the node to see if it is up.
+    times_to_ping = 4
+    # the timeout parameter does not work on d0ensrv2.
+    timeout = 5
+    #cmd = "ping -c %s -w %s %s"%(times_to_ping, timeout, node)
+    cmd = "ping -c %s %s"%(times_to_ping, node)
+    p = os.popen(cmd, 'r').readlines()
+    for line in p:
+        if not string.find(line, "transmitted") == -1:
+            # this is the statistics line
+            stats = string.split(line)
+            if stats[0] == stats[3]:
+                # transmitted packets = received packets
+                return ALIVE
+            else:
+                return DEAD
+    else:
+        # we did not find the stat line
+        return DEAD
 
 def get_remote_file(node, file, newfile):
     # we have to make sure that the rcp does not hang in case the remote node is goofy
@@ -45,20 +68,21 @@ if __name__ == "__main__":
     total = 0.0
     units = ""
     for node in NODES:
-	
-	newfile = "/tmp/%s-%s"%(node, VQFORMATED)
-	rtn = get_remote_file(node, CTR_FILE, newfile)
-	if rtn == 0:
-	    # read it
-	    file = open(newfile)
-	    lines = file.readlines()
-	    for line in lines:
-		fields = string.split(line)
-		if len(fields) == 2:
-		    total = total + float(fields[0])
-		    units = fields[1]
-	    else:
-		file.close()
+        # make sure node is up before rcping
+        if ping(node) == ALIVE:
+	    newfile = "/tmp/%s-%s"%(node, VQFORMATED)
+	    rtn = get_remote_file(node, CTR_FILE, newfile)
+	    if rtn == 0:
+		# read it
+		file = open(newfile)
+		lines = file.readlines()
+		for line in lines:
+		    fields = string.split(line)
+		    if len(fields) == 2:
+			total = total + float(fields[0])
+			units = fields[1]
+		else:
+		    file.close()
     else:
 	# output the total count
 	file = open(TOTAL_FILE, 'w')
