e533c6d61f872106dd0b27b361213bbfe34d7a2a
Alexander Moibenko <moibenko>
2002-05-01 16:47:51 +0000

    do not reread configuration information

--- src/discipline.py
+++ src/discipline.py
@@ -19,7 +19,8 @@ import pprint
 
 class Restrictor:
 
-    def read_config(self):
+    def read_config(self, reread_dict = 0):
+        if not reread_dict: return (e_errors.OK, None)
         self.exists = 0
         disc_dict=self.csc.get('discipline',{})
         if disc_dict['status'][0] == e_errors.OK:
@@ -34,6 +35,7 @@ class Restrictor:
     def __init__(self, csc, library_manager):
         self.csc = csc
         self.library_manager = library_manager
+        self.read_config(reread_dict = 1)
 
 
     def ticket_match(self, dict, ticket, pri_key, conf_key):
@@ -48,7 +50,7 @@ class Restrictor:
         
 
     def match_found(self, ticket):
-        self.read_config()
+        self.read_config(reread_dict = 0)
         if not self.exists:  # no discipline configuration info
             return 0, None, None, None
         # make a "flat" copy of ticket
