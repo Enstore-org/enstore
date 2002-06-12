e7ebb96fa83debefd59aaa34d1d9a87e50c59aa5
Chih Hao Huang <huangch>
2002-06-12 22:49:52 +0000

    use low level cursor to make it much faster

--- src/verify_db.py
+++ src/verify_db.py
@@ -8,18 +8,42 @@ def verify_db(dbname):
     count=0
     try:
         d = db.DbTable(dbname,'.','.',[])
-        c = d.newCursor()
+        c = d.newCursor().C	# use low level cursor
         t0 = time.time()
         k,v = c.first()
 
         while k:
             count=count+1
-            v=d[k]
+            # v=d[k]
+            try:
+                k,v = c.next()
+            except KeyError:
+                kl,vl = c.last()
+                if kl == k and vl == v:
+                    k = None
+                else:
+                    raise
+            if count % 1000 == 0:
+                print count
+
+        # If it gets so far, c must point to the last record
+        # check if the cursor is currupted (beyond the last record)
+
+        notok = 0
+        try:
             k,v = c.next()
+            notok = 1
+        except:
+            pass
+
+        if notok:
+            raise
 
         delta = time.time()-t0
 
         c.close()
+        d.close()
+
         if not count:
             print "DATABASE",dbname,"IS CORRUPT. NO KEYS FOUND"
             return 1
@@ -28,6 +52,8 @@ def verify_db(dbname):
         return 0
 
     except:
+            c.close()
+            d.close()
             exc, msg, tb = sys.exc_info()
             print "DATABASE",dbname,"IS CORRUPT. Current count is",count
             print exc, msg
