952f9ff34f8f01ccf15659bbf1f316eb0e6a6bcd
Eileen Berman <berman>
2002-09-18 19:54:38 +0000

    ad date

--- sbin/cms_pnfs_ls.sh
+++ sbin/cms_pnfs_ls.sh
@@ -4,11 +4,17 @@ cd /fnal/ups/prd/www_pages/enstore
 html_file=cms_pnfs_ls.html
 tmp_file=${html_file}.tmp
 
+theDate=`date`
+
 rm -f $tmp_file
 echo "<HTML><HEAD><TITLE>CMS PNFS DIRECTORY LISTING</TITLE></HEAD>" > $tmp_file
 echo "<BODY TEXT=#000066 BGCOLOR=#FFFFFF LINK=#0000EF VLINK=#55188A ALINK=#FF0000 BACKGROUND=enstore.gif>" >> $tmp_file
 echo "<BR><BR><FONT SIZE=+4><B>Directory Listing of /pnfs/cms</B></FONT>" >> $tmp_file
 echo "<PRE>" >> $tmp_file
+echo "  " >> $tmp_file
+echo "  " >> $tmp_file
+echo "  " >> $tmp_file
+echo $theDate >> $tmp_file
 
 ls -l -F -R /pnfs/cms >> $tmp_file
 
