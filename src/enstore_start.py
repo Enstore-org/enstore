bcd76a0bea0a86e8a931898f23b6a63272e2cd99
Michael Zalokar <zalokar>
2002-10-17 19:31:21 +0000

    Relpacement for the start and stop bash script.  This change was motivated by DESY usage investigation to avoid use of ups/upd/setup.

---
+++ src/enstore_start.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+
+###############################################################################
+# src/$RCSfile$   $Revision$
+#
+# start enstore
+#
+#
+#
+
+
+# system imports
+#
+import sys
+import re
+import os
+import string
+import errno
+import socket
+
+# enstore imports
+import setpath
+import e_errors
+import enstore_functions2
+import udp_client
+import generic_client
+import option
+
+import alarm_client
+import configuration_client
+import configuration_server
+import file_clerk_client
+import inquisitor_client
+import library_manager_client
+import log_client
+import media_changer_client
+import mover_client
+import monitor_client
+import volume_clerk_client
+import ratekeeper_client
+import event_relay_client
+
+
+def get_csc():
+    # get a configuration server
+    config_host = enstore_functions2.default_host()
+    config_port = enstore_functions2.default_port()
+    csc = configuration_client.ConfigurationClient((config_host,config_port))
+
+    return csc
+
+def this_host():
+    rtn = socket.gethostbyaddr(socket.getfqdn())
+
+    return [rtn[0]] + rtn[1] + rtn[2]
+
+def is_on_host(host):
+
+    if host in this_host():
+        return 1
+
+    return 0
+
+def check_csc(csc):
+
+    rtn = csc.alive(configuration_client.MY_SERVER, 5, 3)
+
+    if not e_errors.is_ok(rtn):
+        if csc.server_address[0] in this_host():
+            #Start the configuration server.
+            print "Starting configuration server: %s:%s" % csc.server_address
+            os.system("python $ENSTORE_DIR/src/configuration_server.py "\
+                      "--config-file $ENSTORE_CONFIG_FILE &")
+
+            #Check the restart csc.
+            rtn = csc.alive(configuration_client.MY_SERVER, 5, 3)
+            if not e_errors.is_ok(rtn):
+                print "Configuration server not started."
+                sys.exit(1)
+        else:
+            print "Configuration server not running."
+            sys.exit(1)
+    else:
+        print "Configuration server found: %s:%s" % csc.server_address
+
+def check_db(csc, name, cmd):
+
+    info = csc.get("volume_clerk", 5, 3)
+    if not info.get('host', None) in this_host() and \
+           not info.get('hostip', None) in this_host():
+        return
+
+    print "Checking %s." % name
+
+    rtn = os.popen("ps -elf | grep %s | grep -v grep" % (name,)).readlines()
+
+    if not rtn:
+        print "Starting %s." % name
+        os.system(cmd)
+
+#If the event relay responded to alive messages, this would not be necessary.
+def check_event_relay(csc, cmd):
+
+    info = csc.get("event_relay", 5, 3)
+    if not info.get('host', None) in this_host() and \
+           not info.get('hostip', None) in this_host():
+        return
+
+    print "Checking event_relay."
+
+    rtn = os.popen("ps -elf | grep event_relay | grep -v grep").readlines()
+
+    if not rtn:
+        print "Starting event_relay."
+        os.system(cmd)
+    else:
+        print "Found event_relay."
+
+def check_server(csc, name, cmd):
+    
+    #Initialize, send and receive alive responce.
+    u = udp_client.UDPClient()
+    info = csc.get(name, 5, 3)
+    if not info.get('host', None) in this_host() and \
+           not info.get('hostip', None) in this_host():
+        return
+
+    print "Checking %s." % name
+
+    #Send and receive responce.
+    try:
+        rtn = u.send({'work':"alive"}, (info['hostip'], info['port']), 3, 3)
+    except errno.errorcode[errno.ETIMEDOUT]:
+        rtn = {'status':(e_errors.TIMEDOUT, errno.errorcode[errno.ETIMEDOUT])}
+        
+    #Process responce.
+    if not e_errors.is_ok(rtn):
+        #Start the server.
+        print "Starting %s: %s:%s" % (name, info['hostip'], info['port'])
+        os.system(cmd)
+        
+        
+        #Check the restart csc.
+        rtn = csc.alive(name, 3, 3)
+        if not e_errors.is_ok(rtn):
+            print "Server %s not started." % (name,)
+            sys.exit(1)
+    else:
+        print "Found %s: %s:%s" % (name, info.get('hostip', None),
+                                   info.get('port', None))
+    
+
+class EnstoreStartInterface(generic_client.GenericClientInterface):
+
+    def __init__(self, args=sys.argv, user_mode=1):
+        self.name = "START"
+        self.just = None
+
+        generic_client.GenericClientInterface.__init__(self, args=args,
+                                                       user_mode=user_mode)
+
+    def valid_dictionaries(self):
+        return (self.help_options, self.start_options)
+    
+    # parse the options like normal but make sure we have other args
+    def parse_options(self):
+
+        generic_client.GenericClientInterface.parse_options(self)
+
+        self.complete_server_name()
+
+    def complete_server_name(self):
+        if self.just:
+            count = 0
+            for name in self.complete_names:
+                if self.just == name[:len(self.just)]:
+                    count = count + 1
+                    hold = name
+            if count == 1:
+                self.just = hold
+
+    def should_start(self, server_name):
+
+        if self.just == server_name or self.just == None:
+            return 1
+
+        return 0
+
+    complete_names = [
+        "configuration_server",
+        "event_relay",
+        "log_server",
+        "alarm_server",
+        "volume_clerk",
+        "file_clerk",
+        "db_checkpoint",
+        "db_deadlock",
+        "inquisitor",
+        "ratekeeper",
+        "library",
+        "media",
+        "mover",
+        ]
+        
+
+    start_options = {
+        option.JUST:{option.HELP_STRING:"specify single server",
+                     option.VALUE_NAME:"just",
+                     option.VALUE_USAGE:option.REQUIRED,
+                     option.VALUE_TYPE:option.STRING,
+		     option.USER_LEVEL:option.ADMIN,
+                     }
+        }
+
+def do_work(intf):
+    
+    csc = get_csc()
+
+    if intf.should_start("configuration_server"):
+        check_csc(csc)
+
+    rtn = csc.alive(configuration_client.MY_SERVER, 5, 3)
+    if not e_errors.is_ok(rtn):
+        #If the configuration server was not specifically specified.
+        print "Configuration server not running."
+        sys.exit(1)
+            
+    # We know the config server is up.  Get the database info.
+    db_dir = csc.get('database', {}).get('db_dir', None)
+    if not db_dir:
+        print "Unable to determine database directory."
+        sys.exit(1)
+
+    #Start the servers.
+
+    if intf.should_start("event_relay"):
+        check_event_relay(csc, "python $ENSTORE_DIR/src/event_relay.py &")
+    if intf.should_start("log_server"):
+        check_server(csc, "log_server",
+                     "python $ENSTORE_DIR/src/log_server.py &")
+    if intf.should_start("alarm_server"):
+        check_server(csc, "alarm_server",
+                     "python $ENSTORE_DIR/src/alarm_server.py &")
+    if intf.should_start("volume_clerk"):
+        check_server(csc, "volume_clerk",
+                     "python $ENSTORE_DIR/src/volume_clerk.py &")
+    if intf.should_start("db_checkpoint"):
+        check_db(csc, "db_checkpoint",
+                 "db_checkpoint -h %s  -p 5 &" % db_dir)
+    if intf.should_start("db_deadlock"):
+        check_db(csc, "db_deadlock",
+                 "db_deadlock -h %s  -t 1 &" % db_dir)
+    if intf.should_start("file_clerk"):
+        check_server(csc, "file_clerk",
+                     "python $ENSTORE_DIR/src/file_clerk.py &")
+    if intf.should_start("inquisitor"):
+        check_server(csc, "inquisitor",
+                     "python $ENSTORE_DIR/src/inquisitor.py &")
+    if intf.should_start("ratekeeper"):
+        check_server(csc, "ratekeeper",
+                     "python $ENSTORE_DIR/src/ratekeeper.py &")
+
+    #Get the library names.
+    libraries = csc.get_library_managers({}).keys()
+    libraries = map((lambda l: l + ".library_manager"), libraries)
+
+    if intf.should_start("library"):
+        for library in libraries:
+            check_server(csc, library,
+                   "python $ENSTORE_DIR/src/library_manager.py %s &" % library)
+    elif intf.just[-16:] == ".library_manager":
+        check_server(csc, intf.just,
+                 "python $ENSTORE_DIR/src/library_manager.py %s &" % intf.just)
+
+    if intf.should_start("media"):
+        for library in libraries:
+            media = csc.get_media_changer(library)
+            check_server(csc, media,
+                     "python $ENSTORE_DIR/src/media_changer.py %s &" % media)
+    elif intf.just[-14:] == ".media_changer":
+        check_server(csc, intf.just,
+                 "python $ENSTORE_DIR/src/media_changer.py %s &" % intf.just)
+        
+    if intf.should_start("mover"):
+        for library in libraries:
+            for mover in csc.get_movers(library):
+                mover = mover['mover']
+                check_server(csc, mover,
+                         "python $ENSTORE_DIR/src/mover.py %s &" % mover)
+    elif intf.just[-6:] == ".mover":
+        check_server(csc, intf.just,
+                 "python $ENSTORE_DIR/src/media_changer.py %s &" % intf.just)
+
+if __name__ == '__main__':
+
+    intf = EnstoreStartInterface(user_mode=0)
+
+    do_work(intf)
---
+++ src/enstore_stop.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+
+###############################################################################
+# src/$RCSfile$   $Revision$
+#
+# stop enstore
+#
+#
+#
+
+
+# system imports
+#
+import sys
+import re
+import os
+import string
+import errno
+import socket
+import signal
+import generic_client
+import option
+
+# enstore imports
+import setpath
+import e_errors
+import enstore_functions2
+import udp_client
+
+import alarm_client
+import configuration_client
+import configuration_server
+import file_clerk_client
+import inquisitor_client
+import library_manager_client
+import log_client
+import media_changer_client
+import mover_client
+import monitor_client
+import volume_clerk_client
+import ratekeeper_client
+import event_relay_client
+
+
+def get_csc():
+    # get a configuration server
+    config_host = enstore_functions2.default_host()
+    config_port = enstore_functions2.default_port()
+    csc = configuration_client.ConfigurationClient((config_host,config_port))
+
+    rtn = csc.alive(configuration_client.MY_SERVER, 5, 3)
+
+    if e_errors.is_ok(rtn):
+        return csc
+        
+    return None
+
+def this_host():
+    rtn = socket.gethostbyaddr(socket.getfqdn())
+
+    return [rtn[0]] + rtn[1] + rtn[2]
+
+def is_on_host(host):
+
+    if host in this_host():
+        return 1
+
+    return 0
+
+def check_csc(csc):
+
+    rtn = csc.alive(configuration_client.MY_SERVER, 5, 3)
+
+    if e_errors.is_ok(rtn):
+        if csc.server_address[0] in this_host():
+            #Stop the configuration server.
+            print "Stopping configuration server: %s:%s" % csc.server_address
+            os.kill(rtn['pid'], signal.SIGTERM)
+    else:
+        print "Configuration server already stopped: %s:%s" % \
+              csc.server_address
+
+def check_db(name):
+
+    info = csc.get("volume_clerk", 5, 3)
+    if not info.get('host', None) in this_host() and \
+           not info.get('hostip', None) in this_host():
+        return
+
+    print "Checking %s." % name
+    
+    rtn = os.popen("ps -elf | grep %s | grep -v grep" % (name,)).readlines()
+
+    if rtn:
+        pid = int(re.sub("\s+", " ", rtn[0]).split(" ")[3])
+        print "Stopping %s: %d" % (name, pid)
+        os.kill(pid, signal.SIGTERM)
+
+#If the event relay responded to alive messages, this would not be necessary.
+def check_event_relay(csc):
+
+    info = csc.get("event_relay", 5, 3)
+    if not info.get('host', None) in this_host() and \
+           not info.get('hostip', None) in this_host():
+        return
+
+    print "Checking event_relay."
+    
+    rtn = os.popen("ps -elf | grep event_relay | grep -v grep").readlines()
+
+    if rtn:
+        pid = int(re.sub("\s+", " ", rtn[0]).split(" ")[3])
+        print "Stopping %s: %d" % ("event_relay", pid)
+        os.kill(pid, signal.SIGTERM)
+
+def check_server(csc, name):
+
+    print "Checking %s." % name
+
+    #Initialize, send and receive alive responce.
+    u = udp_client.UDPClient()
+    info = csc.get(name, 5, 3)
+
+    #Process responce.
+    if not info.get('host', None) in this_host() and \
+           not info.get('hostip', None) in this_host():
+        return
+    
+    try:
+        rtn = u.send({'work':"alive"}, (info['hostip'], info['port']), 3, 3)
+    except errno.errorcode[errno.ETIMEDOUT]:
+        rtn = {'status':(e_errors.TIMEDOUT, errno.errorcode[errno.ETIMEDOUT])}
+        
+    
+    if e_errors.is_ok(rtn):
+        #Stop the server.
+        print "Stopping %s: %s:%s" % (name, info['hostip'], info['port'])
+        os.kill(rtn['pid'], signal.SIGTERM)
+    else:
+        print "Already stopped %s: %s:%s" % (name, info.get('hostip', None),
+                                             info.get('port', None))
+    
+
+class EnstoreStopInterface(generic_client.GenericClientInterface):
+
+    def __init__(self, args=sys.argv, user_mode=1):
+        self.name = "STOP"
+        self.just = None
+
+        generic_client.GenericClientInterface.__init__(self, args=args,
+                                                       user_mode=user_mode)
+
+    def valid_dictionaries(self):
+        return (self.help_options, self.start_options)
+    
+    # parse the options like normal but make sure we have other args
+    def parse_options(self):
+
+        generic_client.GenericClientInterface.parse_options(self)
+
+        self.complete_server_name()
+
+    def complete_server_name(self):
+        if self.just:
+            count = 0
+            for name in self.complete_names:
+                if self.just == name[:len(self.just)]:
+                    count = count + 1
+                    hold = name
+            if count == 1:
+                self.just = hold
+
+    def should_start(self, server_name):
+
+        if self.just == server_name or self.just == None:
+            return 1
+
+        return 0
+
+    complete_names = [
+        "configuration_server",
+        "event_relay",
+        "log_server",
+        "alarm_server",
+        "volume_clerk",
+        "file_clerk",
+        "db_checkpoint",
+        "db_deadlock",
+        "inquisitor",
+        "ratekeeper",
+        "library",
+        "media",
+        "mover",
+        ]
+        
+
+    start_options = {
+        option.JUST:{option.HELP_STRING:"specify single server",
+                     option.VALUE_NAME:"just",
+                     option.VALUE_USAGE:option.REQUIRED,
+                     option.VALUE_TYPE:option.STRING,
+		     option.USER_LEVEL:option.ADMIN,
+                     }
+        }
+
+
+def do_work(intf):
+
+    csc = get_csc()
+
+    if csc == None:
+        print "No configuration server running."
+        sys.exit(1)
+    
+    #Get the library names.
+    libraries = csc.get_library_managers({}).keys()
+    libraries = map((lambda l: l + ".library_manager"), libraries)
+
+    #Begin stopping enstore.
+
+    if intf.should_start("mover"):
+        for library in libraries:
+            for mover in csc.get_movers(library):
+                mover = mover['mover']
+                check_server(csc, mover)
+    elif intf.just[-6:] == ".mover":
+        check_server(csc, intf.just)
+
+    if intf.should_start("media"):
+        for library in libraries:
+            media = csc.get_media_changer(library)
+            check_server(csc, media)
+    elif intf.just[-14:] == ".media_changer":
+        check_server(csc, intf.just)
+
+    if intf.should_start("library"):
+        for library in libraries:
+            check_server(csc, library)
+    elif intf.just[-16:] == ".library_manager":
+        check_server(csc, intf.just)
+
+    if intf.should_start("ratekeeper"):
+        check_server(csc, "ratekeeper")
+    if intf.should_start("inquisitor"):
+        check_server(csc, "inquisitor")
+    if intf.should_start("file_clerk"):
+        check_server(csc, "file_clerk")
+    if intf.should_start("volume_clerk"):
+        check_server(csc, "volume_clerk")
+    if intf.should_start("db_checkpoint"):
+        check_db("db_checkpoint")
+    if intf.should_start("db_deadlock"):
+        check_db("db_deadlock")
+    if intf.should_start("alarm_server"):
+        check_server(csc, "alarm_server")
+    if intf.should_start("log_server"):
+        check_server(csc, "log_server")
+    if intf.should_start("event_relay"):
+        check_event_relay(csc)
+    if intf.should_start("configuration_server"):
+        check_csc(csc)
+
+if __name__ == '__main__':
+
+    intf = EnstoreStopInterface(user_mode=0)
+
+    do_work(intf)
