4445c2cbeaaff5fdb1c4bd57dbd5e6b58d88a29d
Alexander Moibenko <moibenko>
2002-11-26 17:42:07 +0000

    changes

--- sbin/read_tape.py
+++ sbin/read_tape.py
@@ -42,10 +42,13 @@ def check_mover(device, position, f_size):
     rest = f_size % block_size
     print "READING DATA"
     buf=block_size*' '
-    t1 = time.time()
+    #t1 = time.time()
+    t=0.
     for i in range (0,nb):
         #print "READING",block_size/1024,"Kbytes"
+        t1=time.time()
         FTT.read(buf, 0, block_size)
+        t=time.time()-t1+t
         #p = i*1.*100/f_size
         #if p%10 == 0:
         #    print "%.3g %s done"%(p,"%")
@@ -53,11 +56,15 @@ def check_mover(device, position, f_size):
     #p=100.
     #print "%.3g %s done"%(p,"%")
     if rest:
+        t1=time.time()
         FTT.read(buf, 0, rest)
+        t=time.time()-t1+t
     t2 = time.time()
     #print block_size*f_size/1024./1024.,"Mbytes read in",t2-t1,"secs"
-    print "READ TIME",t2-t1,"secs"
-    read_t_r = f_size*1./1024./1024./(t2-t1)
+    #print "READ TIME",t2-t1,"secs"
+    #read_t_r = f_size*1./1024./1024./(t2-t1)
+    print "READ TIME",t,"secs"
+    read_t_r = f_size*1./1024./1024./(t)
     FTT.rewind()
 
     FTT.close()
