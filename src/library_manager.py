3cea9435457f8d94f948db22ebed31de7c553f40
Alexander Moibenko <moibenko>
2003-04-30 16:09:32 +0000

    fixed bug

--- src/library_manager.py
+++ src/library_manager.py
@@ -96,15 +96,15 @@ class SG_FF:
             if len(self.sg[sg]) == 0:
                 del(self.sg[sg])
         else:
-            Trace.log('can not remove from sg %s %s' % (mover, volume))
-            Trace.log('SG: %s' % (self.sg,))
+            Trace.log(e_errors.ERROR,'can not remove from sg %s %s' % (mover, volume))
+            Trace.log(e_errors.ERROR, 'SG: %s' % (self.sg,))
         if self.vf.has_key(vf) and (mover, volume) in self.vf[vf]:
             self.vf[vf].remove((mover, volume))
             if len(self.vf[vf]) == 0:
                 del(self.vf[vf])
         else:
-            Trace.log('can not remove from vf %s %s' % (mover, volume))
-            Trace.log('SG: %s' % (self.vf,))
+            Trace.log(e_errors.ERROR,'can not remove from vf %s %s' % (mover, volume))
+            Trace.log(e_errors.ERROR,'SG: %s' % (self.vf,))
 
     def put(self, mover, volume, sg, vf):
         self.delete(mover, volume, sg, vf) # delete entry to update content
