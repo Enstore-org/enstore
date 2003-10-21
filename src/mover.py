351607a69e1f644db837472226bcd4a9b6c951d9
Alexander Moibenko <moibenko>
2003-10-21 20:59:47 +0000

    fixed a bug

--- src/mover.py
+++ src/mover.py
@@ -2412,7 +2412,7 @@ class Mover(dispatching_worker.DispatchingWorker,
             if self.state is HAVE_BOUND:
                 self.dismount_time = time.time() + self.default_dismount_delay
             self.need_lm_update = (1, self.state, 1, None)
-            self.send_error_msg(error_info=(e_errors.ENCP_GONE, "no client socket"), error_source error_source=NETWORK) 
+            self.send_error_msg(error_info=(e_errors.ENCP_GONE, "no client socket"), error_source=NETWORK) 
             #self.update_lm(reset_timer=1)
             return 0
 
