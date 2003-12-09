4288790206e7cf0d7e829d32f7b2a8b4b7a5255b
Chih Hao Huang <huangch>
2003-12-09 15:13:36 +0000

    fix a few typos

--- sbin/swapAB.py
+++ sbin/swapAB.py
@@ -390,10 +390,12 @@ if __name__ == '__main__':
 				te_count = 0
 				print "checking", v2, "...",
 				vol = vcc.inquire_vol(v2)
-				if vol['staus'] != e_errors.OK:
+				if vol['status'] != e_errors.OK:
 					print 'does not exist ... ERROR'
 					continue
-				if vol['system_inhibit'][1] == 'migrated':					print 'already migrated ... WARNING'					continue
+				if vol['system_inhibit'][1] == 'migrated':
+					print 'already migrated ... WARNING'
+					continue
 				print
 				for i in result['active_list']:
 					swap(i)
