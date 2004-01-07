dc2dee91c466a2320ed09d78ad4d33f83ac92850
Alexander Moibenko <moibenko>
2004-01-07 19:24:31 +0000

    major cleanup and reorganization

---
+++ ingest/ktev/ktev_main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include "dcap.h"
+#include "ktevapi.h"
+
+#define O_BINARY 0
+/* for getopt */
+extern char *optarg;
+extern int optind;
+
+extern char *volumeName;
+
+static
+void usage()
+{
+  fprintf(stderr,"ktev test program\n");
+  fprintf(stderr,"must pass volume as only parameter\nor volume and w for write tests\n");
+}
+
+int
+main(int argc, char *argv[])
+{
+  int c,src,record_size,fileError,recs;
+  int write_test = 0;
+  int files_written, records_written;
+  char fileName[FILENAME_LEN];
+  char buffer[75000];
+
+  if (argc < 2) {
+    usage();
+    return -1;
+  }
+
+  while( (c = getopt(argc, argv, "v:w")) != EOF) {
+	
+    switch(c) {
+    case 'v':
+      setVolumeName(optarg);
+      break;
+    case 'w':				
+      write_test = 1;
+      break;
+    }
+  }
+
+  if( write_test ) {
+    for(files_written=0; files_written < 11; files_written ++ ) {
+      fileError = PutFile(volumeName, fileName);
+      src = dc_open(fileName,O_WRONLY|O_CREAT|O_BINARY, S_IRUSR|S_IWUSR );
+      for(records_written=0; records_written < 11; records_written++ ) {
+	putRecord(buffer, 75000, src);
+      }
+    }
+    return 0;
+  }
+  else {
+    record_size = 0;
+    while(record_size >= 0 && (fileError = GetFile(volumeName, fileName)) > 0) {
+	printf("filename %s\n", fileName);
+	src = dc_open(fileName,O_RDONLY | O_BINARY );
+
+	record_size = getRecord(buffer, 75000, src);
+	recs = 0;
+	while(record_size > 0) {
+	  recs++;  
+	  /*printf("record_size: %d\n", record_size);*/
+	  record_size = getRecord(buffer, 75000, src);
+	}
+	printf("file %s has %d records\n", fileName, recs);
+      }
+    if(fileError == -1) {
+      return -1;
+    }
+    else if(record_size <0){
+      return record_size;
+    }
+    else {
+      return 0;
+    }
+  }
+}
