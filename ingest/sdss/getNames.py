93a5cfa8d9ca48de6caf975eaab8fcca6ff28dd9
Michael Zalokar <zalokar>
2004-02-19 22:40:15 +0000

    Added to an error message when rcp fails.  Also, prints to terminal the rcp command used.

--- ingest/sdss/getNames.py
+++ ingest/sdss/getNames.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import string
 
 #Get the file contianing the metadata for the tapetype/volume filename.
 
@@ -7,10 +9,13 @@ def getFile(username, filename, mjd, nodename, metadata_output):
         #Create the full remote file path to the sdss catalog metadata.
         source_filename = username + "@" + nodename + ":/sdss/data/golden/" \
                           + mjd + "/" + filename
+        print string.join(("rcp", source_filename, metadata_output), " ")
+        sys.stdout.flush()
         #Get the medatada file from sdss.
         return os.spawnvp(os.P_WAIT, "rcp",
                           ("rcp", source_filename, metadata_output))
     except:
-        print "unable to fetch file"
+        msg = sys.exc_info()[1]
+        print "unable to fetch file: %s" % str(msg)
         return -1
 
