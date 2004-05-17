dcf375c0c4e497ce2cda06016f22dd1da53a1d0d
Michael Zalokar <zalokar>
2004-05-17 19:57:16 +0000

    There were some actions performed upon the import of this module: setpath. These actions have been placed into a function set_enstore_paths().  The function is called upon import, but now there is a function to call that will shut pychecker up.

--- src/setpath.py
+++ src/setpath.py
@@ -15,23 +15,29 @@ def addpath(p):
         sys.path.append(p)
 
 
-addpath('$ENSTORE_DIR/src')
-addpath('$ENSTORE_DIR/modules')
+def set_enstore_paths():
+    #These two paths are necessary for enstore.
+    addpath('$ENSTORE_DIR/src')
+    addpath('$ENSTORE_DIR/modules')
 
-#
-try:
-    cdir = os.getcwd()
-except OSError, msg:
-    if msg.errno == errno.ENOENT:
-        sys.stderr.write("%s: %s\n" % (os.strerror(msg.errno),
-                                     "No current working directory"))
-        sys.exit(1)
-    else:
-        sys.stderr.write(str(msg) + "\n")
-        sys.exit(1)
-        
-addpath(os.path.join(cdir, '../modules'))
+    #In case we are working in a different location...
+    try:
+        cdir = os.getcwd()
+        addpath(os.path.join(cdir, '../modules'))
+    except OSError, msg:
+        if msg.errno == errno.ENOENT:
+            sys.stderr.write("%s: %s\n" % (os.strerror(msg.errno),
+                                         "No current working directory"))
+            sys.exit(1)
+        else:
+            sys.stderr.write(str(msg) + "\n")
+            sys.exit(1)
 
+### Why should this be called when the module is imported?  Nothing seems
+###  to break when it is commented out (perhaps a code versus cut
+###  difference exists).  If they did need it, they could call this
+###  function.
+set_enstore_paths()
 
 if __name__=='__main__':
     print sys.path
