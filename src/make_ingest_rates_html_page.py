75411431d20e432b546a3c9753b53d65ba351f77
Alexander Moibenko <moibenko>
2004-11-18 19:18:04 +0000

    changes inspired by pychecker

--- src/make_ingest_rates_html_page.py
+++ src/make_ingest_rates_html_page.py
@@ -6,8 +6,8 @@ import os
 
 html_dir = enstore_functions.get_html_dir()
 filepath=os.path.join(html_dir,"all_sg_burn_rates.html")
-file=enstore_files.EnstoreIngestRatesFile(filepath)
-file.open()
-file.write()
-file.close()
-file.install()
+html_file=enstore_files.EnstoreIngestRatesFile(filepath)
+html_file.open()
+html_file.write()
+html_file.close()
+html_file.install()
