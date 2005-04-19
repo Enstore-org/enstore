1be1cd37306a3571d0e224cd964bfccf6a7c3702
Michael Zalokar <zalokar>
2005-04-19 16:05:58 +0000

    Added function is_string_in_character_set().  This allows someone to pass in the character set to compare against.

--- src/charset.py
+++ src/charset.py
@@ -14,22 +14,17 @@ charset=string.join((string.letters,string.digits,'-','_','/'),'')
 #This is for filepaths.
 filenamecharset=string.join((string.printable),'')
 
-def is_in_charset(string):
-    #print "charset",charset
-    if not len(string): return 0
-    for ch in string:
-        if not ch in charset:
+def is_string_in_character_set(check_string, character_set):
+    if not len(check_string): return 0
+    for ch in check_string:
+        if not ch in character_set:
             break
     else:
         return 1
     return 0
 
-def is_in_filenamecharset(string):
-    #print "charset",charset
-    if not len(string): return 0
-    for ch in string:
-        if not ch in filenamecharset:
-            break
-    else:
-        return 1
-    return 0
+def is_in_charset(check_string):
+    return is_string_in_character_set(check_string, charset)
+
+def is_in_filenamecharset(check_string):
+    return is_string_in_character_set(check_string, filenamecharset)
