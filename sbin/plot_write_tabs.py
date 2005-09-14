33169c6acc15b893b3fe76b3fec482e924c8d1e5
Alexander Moibenko <moibenko>
2005-09-14 19:25:48 +0000

    plot write tab statistics

---
+++ sbin/plot_write_tabs.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# $Id$
+import sys
+import popen2
+import time
+import os
+import string
+
+def generate_date():
+    d1 = None
+    for when in 'date --date "1 month ago"  +"%b-%d-%y"','date +"%b-%d-%y"':
+        d = os.popen(when,'r')
+        dat=d.readlines()
+        d.close()
+        if d1 == None:
+            d1 = dat[0][:-1]
+            d1.upper()
+        else:
+            d2 = dat[0][:-1]
+            d2.upper()
+    print 'Generating burn-rate plots from', d1, ' to ',d2
+    return d1, d2
+
+
+def get_stats(host, port, start_date=None, finish_date=None):
+
+    date_fmt = "'YY-MM-DD HH24:MI'"
+    if start_date and finish_date:
+        query_cmd='psql -h %s -p %s -o "write_tabs_%s.report" -c \
+        "select to_char(date, %s),total, should, not_yet, done from write_protect_summary\
+        where date(time) between date(%s%s%s) and date(%s%s%s) \
+        and mb_user_write != 0 order by date desc;" enstore'
+        pipeObj = popen2.Popen3(query_cmd%(host, port, host, date_fmt, "'", start_date, "'", "'", finish_date, "'"), 0, 0)
+    else:
+        query_cmd='psql -h %s -p %s -o "write_tabs_%s.report" -c \
+        "select to_char(date, %s),total, should, not_yet, done from write_protect_summary\
+        order by date desc;" enstore'
+        pipeObj = popen2.Popen3(query_cmd%(host, port, host, date_fmt), 0, 0)
+        
+        
+    if pipeObj is None:
+        sys.exit(1) 
+    stat = pipeObj.wait()
+    result = pipeObj.fromchild.readlines()  # result has returned string
+
+def make_plot_file(host):
+    f=open("write_tabs_%s.report"%(host,),'r')
+    of=open("write_tabs_%s"%(host,),'w')
+    l = f.readline()
+    while 1:
+        l = f.readline()
+        if l:
+            a = l.split('|')
+            if len(a) == 5:
+                a[4] = str(int(a[4][:-1])+int(a[3]))
+                lo = string.join(a)
+                of.write("%s\n"%(lo,))
+        else:
+            break
+
+def make_plot(host):
+   f = open("write_tabs_%s.gnuplot"%(host,),'w')
+   f.write('set terminal postscript color solid\n')
+   f.write('set output "write_tabs_%s.ps"\n' % (host,))
+   f.write('set title "Write Tabs States"\n')
+   f.write('set xlabel "Date (year-month-day)"\n')
+   f.write('set timefmt "%Y-%m-%d"\n')
+   f.write('set xdata time\n')
+   #f.write('set size 1.5,1\n')
+   f.write('set xrange [ : ]\n')
+   f.write('set grid\n')
+   f.write('set yrange [0: ]\n')
+   f.write('set format x "%y-%m-%d"\n')
+   f.write('set ylabel "# tapes that should have write tabs ON"\n')
+   #f.write('plot "write_tabs_%s" using 1:10 t "ON" w impulses lw %s 3 1 using 1:8 t "OFF" w impulses lw %s 1 1\n'%
+   #        (host, 20, 20))
+   f.write('plot "write_tabs_%s" using 1:6 t "ON" w impulses lw %s 2 1, "write_tabs_%s" using 1:5 t "OFF" w impulses lw %s 1 1\n'%
+           (host, 20, host, 20))
+   #f.write('plot "write_tabs_%s" using 1:10 t "ON" w impulses lw %s 3 1\n'%(host, 20))
+   
+   f.close()
+   for cmd in '/usr/bin/gnuplot write_tabs_%s.gnuplot' % (host,),\
+           '/usr/X11R6/bin/convert -rotate 90 -modulate 80 write_tabs_%s.ps write_tabs_%s.jpg' % (host,host),\
+           '/usr/X11R6/bin/convert -rotate 90 -geometry 120x120 -modulate 80 write_tabs_%s.ps write_tabs_%s_stamp.jpg' % (host,host):
+    os.system(cmd)
+    cmd = 'source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp *.ps *.jpg stkensrv2:/fnal/ups/prd/www_pages/enstore/'
+    os.system(cmd)
+   
+
+#d1, d2 =  generate_date()   
+for host in "stkensrv6", "d0ensrv6", "cdfensrv6":
+    get_stats(host, 5432)
+    make_plot_file(host)
+    make_plot(host)
