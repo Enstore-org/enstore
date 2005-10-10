4fc269b8f3fe20fdf2c211ebbbf4ee2cae601979
Chih Hao Huang <huangch>
2005-10-10 15:24:24 +0000

    bug fix

--- src/inventory.py
+++ src/inventory.py
@@ -1162,7 +1162,8 @@ def inventory(output_dir, cache_dir):
     wpa_file.write("\n\n")
     for i in n_rf_vols.keys():
         wpa_file.write("Total: %5d\n"%(n_vols))
-        wpa_file.write("%10s  Should: %5d\n   Done: %5d\nNot yet: %5d\n  Ratio: %5.2f%%\n"%(i, n_rf_vols[i], n_rp_vols[i], n_not_rp_vols[i], float(n_rp_vols[i])*100/n_rf_vols[i]))
+        wpa_file.write("\n%s:\n"%(i))
+        wpa_file.write("  Should: %5d\n   Done: %5d\nNot yet: %5d\n  Ratio: %5.2f%%\n"%(n_rf_vols[i], n_rp_vols[i], n_not_rp_vols[i], float(n_rp_vols[i])*100/n_rf_vols[i]))
     wpa_file.close()
 
     # log wpa info twice a day
