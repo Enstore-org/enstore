d7c2531e04c86cec0415ec3f1999b4baed2fe857
Dmitry Litvinsev <litvinse>
2005-10-10 21:44:14 +0000

    modified the script so it runs in one place and posts the results st stkensrv2

--- sbin/write_tabs.py
+++ sbin/write_tabs.py
@@ -28,106 +28,111 @@ def usage():
 def main():
     intf  = configuration_client.ConfigurationClientInterface(user_mode=0)
     csc   = configuration_client.ConfigurationClient((intf.config_host, intf.config_port))
+    if ( 0 ) :
+        acc = csc.get(enstore_constants.ACCOUNTING_SERVER)
+        inq = csc.get('inquisitor')
+        inq_host=inq.get('www_host').split('/')[2]
+    servers=[]
+    servers=csc.get('known_config_servers')
 
-    acc = csc.get(enstore_constants.ACCOUNTING_SERVER)
-    inq = csc.get('inquisitor')
-    inq_host=inq.get('www_host').split('/')[2]
+    for server in servers:
+        server_name,server_port = servers.get(server)
+        if ( server_port != None ):
+            config_server_client   = configuration_client.ConfigurationClient((server_name, server_port))
+            acc = config_server_client.get(enstore_constants.ACCOUNTING_SERVER)
+            db_server_name = acc.get('dbhost')
+            db_name        = acc.get('dbname')
+            db_port        = acc.get('dbport')
+            name           = db_server_name.split('.')[0]
+            name=db_server_name.split('.')[0]
+            now_time    = time.time()
+            Y, M, D, h, m, s, wd, jd, dst = time.localtime(now_time)
+            now_time = time.mktime((Y, M, D, 23, 59, 59, wd, jd, dst))
+            start_time  = now_time-30*3600*24-7*3600*24
+            
+            h  = histogram.Histogram1D("write_tabs_%s"%(name,),"Write tab states %s"%(name,),37,float(start_time),float(now_time))
+            h1 = histogram.Histogram1D("write_tabs_not_done_%s"%(name,),"Write tab states %s"%(name,),37,float(start_time),float(now_time))
+            h2 = histogram.Histogram1D("write_tabs_done_%s"%(name,),"Number of tabs flipped per day %s"%(name,),37,float(start_time),float(now_time))
 
-    db_server_name = acc.get('dbhost')
-    db_name        = acc.get('dbname')
-    db_port        = acc.get('dbport')
+            h.set_time_axis(True)
+            h.set_profile(True)
+            h.set_ylabel("# of tapes that should have write tabs ON")
+            h.set_xlabel("Date (year-month-day)")
 
-    name=db_server_name.split('.')[0]
-    now_time    = time.time()
-    Y, M, D, h, m, s, wd, jd, dst = time.localtime(now_time)
-    now_time = time.mktime((Y, M, D, 23, 59, 59, wd, jd, dst))
-    start_time  = now_time-30*3600*24-7*3600*24
+            h1.set_time_axis(True)
+            h1.set_profile(True)
+            h1.set_ylabel("# of tapes that  have write tabs OFF")
+            h1.set_xlabel("Date (year-month-day)")
 
-    h  = histogram.Histogram1D("write_tabs_%s"%(name,),"Write tab states %s"%(name,),37,float(start_time),float(now_time))
-    h1 = histogram.Histogram1D("write_tabs_not_done_%s"%(name,),"Write tab states %s"%(name,),37,float(start_time),float(now_time))
-    h2 = histogram.Histogram1D("write_tabs_done_%s"%(name,),"Number of tabs flipped per day %s"%(name,),37,float(start_time),float(now_time))
+            h2.set_time_axis(True)
+            h2.set_profile(True)
+            h2.set_ylabel("# of tapes that should have write tabs ON")
+            h2.set_xlabel("Date (year-month-day)")
 
-    h.set_time_axis(True)
-    h.set_profile(True)
-    h.set_ylabel("# of tapes that should have write tabs ON")
-    h.set_xlabel("Date (year-month-day)")
+            if db_port:
+                db = pg.DB(host=db_server_name, dbname=db_name, port=db_port);
+            else:
+                db = pg.DB(host=db_server_name, dbname=db_name);
 
-    h1.set_time_axis(True)
-    h1.set_profile(True)
-    h1.set_ylabel("# of tapes that  have write tabs OFF")
-    h1.set_xlabel("Date (year-month-day)")
+            res=db.query(SELECT_STMT)
+            should  =  res.getresult()[0][2]
+            not_yet =  res.getresult()[0][3]
+            done    =  res.getresult()[0][4]
+            date=""
+            for row in res.getresult():
+                if not row:
+                    continue
+                tmp = row[0].split(' ')[0]
+                if ( date  != tmp ) :
+                    date =  tmp
+                    h.fill(time.mktime(time.strptime(row[0],'%Y-%m-%d %H:%M:%S')),row[2])
+                    h1.fill(time.mktime(time.strptime(row[0],'%Y-%m-%d %H:%M:%S')),row[3])
+                    h2.fill(time.mktime(time.strptime(row[0],'%Y-%m-%d %H:%M:%S')),row[4])
+            db.close()
 
-    h2.set_time_axis(True)
-    h2.set_profile(True)
-    h2.set_ylabel("# of tapes that should have write tabs ON")
-    h2.set_xlabel("Date (year-month-day)")
+            h.set_line_color(2)
+            h.set_line_width(20)
+            h.set_marker_type("impulses")
+            h.set_marker_text("ON")
 
-    if db_port:
-        db = pg.DB(host=db_server_name, dbname=db_name, port=db_port);
-    else:
-        db = pg.DB(host=db_server_name, dbname=db_name);
+            t = time.ctime(time.time())
 
-    res=db.query(SELECT_STMT)
-    should  =  res.getresult()[0][2]
-    not_yet =  res.getresult()[0][3]
-    done    =  res.getresult()[0][4]
-    date=""
-    for row in res.getresult():
-        if not row:
-            continue
-        tmp = row[0].split(' ')[0]
-        if ( date  != tmp ) :
-            date =  tmp
-            h.fill(time.mktime(time.strptime(row[0],'%Y-%m-%d %H:%M:%S')),row[2])
-            h1.fill(time.mktime(time.strptime(row[0],'%Y-%m-%d %H:%M:%S')),row[3])
-            h2.fill(time.mktime(time.strptime(row[0],'%Y-%m-%d %H:%M:%S')),row[4])
-    db.close()
+            h.add_text("set label \"Plotted %s \" at graph .99,0 rotate font \"Helvetica,10\"\n"% (t,))
+            h.add_text("set label \"Should %s, Done %s(%3.1f%%), Not Done %s.\" at graph .05,.90\n" % (should,done,100.*done/should,not_yet,))
 
-    h.set_line_color(2)
-    h.set_line_width(20)
-    h.set_marker_type("impulses")
-    h.set_marker_text("ON")
-
-    t = time.ctime(time.time())
-    
-    h.add_text("set label \"Plotted %s \" at graph .99,0 rotate font \"Helvetica,10\"\n"% (t,))
-    h.add_text("set label \"Should %s, Done %s(%3.1f%%), Not Done %s.\" at graph .05,.90\n" % (should,done,100.*done/should,not_yet,))
-
-    h1.set_line_color(1)
-    h1.set_line_width(20)
-    h1.set_marker_type("impulses")
-    h1.set_marker_text("OFF")
+            h1.set_line_color(1)
+            h1.set_line_width(20)
+            h1.set_marker_type("impulses")
+            h1.set_marker_text("OFF")
 
 
-    h2.set_line_color(2)
-    h2.set_line_width(20)
-    h2.set_marker_type("impulses")
-    h2.set_marker_text("OFF")
-    
+            h2.set_line_color(2)
+            h2.set_line_width(20)
+            h2.set_marker_type("impulses")
+            h2.set_marker_text("OFF")
 
-#    h.plot2(h1)
-    
+
+            h.plot2(h1)
+
 #    os.system("display %s.jpg&"%(h.get_name()))
 
-    h2.set_time_axis(True)
-    h2.set_profile(True)
-    h2.set_logy(True)
-    h2.set_ylabel("# of tabs flipped per day")
-    h2.set_xlabel("Date (year-month-day)")
-    h2.set_marker_text("")
-    h2.add_text("set label \"Plotted %s \" at graph .99,0 rotate font \"Helvetica,10\"\n"% (t,))
+            h2.set_time_axis(True)
+            h2.set_profile(True)
+            h2.set_logy(True)
+            h2.set_ylabel("# of tabs flipped per day")
+            h2.set_xlabel("Date (year-month-day)")
+            h2.set_marker_text("")
+            h2.add_text("set label \"Plotted %s \" at graph .99,0 rotate font \"Helvetica,10\"\n"% (t,))
 
-    h2.plot_derivative()
+            h2.plot_derivative()
 
 #    os.system("display %s.jpg&"%(h2.get_name()))
 
-    cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp *.ps *.jpg %s:/fnal/ups/prd/www_pages/enstore/write_tabs"%(inq_host,)
-    os.system(cmd)
+            cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp *.ps *.jpg stkensrv2.fnal.gov:/fnal/ups/prd/www_pages/enstore/write_tabs"
+#           cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp *.ps *.jpg %s:/fnal/ups/prd/www_pages/enstore/write_tabs"%(inq_host,)
+            os.system(cmd)
 
-    
+
     sys.exit(0)
-
-    
-    
 if __name__ == "__main__":
     main()
