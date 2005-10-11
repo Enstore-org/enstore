0e8ed377454c99ad3a5a1b76c87910cab5db5a6d
Chih Hao Huang <huangch>
2005-10-11 05:17:11 +0000

    New born

---
+++ src/operation.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+"""
+Schema
+
+ Schema |       Name        |   Type   |  Owner  
+--------+-------------------+----------+---------
+ public | job               | table    | huangch
+ public | job_definition    | table    | huangch
+ public | job_definition_id | sequence | huangch
+ public | job_id            | sequence | huangch
+ public | object            | table    | huangch
+ public | progress          | table    | huangch
+ public | task              | table    | huangch
+ public | task_id           | sequence | huangch
+
+                                Table "public.job"
+ Column  |            Type             |                Modifiers                                                                                 
+---------+-----------------------------+------------------------------------------
+ id      | integer                     | not null default nextval('job_id'::text)
+ name    | character varying           | not null
+ type    | integer                     | not null
+ start   | timestamp without time zone | default now()
+ finish  | timestamp without time zone |
+ comment | character varying           |
+ id2     | bigint                      | not null
+Indexes:
+    "job_pkey" PRIMARY KEY, btree (id)
+    "job_id2_key" UNIQUE, btree (id2)
+    "job_finish_idx" btree (finish)
+    "job_name_idx" btree (name)
+    "job_start_idx" btree ("start")
+    "job_type_idx" btree ("type")
+Foreign-key constraints:
+    "job_type_fkey" FOREIGN KEY ("type") REFERENCES job_definition(id) ON UPDATE CASCADE ON DELETE CASCADE
+ 
+                           Table "public.job_definition"
+ Column  |       Type        |                      Modifiers                                                                                 
+---------+-------------------+-----------------------------------------------------
+ id      | integer           | not null default nextval('job_definition_id'::text)
+ name    | character varying | not null
+ tasks   | integer           |
+ remarks | character varying |
+Indexes:
+    "job_definition_pkey" PRIMARY KEY, btree (id)
+    "job_definition_name_idx" btree (name)
+ 
+                            Table "public.task"
+   Column   |       Type        |                 Modifiers
+------------+-------------------+-------------------------------------------
+ id         | integer           | not null default nextval('task_id'::text)
+ seq        | integer           | not null
+ job_type   | integer           | not null
+ dsc        | character varying |
+ action     | character varying |
+ comment    | character varying |
+ auto_start | character(1)      | default 'm'::bpchar
+Indexes:
+    "task_pkey" PRIMARY KEY, btree (seq, job_type)
+Foreign-key constraints:
+    "task_job_type_fkey" FOREIGN KEY (job_type) REFERENCES job_definition(id) ON UPDATE CASCADE ON DELETE CASCADE
+ 
+                Table "public.progress"
+ Column  |            Type             |   Modifiers
+---------+-----------------------------+---------------
+ job     | integer                     | not null
+ task    | integer                     | not null
+ start   | timestamp without time zone | default now()
+ finish  | timestamp without time zone |
+ comment | character varying           |
+ args    | character varying           |
+ result  | character varying           |
+Indexes:
+    "progress_job_idx" btree (job)
+    "progress_start_idx" btree ("start")
+Foreign-key constraints:
+    "progress_job_fkey" FOREIGN KEY (job) REFERENCES job(id) ON UPDATE CASCADE ON DELETE CASCADE
+ 
+         Table "public.object"
+ Column |       Type        | Modifiers
+--------+-------------------+-----------
+ job    | integer           | not null
+ object | character varying |
+Indexes:
+    "object_job_idx" btree (job)
+    "object_object_idx" btree ("object")
+Foreign-key constraints:
+    "object_job_fkey" FOREIGN KEY (job) REFERENCES job(id) ON UPDATE CASCADE ON DELETE CASCADE
+ 
+
+"""
+
+import pg
+import time
+import pprint
+import types
+
+# timestamp2time(ts) -- convert "YYYY-MM-DD HH:MM:SS" to time 
+def timestamp2time(s):
+	if s == '1969-12-31 17:59:59':
+		return -1
+	else:
+		# take care of daylight saving time
+		tt = list(time.strptime(s, "%Y-%m-%d %H:%M:%S"))
+		tt[-1] = -1
+		return time.mktime(tuple(tt))
+
+# time2timestamp(t) -- convert time to "YYYY-MM-DD HH:MM:SS"
+def time2timestamp(t):
+	return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
+
+DATABASEHOST = 'localhost'
+DATABASEPORT = 5432;
+DATABASENAME = 'operation'
+
+# get_db() -- initialize a database connection
+def get_db():
+	return pg.DB(dbname = DATABASENAME, port = DATABASEPORT, host = DATABASEHOST)
+
+# global db connection
+db = get_db()
+
+# create_job() -- generic job creation
+def create_job(name, type, args, comment = ''):
+	id2 = int(time.time())
+	q = "insert into job (name, type, comment, id2) \
+		values ('%s', (select id from job_definition where \
+				name = '%s'), '%s', %d);"%(
+		name, type, comment, id2)
+	db.query(q)
+	# get job id
+	q = "select id from job where name = '%s' and id2 = %d;"%(
+		name, id2)
+	id = db.query(q).getresult()[0][0]
+	for i in args:
+		q = "insert into object values (%d, '%s');"%(id, i)
+		db.query(q)
+	return id
+
+# get_job_by_name() -- from a name to find the job; could be many
+def get_job_by_name(name):
+	q = "select * from job where name = '%s' order by id;"%(name)
+	res = db.query(q).dictresult()
+	# it is possible for multiple results
+	job = []
+	for rec in res:
+		job.append(retrieve_job(rec))
+	return job 
+
+# get+job_by_id() -- get_job_using internal id
+def get_job_by_id(id):
+	q = "select * from job where id = %d;"%(id)
+	res = db.query(q).dictresult()
+	if res:
+		return retrieve_job(res[0])
+	else:
+		return None
+
+# get_job_by_time() --get job using time frame
+def get_job_by_time(after, before = None):
+	if not before:	# default now()
+		before = time2timestamp(time.time())
+	if type(after) != types.StringType:
+		after = time2timestamp(after)
+	if type(before) != types.StringType:
+		before = time2timestamp(before)
+	q = "select * from job where start >= '%s' and start <= '%s' \
+		order by start;"%(after, before)
+	res = db.query(q).dictresult()
+	# it is possible for multiple results
+	job = []
+	for rec in res:
+		job.append(retrieve_job(rec))
+	return job 
+
+# retrieve_job() -- get all related information of this job
+def retrieve_job(job):
+	# assemble its related objects
+	q = "select * from object where job = %d order by object;"%(job['id'])
+	object = []
+	res = db.query(q).getresult()
+	for j in res:
+		object.append(j[1])
+	job['object'] = object
+	# list its related tasks
+	q = "select * from job_definition where id = %d;"%(job['type'])
+	job_definition = db.query(q).dictresult()[0]
+	job['job_definition'] = job_definition
+	q = "select * from task left outer join progress \
+		on (progress.job = %d and task.id = progress.task) \
+		where task.job_type = %d \
+			order by seq;"%(job['id'], job['type'])
+	job['task'] = db.query(q).dictresult()
+	return job
+
+# get_job_tasks(id) -- show the tasks related to this job
+def get_job_tasks(id):
+	q = "select seq, dsc, action from task, job \
+		where job.id = %d and job.type = task.job_type \
+		order by seq;"%(id)
+	return db.query(q).dictresult()
+
+# start_job_task(job_id, task_id) -- start a task
+def start_job_task(job_id, task_id, args=None, comment=None):
+	if args:
+		args = "'%s'"%(args)
+	else:
+		args = "null"
+	if comment:
+		comment = "'%s'"%(comment)
+	else:
+		comment = "null"
+	q = "insert into progress (job, task, comment, args) \
+		values (%d, %d, %s, %s);"%(
+			job_id, task_id, comment, args)
+	res = db.query(q)
+
+def finish_job_task(job_id, task_id, comment=None, result=None):
+	if result:
+		result = "'%s'"%(string(result))
+	else:
+		result = "null"
+	if comment:
+		q = "update progress \
+			set finish = now(), comment = '%s', \
+				result = %s \
+			where job = %d and task = %d"%(
+			comment, result, job_id, task_id)
+	else:
+		q = "update progress \
+			set finish = now(), result = %s \
+			where job = %d and task = %d"%(
+				result, job_id, task_id)
+	res = db.query(q)
+
+
+
+def create_write_protect_on_job(name, args, comment = ''):
+	return create_job(name, 'WRITE_PROTECTION_TAB_ON', args, comment)
+
+def create_write_protect_off_job(name, args, comment = ''):
+	return create_job(name, 'WRITE_PROTECTION_TAB_OFF', args, comment)
+
+if __name__ == '__main__':
+	# test
+	a = ['VO1000', 'VO1001', 'VO1002']
+	job_name = 'TEST01'
+	res = create_write_protect_on_job(job_name, a, 'tesing')
+	time.sleep(5)
+	res = create_write_protect_off_job(job_name, a, 'tesing')
+	job = get_job_by_name(job_name)
+	pprint.pprint(job)
