3c63155a6b5a3587ce3116a03ca2a42aa404110f
Dmitry Litvinsev <litvinse>
2005-10-11 13:12:41 +0000

    improvements (coy only the files generated and delete files when done)

--- sbin/write_tabs.py
+++ sbin/write_tabs.py
@@ -128,8 +128,9 @@ def main():
 
 #    os.system("display %s.jpg&"%(h2.get_name()))
 
-            cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp *.ps *.jpg stkensrv2.fnal.gov:/fnal/ups/prd/www_pages/enstore"
-#           cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp *.ps *.jpg %s:/fnal/ups/prd/www_pages/enstore/write_tabs"%(inq_host,)
+            cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp %s.jpg %s.ps %s_stamp.jpg stkensrv2.fnal.gov:/fnal/ups/prd/www_pages/enstore"%(h.get_name(),h.get_name(),h.get_name(),)
+            os.system(cmd)
+            cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp %s.jpg %s.ps %s_stamp.jpg stkensrv2.fnal.gov:/fnal/ups/prd/www_pages/enstore"%(h2.get_name(),h2.get_name(),h2.get_name(),)
             os.system(cmd)
             cmd = "rm %s.jpg %s.ps %.jpg %.ps %s_stamp.jpg"%(h.get_name(),)
             os.system(cmd)
