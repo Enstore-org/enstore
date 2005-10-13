c14d752f3ba1ccf2d8dc3b317a17b5f5067aaa14
Chih Hao Huang <huangch>
2005-10-13 18:41:53 +0000

    finalize wpa logging with library

--- src/inventory.py
+++ src/inventory.py
@@ -1183,9 +1183,9 @@ def inventory(output_dir, cache_dir):
         wpa_file.write("  Total: %5d\n  Should: %5d\n   Done: %5d\nNot yet: %5d\n  Ratio: %5.2f%%\n"%(n_vols_lib[i], n_rf_vols[i], n_rp_vols[i], n_not_rp_vols[i], float(n_rp_vols[i])*100/n_rf_vols[i]))
     wpa_file.close()
 
-    # log wpa info twice a day
+    # log wpa info once a day
     hour = time.localtime(t0)[3]
-    if hour == 7 or hour == 18:
+    if hour == 22 :
         accinfo = csc.get(enstore_constants.ACCOUNTING_SERVER)
         acs = accounting.accDB(accinfo['dbhost'], accinfo['dbname'], accinfo.get("dbport"))
         q = "insert into write_protect_summary (date, total, should, not_yet, done) values('%s', %d, %d, %d, %d);"%(time2timestamp(t0), n_vols, sum(n_rf_vols.values()), sum(n_not_rp_vols.values()), sum(n_rp_vols.values()))
