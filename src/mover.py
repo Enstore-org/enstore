c9afcfd709c71f024ac6d8f05457b4381298f6aa
Alexander Moibenko <moibenko>
2005-11-11 23:31:35 +0000

    some mods

--- src/mover.py
+++ src/mover.py
@@ -3158,14 +3158,14 @@ class Mover(dispatching_worker.DispatchingWorker,
                 self.state = HAVE_BOUND
                 if self.maybe_clean():
                     Trace.trace(26,"cleaned")
-                    self.state = IDLE
-                    self.tr_failed = 0
                     if self.mode == WRITE:
                         ## this is a temporary solution for not overwritin files
                         Trace.log(e_errors.ERROR, "possible ovewrite condition. Will set tape readonly");
                         self.vcc.set_system_readonly(self.current_volume)
                         self.dismount_volume(after_function=self.idle)
                     
+                    self.state = IDLE
+                    self.tr_failed = 0
                     return
                 
         self.send_error_msg(error_info = (exc, msg),error_source=error_source)
