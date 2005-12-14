694860a4108fab01066fcf3f8cb88454a1010a39
Eileen Berman <berman>
2005-12-14 19:57:30 +0000

    remove ttt import

--- src/inquisitor.py
+++ src/inquisitor.py
@@ -916,9 +916,7 @@ class InquisitorMethods(dispatching_worker.DispatchingWorker):
         #self.encp_xfer_but_no_update = 0
         enstore_functions.inqTrace(enstore_constants.INQERRORDBG,
                       "starting search of accounting db for %s encp history lines"%(self.max_encp_lines,))
-        #encplines = enstore_functions.acc_encp_lines(self.csc, self.max_encp_lines)
-        import ttt
-        encplines = ttt.res_l
+        encplines = enstore_functions.acc_encp_lines(self.csc, self.max_encp_lines)
         encplines.sort()
         enstore_functions.inqTrace(enstore_constants.INQERRORDBG,
                                    "update_encp - found %s lines"%(len(encplines,)))
