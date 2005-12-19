a077a820f94861de5496976cb6391618dbba23f5
Eileen Berman <berman>
2005-12-19 19:08:36 +0000

    catch when the db is down and continue

--- src/enstore_pg.py
+++ src/enstore_pg.py
@@ -12,7 +12,11 @@ import pg
 def acc_encp_lines(csc, numEncps=100):
     acc = csc.get(enstore_constants.ACCOUNTING_SERVER)
     # connect to the db
-    db = pg.DB(host=acc['dbhost'], dbname=acc['dbname'])
+    try:
+        db = pg.DB(host=acc['dbhost'], dbname=acc['dbname'])
+    except pg.error, detail:
+        # could not connect to the db
+        return []
     res = db.query("select * from encp_xfer order by date desc limit %s;"%(numEncps,))
     res_err = db.query("select * from encp_error order by date desc limit %s;"%(numEncps,))
     # combine the 2 lists and take the top numEncps entries to return
