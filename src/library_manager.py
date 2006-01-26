5c60a763a45fb7b1e60a1598539d7741386fba41
Alexander Moibenko <moibenko>
2006-01-26 19:17:02 +0000

    fixed a bug

--- src/library_manager.py
+++ src/library_manager.py
@@ -1001,6 +1001,7 @@ class LibraryManagerMethods:
                 status[0] == e_errors.NOWORK):
                 if rq and rq.ticket.has_key('reject_reason') and rq.ticket['reject_reason'][0] == "RESTRICTED_ACCESS":
                     Trace.trace(30, "This request should not get here %s"%(rq,))
+                    status[0] = e_errors.NOWORK
                     rq = None
                 return rq, status
             # some sort of error, like write work and no volume available
