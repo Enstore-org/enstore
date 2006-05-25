568102ecac13929fa6c5a4da32bb7e1e456d3437
Chih Hao Huang <huangch>
2006-05-25 21:22:32 +0000

    send email after 10 caps

--- src/tab_flipping_nanny.py
+++ src/tab_flipping_nanny.py
@@ -2,8 +2,15 @@
 
 import operation
 import time
+import sys
+import os
+
+LIMIT = 10 
 
 if __name__ == '__main__':
+	stdout_save = sys.stdout
+	if len(sys.argv) > 1:
+		sys.stdout = open(sys.argv[1], "w")
 	print "Recommended tap flipping jobs on %s"%(operation.cluster), time.ctime(time.time())
 	print
 	print "Recommended write protect on jobs:"
@@ -12,10 +19,10 @@ if __name__ == '__main__':
 	for i in res:
 		operation.show_cap(i, res[i])
 	total = 0
-	caps = len(res)
+	oncaps = len(res)
 	for i in res.keys():
 		total = total + len(res[i])
-	print "%d tapes in %d caps"%(total, caps)
+	print "%d tapes in %d caps"%(total, oncaps)
 	print
 	print"Recommended write protect off jobs:"
 	print
@@ -23,9 +30,14 @@ if __name__ == '__main__':
 	for i in res:
 		operation.show_cap(i, res[i])
 	total = 0
-	caps = len(res)
+	offcaps = len(res)
 	for i in res.keys():
 		total = total + len(res[i])
-	print "%d tapes in %d caps"%(total, caps)
+	print "%d tapes in %d caps"%(total, offcaps)
 
-
+	sys.stdout = stdout_save
+	if len(sys.argv) > 1 and (oncaps >= LIMIT or offcaps >= LIMIT):
+		cmd = 'cat %s'%(sys.argv[1])
+		os.system(cmd)
+		cmd = '/usr/bin/Mail -s "tab flipping watch" %s < %s '%(os.environ['ENSTORE_MAIL'], sys.argv[1])
+		os.system(cmd)
