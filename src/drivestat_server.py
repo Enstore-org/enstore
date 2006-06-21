db0c29ff43aa37df5c84125cb588818076784add
cvs2svn <UNKNOWN@fnal.gov>
2006-06-21 19:12:23 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_5e_encp_Linux+2'.
    Sprout from master 2006-06-21 19:12:22 UTC Michael Zalokar <zalokar> 'bumping version to v3_5e because of encpCut'
    Cherrypick from master 2005-08-16 21:40:50 UTC Eileen Berman <berman> 'summary page of available encp release notes':
        etc/REL_V217.html
        etc/REL_V218.html
        etc/REL_V219.html
        etc/REL_V3_1.html
        etc/REL_V3_2.html
        etc/REL_V3_4.html
        etc/encpReleaseNotes.html
        src/accounting_server.py
        src/drivestat_server.py
        src/migrate.py
    Cherrypick from master 2006-01-24 18:21:01 UTC Michael Zalokar <zalokar> 'Added additional nodes.  Made some client_color sections done by regular':
        etc/entvrc
        etc/krb5.conf
        src/file_clerk.py
    Cherrypick from master 2002-01-28 23:35:44 UTC Jon Bakken <bakken> 'change in adic log processing':
        sbin/log-stash
    Cherrypick from master 2004-01-28 17:28:29 UTC Alexander Moibenko <moibenko> 'updated test media changer':
        src/media_changer_test.py
    Cherrypick from master 2005-08-18 20:23:52 UTC Michael Zalokar <zalokar> 'Changed the background image from enstore.gif to enstore_background.gif.':
        doc/WWW/encpReleaseNotes.html
    Cherrypick from master 2006-02-20 22:50:41 UTC Chih Hao Huang <huangch> 'change the description of --restore':
        src/file_clerk_client.py
    Cherrypick from master 2006-04-24 18:01:11 UTC Michael Zalokar <zalokar> 'Patch to accept a single blank hardrive to install on.':
        etc/ks.cfg
    Delete:
        etc/gccen.conf
        sbin/xfer_stats.py
        src/file_clerk.py-1.217
        src/volume_clerk.py-1.349
        tools/scanner.c

--- src/drivestat_server.py
+++ src/drivestat_server.py
@@ -53,11 +53,8 @@ class Server(dispatching_worker.DispatchingWorker, generic_server.GenericServer)
 		self.hostip = att['hostip']
 		dispatching_worker.DispatchingWorker.__init__(self,
 			(att['hostip'], att['port']))
-		try:
-			self.dsDB = drivestat2.dsDB(att['dbhost'], att['dbname'], att['dbport'])
-		except: # wait for 30 seconds and retry
-			time.sleep(30)
-			self.dsDB = drivestat2.dsDB(att['dbhost'], att['dbname'], att['dbport'])
+		self.dsDB = drivestat2.dsDB(att['dbhost'], att['dbname'], att['dbport'])
+
 		# setup the communications with the event relay task
 		self.resubscribe_rate = 300
 		self.erc.start([event_relay_messages.NEWCONFIGFILE],
