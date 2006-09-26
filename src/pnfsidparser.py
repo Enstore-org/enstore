17cf184330f9b9a2d58221c39b76503f7ce2f338
Dmitry Litvinsev <litvinse>
2006-09-26 20:05:46 +0000

    added module to parse pnfs id to/from pnfs database representation

---
+++ src/pnfsidparser.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+###############################################################################
+#
+# $Author$
+# $Date$
+# $Id$
+#
+# convert pnfs from pnfs db representation and back
+# 
+#
+###############################################################################
+import string
+
+def parse_id(txt):
+    pnfsid_string=""
+    for i in [0,4,8,12]:
+        l=i+2
+        h=l+2
+        l1=i
+        h1=l1+2
+        pnfsid_string=pnfsid_string+txt[l:h]+txt[l1:h1]
+    for i in [20,16]:
+        l=i+2
+        h=l+2
+        l1=i
+        h1=l1+2
+        pnfsid_string=pnfsid_string+txt[l:h]+txt[l1:h1]
+    pnfsid_string=string.upper(pnfsid_string)
+    return pnfsid_string
+     
+
+def inverse_parse_id(txt):
+    id=""
+    for i in [0,4,8,12]:
+        l=i+2
+        h=l+2
+        l1=i
+        h1=l1+2
+        id=id+txt[l:h]+txt[l1:h1]
+    for i in [20,16]:
+        l=i+2
+        h=l+2
+        l1=i
+        h1=l1+2
+        id=id+txt[l:h]+txt[l1:h1]
+    id=string.lower(id)
+    return id
