ef7de77a2f7b45b05218f42967bd2ea150144bb3
Dmitry Litvinsev <litvinse>
2006-09-28 19:37:13 +0000

    *** empty log message ***

--- src/dcache_monitor.py
+++ src/dcache_monitor.py
@@ -215,6 +215,7 @@ def prepare_html(db_name):
     if ( count != 0 ) :
         fname="%s_bad.txt"%(db_name,)
         sql_txt = "select date, pnfsid_string, layer1, layer2, layer4, pnfs_path from volatile_files where layer2='n' and unix_date<%d order by date asc"%(int(now_time-3600))
+        os.system("rm %s"%fname);
         cmd = "psql  %s  -o %s -c \"%s;\""%(db_name,fname,sql_txt)
         os.system(cmd)
         cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrcp %s  stkensrv2.fnal.gov:/diska/www_pages/dcache_monitor/"%(fname,)
@@ -267,7 +268,7 @@ if __name__ == '__main__':
         dbs.append(line[:-1])
     out.close()
 
-#    cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrsh  stkensrv2.fnal.gov \"rm /diska/www_pages/dcache_monitor/*.txt\""
+    cmd = "source /home/enstore/gettkt; $ENSTORE_DIR/sbin/enrsh  stkensrv2.fnal.gov \"rm /diska/www_pages/dcache_monitor/*.txt\""
     os.system(cmd)
 
 #    for db_name in ['minos']:
