cad514666777c245e2b2f8b5a5ca4b377a30c360
Michael Zalokar <zalokar>
2007-01-03 21:11:03 +0000

    Building encp/enstore against python 2.4.3 succeeds, but then running enstore fails with an ImportError stating that the _strptime module could not be found.  In reality this is a python library, not a module. Explicitly importing this library solves the problem.

--- src/enstore_admin.py
+++ src/enstore_admin.py
@@ -3,6 +3,17 @@
 # src/$RCSfile$   $Revision$
 #
 
+try:
+	#Sometime after python 2.2.3 but before 2.4.3 this python library was
+	# added.  It causes a problem with freeze if not imported explicitly.
+        # The problem being that the executable builds, but when run throws an
+        # InputError stating that the module _strptime can not be found.
+        # In reality _strptime is a python library, not a module.
+	import _strptime
+	__pychecker__ = "no-import"
+except ImportError:
+	pass
+
 import enstore
 
 def do_work():
