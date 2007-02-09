0632eb2ec06c98508e8d3e641042dc7d79b72540
Michael Zalokar <zalokar>
2007-02-09 23:14:39 +0000

    Handle TapeLog tapes with multiple files (at least in name) on the same tape. The later copies get -copy# (with # equal to the copy number) appended to them.

--- ingest/sdss/parseTapeLog.py
+++ ingest/sdss/parseTapeLog.py
@@ -87,6 +87,7 @@ def parseTarTapeParFile(filename):
     
 def parseTapeLogParFile(filename):
     filelist = []
+    filecounts = {}
     f = open(filename)
     line = f.readline()
     while line:
@@ -95,8 +96,24 @@ def parseTapeLogParFile(filename):
             # (tape, filemark, run, frame, ccd)
             try:
                 (unused, filemark, run, frame, ccd) = line.split()
-                filelist.append((filemark,
-                                 makeTapeLogFilename(run, frame, ccd)))
+                filename = makeTapeLogFilename(run, frame, ccd)
+
+                #We need to handle the possibility that the same file
+                # (at least in name) is written on the tape more than once.
+                try:
+                    previous_count = filecounts[filename]
+                except KeyError:
+                    previous_count = 0
+
+                if previous_count:
+                    use_filename = "%s-copy%d" % (filename, previous_count)
+                else:
+                    use_filename = filename
+
+                filelist.append((filemark, use_filename))
+
+                #Store the new tally for occurances for this filename.
+                filecounts[filename] = previous_count + 1 
             except ValueError:
                 pass
 
