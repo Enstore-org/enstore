f7670af4616dad1f9c014cffc824d3b1971b745e
Dmitry Litvinsev <litvinse>
2007-03-30 19:46:20 +0000

    *** empty log message ***

--- sbin/filler.py
+++ sbin/filler.py
@@ -14,16 +14,21 @@
 
 import sys
 import time
-import popen2
+import pg
 import os
 import getopt, string
 import math
-import accounting_query
 import enstore_constants
 import enstore_functions
 import configuration_client
 
-MB=1024*1024.
+PB=1024.*1024.*1024.*1024.*1024.
+TB=1024.*1024.*1024.*1024.
+GB=1024.*1024.*1024.
+MB=1024.*1024.
+KB=1024.
+
+SELECT_LAST_TIME="select max(unix_time) from encp_xfer_average_by_storage_group"
 
 
 def showError(msg):
@@ -39,73 +44,62 @@ def main():
 
     accounting_db_server_name = acc.get('dbhost')
     accounting_db_name        = acc.get('dbname')
-    port                      = acc.get('dbport', None)
-
-    if port:
-        login_string = "psql  %s -h %s -p %s -t -q -c "%(accounting_db_name,accounting_db_server_name, port)
-    else:
-        login_string = "psql  %s -h %s -t -q -c "%(accounting_db_name,accounting_db_server_name)
-    cmd = "%s \" select max(unix_time) from encp_xfer_average_by_storage_group;\""%(login_string,)
-    inp,out = os.popen2 (cmd, 'r')
-    inp.write (cmd)
-    inp.close ()
+    accounding_db_port        = acc.get('dbport', 5432)
 
     zero_time  = 1045689052
-    for line in out.readlines() :
-        if line.isspace():
+    db = pg.DB(host=accounting_db_server_name, dbname=accounting_db_name, port=accounding_db_port);
+    res=db.query(SELECT_LAST_TIME);
+    for row in res.getresult():
+        if not row:
             continue
-        zero_time=int(line.strip(' '))
-    out.close()
-    
+        zero_time=row[0]
+
     delta_time = 60 * 20
     zero_time  = int(zero_time+0.5*delta_time)
-
-#    print 'Max time',zero_time
-
     now_time   =  int(time.time())
 
     while zero_time < now_time:
-      stop_time       = zero_time + delta_time;
-      middle_time     = int(zero_time + 0.5*delta_time)
-      str_middle_time   = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(middle_time))
-      str_from_time   = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(zero_time))
-      str_to_time     = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(stop_time))
-      select_stmt = "%s \""%(login_string,)
-      select_stmt = select_stmt +  "insert into encp_xfer_average_by_storage_group "
-      select_stmt = select_stmt +  " ( select "
-      select_stmt = select_stmt + str(middle_time)
-      select_stmt = select_stmt + ",'"
-      select_stmt = select_stmt + str_middle_time
-      select_stmt = select_stmt + "','"
-      select_stmt = select_stmt + str_from_time
-      select_stmt = select_stmt + "','"
-      select_stmt = select_stmt + str_to_time
-      select_stmt = select_stmt + "',storage_group, rw,"
-      select_stmt = select_stmt + "avg(overall_rate)/1024./1024,"
-      select_stmt = select_stmt + "avg(network_rate)/1024./1024,"
-      select_stmt = select_stmt + "avg(disk_rate)/1024./1024,"
-      select_stmt = select_stmt + "avg(transfer_rate)/1024./1024,"
-      select_stmt = select_stmt + "avg(drive_rate)/1024./1024,"
-      select_stmt = select_stmt + "avg(size)/1024./1024,"
-      select_stmt = select_stmt + "stddev(overall_rate)/1024./1024,"
-      select_stmt = select_stmt + "stddev(network_rate)/1024./1024,"
-      select_stmt = select_stmt + "stddev(disk_rate)/1024./1024,"
-      select_stmt = select_stmt + "stddev(transfer_rate)/1024./1024,"
-      select_stmt = select_stmt + "stddev(drive_rate)/1024./1024,"
-      select_stmt = select_stmt + "stddev(size)/1024./1024, count(*) from"
-      select_stmt  = select_stmt + " encp_xfer where date between '"
-      select_stmt  = select_stmt + str_from_time
-      select_stmt  = select_stmt + "' and '"
-      select_stmt  = select_stmt + str_to_time
-      select_stmt  = select_stmt + "' group by storage_group, rw)"
-      select_stmt  = select_stmt + ";\" enstore"
+        stop_time       = zero_time + delta_time;
+        middle_time     = int(zero_time + 0.5*delta_time)
+        str_middle_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(middle_time))
+        str_from_time   = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(zero_time))
+        str_to_time     = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(stop_time))
+        select_stmt = "insert into encp_xfer_average_by_storage_group "
+        select_stmt = select_stmt +  " ( select "
+        select_stmt = select_stmt + str(middle_time)
+        select_stmt = select_stmt + ",'"
+        select_stmt = select_stmt + str_middle_time
+        select_stmt = select_stmt + "','"
+        select_stmt = select_stmt + str_from_time
+        select_stmt = select_stmt + "','"
+        select_stmt = select_stmt + str_to_time
+        select_stmt = select_stmt + "',storage_group, rw,"
+        select_stmt = select_stmt + "avg(overall_rate)/1024./1024,"
+        select_stmt = select_stmt + "avg(network_rate)/1024./1024,"
+        select_stmt = select_stmt + "avg(disk_rate)/1024./1024,"
+        select_stmt = select_stmt + "avg(transfer_rate)/1024./1024,"
+        select_stmt = select_stmt + "avg(drive_rate)/1024./1024,"
+        select_stmt = select_stmt + "avg(size)/1024./1024,"
+        select_stmt = select_stmt + "stddev(overall_rate)/1024./1024,"
+        select_stmt = select_stmt + "stddev(network_rate)/1024./1024,"
+        select_stmt = select_stmt + "stddev(disk_rate)/1024./1024,"
+        select_stmt = select_stmt + "stddev(transfer_rate)/1024./1024,"
+        select_stmt = select_stmt + "stddev(drive_rate)/1024./1024,"
+        select_stmt = select_stmt + "stddev(size)/1024./1024, count(*) from"
+        select_stmt  = select_stmt + " encp_xfer where date between '"
+        select_stmt  = select_stmt + str_from_time
+        select_stmt  = select_stmt + "' and '"
+        select_stmt  = select_stmt + str_to_time
+        select_stmt  = select_stmt + "' group by storage_group, rw)"
+        db.query(select_stmt);
+        zero_time = zero_time + delta_time
+    db.close()
 
-#      print 'Executing:',select_stmt
-#       sys.exit(0)
-      os.system(select_stmt)
-      zero_time = zero_time + delta_time
-
-    sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    try: 
+        main()
+    except:
+        print "Failed"
+        sys.exit(1)
+    sys.exit(0)    
