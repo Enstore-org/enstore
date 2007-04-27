4e3eb61ebb1db536577ad3cbd55068b46f607eff
Alexander Moibenko <moibenko>
2007-04-27 21:36:57 +0000

    added startup scripts

--- external_distr/rpm_postinstall.sh
+++ external_distr/rpm_postinstall.sh
@@ -34,7 +34,7 @@ echo "Cmnd_Alias      PYTHON  = ${PYTHON_DIR}/bin/python" >> /etc/sudoers.e
 echo "Cmnd_Alias      PIDKILL = ${ENSTORE_DIR}/bin/pidkill, ${ENSTORE_DIR}/bin/pidkill_s, /bin/kill" >> /etc/sudoers.e
 echo "enstore ALL=NOPASSWD:PYTHON, NOPASSWD:PIDKILL" >> /etc/sudoers.e
 cp /etc/sudoers.e /etc/sudoers
-chmod 440 /etc/sudoers.e
+chmod 440 /etc/sudoers
 
 echo "Copying $ENSTORE_DIR/external_distr/setups.sh to /usr/local/etc"
 if [ ! -d "/usr/local/etc" ]
@@ -52,6 +52,16 @@ fi
 cp -rpf $ENSTORE_DIR/external_distr/setup-enstore $ENSTORE_DIR/config
 rm -f $ENSTORE_DIR/debugfiles.list
 rm -f $ENSTORE_DIR/debugsources.list
+
+echo "Copying $ENSTORE_DIR/bin/enstore-boot to /etc/rc.d/init.d"
+cp -f $ENSTORE_DIR/bin/enstore-boot /etc/rc.d/init.d
+echo "Configuring the system to start enstore on boot"
+`/etc/rc.d/init.d/enstore-boot install`
+echo "Saving /etc/rc.d/rc.local to /etc/rc.d/rc.local.enstore_save"
+cp -pf /etc/rc.d/rc.local /etc/rc.d/rc.local.enstore_save
+echo "Copying $ENSTORE_DIR/external_distr/rc.local to /etc/rc.d"
+cp -f $ENSTORE_DIR/external_distr/rc.local /etc/rc.d
+
 
 echo "To finish the installation you need to run $ENSTORE_DIR/external_distr/install.sh
 as user root on the host where the enstore configuration server will be running and only on
