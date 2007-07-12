acd1b82ac3997c9c3af77b803e670bfcad41eb68
Dmitry Litvinsev <litvinse>
2007-07-12 15:11:22 +0000

    make day-hour-minute axis

--- sbin/pnfs_test.py
+++ sbin/pnfs_test.py
@@ -115,7 +115,9 @@ if __name__ == '__main__':
     ntuple.set_line_color(1)
     ntuple.set_line_width(2)
     ntuple.set_marker_type("lines")
-    ntuple.set_time_axis_format("%m-%d-%H");
+    ntuple.set_time_axis_format("%d-%H-%M");
+    ntuple.set_ylabel("Transactions per second") 
+    ntuple.set_xlabel("(day-hour-minute)") 
     so=0
     sn=0
     n=0
