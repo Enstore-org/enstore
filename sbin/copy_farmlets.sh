2c3bfc09dcbfea7b7d31f064038f204532e95ad0
Alexander Moibenko <moibenko>
2007-08-17 21:45:06 +0000

    do not create framlets on config node

--- sbin/copy_farmlets.sh
+++ sbin/copy_farmlets.sh
@@ -8,5 +8,11 @@
 set -u  # force better programming and ability to use check for not set
 . /usr/local/etc/setups.sh
 setup enstore
-echo "Copying farmlets from $ENSTORE_CONFIG_HOST"
-enrcp $ENSTORE_CONFIG_HOST:/usr/local/etc/farmlets/* /usr/local/etc/farmlets
+this_host=`uname -n`
+if [ $this_host != $ENSTORE_CONFIG_HOST ];
+then
+    echo "Copying farmlets from $ENSTORE_CONFIG_HOST"
+    enrcp $ENSTORE_CONFIG_HOST:/usr/local/etc/farmlets/* /usr/local/etc/farmlets
+else:
+    echo "You need to create farmlets in $FARMLETS_DIR"
+fi
