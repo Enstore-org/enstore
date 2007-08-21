2e17183d18531fe70762af412cc050843db653d0
Michael Zalokar <zalokar>
2007-08-21 16:29:16 +0000

    Making delfile.py SDE ready.  It now obtains the trash directory by reading the /usr/etc/pnfsSetup file.

--- sbin/delfile.py
+++ sbin/delfile.py
@@ -1,58 +1,88 @@
 #!/usr/bin/env python
+
+###############################################################################
+#
+# $Id$
+#
+###############################################################################
+
 '''
+Update the deleted status in the Enstore DB, from information in the
+PNFS trash directory.  Parses /usr/etc/pnfsSetup to obtain the trash
+directory.
+
 This is a replacement for $ENSTORE_DIR/sbin/delfile
 '''
 
+# system imports
+import os
+import string
+import sys
+import traceback
+
+# enstore modules
 import option
 import file_clerk_client
 import volume_clerk_client
-import os
-import string
 import e_errors
-import sys
+import Trace
 
-vols = []
+PNFS_SETUP = "/usr/etc/pnfsSetup"
 
 def get_trash():
-    if os.environ.has_key('TRASH_CAN'):
-        return os.environ['TRASH_CAN']
-    elif os.path.exists('/usr/etc/pnfsSetup.sh'):
-        f=open('/usr/etc/pnfsSetup.sh', 'r')
-        while 1:
-            l = f.readline()
-            if l:
-                l=l[:-1]
-                if l.find('trash') == 0 # initial position
-                return os.path.join(l.split('=')[1],'4')
-            else:
-                break
-    else:
-        if os.uname()[1][:3] == 'rip':
-            return '/rip6a/pnfs/trash/4'
-        else:
-            return '/diska/pnfs/trash/4'
+    
+    #Historical note: This function used to look at the 'TRASH_CAN'
+    # environmental variable for the value to return.
+    
+    #We need to automatically detect if there is a pnfs server configured.
+    # If there is, then we need to find the trash value and return it.
+
+    try:
+        f_ps = open(PNFS_SETUP) #f_ps = File Pnfs Setup
+        d_ps = f_ps.readlines() #d_ps = Data Pnfs Setup
+        f_ps.close()
+    except (OSError, IOError), detail:
+        sys.stderr.write("Unable to access pnfsSetup: %s\n" % (str(detail),))
+        return None
+
+    for line in d_ps:
+        line = line.strip()
+        words = line.split("=")
+        if words[0] == "trash":
+            return os.path.join(words[1].strip(), "4")
+    
+    sys.stderr.write("No trash directory listed in %s.\n" % (PNFS_SETUP,))
+    return None
+
 
 def get_bfid(mf):
     try:
         f = open(mf)
         r = f.readlines()
         f.close()
-    except:
+    except (OSError, IOError):
         return None, None
 
     if len(r) > 8:
         return string.strip(r[0]), string.strip(r[8])
-    else:
-        return None, None
+    
+    return None, None
 
-if __name__ == '__main__':
+def main(intf):
     success = True
-    intf = option.Interface()
+    vols = []
+
+    if os.geteuid() != 0:
+        sys.stderr.write("Must be user root.\n")
+        return False
+
     fcc = file_clerk_client.FileClient((intf.config_host, intf.config_port))
     vcc = volume_clerk_client.VolumeClerkClient(fcc.csc)
     trash = get_trash()
     # print trash
     files = os.listdir(trash)
+
+    #Update the files.  And get a list of volumes at the same time.
     for i in files:
         fp = os.path.join(trash, i)
         vol, bfid = get_bfid(fp)
@@ -74,6 +104,7 @@ if __name__ == '__main__':
                     print 'can not delete', fp
                     success = False
 
+    #Touch the list of volumes.
     for i in vols:
         print 'touching', i, '...',
         result = vcc.touch(i)
@@ -84,5 +115,39 @@ if __name__ == '__main__':
             success = False
 
     if not success:
+        return 1
         # this will keep *-output file
+        #sys.exit(1)
+
+    return 0
+
+def do_work(intf):
+
+    Trace.init("DELFILE")
+
+    try:
+        exit_status = main(intf)
+    except (SystemExit, KeyboardInterrupt), msg:
+        Trace.log(e_errors.ERROR, "delfile aborted from: %s" % str(msg))
         sys.exit(1)
+    except:
+        #Get the uncaught exception.
+        exc, msg, tb = sys.exc_info()
+        #Print it to terminal.
+        traceback.print_exception( exc, msg, tb )
+        #Also, send it to the log file.
+        Trace.handle_error(exc, msg, tb)
+        
+        del tb #No cyclic references.
+        sys.exit(1)
+
+    sys.exit(exit_status)
+
+class DelfileInterface(option.Interface):
+    def valid_dictionaries(self):
+        return (self.help_options,)
+
+if __name__ == '__main__':
+    intf_of_delfile = DelfileInterface()
+
+    do_work(intf_of_delfile)
