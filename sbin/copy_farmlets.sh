d1acfa3211ce0ba4161fd4b4be3b095d452a8d59
Alexander Moibenko <moibenko>
2007-09-05 19:46:35 +0000

    create farmlets dir

--- sbin/copy_farmlets.sh
+++ sbin/copy_farmlets.sh
@@ -11,8 +11,12 @@ setup enstore
 this_host=`uname -n`
 if [ $this_host != $ENSTORE_CONFIG_HOST ];
 then
-    echo "Copying farmlets from $ENSTORE_CONFIG_HOST"
-    enrcp $ENSTORE_CONFIG_HOST:/usr/local/etc/farmlets/* /usr/local/etc/farmlets
+    if [ ! -d /usr/local/etc/farmlets ]
+    then
+	echo "Copying farmlets from $ENSTORE_CONFIG_HOST"
+	mkdir -p /usr/local/etc/farmlets
+	enrcp $ENSTORE_CONFIG_HOST:/usr/local/etc/farmlets/* /usr/local/etc/farmlets
+    fi
 else
     echo "You need to create farmlets in $FARMLETS_DIR"
 fi
