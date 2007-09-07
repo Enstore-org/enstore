8f571b51662cc055d7af9f8a388017de2e707e58
Alexander Moibenko <moibenko>
2007-09-07 14:32:31 +0000

    for fnal copy bashrc from config server

--- external_distr/create_enstore_environment.sh
+++ external_distr/create_enstore_environment.sh
@@ -11,7 +11,7 @@
 #set -u  # force better programming and ability to use check for not set
 if [ "${1:-}" = "-x" ] ; then set -xv; shift; fi
 if [ "${1:-}" = "-q" ] ; then export quiet=1; shift; else quiet=0; fi
-if [ "${1:-x}" = "fnal" ]; then export fnal=$1; shift; else fnal="";fi
+if [ "${1:-x}" = "fnal" ]; then export fnal=$1; shift; else fnal="x";fi
 if [ "`whoami`" != 'root' ]
 then
     echo You need to run this script as user "root"
@@ -25,21 +25,44 @@ ENSTORE_DIR=`rpm -ql enstore_sa | head -1`
 FTT_DIR=`rpm -ql ftt | head -1`
 ENSTORE_HOME=`ls -d ~enstore`
 
-if [ -n $fnal ]; then
+if [ $fnal = "fnal" ]; then
     rm -rf $ENSTORE_HOME/enstore
     ln -s $ENSTORE_DIR $ENSTORE_HOME/enstore
+    ENSTORE_CONFIG_HOST=`$ENSTORE_DIR/ups/chooseConfig`
+    kdestroy
+    KRB5CCNAME=/tmp/krb5cc_enstore_$$;export KRB5CCNAME
+    defaultDomain=".fnal.gov"
+
+    # we need the full domain name, if no domain is there, add default one on
+
+    if expr $this_host : '.*\.' >/dev/null;then 
+       thisHost=$this_host;
+    else 
+       thisHost=${this_host}${defaultDomain};
+    fi
+    kinit -k -t /local/ups/kt/enstorekt enstore/cd/${thisHost}
+    # change permissions for credentials file
+    cred_f=`echo $KRB5CCNAME | cut -f2 -d\:`
+    if [ $? -eq 0 ]; then
+	chmod 666 $cred_f
+    fi
 fi
 if [ ! -r $ENSTORE_HOME/site_specific/config/setup-enstore ]
 then
     # this allows to not run this script on remote nodes.
     $ENSTORE_DIR/external_distr/create_setup_file.sh $fnal
-else
-    source $ENSTORE_HOME/site_specific/config/setup-enstore
-    
 fi
+source $ENSTORE_HOME/site_specific/config/setup-enstore
 
 echo "Creating .bashrc"
-cp $ENSTORE_DIR/external_distr/.bashrc $ENSTORE_HOME
+if [ $fnal = "fnal" ];
+then 
+    echo "trying to get .bashrc configuration host"
+    scp -rp enstore\@$ENSTORE_CONFIG_HOST:$ENSTORE_HOME/.bashrc $ENSTORE_HOME
+else
+    echo "Creating .bashrc"
+    cp $ENSTORE_DIR/external_distr/.bashrc $ENSTORE_HOME
+fi
 chown enstore.enstore $ENSTORE_HOME/.bashrc
 
 if [ ! -d $ENSTORE_HOME/CRON ]
