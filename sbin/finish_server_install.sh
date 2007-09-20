5a840eeae245d730f13b59792420906ac704659e
Alexander Moibenko <moibenko>
2007-09-20 19:50:40 +0000

    some fixes

--- sbin/finish_server_install.sh
+++ sbin/finish_server_install.sh
@@ -5,10 +5,11 @@
 #
 ###############################################################################
 set -u  # force better programming and ability to use check for not set
+if [ "${1:-}" = "-x" ] ; then set -xv; shift; fi
 . /usr/local/etc/setups.sh
 setup enstore
 echo "installing enstore_html"
-rpm -U --force ftp://ssasrv1.fnal.gov/en/enstore_related/enstore_html-1.0-0.i386.rpm
+rpm -U --force --nodeps ftp://ssasrv1.fnal.gov/en/enstore_related/enstore_html-1.0-0.i386.rpm
 echo "installing postgres"
 rpm -U --force ftp://ssasrv1.fnal.gov/en/enstore_related/postgresql-libs-8.2.4-1PGDG.i686.rpm \
 ftp://ssasrv1.fnal.gov/en/enstore_related/postgresql-8.2.4-1PGDG.i686.rpm \
