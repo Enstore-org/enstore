f8ae30f4b54cd6529646b6b186dfc59c319591cf
Alexander Moibenko <moibenko>
2007-10-02 17:19:05 +0000

    create /pnfs

--- sbin/finish_server_install.sh
+++ sbin/finish_server_install.sh
@@ -66,6 +66,13 @@ if [ ! -d /usr/etc ];then mkdir /usr/etc;fi
 if [ ! -r /usr/etc/pnfsSetup ]; then cp ${ENSTORE_DIR}/etc/${pnfsSetup_file} /usr/etc/pnfsSetup; fi
 if [ ! -r /usr/etc/pnfsSetup.sh ]; then ln -s /usr/etc/pnfsSetup /usr/etc/pnfsSetup.sh; fi
 
+#create pnfs directory
+if [ ! -d /pnfs ];
+then
+    mkdir /pnfs
+    chmod 777 /pnfs
+fi
+
 echo "Enabling Enstore log directory"
 $ENSTORE_DIR/external_distr/extract_config_parameters.py log_server | cut -f1,2 -d\: --output-delimiter=" " > /tmp/log_conf.tmp
 while read f1 f2; do eval $f1=$f2; done < /tmp/log_conf.tmp
@@ -77,3 +84,4 @@ then
     chown -R enstore.enstore `dirname $log_file_path`
 fi
 
+#create pnfs directory
