4d15485faa36a5a9658e4a6dca420709b9836d8c
Alexander Moibenko <moibenko>
2007-10-10 14:51:12 +0000

    some additional fixes

--- src/library_manager.py
+++ src/library_manager.py
@@ -1018,7 +1018,8 @@ class LibraryManagerMethods:
                 elif self.tmp_rq:
                     rq = self.tmp_rq
                     Trace.trace(16,"next_work_any_volume: get from tmp_rq %s"%(rq,))
-                    rq, key = self.process_write_request(rq, requestor)
+                    if rq.work == "write_to_hsm":
+                        rq, key = self.process_write_request(rq, requestor)
         # check if this volume is ok to work with
         if rq:
             ## check if there are any additional restrictions for postponed request
