39f12fc96db5956e4712aa05c7e06ad55db030a7
Alexander Moibenko <moibenko>
2007-10-23 22:26:04 +0000

    mods

--- external_distr/finish_demo_server_install.sh
+++ external_distr/finish_demo_server_install.sh
@@ -7,19 +7,20 @@
 set -u  # force better programming and ability to use check for not set
 if [ "${1:-}" = "-x" ] ; then set -xv; shift; fi
 place="${1:-ftp://ssasrv1.fnal.gov/en/enstore_related}"
-psql_place=$place
+psql_place=/home/moibenko
 . /usr/local/etc/setups.sh
 setup enstore
 
 echo "installing enstore_html"
 /sbin/service httpd stop
-rpm -U --force --nodeps ${place}/enstore_html-1.0-0.i386.rpm
+#rpm -U --force --nodeps ${place}/enstore_html-1.0-0.i386.rpm
+rpm -U --force --nodeps /usr/src/redhat/RPMS/i386/enstore_html-1.0-0.i386.rpm
 
 rpm -q postgesql > /dev/null
 if [ $? -ne 0 ]; 
 then
     echo "installing postgres"
-rpm -U --force ${psql_place}/postgresql-libs-8.2.4-1PGDG.i686.rpm ${psql_place}/postgresql-8.2.4-1PGDG.i686.rpm ${psql_place}/postgresql-server-8.2.4-1PGDG.i686.rpm ${psql_place}/postgresql-devel-8.2.4-1PGDG.i686.rpm
+rpm -U --force ${psql_place}/postgresql-libs-8.1.10-1PGDG.i686.rpm ${psql_place}/postgresql-8.1.10-1PGDG.i686.rpm ${psql_place}/postgresql-server-8.1.10-1PGDG.i686.rpm ${psql_place}/postgresql-devel-8.1.10-1PGDG.i686.rpm
 fi
 
 echo "installing pnfs"
@@ -30,7 +31,8 @@ then
     if [ -x /etc/rc.d/init.d/postgres ]; then /etc/rc.d/init.d/postgres stop; fi
     #/etc/rc.d/init.d/postgresql stop
 fi
-rpm -U --force http://www.dcache.org/downloads/releases/pnfs-postgresql-3.1.10-7.i386.rpm
+echo "Installing pnfs"
+rpm -U --force --nodeps http://www.dcache.org/downloads/releases/pnfs-postgresql-3.1.10-7.i386.rpm
 # complete after install pnfs configuration
 # copy setup
 
@@ -44,6 +46,7 @@ then
     echo "Configuring this host to run postgres"
     /sbin/chkconfig postgresql on
     echo "Starting postges"
+    /sbin/service postgresql initdb
     /etc/init.d/postgresql start
     echo "Configuring this host to run pnfs server"
     /sbin/chkconfig pnfs add
@@ -52,17 +55,7 @@ then
     #/etc/init.d/pnfs start
 fi
 echo "Creating pnfsSetup"
-case $this_host in
-	stken*)
-	    pnfsSetup_file=stken-pnfsSetup
-	    ;;
-	d0en*)
-	    pnfsSetup_file=d0en-pnfsSetup
-	    ;;
-	cdfen*)
-	    pnfsSetup_file=cdfen-pnfsSetup
-	    ;;
-esac
+pnfsSetup_file=stken-pnfsSetup
 if [ ! -d /usr/etc ];then mkdir /usr/etc;fi
 if [ ! -r /usr/etc/pnfsSetup ]; then cp ${ENSTORE_DIR}/etc/${pnfsSetup_file} /usr/etc/pnfsSetup; fi
 if [ ! -r /usr/etc/pnfsSetup.sh ]; then ln -s /usr/etc/pnfsSetup /usr/etc/pnfsSetup.sh; fi
@@ -75,7 +68,7 @@ then
 fi
 
 echo "Enabling Enstore log directory"
-$ENSTORE_DIR/external_distr/extract_config_parameters.py log_server | cut -f1,2 -d\: --output-delimiter=" " > /tmp/log_conf.tmp
+$ENSTORE_DIR/external_distr/extract_config_parameters.py log_server | grep log_file_path | cut -f1,2 -d\: --output-delimiter=" " > /tmp/log_conf.tmp
 while read f1 f2; do eval $f1=$f2; done < /tmp/log_conf.tmp
 rm -rf /tmp/log_conf.tmp
 
