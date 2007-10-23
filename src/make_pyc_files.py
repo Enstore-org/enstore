dfe655957cce7a47ab60bccee5e6d57c2be9a76b
Alexander Moibenko <moibenko>
2007-10-23 13:34:26 +0000

    creates pyc files for a give directory

---
+++ src/make_pyc_files.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+###############################################################################
+# $Id:
+#
+##############################################################################
+import os
+import sys
+
+
+s=sys.argv[0]
+a=s.split('/')
+b = a[len(a)-1]
+myname=b.split('.')[0]
+print "my name", myname
+
+dir_list=os.listdir(".")
+
+for f in dir_list:
+    a = f.split('.')
+    if a[len(a)-1] == 'py':
+        #print f
+        del a[len(a)-1]
+        s=''
+        module = s.join(a)
+        if myname != module:
+            print "will import", module
+            try:
+                __import__(module)
+            except:
+                pass
+            
+        
