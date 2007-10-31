8b06a13ec67795d8e6b757f0f4d67c188dd12dbe
Alexander Moibenko <moibenko>
2007-10-31 23:06:37 +0000

    takes rpm version and release as arguments. enter version like x.x.x and release as x

--- external_distr/build_rpm.sh
+++ external_distr/build_rpm.sh
@@ -7,6 +7,9 @@
 
 
 set -u  # force better programming and ability to use check for not set
+EVersion="${1:-1.0.1}"
+ERelease="${2:-9}"
+
 export CVSROOT=hppccvs@cdcvs.fnal.gov:/cvs/hppc
 if [ "`whoami`" != 'root' ]
 then
@@ -17,15 +20,32 @@ cd /tmp
 mkdir enstore_build
 cd enstore_build
 cvs co -r production enstore
+
+#cvs does no like dots
+cvs_EVersion=`echo '1.0.1' | sed -e "s/\./_/g"`
+
+cvs tag -F -r production ENSTORE_RPM_${cvs_EVersion}_${ERelease} enstore
+
 cd enstore
 tar czf enstore.tgz *
 cp -f enstore.tgz /usr/src/redhat/SOURCES
 rm -f enstore.tgz  
 tar czf enstore_sa.tgz *
+
 cp -f enstore_sa.tgz /usr/src/redhat/SOURCES
 rm -f enstore_sa.tgz
-cp -f spec/enstore.spec /usr/src/redhat/SPECS
-cp -f spec/enstore_sa.spec /usr/src/redhat/SPECS
+rm -f /usr/src/redhat/SPECS/enstore.spec
+rm -f /usr/src/redhat/SPECS/enstore_sa.spec
+#cp -f spec/enstore.spec /usr/src/redhat/SPECS
+sed "
+/Version:/c Version: $EVersion
+/Release:/c Release: $ERelease
+" < spec/enstore.spec > /usr/src/redhat/SPECS/enstore.spec
+#cp -f spec/enstore_sa.spec /usr/src/redhat/SPECS
+sed "
+/Version:/c Version: $EVersion
+/Release:/c Release: $ERelease
+" < spec/enstore_sa.spec > /usr/src/redhat/SPECS/enstore_sa.spec
 rpmbuild -ba /usr/src/redhat/SPECS/enstore.spec
 rpmbuild -ba /usr/src/redhat/SPECS/enstore_sa.spec
 
