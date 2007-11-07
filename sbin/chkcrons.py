9e877219112e0ced47e3600d2bbbe2bd33786a95
Chih Hao Huang <huangch>
2007-11-07 21:36:47 +0000

    check monitored_nodes

--- sbin/chkcrons.py
+++ sbin/chkcrons.py
@@ -11,10 +11,11 @@ if not os.access(edata.crons_dir, os.F_OK):
 
 names_and_nodes = edata.get_all_names_and_nodes()
 for i in names_and_nodes:
-	print i[0], i[1]
-	dir = os.path.join(edata.crons_dir, i[1].split('.')[0])
-	# check if the directory exists
-	if not os.access(dir, os.F_OK):
-		os.makedirs(dir)
-	res = edata.get_result(i[0], i[1], ecron_util.one_week_ago())
-	edata.plot(os.path.join(dir, i[0]), i[0], res)
+	if i[1].split(".")[0] in edata.monitored_nodes:
+		print i[0], i[1]
+		dir = os.path.join(edata.crons_dir, i[1].split('.')[0])
+		# check if the directory exists
+		if not os.access(dir, os.F_OK):
+			os.makedirs(dir)
+		res = edata.get_result(i[0], i[1], ecron_util.one_week_ago())
+		edata.plot(os.path.join(dir, i[0]), i[0], res)
