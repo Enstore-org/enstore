d65b95a5624f63ae9066bef1b46b74113c2a2261
Chih Hao Huang <huangch>
2007-11-16 20:56:21 +0000

    revert to 1.5

--- src/enstore_pg.py
+++ src/enstore_pg.py
@@ -7,27 +7,27 @@
 ###############################################################################
 
 import enstore_constants
-#import pg
-import accounting_client
-import e_errors
-import Trace
+import pg
 
 def acc_encp_lines(csc, numEncps=100):
-    acc = accounting_client.accClient(csc)
-    r = acc.last_xfers(numEncps)
-    if r and r['status'][0] == e_errors.OK:
-        res=r['result']
-    else:
-        Trace.log(e_errors.WARNING, "Can not get Last xfers: %s"%(r,))
-        res = []
-    r = acc.last_bad_xfers(numEncps)
-    if r and r['status'][0] == e_errors.OK:
-                res_err=r['result']
-    else:
-       Trace.log(e_errors.WARNING, "Can not get Last bad xfers: %s"%(r,))
-       res_err = [] 
+    acc = csc.get(enstore_constants.ACCOUNTING_SERVER)
+    # connect to the db
+    try:
+        dbport = acc.get('dbport', None)
+        if dbport:
+            db = pg.DB(host=acc['dbhost'], port=dbport, dbname=acc['dbname'])
+        else:
+            db = pg.DB(host=acc['dbhost'], dbname=acc['dbname'])
+    except pg.Error, detail:
+        # could not connect to the db
+        print " %s  %s"%(pg.Error, detail)
+        return []
+    res = db.query("select * from encp_xfer order by date desc limit %s;"%(numEncps,))
+    res_err = db.query("select * from encp_error order by date desc limit %s;"%(numEncps,))
     # combine the 2 lists and take the top numEncps entries to return
-    res_total = res + res_err
+    res_total = res.getresult() + res_err.getresult()
     res_total.sort()
+    # close connection to the db
+    db.close()
     # return the results as a list, returning only the last numEncps elements.
     return res_total[-numEncps:]
