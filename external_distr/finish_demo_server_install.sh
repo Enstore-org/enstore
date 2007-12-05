452c430138c4377f3f41dfe9f21316336932ee85
Alexander Moibenko <moibenko>
2007-12-05 20:25:35 +0000

    mods

--- external_distr/finish_demo_server_install.sh
+++ external_distr/finish_demo_server_install.sh
@@ -20,7 +20,16 @@ rpm -q postgesql > /dev/null
 if [ $? -ne 0 ]; 
 then
     echo "installing postgres"
-rpm -U --force ${psql_place}/postgresql-libs-8.1.10-1PGDG.i686.rpm ${psql_place}/postgresql-8.1.10-1PGDG.i686.rpm ${psql_place}/postgresql-server-8.1.10-1PGDG.i686.rpm ${psql_place}/postgresql-devel-8.1.10-1PGDG.i686.rpm
+    echo "installing postgres"
+    rpm -U --force ${place}/postgresql-libs-8.2.4-1PGDG.i686.rpm ${place}/postgresql-8.2.4-1PGDG.i686.rpm ${place}/postgresql-server-8.2.4-1PGDG.i686.rpm ${place}/postgresql-devel-8.2.4-1PGDG.i686.rpm
+    rm -f /tmp/postgresql
+    mv /etc/rc.d/init.d/postgresql /tmp/postgresql
+    echo "Modifying dbuser name"
+    sed -e 's/postgres:postgres/enstore:enstore/' -e 's/l postgres/l enstore/' /tmp/postgresql > /etc/init.d/postgresql 
+    chmod a+x /etc/init.d/postgresql
+    echo "Change shared memory settings"
+    echo "kernel.shmmax=268435456" >> /etc/sysctl.conf
+    echo "kernel.shmall=268435456" >> /etc/sysctl.conf
 fi
 
 echo "installing pnfs"
@@ -87,3 +96,7 @@ then
 fi
 
 #create pnfs directory
+
+# create enstore databases
+echo "Creating enstore databases"
+$ENSTORE_DIR/external_dist
