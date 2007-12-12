7b81ae68b9950fb5ef4d2a408283f75849da6e1f
Alexander Moibenko <moibenko>
2007-12-12 22:24:48 +0000

    mods

--- external_distr/create_demo_enstore_environment.sh
+++ external_distr/create_demo_enstore_environment.sh
@@ -54,23 +54,42 @@ echo "Copying $ENSTORE_DIR/external_distr/setups.sh to /usr/local/etc"
 if [ ! -d "/usr/local/etc" ];
 then
     mkdir -p /usr/local/etc
+    install=1
 else
-    d=`date +%F.%R`
-    mv -f /usr/local/etc/setups.sh /usr/local/etc/setups.sh.$d
+install=0
+    if [ -r "/usr/local/etc/setups.sh" ]; then
+	grep "e_home=" /usr/local/etc/setups.sh
+	if [ $? -ne 0 ]; then
+	    # real ups setup file
+	    d=`date +%F.%R`
+	    mv -f /usr/local/etc/setups.sh /usr/local/etc/setups.sh.$d
+	    install=1
+	else
+	    # check if e_home is empty and if yes install correct value
+	    s=` grep "e_home=" /usr/local/etc/setups.sh | sed -e "s/^ *//" | sed -e "s/^[\t] *//" | cut -f2 -d"="` > /dev/null 2>&1
+	    if [ -z $s ]; then
+		rm -rf /usr/local/etc/setups.sh
+		install=1
+	    fi	
+	fi
+    else
+	install=1
+    fi
 fi
+
 if [ ! -f $ENSTORE_CONFIG_FILE -a $this_host = $ENSTORE_CONFIG_HOST ];
 then
     echo "will install a minimal enstore configuration file: ${ENSTORE_DIR}/etc/minimal_enstore.conf"
     echo "it can be replased later"
     cp -p ${ENSTORE_DIR}/etc/minimal_enstore.conf $ENSTORE_CONFIG_FILE
+else
+    sed -e "s?e_home=?e_home=$ENSTORE_HOME?" $ENSTORE_DIR/external_distr/setups.sh > /usr/local/etc/setups_rpm.sh
+    ln -s /usr/local/etc/setups_rpm.sh /usr/local/etc/setups.sh
+    host_name=`uname -n | cut -f1 -d\.`
+    sed -e "s?the_host=?the_host=\'$this_host\'?" $ENSTORE_CONFIG_FILE | sed -e "s?for_map=?for_map=\'$host_name\'?" > /tmp/enstore_config_file
+    rm $ENSTORE_CONFIG_FILE
+    mv /tmp/enstore_config_file $ENSTORE_CONFIG_FILE
 fi
-
-sed -e "s?e_home=?e_home=$ENSTORE_HOME?" $ENSTORE_DIR/external_distr/setups.sh > /usr/local/etc/setups_rpm.sh
-ln -s /usr/local/etc/setups_rpm.sh /usr/local/etc/setups.sh
-host_name=`uname -n | cut -f1 -d\.`
-sed -e "s?the_host=?the_host=\'$this_host\'?" $ENSTORE_CONFIG_FILE | sed -e "s?for_map=?for_map=\'$host_name\'?" > /tmp/enstore_config_file
-rm $ENSTORE_CONFIG_FILE
-mv /tmp/enstore_config_file $ENSTORE_CONFIG_FILE
 
 echo ${host_name} > ${FARMLETS_DIR}/${host_name}
 echo ${host_name} > ${FARMLETS_DIR}/enstore
