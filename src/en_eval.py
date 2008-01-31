0a36208980e00cbde0ffdfa370977fb573c66506
Michael Zalokar <zalokar>
2008-01-31 17:15:13 +0000

    Patched DoS security hole when a zero length UDP datagram was received.

--- src/en_eval.py
+++ src/en_eval.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+# $Id$
+
+# Wrapper code for builtin eval() to run in restricted mode/environment.
+
+# system imports
 import re
 import compiler
+import sys
 
 # looking for "CallFunc("
 re_CallFunc = re.compile("CallFunc\(")
@@ -16,14 +22,36 @@ def en_eval(expr, debug=False):
 	# reject anything that is NOT a string
 	if type(expr) != type(""):
 		if debug:
-			print "Error: not a string type"
+			sys.stderr.write("en_eval Error: not a string type\n")
 		return None
 
 	# reject function invocation
 	if re_CallFunc.search(str(compiler.parse(expr).node.nodes)) != None:
 		if debug:
-			print "Error: function invocation"
+			sys.stderr.write("en_eval Error: function invocation\n")
 		return None
 
+	# reject empty UDP datagrams.
+	#
+	## On Wednesday, January 30th 2008, it was discovered that 
+	## "intrusion detection" software called samhain would send an
+	## empty UDP datagram to open UDP sockets.  The revalation that
+	## that any hacker could initiate a denial of service attack
+	## on Enstore inspired this additional error handling.  Otherwise,
+	## a traceback occurs disrupting operations and unnecessarily
+	## floods the log server with traceback messages.  MZ
+	if expr == "":
+		if debug:
+			sys.stderr.write("en_eval Error: empty UDP datagram ignored\n")
+		return None
+ 
+
 	# no access to globals nor locals
-	return eval(expr, {}, {})
+	try:
+		val = eval(expr, {}, {})
+	except SyntaxError, msg:
+		if debug:
+			sys.stderr.write("en_eval Error: %s parsing string: %s\n" % (str(msg), expr))
+		return None
+
+	return val
