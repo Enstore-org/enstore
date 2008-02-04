5a600e1720171615fc8b231bb1f7c29627cef2a7
Michael Zalokar <zalokar>
2008-02-04 21:16:15 +0000

    Define __get_callback() that is used by get_default_callback() and get_callback().

--- src/udp_common.py
+++ src/udp_common.py
@@ -10,32 +10,64 @@
 import socket
 import sys
 import exceptions
+#import rexec
+import errno
 
 # enstore imports
 import host_config
 import cleanUDP
 import Trace
 import e_errors
+import Interfaces
+
+def __get_callback(host, port):
+    sock = cleanUDP.cleanUDP(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.socket.bind((host, port))
+    except socket.error, msg:
+        if msg.args[0] == errno.EADDRNOTAVAIL:
+            error_message = "%s: %s" % (msg.args[1], host)
+            
+            #If we get EADDRNOTAVAIL, we should check to see if the interfaces
+            # list returns information about the defualt IP.  If the
+            # default IP is not currently configured then there is an
+            # inconsistency between /etc/hosts and ifconfig.
+            interfaces_dict = Interfaces.interfacesGet()
+            for intf_dict in interfaces_dict.values():
+                if intf_dict['ip'] == host:
+                    break
+            else:
+                error_message = "%s\n%s" % (error_message,
+                    "Check /etc/hosts and ifconfig -a"
+                    " for inconsistent information.")
+        elif msg.args[0] == errno.EADDRINUSE:
+            #We should include the address information since we know it
+            # is currently in use by another process.
+            error_message = "%s: %s" % (msg.args[1], (host, port))
+        else:
+            error_message = msg.args[1]
+
+        sys.stdout.write("%s\n" % error_message)
+        sys.exit(1)
+        
+    host, port = sock.socket.getsockname()
+    return host, port, sock
+
+
 from en_eval import en_eval
 
 # try to get a port from a range of possibilities
 def get_default_callback(use_port=0):
     host = host_config.get_default_interface()['ip']
-    sock = cleanUDP.cleanUDP(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.socket.bind((host, use_port))
-    host, port = sock.socket.getsockname()
-    return host, port, sock
+    return __get_callback(host, use_port)
 
 # try to get a port from a range of possibilities
 def get_callback(use_host=None, use_port=0):
-    if use_host in [None, ""]:
-        host = host_config.choose_interface()['ip']
-    else:
+    if use_host not in [None, ""]:
         host = use_host
-    sock = cleanUDP.cleanUDP(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.socket.bind((host, use_port))
-    host, port = sock.socket.getsockname()
-    return host, port, sock
+    else:
+        host = host_config.choose_interface()['ip']
+    return __get_callback(host, use_port)
 
 ### These function deal with encoding and decoding the raw bytes from
 ### udp messages.
