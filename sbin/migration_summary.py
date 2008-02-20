9dc48f5c4f6634208cc86cded9c978b930894f6f
Chih Hao Huang <huangch>
2008-02-20 06:05:06 +0000

    default output file

--- sbin/migration_summary.py
+++ sbin/migration_summary.py
@@ -4,11 +4,15 @@ import pg
 import configuration_client
 import option
 import time
+import sys
 
 intf = option.Interface()
 csc = configuration_client.ConfigurationClient((intf.config_host, intf.config_port))
 
 dbinfo = csc.get('database')
+outfile = csc.get('crons').get('html_dir')+'/tape_inventory/MIGRATION_SUMMARY'
+sys.stdout = open(outfile, 'w')
+
 db = pg.DB(host=dbinfo['dbhost'], port=dbinfo['dbport'], dbname=dbinfo['dbname'])
 print time.ctime(time.time())
 print
