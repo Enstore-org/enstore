56556026107991e76bc77eb6e7d15894c8e12d2e
Alexander Moibenko <moibenko>
2008-06-06 14:05:06 +0000

    put a volume into suspect list only if its system inhibit is none

--- src/library_manager.py
+++ src/library_manager.py
@@ -2958,10 +2958,12 @@ class LibraryManager(dispatching_worker.DispatchingWorker,
         
         # update suspected volume list if error source is ROBOT or TAPE
         error_source = mticket.get('error_source', 'none')
+        vol_status = mticket.get('volume_status', 'none')
         if error_source in ("ROBOT", "TAPE"):
-            vol = self.update_suspect_vol_list(mticket['external_label'], 
-                                    mticket['mover'])
-            Trace.log(e_errors.INFO,"mover_error updated suspect volume list for %s"%(mticket['external_label'],))
+            if vol_status and vol_status[0][0] == 'none':
+                vol = self.update_suspect_vol_list(mticket['external_label'], 
+                                                   mticket['mover'])
+                Trace.alarm(e_errors.INFO,"mover_error updated suspect volume list for %s"%(mticket['external_label'],))
             if vol:
                 # need a special processing for FTT_EBLANK. For 9940A tape drives
                 # it is mainly a firmware bug, but we need to make tape to not
