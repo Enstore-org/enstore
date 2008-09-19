9c232e6882ad4a3c143c5e71dee39b979b2f3dc7
Alexander Moibenko <moibenko>
2008-09-19 20:12:09 +0000

    some fixes

--- test/torture/write_test.sh
+++ test/torture/write_test.sh
@@ -5,41 +5,65 @@
 #
 #############################################################
 
-
+pnfs_path=''
+data=''
+ff_width=3
+batch=10
 if [ "${1:-}" = "-x" ] ; then set -xv; shift; fi
+if [ "${1:-}" = "-p" ] ; then shift; pnfs_path=$1; shift; fi
+if [ "${1:-}" = "-d" ] ; then shift; data=$1; shift; fi
+if [ "${1:-}" = "-f" ] ; then shift; ff_width=$1; shift; fi
+if [ "${1:-}" = "-b" ] ; then shift; batch=$1; shift; fi
 
-pnfs_path="${1:-/pnfs/cdfen/test/sl8500/test/}"
-sizes="1 10 50 100 200 500 800 1000 2000 3000 4000 5000"
-#sizes="1 2 3 4 5 6 7 8 9 10 11 12"
+if [ -z $pnfs_path ]; then pnfs_path="/pnfs/cdfen/test/sl8500/test"; fi
+if [ ! -z $data ]; then data=${data}/; fi
+#sizes="1 10 50 100 200 500 800 1000 2000 3000 4000 5000"
+sizes="3 4 5 6 7 8 9 10 11 12"
 
-
+if [ ! -d $data ]; then mkdir -p $data;fi
+#echo  $data*$$.data
+files=`ls $data*.data`
+host=`hostname | cut -f 1 -d \.`
+fsz=${#files}
+date=`date +%F`
+#echo fsz $fsz
 for s in $sizes
-do
-  if [ ! -e $pnfs_path/$s ] 
-  then 
-      mkdir $pnfs_path/$s
-      cd $pnfs_path/$s 
+  do
+  if [ ! -e $pnfs_path/$date/${host}/$s ] 
+      then 
+      mkdir -p $pnfs_path/$date/${host}/${s}
+      cd $pnfs_path/$date/${host}/$s 
       enstore pnfs --file_family=test
-      enstore pnfs --file_family_width=3
+      enstore pnfs --file_family_width=$ff_width
       cd - > /dev/null 2>&1
   fi
+done
 
-  i=0
-  while [ $i -lt 10 ] 
+if [ $fsz -eq 0 ]; then
+      for s in $sizes
+      do
+	i=1
+	while [ $i -lt $batch ] 
+	do
+	  #name=test_${s}_`date +"%s"`.data
+	  name=${host}_test_${s}_`date +"%s"`_$$.data
+	  sz=`~/enstore/test/torture/gauss ${s}|awk '{ print $1}'`
+	  #echo "$$ $s $sz "
+	  ~/enstore/test/torture/createfile $sz $data$name 
+	  let i=i+1
+	  sleep 1
+	done 
+      done
+fi
+files=`ls $data*.data`
+
+for s in $sizes
   do
-    #name=test_${s}_`date +"%s"`.data
-    name=`hostname | cut -f 1 -d \.`_test_${s}_`date +"%s"`_$$.data
-    sz=`./gauss ${s}|awk '{ print $1}'`
-    echo "$$ $s $sz "
-    ./createfile $sz $name 
-    let i=i+1
-    sleep 1 
-  done
-  files=`ls *$$.data`
+  files=`ls $data/${host}_test_${s}*.data`
   for f in $files
-  do
-    encp --threaded  $f $pnfs_path/$s &
-  done
-  wait
-  rm $files
-done 
+    do
+      encp --threaded  $f $pnfs_path/$date/${host}/$s/`basename $f`.$$ &
+    done
+done
+wait
+#  rm $files
