5b00cf820662f5a8c8738834cb4b9ce3a7417a38
Michael Zalokar <zalokar>
2008-09-25 20:54:19 +0000

    Rewrote the inventory and inventory summary sgi scripts to not display a hard-coded list of (special) files located in the tape_inventory directory. Instead the contents of the tape_inventory directory are searched, and every file that does not match a volume name is automatically put into the list of (special) files to display.  The summary version imports the non-summary version and uses what it needs; thus avoiding a lot of duplicate code.

--- src/enstore_show_inv_summary_cgi.py
+++ src/enstore_show_inv_summary_cgi.py
@@ -1,75 +1,32 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import string
-import socket
+import enstore_functions2
+import configuration_client
+import enstore_show_inventory_cgi
 
-inv_dir = "/enstore/tape_inventory"
+def main():
+    # Obtain the correct values for ENSTORE_CONFIG_HOST and ENSTORE_CONFIG_PORT
+    # if they are not already available.
+    enstore_show_inventory_cgi.get_environment()
 
-host = string.split(socket.gethostname(), '.')[0]
-if host[:2] == "d0":
-	cluster = "d0en"
-elif host[:3] == "stk":
-	cluster = "stken"
-elif host[:3] == "cdf":
-	cluster = "cdfen"
-else:
-	cluster = host
+    #Get the configuration server client.
+    config_host = enstore_functions2.default_host()
+    config_port = enstore_functions2.default_port()
+    csc = configuration_client.ConfigurationClient((config_host, config_port))
+   
+    #Get information.
+    (special, cluster) = enstore_show_inventory_cgi.setup(csc)
 
-special = ['TOTAL_BYTES_ON_TAPE', 'VOLUMES', 'VOLUMES_DEFINED.html', 'VOLUMES_TOO_MANY_MOUNTS', 'VOLUME_QUOTAS', 'VOLUME_SIZE', 'LAST_ACCESS', 'NOACCESS', 'CLEANING', 'VOLUME_CROSS_CHECK', 'COMPLETE_FILE_LISTING', 'DECLARATION_ERROR', 'MIGRATED_VOLUMES', 'RECYCLABLE_VOLUMES', 'WRITE_PROTECTION_ALERT', 'WEEKLY_SUMMARY', 'TAB_FLIPPING_WATCH']
+    #Set the list (yes, it is really a dictionary) of volumes to empty for
+    # the summary.
+    catalog = {}
 
-if cluster == "d0en":
-	special.append('TAB_FLIPPING_WATCH-samlto2')
-	special.append('TAB_FLIPPING_WATCH-D0-LTO4G1')
-	special.append('TAB_FLIPPING_WATCH-D0-LTO4F1')
-	special.append('STK-VOLUMES.html')
-	special.append('AML2-VOLUMES.html')
-	special.append('SL8500G1-VOLUMES.html')
-	special.append('SL8500F1-VOLUMES.html')
-	special.append('MIGRATION_SUMMARY.html')
-elif cluster == "stken":
-	special.append('TAB_FLIPPING_WATCH-SL8500G1')
-	special.append('STK-VOLUMES.html')
-	special.append('SL8500-VOLUMES.html')
-	special.append('MIGRATION_STATUS')
-	special.append('QUOTA_ALERT')
-	special.append('VOLUME_QUOTAS_UPDATE')
-	special.append('CMS_VOLUMES_WITH_ONLY_DELETED_FILES')
-elif cluster == "cdfen":
-	special.append('TAB_FLIPPING_WATCH-CDF-LTO3')
-	special.append('STK-VOLUMES.html')
-	special.append('STKD0-VOLUMES.html')
-	special.append('SL8500-VOLUMES.html')
+    #
+    #Print the html output.
+    #
+    enstore_show_inventory_cgi.print_html(catalog, special, cluster)
+    
 
-# in the beginning ...
+if __name__ == '__main__':
 
-print "Content-type: text/html"
-print
-
-# taking care of the header
-
-print '<html>'
-print '<head>'
-print '<title>Tape Inventory Summary on '+cluster+'</title>'
-print '</head>'
-print '<body bgcolor="#ffffd0">'
-print '<font size=7 color="#ff0000">Enstore Tape Inventory Summary on '+cluster+'</font>'
-print '<hr>'
-
-# handle special files
-
-print '<font size=4>'
-print '<p>'
-for i in special:
-	if i == 'COMPLETE_FILE_LISTING':
-		print '<a href="enstore_file_listing_cgi.py">', string.split(i, '.')[0], '</a>&nbsp;&nbsp;'
-	else:
-		print '<a href="'+os.path.join(inv_dir, i)+'">', string.split(i, '.')[0], '</a>&nbsp;&nbsp;'
-	print '<br>'
-print '<p><a href="'+inv_dir+'">Raw Directory Listing</a>'
-print '</font>'
-
-# the end
-print '</body>'
-print '</html>'
+    main()
--- src/enstore_show_inventory_cgi.py
+++ src/enstore_show_inventory_cgi.py
@@ -3,99 +3,203 @@
 import os
 import sys
 import string
-import socket
+
+import enstore_functions2
+import enstore_functions3
+import configuration_client
+import info_client
+import e_errors
 
 inv_dir = "/enstore/tape_inventory"
 
-host = string.split(socket.gethostname(), '.')[0]
-if host[:2] == "d0":
-	cluster = "d0en"
-elif host[:3] == "stk":
-	cluster = "stken"
-elif host[:3] == "cdf":
-	cluster = "cdfen"
-else:
-	cluster = host
+# Obtain the correct values for ENSTORE_CONFIG_HOST and ENSTORE_CONFIG_PORT
+# if they are not already available.
+def get_environment():
+    #This is a very messy way to determine the host and port of the
+    # configuration server.  But at least it is in only one place now.
+    if os.environ.get('ENSTORE_CONFIG_HOST', None) == None:
+        cmd = ". /usr/local/etc/setups.sh; setup enstore; echo $ENSTORE_CONFIG_HOST; echo $ENSTORE_CONFIG_PORT"
+        pfile = os.popen(cmd)
+        config_host = pfile.readline().strip()
+        if config_host:
+            os.environ['ENSTORE_CONFIG_HOST'] = config_host
+        else:
+            print "Unable to determine ENSTORE_CONFIG_HOST."
+            sys.exit(1)
+        config_port = pfile.readline().strip()
+        if config_port:
+            os.environ['ENSTORE_CONFIG_PORT'] = config_port
+        else:
+            print "Unable to determine ENSTORE_CONFIG_PORT."
+            sys.exit(1)
+        pfile.close()
+        
+def setup(csc):
 
-special = ['TOTAL_BYTES_ON_TAPE', 'VOLUMES', 'VOLUMES_DEFINED.html', 'VOLUMES_TOO_MANY_MOUNTS', 'VOLUME_QUOTAS', 'VOLUME_SIZE', 'LAST_ACCESS', 'NOACCESS', 'CLEANING', 'VOLUME_CROSS_CHECK', 'COMPLETE_FILE_LISTING', 'DECLARATION_ERROR', 'MIGRATED_VOLUMES', 'RECYCLABLE_VOLUMES', 'WRITE_PROTECTION_ALERT', 'WEEKLY_SUMMARY', 'TAB_FLIPPING_WATCH']
+    #Determine the local inventory directory.
+    inventory_config_dict = csc.get("inventory", 3, 3)
+    raw_inv_dir = None
+    if e_errors.is_ok(inventory_config_dict):
+        raw_inv_dir = inventory_config_dict.get("inventory_rcp_dir", "")
+    if not raw_inv_dir:
+        print "Unable to determine inventory | inventory_rcp_dir from config file."
+        sys.exit(1)
+    #Remove the hostname that is at the beginning of this string.
+    raw_inv_dir = string.join(raw_inv_dir.split(":")[1:], "")
 
-if cluster == "d0en":
-	special.append('TAB_FLIPPING_WATCH-samlto2')
-	special.append('TAB_FLIPPING_WATCH-D0-LTO4G1')
-	special.append('TAB_FLIPPING_WATCH-D0-LTO4F1')
-	special.append('STK-VOLUMES.html')
-	special.append('AML2-VOLUMES.html')
-	special.append('SL8500G1-VOLUMES.html')
-	special.append('SL8500F1-VOLUMES.html')
-	special.append('MIGRATION_SUMMARY.html')
-elif cluster == "stken":
-	special.append('TAB_FLIPPING_WATCH-SL8500G1')
-	special.append('STK-VOLUMES.html')
-	special.append('SL8500-VOLUMES.html')
-	special.append('MIGRATION_STATUS')
-	special.append('QUOTA_ALERT')
-	special.append('VOLUME_QUOTAS_UPDATE')
-	special.append('CMS_VOLUMES_WITH_ONLY_DELETED_FILES')
-elif cluster == "cdfen":
-	special.append('TAB_FLIPPING_WATCH-CDF-LTO3')
-	special.append('STK-VOLUMES.html')
-	special.append('STKD0-VOLUMES.html')
-	special.append('SL8500-VOLUMES.html')
+    #Obtain the contents of the local inventory directory.
+    special = []
+    dir_contents = os.listdir(raw_inv_dir)
+    if not dir_contents:
+            sys.stderr.write("No tape_inventory directory contents found.\n")
+            sys.exit(1)
 
-catalog = {}
 
-cmd = '. /usr/local/etc/setups.sh; setup enstore; enstore info --labels'
+    #Determine the correct name for the cluster.  This is useful when
+    # you have more than one Enstore system.
+    crons_config_dict = csc.get("crons", 3, 3)
+    cluster = None
+    if e_errors.is_ok(crons_config_dict):
+        cluster = crons_config_dict.get("enstore_name", "")
 
-for i in os.popen(cmd).readlines():
-	f = string.strip(i)
-	if f[-8:] != '.deleted':
-		prefix = f[:3]
-		if catalog.has_key(prefix):
-			catalog[prefix].append(f)
-		else:
-			catalog[prefix] = [f]
+    #Loop over the contents of the directory looking for non-external_label
+    # style names.
+    dir_contents.sort()
+    special = []
+    for fname in dir_contents:
+            if fname[0] == ".":
+                    continue #skip hidden files.
+            if not enstore_functions3.is_volume(fname):
+                    #full_path = os.path.join(raw_inv_dir, fname)
+                    special.append(fname)
 
-# in the beginning ...
+    return (special, cluster)
 
-print "Content-type: text/html"
-print
+def get_volumes(csc):
+    #Get the information server client.
+    ifc = info_client.infoClient(csc, rcv_timeout = 3, rcv_tries = 3)
 
-# taking care of the header
+    ###Build up the listing of all volumes currently defined to Enstore.
+    catalog = {}
+    #Only get here when the full inventory needs to be displayed.
+    ticket = ifc.get_vol_list()
+    if e_errors.is_ok(ticket):
+        for i in ticket['volumes']:
+            f = string.strip(i)
+            if f[-8:] != '.deleted':
+                    prefix = f[:3]
+                    if catalog.has_key(prefix):
+                            catalog[prefix].append(f)
+                    else:
+                            catalog[prefix] = [f]
 
-print '<html>'
-print '<head>'
-print '<title>Tape Inventory on '+cluster+'</title>'
-print '</head>'
-print '<body bgcolor="#ffffd0">'
-print '<font size=7 color="#ff0000">Enstore Tape Inventory on '+cluster+'</font>'
-print '<hr>'
+    return catalog
 
-# handle special files
+#If catalog is an empty directory, then display just the summary.  Otherwise,
+# catalog should be a dictionary sorted into groups based on the first
+# few letters of the volume.  enstore_show_inv_summary_cgi.py passes an
+# empty dictionary here to generate the summary page.
+#
+#Special is a list of fnames in the tape_inventory directory that are not
+# volume names.
+#
+#Cluster is the name of the current Enstore system.
+def print_html(catalog, special, cluster):
+    
+    # in the beginning ...
 
-print '<p>'
-for i in special:
-	if i == 'COMPLETE_FILE_LISTING':
-		print '<a href="enstore_file_listing_cgi.py">', string.split(i, '.')[0], '</a>&nbsp;&nbsp;'
-	else:
-		print '<a href="'+os.path.join(inv_dir, i)+'">', string.split(i, '.')[0], '</a>&nbsp;&nbsp;'
-print '<p><a href="'+inv_dir+'">Raw Directory Listing</a>'
-print '<hr>'
-print '<p>'
-print '<h2><font color="#aa0000">Index</font></h2>'
-keys = catalog.keys()
-keys.sort()
+    print "Content-type: text/html"
+    print
 
-for i in keys:
-	print '<a href=#'+i+'>'+i+'</a>&nbsp;&nbsp;'
+    # taking care of the header
 
-for i in keys:
-	print '<hr>'
-	print '<p>'
-	print '<h2><a name="'+i+'"><font color="#aa0000">'+i+'</font></a></h2>'
-	for j in catalog[i]:
-		# print '<a href="'+os.path.join(inv_dir, j)+'">', j, '</a>&nbsp;&nbsp;'
-		print '<a href=/cgi-bin/show_volume_cgi.py?volume='+j+'>', j, '</a>&nbsp;&nbsp;'
+    if catalog:
+        use_title = "Enstore Tape Inventory"
+    else: #Just summary
+        use_title = "Enstore Tape Inventory Summary"
+    if cluster:
+        use_title = "%s on %s" % (use_title, cluster)
+        
+    print '<html>'
+    print '<head>'
+    print '<title>' + use_title + '</title>'
+    print '</head>'
+    print '<body bgcolor="#ffffd0">'
 
-# the end
-print '</body>'
-print '</html>'
+    print '<font size=7 color="#ff0000">' + use_title + '</font>'
+    print '<hr>'
+
+    # output the index
+
+    if catalog:  #Only do for full inventory.
+        print '<h2><font color="#aa0000">Index</font></h2>'
+
+        keys = catalog.keys()
+        keys.sort()
+        for i in keys:
+            print '<a href=#' + i + '>' + i + '</a>&nbsp;&nbsp;'
+
+    # handle special files
+
+    if catalog: #Only output the special file header for the full inventory.
+        print '<hr>'
+        print '<p>'
+        print '<h2><font color="#aa0000">Special Files</font></h2>'
+
+    print '<p>'
+    for i in special:
+        if i == 'COMPLETE_FILE_LISTING':
+            print '<a href="enstore_file_listing_cgi.py">', string.split(i, '.')[0], '</a>&nbsp;&nbsp;'
+        else:
+            print '<a href="' + os.path.join(inv_dir, i) + '">', string.split(i, '.')[0], '</a>&nbsp;&nbsp;'
+            
+        if not catalog:  #Only do for summary.
+            print '<br>'
+
+    ### The raw directory listing used to work.  But now that the security
+    ### baseline STRONGLY recommends that this be disabled and we have
+    ### disabled it; it does not make sense to include a link to something
+    ### no one has permissions to see.
+    print '<p><a href="' + inv_dir + '">Raw Directory Listing</a>'
+
+    # output the main volume listing
+
+    if catalog:
+        for i in keys:
+            print '<hr>'
+            print '<p>'
+            print '<h2><a name="' + i + '"><font color="#aa0000">' + i + '</font></a></h2>'
+            for j in catalog[i]:
+                print '<a href=/cgi-bin/show_volume_cgi.py?volume=' + j +'>', j, '</a>&nbsp;&nbsp;'
+
+    # the end
+    print '</body>'
+    print '</html>'
+    
+
+
+def main():
+    # Obtain the correct values for ENSTORE_CONFIG_HOST and ENSTORE_CONFIG_PORT
+    # if they are not already available.
+    get_environment()
+
+    #Get the configuration server client.
+    config_host = enstore_functions2.default_host()
+    config_port = enstore_functions2.default_port()
+    csc = configuration_client.ConfigurationClient((config_host, config_port))
+   
+    #Get information.
+    (special, cluster) = setup(csc)
+
+    #Get the list of volumes.
+    catalog = get_volumes(csc)
+
+    #
+    #Print the html output.
+    #
+    print_html(catalog, special, cluster)
+    
+
+if __name__ == '__main__':
+
+    main()
