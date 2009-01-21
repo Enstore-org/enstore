b85e064f3ad6dedbc45d0f0126789a159a2747dc
Michael Zalokar <zalokar>
2009-01-21 00:14:05 +0000

    Added the ability to report MAC addresses currently located in the arp table.

--- modules/Interfaces.c
+++ modules/Interfaces.c
@@ -20,6 +20,8 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/ioctl.h>
+#include <net/if_arp.h>
+#include <netdb.h>
 
 
 /***************************************************************************
@@ -41,14 +43,21 @@
 void initInterfaces(void);
 static PyObject * raise_exception(char *msg);
 static struct ifconf * interfaces(void);
+
 static PyObject * Interfaces_info(PyObject *self, PyObject *args);
+static PyObject * Arp_info(PyObject *self, PyObject *args);
+
 static PyObject * interfacesGet(void);
+static PyObject * arpGet(char*);
 
 static PyObject *InterfacesErrObject;
 
 static char Interfaces_Doc[] =  "Interfaces accesses NICs.";
+/*static char Arp_Doc[] =  "Interfaces accesses ARP.";*/
 
 static char Interfaces_get_Doc[] = "\
+fd_xfer(fr_fd, to_fd, no_bytes, blk_siz, crc_flag[, crc])";
+static char Arp_get_Doc[] = "\
 fd_xfer(fr_fd, to_fd, no_bytes, blk_siz, crc_flag[, crc])";
 
 /*  Module Methods table. 
@@ -63,6 +72,7 @@ fd_xfer(fr_fd, to_fd, no_bytes, blk_siz, crc_flag[, crc])";
 
 static PyMethodDef Interfaces_Methods[] = {
     { "interfacesGet", Interfaces_info,  1, Interfaces_get_Doc},
+    { "arpGet", Arp_info,  1, Arp_get_Doc},
     { 0, 0}        /* Sentinel */
 };
 
@@ -101,13 +111,21 @@ Interfaces_info(PyObject *self, PyObject *args)
 
   errno = 0;
   return interfacesGet();
+}
 
-#if 0
-  if(errno == 0)
-     return PyLong_FromUnsignedLong(crc);
-  else
-     return raise_exception("interfaces info - error");
-#endif
+static PyObject *
+Arp_info(PyObject *self, PyObject *args)
+{
+  int sts;
+  char *address; 
+  
+  /* Get the parameter. */
+  sts = PyArg_ParseTuple(args, "s", &address);
+  if (!sts)
+     return(raise_exception("arp info - invalid parameter"));
+
+  errno = 0;
+  return arpGet(address);
 }
 
 /***************************************************************************
@@ -125,6 +143,10 @@ Interfaces_info(PyObject *self, PyObject *args)
  * 
  *   Fourth & Fifth - see Python/modsupport.c
  */
+
+/***************************************************************************
+ * Functions for interfaces.
+ **************************************************************************/
 
 void
 initInterfaces()
@@ -405,4 +427,177 @@ PyObject * interfacesGet(void)
    free(intf_confs->ifc_buf);
    free(intf_confs);
    return interface_list;
+}
+
+/***************************************************************************
+ * Functions for arp.
+ **************************************************************************/
+
+/*
+ * Helper function.  Since, hostname can have multiple IPs, we pass the
+ * address here and find the hardware/ethernet/mac address.
+ */
+static PyObject* __arp(struct in_addr *ip, PyObject *arp_list)
+{
+   struct arpreq arp_msg;
+   struct sockaddr_in addr, addr_dummy;
+   char dummy[64]; /*holder for the hardware addr*/
+   unsigned char *dummy_ptr;  /*used to point to dummy*/
+   int as;   /*arping socket*/
+#ifdef __linux__
+   struct ifconf *intf_confs;
+   int number_of_items;
+   int k;
+#endif /* __linux__ */
+
+   PyObject *arp_dict;
+   
+   /*arping socket*/
+
+   as = socket(AF_INET, SOCK_DGRAM, 0);
+
+   (void) memset(&arp_msg, 0, sizeof(struct arpreq));
+   (void) memset(dummy, 0 , sizeof(dummy));   
+
+   /*
+    * Stuff the arp struct with the information it needs.
+    */
+   
+   addr.sin_family = AF_INET;
+   addr.sin_addr = *ip; /**((struct in_addr*) host->h_addr_list[0]);*/
+   (void) memcpy(&(arp_msg.arp_pa), &addr, sizeof(addr));
+   /*
+    * Set the address family for the hardware address.  Stevens says that
+    * this should be AF_UNSPEC, however Linux needs ARPHRD_ETHER instead.
+    */
+#ifdef __linux__
+   arp_msg.arp_ha.sa_family = ARPHRD_ETHER;
+#else
+   arp_msg.arp_ha.sa_family = AF_UNSPEC;
+#endif /* __linux__ */
+
+   /* Linux insists that you need to specify the interface name.  Other
+    * systems don't requrire this extra step.  Since, there can be multiple
+    * interfaces lets check them all and let the caller determine which
+    * one they want.
+    */
+#ifdef __linux__
+   intf_confs = interfaces();
+
+   if(intf_confs == NULL)
+      return arp_list;
+   
+   number_of_items = intf_confs->ifc_len / sizeof(struct ifreq);
+
+   for(k = 0; k < number_of_items; k++)
+#endif /* __linux__ */
+   {
+#ifdef __linux__
+       /*
+	* For Linux, we need to set the interface here.  These are values
+	* like "eth0" or "eg1".
+	*/
+       strcpy(arp_msg.arp_dev, intf_confs->ifc_req[k].ifr_name);
+#endif /* __linux__ */
+
+       errno = 0;
+       printf("%s\n", intf_confs->ifc_req[k].ifr_name);
+       if(ioctl(as, SIOCGARP, &arp_msg) < 0)
+       {
+	  /* This will only work if the destination is on a different subnet
+	   * than every interface this host is on.  Thus, ENETUNREACH should
+	   * be ignored when this occurs.
+	   * For Linux only:  If the destination ip is on the local machine we
+	   * get EINVAL.  On non-Linux machines this is valid.
+	   */
+	  if(errno != ENETUNREACH
+#ifdef __linux__
+	     && errno != EINVAL
+#endif /* __linux__ */
+	 )
+	  {
+	     printf("%d\n", errno);
+	     continue;
+	  }  
+       }
+
+       /*
+	* Now pack the information into the dictionary.
+	*/
+       arp_dict = PyDict_New(); /* Get a new dict for each iteration. */
+
+       /*
+	* Set these items as values in the new dictionary.
+	*/
+       /* hardware address */
+       dummy_ptr = &arp_msg.arp_ha.sa_data[0];
+       snprintf(dummy, sizeof(dummy), "%x:%x:%x:%x:%x:%x",
+		*(dummy_ptr + 0),
+		*(dummy_ptr + 1),
+		*(dummy_ptr + 2),
+		*(dummy_ptr + 3),
+		*(dummy_ptr + 4),
+		*(dummy_ptr + 5));
+       PyDict_SetItemString(arp_dict, "hwaddr",
+			    PyString_FromString(dummy));
+       /* protocal address */
+       /* Needing to copy the arp_pa structure to addr_dummy should not
+	* be needed.  However, the (gcc) complier just refused to cast
+	* refused to case arp_msg.arp_pa to "struct sockaddr_in. */
+       memcpy(&addr_dummy, &arp_msg.arp_pa, sizeof(struct sockaddr));
+       if(inet_ntop(arp_msg.arp_pa.sa_family, &(((struct sockaddr_in)addr_dummy).sin_addr),
+		    dummy, sizeof(dummy)) > 0)
+       {
+	  PyDict_SetItemString(arp_dict, "addr",
+			       PyString_FromString(dummy));
+       }
+       /* flags address */
+       PyDict_SetItemString(arp_dict, "flags",
+			    PyInt_FromLong(arp_msg.arp_flags));
+#ifdef __linux__
+       /* interface */
+       PyDict_SetItemString(arp_dict, "dev",
+			    PyString_FromString(arp_msg.arp_dev));
+#endif /* __linux */
+       
+       /*
+	* Add the dictionary to the list.
+	*/
+       PyList_Append(arp_list, arp_dict);
+   }
+   
+   free(intf_confs->ifc_buf);
+   free(intf_confs);
+   return arp_list;
+}
+
+
+PyObject* arpGet(char *dest)
+{
+   struct hostent *host;
+   char **h_ptr;
+
+   PyObject *arp_dict;
+   PyObject *arp_list;
+
+   arp_list = PyList_New(0);
+   arp_dict = PyDict_New();
+
+   /*
+    * At some point we need to remove gethostbyname() and instead use
+    * getaddrinfo().  getaddrinfo() can do IPv6 which gethostbyname() can
+    * not.
+    */
+   
+   /*destination ip addr*/
+   host = gethostbyname(dest);
+
+   /* Loop over all the ips that refer to this hostname or ip. */
+   h_ptr = host->h_addr_list;
+   for(h_ptr = host->h_addr_list; *h_ptr != NULL; h_ptr++)
+   {
+      arp_list = __arp((struct in_addr*)*h_ptr, arp_list);
+   }
+
+   return arp_list;
 }
