822a1e732b73c2298ea15571c0e80af6bb41e8e2
Alexander Moibenko <moibenko>
2009-01-23 17:29:33 +0000

    made it working in grid and non-grid environment

--- test/torture/write_test.sh
+++ test/torture/write_test.sh
@@ -4,9 +4,17 @@
 #  $Id$
 #
 #############################################################
-
+if [ "${OSG_APP:-x}" != "x" ]
+then
+    . $OSG_APP/moibenko/config/setup-enstore 
+fi
 pnfs_path=''
-data=''
+if [ "${OSG_WN_TMP:-x}" = "x" ];
+then 
+    data='.'
+else
+data=${OSG_WN_TMP}/enstore_w_test
+fi
 ff_width=3
 sg='TEST'
 lib='null1'
@@ -27,7 +35,8 @@ if [ "${1:-}" = "-b" ] ; then shift; batch=$1; shift; fi
 if [ -z $pnfs_path ]; then pnfs_path="/pnfs/cdfen/test/sl8500/test"; fi
 if [ ! -z $data ]; then data=${data}/; fi
 #sizes="1 10 50 100 200 500 800 1000 2000 3000 4000 5000"
-sizes="3 4 5 6 7 8 9 10 11 12"
+#sizes="3 4 5 6 7 8 9 10 11 12"
+sizes="3 4 5 6 7 8"
 
 if [ ! -d $data ]; then mkdir -p $data;fi
 #echo  $data*$$.data
@@ -38,8 +47,13 @@ date=`date +%F`
 #echo fsz $fsz
 for s in $sizes
   do
-     # check anf if needed create pnfs path
-     $ENSTORE_DIR/test/torture/make_pnfs_dir.py $pnfs_path/$date/${host}/${s} $sg $lib $ff $ff_width $wrap 
+    # check anf if needed create pnfs path
+    if [ "${OSG_APP:-x}" = "x" ]
+	then
+	    $ENSTORE_DIR/test/torture/make_pnfs_dir.py $pnfs_path/$date/${host}/${s} $sg $lib $ff $ff_width $wrap
+     else	    
+	$OSG_APP/moibenko/bin/make_pnfs_dir $pnfs_path/$date/${host}/${s} $sg $lib $ff $ff_width $wrap 
+     fi
      cd - > /dev/null 2>&1
 done
 
@@ -51,9 +65,14 @@ if [ $fsz -eq 0 ]; then
 	do
 	  #name=test_${s}_`date +"%s"`.data
 	  name=${host}_test_${s}_`date +"%s"`_$$.data
-	  sz=`~/enstore/test/torture/gauss ${s}|awk '{ print $1}'`
-	  #echo "$$ $s $sz "
-	  ~/enstore/test/torture/createfile $sz $data$name 
+	  if [ "${OSG_APP:-x}" = "x" ]
+	  then
+	    sz=`$ENSTORE_DIR/test/torture/gauss ${s}|awk '{ print $1}'`
+	    $ENSTORE_DIR/test/torture/createfile $sz $data$name 
+	  else
+	    sz=`$OSG_APP/moibenko/bin/gauss ${s}|awk '{ print $1}'`
+	    $OSG_APP/moibenko/bin/createfile $sz $data$name 
+	  fi
 	  let i=i+1
 	  sleep 1
 	done 
@@ -66,7 +85,12 @@ for s in $sizes
   files=`ls $data/${host}_test_${s}*.data`
   for f in $files
     do
-      encp --threaded  $f $pnfs_path/$date/${host}/$s/`basename $f`.$$ &
+	if [ "${OSG_APP:-x}" = "x" ]
+	then
+	    $ENSTORE_DIR/bin/encp --threaded  $f $pnfs_path/$date/${host}/$s/`basename $f`.$$ &
+	else
+	    $OSG_APP/moibenko/bin/encp --threaded  $f $pnfs_path/$date/${host}/$s/`basename $f`.$$ &
+	fi
     done
 done
 wait
