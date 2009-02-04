8a2142dc4b7f23173ebf146c8fe3dc752b007602
Michael Zalokar <zalokar>
2009-02-04 21:39:58 +0000

    Renamed record_recv_error() to log_socket_state().  Use log_socket_state() to log sending/writing errors too.

--- src/callback.py
+++ src/callback.py
@@ -115,6 +115,69 @@ def __get_socket_state(fd):
             
     return None
 
+def log_socket_state(sock):
+        #According to python documentation when recv() returns the empty
+        # string the other end has closed the connection.
+
+        try:
+            #Verify if there is an error on the socket.
+            socket_error = sock.getsockopt(socket.SOL_SOCKET,
+                                           socket.SO_ERROR)
+            if socket_error != 0:
+                Trace.log(e_errors.ERROR,
+                          "socket state: pending SO_ERROR: %s" \
+                          % (socket_error,))
+        except socket.error, msg:
+            Trace.log(e_errors.ERROR,
+                      "socket state: getsockopt(SO_ERROR): %s" \
+                      % (str(msg),))
+
+        try:
+            #Verify if the connection is still up.
+            peer_name = sock.getpeername()
+        except socket.error, msg:
+            peer_name = None
+            Trace.log(e_errors.ERROR,
+                      "socket state: getpeername(): %s" % str(msg))
+
+        Trace.log(e_errors.ERROR,
+                  "socket state: received no data from %s" % (peer_name,))
+
+        #Log the current socket state (only works on Linux).
+        socket_state = __get_socket_state(sock.fileno())
+        Trace.log(e_errors.ERROR,
+                  "socket state: socket state: %s" % str(socket_state))
+
+        # It would be useful to output the number of bytes in the
+        # read buffer.  Python does not yet support it.
+        try:
+            nbytes = get_socket_read_queue_length(sock)
+
+            Trace.log(e_errors.ERROR,
+                      "socket state: fcntl(FIONREAD): %s"
+                      % (str(nbytes),))
+        except AttributeError:
+            #FIONREAD not known on this system.
+            pass
+        except IOError, msg:
+            Trace.log(e_errors.ERROR,
+                      "socket state: ioctl(FIONREAD): %s" % (str(msg),))
+
+        #Get any mac addresses.
+        arpGetFunc = getattr(Interfaces, "arpGet", None)
+        if peer_name and arpGetFunc:
+            try:
+                mac_addresses = arpGetFunc(peer_name[0])
+                if mac_addresses:
+                    Trace.log(e_errors.ERROR, 
+                              "socket state: arp get[%s]: %s" % \
+                              (peer_name[0], str(mac_addresses)))
+
+            except:
+                Trace.log(e_errors.ERROR,
+                          "socket state: arp get[%s]: %s" % \
+                          (peer_name[0], str(msg)))
+
 # get an unused tcp port for control communication
 def get_callback(ip=None):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -136,6 +199,13 @@ def timeout_send(sock,msg,timeout=15*60):
     timeout = float(timeout)
     junk,fds,junk = select.select([],[sock],[],timeout)
     if sock not in fds:
+        #Send to the debug log the current stack trace.  Hopefully, this will
+        # be useful in debugging hanging connections.
+        try:
+            if "log_stack_trace" in dir(Trace):
+                Trace.log_stack_trace()
+        except:
+            Trace.handle_error()
         return ""
     if type(sock) == types.IntType: #In case sock is an fd for a pipe.
         return os.write(sock, msg)
@@ -179,10 +249,10 @@ def write_raw(sock,msg,timeout=15*60):
                 break
             ptr = ptr + nwritten
         if ptr < msg_len_len:
-            return 1, "short write: message length (expected 8, received %s)" \
+            return 1, "short write: message length (expected 8, sent %s)" \
                    % (ptr,)
         if ptr != msg_len_len:
-            return 1, "bad write: message length (expected 8, received %s)" \
+            return 1, "bad write: message length (expected 8, sent %s)" \
                    % (ptr,)
 
         #This time write out the 'signature'.
@@ -193,9 +263,9 @@ def write_raw(sock,msg,timeout=15*60):
                 break
             ptr = ptr + nwritten
         if ptr < msg_signature_len:
-            return 1, "short write: salt (expected 8, received %s)" % (ptr,)
+            return 1, "short write: salt (expected 8, sent %s)" % (ptr,)
         if ptr != msg_signature_len:
-            return 1, "bad write: salt (expected 8, received %s)" % (ptr,)
+            return 1, "bad write: salt (expected 8, sent %s)" % (ptr,)
 
         #Write the payload to the socket.
         ptr = 0
@@ -205,10 +275,10 @@ def write_raw(sock,msg,timeout=15*60):
                 break
             ptr = ptr + nwritten
         if ptr < msg_len:
-            return 1, "short write: message (expected %d, received %s)" \
+            return 1, "short write: message (expected %d, sent %s)" \
                    % (msg_len, ptr,)
         if ptr != msg_len:
-            return 1, "bad write: message (expected %d, received %s)" \
+            return 1, "bad write: message (expected %d, sent %s)" \
                    % (msg_len, ptr,)
 
         #Lastly, write out the checksum of the payload.
@@ -219,15 +289,15 @@ def write_raw(sock,msg,timeout=15*60):
                 break
             ptr = ptr + nwritten
         if ptr < checksum_len:
-            return 1, "short write: checksum (expected 4, received %s)" \
+            return 1, "short write: checksum (expected 4, sent %s)" \
                    % (ptr,)
         if ptr != checksum_len:
-            return 1, "bad write: checksum (expected 4, received %s)" \
+            return 1, "bad write: checksum (expected 4, sent %s)" \
                    % (ptr,)
             
 
         return 0, ""
-    except (socket.error, OSError), detail:
+    except (socket.error, select.error, OSError), detail:
         error_string = "write_raw: socket.error %s"%(detail,)
         #Trace.log(e_errors.ERROR, error_string)
         return 1, error_string
@@ -243,14 +313,20 @@ def write_tcp_obj(sock,obj,timeout=15*60):
     rtn, e = write_tcp_raw(sock,repr(obj),timeout)
 
     if e:
+        log_socket_state(sock) #Log the state of the socket.
         Trace.log(e_errors.ERROR, e)
+        raise e_errors.TCP_EXCEPTION
+
     return rtn
 
 # send a message over the network which is a Python object
 def write_tcp_obj_new(sock,obj,timeout=15*60):
     rtn, e = write_tcp_raw(sock,cPickle.dumps(obj),timeout)
     if e:
+        log_socket_state(sock) #Log the state of the socket.
         Trace.log(e_errors.ERROR, e)
+        raise e_errors.TCP_EXCEPTION
+
     return rtn
 
 # send a message to a co-process which is a Python object
@@ -259,77 +335,13 @@ def write_obj(fd, obj, timeout=15*60, verbose = True):
 
     if e and verbose:
         Trace.log(e_errors.ERROR, e)
-
+        raise e_errors.TCP_EXCEPTION #What should this be?
+    
     return rtn
 
 ###############################################################################
 ###############################################################################
-
-def record_recv_error(sock):
-        #According to python documentation when recv() returns the empty
-        # string the other end has closed the connection.
-
-        #Log the time spent waiting.
-        #Trace.log(e_errors.ERROR,
-        #          "timeout_recv(): time passed: %s sec of %s sec" %
-        #          (time.time() - total_start_time, timeout))
-
-        try:
-            #Verify if there is an error on the socket.
-            socket_error = sock.getsockopt(socket.SOL_SOCKET,
-                                           socket.SO_ERROR)
-            if socket_error != 0:
-                Trace.log(e_errors.ERROR,
-                          "timeout_recv(): socket error: %s" % socket_error)
-        except socket.error, msg:
-            Trace.log(e_errors.ERROR,
-                      "timeout_recv(): getsockopt(SO_ERROR): %s" % str(msg))
-
-        try:
-            #Verify if the connection is still up.
-            peer_name = sock.getpeername()
-        except socket.error, msg:
-            peer_name = None
-            Trace.log(e_errors.ERROR,
-                      "timeout_recv(): getpeername: %s" % str(msg))
-
-        Trace.log(e_errors.ERROR, "timeout_recv(): received no data")
-
-        #Log the current socket state (only works on Linux).
-        socket_state = __get_socket_state(sock.fileno())
-        Trace.log(e_errors.ERROR,
-                  "timeout_recv(): socket state: %s" % str(socket_state))
-
-        # It would be useful to output the number of bytes in the
-        # read buffer.  Python does not yet support it.
-        try:
-            nbytes = get_socket_read_queue_length(sock)
-
-            Trace.log(e_errors.ERROR,
-                      "timeout_recv(): fcntl(FIONREAD): %s"
-                      % (str(nbytes),))
-        except AttributeError:
-            #FIONREAD not known on this system.
-            pass
-        except IOError, msg:
-            Trace.log(e_errors.ERROR,
-                      "timeout_recv(): ioctl(FIONREAD): %s" % (str(msg),))
-
-        #Get any mac addresses.
-        arpGetFunc = getattr(Interfaces, "arpGet", None)
-        if peer_name and arpGetFunc:
-            try:
-                mac_addresses = arpGetFunc(peer_name[0])
-                if mac_addresses:
-                    Trace.log(e_errors.ERROR, 
-                              "timeout_recv(): arp get[%s]: %s" % \
-                              (peer_name[0], str(mac_addresses)))
-
-            except:
-                Trace.log(e_errors.ERROR,
-                          "timeout_recv(): arp get[%s]: %s" % \
-                          (peer_name[0], str(msg)))
-
+        
 #recv with a timeout
 def timeout_recv(sock, nbytes, timeout = 15 * 60):
     total_start_time = time.time()
@@ -437,8 +449,8 @@ read_tcp_raw = read_raw
 def read_tcp_obj(sock, timeout=15*60) :
     s, e = read_tcp_raw(sock, timeout)
     if not s:
-        record_recv_error(sock) #Log the state of the socket.
-        
+        log_socket_state(sock) #Log the state of the socket.
+
         #Gather additional information and log the error.
         try:
             peername = sock.getpeername()
@@ -464,8 +476,8 @@ def read_tcp_obj(sock, timeout=15*60) :
 def read_tcp_obj_new(sock, timeout=15*60):
     s, e = read_tcp_raw(sock, timeout)
     if not s:
-        record_recv_error(sock) #Log the state of the socket.
-        
+        log_socket_state(sock) #Log the state of the socket.
+
         #Gather additional information and log the error.
         try:
             peername = sock.getpeername()
