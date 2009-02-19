0faa96ec3835380622829a3b78b05ca7587892a3
Alexander Moibenko <moibenko>
2009-02-19 23:02:00 +0000

    added repeat work

--- test/torture/read_test.sh
+++ test/torture/read_test.sh
@@ -4,12 +4,20 @@
 #  $Id$
 #
 #############################################################
+if [ "${OSG_APP:-x}" != "x" ]
+then
+    . $OSG_APP/moibenko/config/setup-enstore 
+else
+    . ~/site_specific/config/setup-enstore 
+fi
 bfids=""
+w=1
 n_cycles=""
 if [ "${1:-}" = "-x" ] ; then set -xv; shift; fi
 if [ "${1:-}" = "-p" ] ; then shift; pnfs_path=$1; shift; fi
 if [ "${1:-}" = "-b" ] ; then shift; bfids=1; bfid_list_file=$1; shift; fi
 if [ "${1:-}" = "-c" ] ; then shift; n_cycles=$1; shift; fi
+if [ "${1:-}" = "-w" ] ; then shift; w=$1; shift; fi
 
 if [ -z $pnfs_path ]; then pnfs_path="/pnfs/cdfen/test/sl8500/test"; fi
 if [ -z $n_cycles ]; then n_cycles="${1:-300}";fi
@@ -66,18 +74,22 @@ read_by_bf_id() {
     echo $entry
     let i=i+1
     echo $i
-    encp --get_bfid $entry /dev/null
+    encp --get_bfid $entry /dev/null &
     if [ $i -gt $n_cycles ]; then break;fi
   done
   wait
 }
-
-if [ -z $bfids ]
-then
-  read_by_filename
-else
-  read_by_bf_id
-fi
+c=0
+while [ $c -lt $w ];
+do
+  if [ -z $bfids ]
+      then
+      read_by_filename
+  else
+      read_by_bf_id
+  fi
+  let c=c+1
+done
 
 #echo "sending mail"
 #echo "READ TEST COMPLETED ON NODE ${node}" | /bin/mail -s "READ TEST COMPLETED ON NODE ${node}: " $mail
