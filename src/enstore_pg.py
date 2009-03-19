66ce7b2f54f4dec3ad0b7527fc26a387064c2f97
Dmitry Litvinsev <litvinse>
2009-03-19 22:03:49 +0000

    replace "select *" with "select date,node ...." skipping library cuz inquisitor code chokes on extra columns. NB: inquisitor needs fixing as well

--- src/enstore_pg.py
+++ src/enstore_pg.py
@@ -22,8 +22,8 @@ def acc_encp_lines(csc, numEncps=100):
         # could not connect to the db
         print " %s  %s"%(pg.Error, detail)
         return []
-    res = db.query("select * from encp_xfer order by date desc limit %s;"%(numEncps,))
-    res_err = db.query("select * from encp_error order by date desc limit %s;"%(numEncps,))
+    res = db.query("select  date, node, pid, username, src, dst, size, rw, overall_rate, network_rate, drive_rate, volume, mover, drive_id, drive_sn, elapsed,  media_changer, mover_interface, driver, storage_group, encp_ip, encp_id, disk_rate, transfer_rate, encp_version, file_family, wrapper from encp_xfer order by date desc limit %s;"%(numEncps,))
+    res_err = db.query("select date, node, pid, username, encp_id, version, type, error, src, dst, size, storage_group, file_family, wrapper,  mover, drive_id,  drive_sn,  rw, volume from encp_error order by date desc limit %s;"%(numEncps,))
     # combine the 2 lists and take the top numEncps entries to return
     res_total = res.getresult() + res_err.getresult()
     res_total.sort()
