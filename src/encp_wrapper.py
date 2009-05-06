cd574dee643c56a63bf4906dd545cabbc9e11af0
Michael Zalokar <zalokar>
2009-05-06 19:34:37 +0000

    1) Accept the encp command as a list of argv values instead of as a string. Spaces in filenames caused the old implimentation to fail. 2) Set the effective UID and GID back to root after the encp.

--- src/encp_wrapper.py
+++ src/encp_wrapper.py
@@ -4,10 +4,11 @@ a quick and not so dirty wrapper for encp so that multiple encps can be
 instantiated in the same program, probably in different threads.
 """
 
-import string
+#import string
 import sys
 
 import Trace
+import file_utils
 
 class Encp:
 	def __init__(self, tid=None):
@@ -17,24 +18,24 @@ class Encp:
 		self.exit_status = -10
 		self.err_msg = ""
 
-	# encp(cmd) -- cmd is the same as the command line
-	# eg. encp("encp --verbose=4 /pnfs/.../file file")
-	def encp(self, cmd):
+	# encp(argv) -- argv is the same as the command line
+	# eg. encp(["encp", "--verbose=4", "/pnfs/.../file", "file"])
+	def encp(self, argv):
 		self.exit_status = -10 #Reset this every time.
 		self.err_msg = ""
 
 		#Insert the command if it is not already there.
                 CMD = "encp"
-		if cmd[:len(CMD)] != CMD:
-			cmd = CMD + " " + cmd
-			
-		argv = string.split(cmd)
-		intf = self.my_encp.EncpInterface(argv, 0)
-		intf.migration_or_duplication = 1 #Set true for performance.
-		if self.tid:
-			intf.include_thread_name = self.tid
-		logname=Trace.logname #Grab this to reset this after encp.
+		if argv[0] != CMD:
+			argv = [CMD] + argv
+		
 		try:
+			logname=Trace.logname #Grab this to reset this after encp.
+			intf = self.my_encp.EncpInterface(argv, 0)
+			intf.migration_or_duplication = 1 #Set true for performance.
+			if self.tid:
+				intf.include_thread_name = self.tid
+			
 			res = self.my_encp.do_work(intf)
 			if res == None:
 				#return -10
@@ -46,9 +47,27 @@ class Encp:
 			raise sys.exc_info()[0], sys.exc_info()[1], \
 			      sys.exc_info()[2]
 		except:
-			self.err_msg = str(sys.exc_info()[:2])
+			self.err_msg = str((str(sys.exc_info()[0]),
+					    str(sys.exc_info()[1])))
+			sys.stderr.write("%s\n" % self.err_msg)
 			res = 1
 
+		#If we end up with encp being owned not by root at this
+		# point, we need to set it back.
+		file_utils.acquire_lock_euid_egid()
+		try:
+			file_utils.set_euid_egid(0, 0)
+		except (KeyboardInterrupt, SystemExit):
+			raise sys.exc_info()[0], sys.exc_info()[1], \
+			      sys.exc_info()[2]
+		except:
+			Trace.logname = logname #Reset the log file name.
+			self.err_msg = str((str(sys.exc_info()[0]),
+					    str(sys.exc_info()[1])))
+			sys.stderr.write("%s\n" % self.err_msg)
+			res = 1
+		file_utils.release_lock_euid_egid()
+		
 		Trace.logname = logname #Reset the log file name.
 		self.exit_status = res #Return value if used in a thread.
 		return res  #Return value if used directly.
