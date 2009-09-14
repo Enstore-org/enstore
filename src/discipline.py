61e571958851f5495dcbc66fa92e5f30000f0a6b
Alexander Moibenko <moibenko>
2009-09-14 20:08:45 +0000

    Codew review recommended changes (bugzilla ticket 388) 1. self.dict -> self.discipline_dict 2. in match_found added commenst describing default return values 3. replaced string.atoi(..) with int(...) 4. More readable ticket in unit test

--- src/discipline.py
+++ src/discipline.py
@@ -57,7 +57,7 @@ class Restrictor:
             ldict = {}
         if dict:
             self.exists = 1
-        self.dict = ldict
+        self.discipline_dict = ldict
         return (e_errors.OK, None)
 
     def __init__(self, csc, library_manager):
@@ -68,10 +68,12 @@ class Restrictor:
 
 
     def ticket_match(self, key, item):
-        pattern = "^%s" % (self.dict[key]['host'],)
+        pattern = "^%s" % (self.discipline_dict[key]['host'],)
         try:
-            if re.search(pattern, item): return 1
-            else: return 0
+            if re.search(pattern, item):
+                return 1
+            else:
+                return 0
         except:
             Trace.log(e_errors.ERROR,"parse errorr")
             Trace.handle_error()
@@ -80,12 +82,11 @@ class Restrictor:
     def match_found(self, ticket):
         # returns a tuple:
         # 1 if match was found, 0 - if not
-        # function name to run when match was founsd
-        # list of function arguments
-        # what to do with request ("ignore", "reject")
+        # function name to run when match was found, None othrewise
+        # list of function arguments, None if fuction was not found
+        # what to do with request ("ignore", "reject"), None if fuction was not found
         
         # default match settings
-        #match = 1, "restrict_host_access", [host, 1], "ignore" 
         match = 0, None, None, None
         failed = False
         self._lock.acquire()
@@ -105,11 +106,11 @@ class Restrictor:
         if not self.exists:  # no discipline configuration info
             return match
 
-        for key in self.dict.keys():
+        for key in self.discipline_dict.keys():
             # try to match a ticket
             if self.ticket_match(key, host) == 1:
                 # use deep copy to make sure that the original set of arguments is returned
-                match = 1, self.dict[key]['function'], copy.deepcopy(self.dict[key]['args']),self.dict[key]['action']
+                match = 1, self.discipline_dict[key]['function'], copy.deepcopy(self.discipline_dict[key]['args']),self.discipline_dict[key]['action']
                 break
 
         return match
@@ -119,13 +120,37 @@ if __name__ == "__main__":
     import socket
     import configuration_client
     def_addr = (os.environ['ENSTORE_CONFIG_HOST'],
-                string.atoi(os.environ['ENSTORE_CONFIG_PORT']))
+                int(os.environ['ENSTORE_CONFIG_PORT']))
     csc = configuration_client.ConfigurationClient( def_addr )
     host = socket.gethostname()
     ip = socket.gethostbyname(host)
     r = Restrictor(csc,'null1.library_manager')
     #ps.read_config()
-    ticket={'lm': {'address': (ip, 7520)}, 'unique_id': '%s-1005321365-0-28872'%(host,), 'infile': '/pnfs/rip6/happy/mam/aci.py', 'bfid': 'HAMS100471636100000', 'mover': 'MAM01.mover', 'at_the_top': 3, 'client_crc': 1, 'encp_daq': None, 'encp': {'delayed_dismount': None, 'basepri': 1, 'adminpri': -1, 'curpri': 1, 'agetime': 0, 'delpri':0}, 'fc': {'size': 1434L, 'sanity_cookie': (1434L, 657638438L), 'bfid': 'HAMS100471636100000', 'location_cookie': '0000_000000000_0000001', 'address': ('131.225.84.122', 7501), 'pnfsid': '00040000000000000040F2F8', 'pnfs_mapname': '/pnfs/rip6/volmap/alex/MM0001/0000_000000000_0000001', 'drive': 'happy:/dev/rmt/tps0d4n:0060112307', 'external_label': 'MM0001', 'deleted': 'no', 'pnfs_name0': '/pnfs/rip6/happy/mam/aci.py', 'pnfsvid': '00040000000000000040F360', 'complete_crc': 657638438L, 'status': ('ok', None)}, 'file_size': 1434, 'outfile': '/dev/null', 'volume': 'MM0001', 'times': {'t0': 1005321364.951048, 'in_queue': 14.586493015289307, 'job_queued': 1005321365.7764519, 'lm_dequeued': 1005321380.363162}, 'version': 'v2_14  CVS $Revision$ ', 'retry': 0, 'work': 'read_from_hsm', 'callback_addr': ('131.225.13.132', 1463), 'wrapper': {'minor': 5, 'inode': 0, 'fullname': '/dev/null', 'size_bytes': 1434, 'rmajor': 0, 'mode': 33268, 'pstat': (33204, 71365368, 5L, 1, 6849, 5440, 1434, 1004716362, 1004716362, 1004716329), 'gname': 'hppc', 'sanity_size': 65536, 'machine': ('Linux', 'gccensrv2.fnal.gov', '2.2.17-14', '#1 Mon Feb 5 18:48:50 EST 2001', 'i686'), 'uname': 'moibenko', 'pnfsFilename': '/pnfs/rip6/happy/mam/aci.py', 'uid': 6849, 'gid': 5440, 'rminor': 0, 'major': 0}, 'vc': {'first_access': 1004716170.54972, 'sum_rd_err': 0, 'last_access': 1004741744.274856, 'media_type': '8MM', 'capacity_bytes': 5368709120L, 'declared': 1004474612.7774431, 'remaining_bytes': 20105625600L, 'wrapper': 'cpio_odc', 'external_label': 'MM0001', 'system_inhibit': ['none', 'none'], 'user_inhibit': ['none', 'none'], 'current_location': '0000_000000000_0000001', 'sum_rd_access': 7, 'volume_family': 'D0.alex.cpio_odc', 'address': ('131.225.84.122', 7502), 'file_family': 'alex', 'sum_wr_access': 2, 'library': 'mam', 'sum_wr_err': 1, 'non_del_files': 1, 'blocksize': 131072, 'eod_cookie': '0000_000000000_0000002', 'storage_group': 'D0', 'status': ('ok', None)}, 'status': ('ok', None)}
+    ticket={'lm': {'address': (ip, 7520)}, 'unique_id': '%s-1005321365-0-28872'%(host,), 'infile': '/pnfs/rip6/happy/mam/aci.py',
+            'bfid': 'HAMS100471636100000', 'mover': 'MAM01.mover', 'at_the_top': 3, 'client_crc': 1, 'encp_daq': None,
+            'encp': {'delayed_dismount': None, 'basepri': 1, 'adminpri': -1, 'curpri': 1, 'agetime': 0, 'delpri':0},
+            'fc': {'size': 1434L, 'sanity_cookie': (1434L, 657638438L), 'bfid': 'HAMS100471636100000', 'location_cookie':
+                   '0000_000000000_0000001', 'address': ('131.225.84.122', 7501), 'pnfsid': '00040000000000000040F2F8',
+                   'pnfs_mapname': '/pnfs/rip6/volmap/alex/MM0001/0000_000000000_0000001', 'drive':
+                   'happy:/dev/rmt/tps0d4n:0060112307', 'external_label': 'MM0001', 'deleted': 'no', 'pnfs_name0':
+                   '/pnfs/rip6/happy/mam/aci.py', 'pnfsvid': '00040000000000000040F360', 'complete_crc': 657638438L,
+                   'status': ('ok', None)},
+            'file_size': 1434, 'outfile': '/dev/null', 'volume': 'MM0001',
+            'times': {'t0': 1005321364.951048,'in_queue': 14.586493015289307, 'job_queued': 1005321365.7764519, 'lm_dequeued': 1005321380.363162},
+            'version': 'v2_14  CVS $Revision$ ', 'retry': 0, 'work': 'read_from_hsm', 'callback_addr': ('131.225.13.132', 1463),
+            'wrapper': {'minor': 5, 'inode': 0, 'fullname': '/dev/null', 'size_bytes': 1434, 'rmajor': 0, 'mode': 33268,
+                        'pstat': (33204, 71365368, 5L, 1, 6849, 5440, 1434, 1004716362, 1004716362, 1004716329), 'gname': 'hppc',
+                        'sanity_size': 65536, 'machine': ('Linux', 'gccensrv2.fnal.gov', '2.2.17-14', '#1 Mon Feb 5 18:48:50 EST 2001', 'i686'),
+                        'uname': 'moibenko', 'pnfsFilename': '/pnfs/rip6/happy/mam/aci.py', 'uid': 6849, 'gid': 5440, 'rminor': 0, 'major': 0},
+            'vc': {'first_access': 1004716170.54972, 'sum_rd_err': 0, 'last_access': 1004741744.274856, 'media_type': '8MM',
+                   'capacity_bytes': 5368709120L, 'declared': 1004474612.7774431, 'remaining_bytes': 20105625600L,
+                   'wrapper': 'cpio_odc', 'external_label': 'MM0001', 'system_inhibit': ['none', 'none'],
+                   'user_inhibit': ['none', 'none'], 'current_location': '0000_000000000_0000001', 'sum_rd_access': 7,
+                   'volume_family': 'D0.alex.cpio_odc', 'address': ('131.225.84.122', 7502), 'file_family': 'alex',
+                   'sum_wr_access': 2, 'library': 'mam', 'sum_wr_err': 1, 'non_del_files': 1, 'blocksize': 131072,
+                   'eod_cookie': '0000_000000000_0000002', 'storage_group': 'D0', 'status': ('ok', None)},
+            'status': ('ok', None)
+            }
     ret = r.match_found(ticket)
     print "END"
     print "Access enabled=:",ret
