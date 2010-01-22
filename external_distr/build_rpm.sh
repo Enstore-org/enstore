37610b21dfa13733ce464677e9ebe757424a1944
Alexander Moibenko <moibenko>
2010-01-22 00:12:28 +0000

    removed enstore_sa: no need to build anymore

--- external_distr/build_rpm.sh
+++ external_distr/build_rpm.sh
@@ -57,24 +57,25 @@ fi
 tar czf enstore.tgz *
 cp -f enstore.tgz /usr/src/redhat/SOURCES
 rm -f enstore.tgz  
-tar czf enstore_sa.tgz *
+#tar czf enstore_sa.tgz *
 
-cp -f enstore_sa.tgz /usr/src/redhat/SOURCES
-rm -f enstore_sa.tgz
+#cp -f enstore_sa.tgz /usr/src/redhat/SOURCES
+#rm -f enstore_sa.tgz
 rm -f /usr/src/redhat/SPECS/enstore.spec
-rm -f /usr/src/redhat/SPECS/enstore_sa.spec
+#rm -f /usr/src/redhat/SPECS/enstore_sa.spec
 #cp -f spec/enstore.spec /usr/src/redhat/SPECS
 sed "
 /Version:/c Version: $EVersion
 /Release:/c Release: $ERelease
 " < spec/enstore.spec > /usr/src/redhat/SPECS/enstore.spec
 #cp -f spec/enstore_sa.spec /usr/src/redhat/SPECS
-sed "
-/Version:/c Version: $EVersion
-/Release:/c Release: $ERelease
-" < spec/enstore_sa.spec > /usr/src/redhat/SPECS/enstore_sa.spec
+#sed "
+
+#/Version:/c Version: $EVersion
+#/Release:/c Release: $ERelease
+#" < spec/enstore_sa.spec > /usr/src/redhat/SPECS/enstore_sa.spec
 rpmbuild -ba /usr/src/redhat/SPECS/enstore.spec
-rpmbuild -ba /usr/src/redhat/SPECS/enstore_sa.spec
+#rpmbuild -ba /usr/src/redhat/SPECS/enstore_sa.spec
 
 cd
 rm -rf /tmp/enstore_build
