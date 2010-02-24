c82969a050c2c1f37267b6ba3006fedddcb533f0
Alexander Moibenko <moibenko>
2010-02-24 21:14:23 +0000

    Update enstore symbolic links. This is needed to restore correct symbolic links during enstore rpm installation, as well in setup-enstore script.

--- external_distr/update_sym_links.sh
+++ external_distr/update_sym_links.sh
@@ -1,67 +1,97 @@
+#!/bin/sh
 ###############################################################################
 #
 # $Id$
 #
 ###############################################################################
-#!/bin/sh
-rm -f  $ENSTORE_DIR/bin/encp;
-ln -s $ENSTORE_DIR/src/encp.py $ENSTORE_DIR/bin/encp;
-rm -f $ENSTORE_DIR/bin/enstore;
-ln -s $ENSTORE_DIR/src/enstore_admin.py $ENSTORE_DIR/bin/enstore; 
-rm -f $ENSTORE_DIR/bin/enmv;
-ln -s $ENSTORE_DIR/src/enmv.py $ENSTORE_DIR/bin/enmv; 
-rm -f $ENSTORE_DIR/sbin/enstoreCut;
-ln -s $ENSTORE_DIR/sbin/encpCut $ENSTORE_DIR/sbin/enstoreCut; 
-rm -f $ENSTORE_DIR/sbin/enstore_up_down;
-ln -s $ENSTORE_DIR/src/enstore_up_down.py $ENSTORE_DIR/sbin/enstore_up_down; 
-rm -f $ENSTORE_DIR/sbin/enmonitor; 
-ln -s $ENSTORE_DIR/src/monitor_client.py  $ENSTORE_DIR/sbin/enmonitor; 
-rm -f $ENSTORE_DIR/bin/pnfs
-ln -s $ENSTORE_DIR/src/pnfs.py $ENSTORE_DIR/bin/pnfs; 
-rm $ENSTORE_DIR/bin/entv 
-ln -s $ENSTORE_DIR/src/entv.py $ENSTORE_DIR/bin/entv; 
-rm -f $ENSTORE_DIR/bin/volume_assert 
-ln -s $ENSTORE_DIR/src/volume_assert.py   $ENSTORE_DIR/bin/volume_assert; 
-rm -f $ENSTORE_DIR/bin/ensync 
-ln -s $ENSTORE_DIR/src/ensync.py   $ENSTORE_DIR/bin/ensync; 
-rm -f $ENSTORE_DIR/bin/get;
-ln -s $ENSTORE_DIR/src/get.py   $ENSTORE_DIR/bin/get; 
-rm -f $ENSTORE_DIR/sbin/quickquota
-ln -s $ENSTORE_DIR/src/quickquota.py   $ENSTORE_DIR/sbin/quickquota; 
-rm -f $ENSTORE_DIR/bin/migrate
-ln -s $ENSTORE_DIR/src/migrate.py   $ENSTORE_DIR/bin/migrate; 
-rm -f $ENSTORE_DIR/bin/duplicate
-ln -s $ENSTORE_DIR/src/duplicate.py   $ENSTORE_DIR/bin/duplicate; 
+set -u  # force better programming and ability to use check for not set
+
+# check if file exist
+check="${1:-x}" # check if file exist if argiment is "c"
 ###
-### Enstore server links.
+### Links in sbin directory
 ###
-rm -f $ENSTORE_DIR/sbin/configuration_server 
-ln -s $ENSTORE_DIR/src/configuration_server.py $ENSTORE_DIR/sbin/configuration_server; 
-rm $ENSTORE_DIR/sbin/log_server
-ln -s $ENSTORE_DIR/src/log_server.py   $ENSTORE_DIR/sbin/log_server; 
-rm -f $ENSTORE_DIR/sbin/alarm_server
-ln -s $ENSTORE_DIR/src/alarm_server.py   $ENSTORE_DIR/sbin/alarm_server; 
-rm -f $ENSTORE_DIR/sbin/inquisitor
-ln -s $ENSTORE_DIR/src/inquisitor.py   $ENSTORE_DIR/sbin/inquisitor; 
-rm -f $ENSTORE_DIR/sbin/ratekeeper
-ln -s $ENSTORE_DIR/src/ratekeeper.py   $ENSTORE_DIR/sbin/ratekeeper; 
-rm $ENSTORE_DIR/sbin/event_relay
-ln -s $ENSTORE_DIR/src/event_relay.py   $ENSTORE_DIR/sbin/event_relay; 
-rm -f $ENSTORE_DIR/sbin/info_server 
-ln -s $ENSTORE_DIR/src/info_server.py   $ENSTORE_DIR/sbin/info_server; 
-rm -f $ENSTORE_DIR/sbin/file_clerk
-ln -s $ENSTORE_DIR/src/file_clerk.py   $ENSTORE_DIR/sbin/file_clerk; 
-rm -f $ENSTORE_DIR/sbin/volume_clerk
-ln -s $ENSTORE_DIR/src/volume_clerk.py   $ENSTORE_DIR/sbin/volume_clerk; 
-rm -f $ENSTORE_DIR/sbin/accounting_server
-ln -s $ENSTORE_DIR/src/accounting_server.py   $ENSTORE_DIR/sbin/accounting_server; 
-rm -f $ENSTORE_DIR/sbin/drivestat_server 
-ln -s $ENSTORE_DIR/src/drivestat_server.py   $ENSTORE_DIR/sbin/drivestat_server; 
-rm -f $ENSTORE_DIR/sbin/library_manager
-ln -s $ENSTORE_DIR/src/library_manager.py   $ENSTORE_DIR/sbin/library_manager; 
-rm -f $ENSTORE_DIR/sbin/media_changer 
-ln -s $ENSTORE_DIR/src/media_changer.py   $ENSTORE_DIR/sbin/media_changer; 
-rm -f $ENSTORE_DIR/sbin/mover
-ln -s $ENSTORE_DIR/src/mover.py   $ENSTORE_DIR/sbin/mover; 
-rm -f $ENSTORE_DIR/sbin/monitor_server 
-ln -s $ENSTORE_DIR/src/monitor_server.py   $ENSTORE_DIR/sbin/monitor_server; 
+for file in `cat $ENSTORE_DIR/external_distr/sbin_links | fgrep -v "#"`
+do
+    if [ -n ${file} ]
+    then
+	do_update=1
+	if [ $check = "-c" -a -r $ENSTORE_DIR/sbin/${file} ]
+	then
+	    # if we wanted to check for the existance of the file
+	    # and it existed do not update it
+	    do_update=0
+	fi
+	if [ $do_update -ne 0 ]
+	then
+	    rm -f $ENSTORE_DIR/sbin/${file} 
+	    ln -s $ENSTORE_DIR/src/${file}.py $ENSTORE_DIR/sbin/${file}
+	fi
+    fi
+done
+
+###
+### Additional links in bin directory
+for file in `cat $ENSTORE_DIR/external_distr/bin_links | fgrep -v "#"`
+do
+    if [ -n ${file} ]
+    then
+	do_update=1
+	if [ $check = "-c" -a -r $ENSTORE_DIR/bin/${file} ]
+	then 
+	    # if we wanted to check for the existance of the file
+	    # and it existed do not update it
+	    do_update=0
+	fi
+	if [ $do_update -ne 0 ]
+	then
+	    rm -f $ENSTORE_DIR/bin/${file} 
+	    ln -s $ENSTORE_DIR/src/${file}.py $ENSTORE_DIR/bin/${file}
+	fi
+    fi
+done
+#######
+
+do_update=1
+if [ $check = "-c" -a  -r $ENSTORE_DIR/bin/enstore ]
+then 
+    # if we wanted to check for the existance of the file
+    # and it existed do not update it
+    do_update=0
+fi
+if [ $do_update -ne 0 ]
+then
+    rm -f $ENSTORE_DIR/bin/enstore
+    ln -s $ENSTORE_DIR/src/enstore_admin.py $ENSTORE_DIR/bin/enstore
+fi
+######
+
+do_update=1
+if [ $check = "-c" -a -r $ENSTORE_DIR/sbin/enstoreCut ]
+then 
+    # if we wanted to check for the existance of the file
+    # and it existed do not update it
+    do_update=0
+fi
+if [ $do_update -ne 0 ]
+then
+    rm -f $ENSTORE_DIR/sbin/enstoreCut;
+    ln -s $ENSTORE_DIR/sbin/encpCut $ENSTORE_DIR/sbin/enstoreCut
+fi
+######
+
+do_update=1
+if [ $check = "-c" -a -r $ENSTORE_DIR/sbin/enmonitor ]
+then 
+    # if we wanted to check for the existance of the file
+    # and it existed do not update it
+    do_update=0
+fi
+if [ $do_update -ne 0 ]
+then
+    rm -f $ENSTORE_DIR/sbin/enmonitor
+    ln -s $ENSTORE_DIR/src/monitor_client.py  $ENSTORE_DIR/sbin/enmonitor 
+
+fi
+######
+
