a7a81a939a9a8810906c627fb8e8d89a005c9dbb
cvs2svn <UNKNOWN@fnal.gov>
2010-06-23 21:05:35 +0000

    This commit was manufactured by cvs2svn to create tag 'migration_6_23_2010'.
    Sprout from master 2010-06-23 21:05:34 UTC Michael Zalokar <zalokar> 'New switches for migration: --migration-only and --multiple-copy-only'
    Cherrypick from master 2008-06-27 18:52:41 UTC Alexander Moibenko <moibenko> 'new rpm versions':
        databases/control_files/pg_hba.conf-cdfensrv0
        databases/control_files/pg_hba.conf-cdfensrv0-accounting
        databases/control_files/pg_hba.conf-cdfensrv0-drivestat
        databases/control_files/pg_hba.conf-cdfensrv0-enstoredb
        databases/control_files/pg_hba.conf-cdfensrv6-accounting
        databases/control_files/pg_hba.conf-cdfensrv6-drivestat
        databases/control_files/pg_hba.conf-d0ensrv0
        databases/control_files/pg_hba.conf-d0ensrv0-accounting
        databases/control_files/pg_hba.conf-d0ensrv0-drivestat
        databases/control_files/pg_hba.conf-d0ensrv0-enstoredb
        databases/control_files/pg_hba.conf-d0ensrv6-accounting
        databases/control_files/pg_hba.conf-d0ensrv6-drivestat
        databases/control_files/pg_hba.conf-stkensrv0
        databases/control_files/pg_hba.conf-stkensrv0-accounting
        databases/control_files/pg_hba.conf-stkensrv0-drivestat
        databases/control_files/pg_hba.conf-stkensrv0-enstoredb
        databases/control_files/pg_hba.conf-stkensrv6-accounting
        databases/control_files/pg_hba.conf-stkensrv6-drivestat
        external_distr/create_demo_enstore_environment.sh
        external_distr/create_demo_setup_file.sh
        external_distr/install.sh
        external_distr/install_enstore_demo.sh
        external_distr/rpm_postinstall.sh
        release-notes/ENSTORE_RPM_1_0_1_10.notes
        release-notes/ENSTORE_RPM_1_0_1_11.notes
        spec/swig_enstore.spec
        src/cpio_odc_wrapper.py
        src/enstore_plotter_framework.py
        src/web_server.py
    Cherrypick from master 2009-10-22 21:23:46 UTC Dmitry Litvinsev <litvinse> 'added pieces needed for database schema creation and update':
        databases/schemas/ddl/enstoredb/enstoredb_constraints.sql
        databases/schemas/ddl/enstoredb/enstoredb_indexes.sql
        databases/schemas/xml/enstoredb.xml
        src/enstore.py
        src/pnfs_agent.py
    Cherrypick from master 2006-09-08 20:17:09 UTC Michael Zalokar <zalokar> 'Added documentation for alarm descriptions and for removing ghost files.':
        databases/scripts/enstore-drivestat
        databases/scripts/enstore-pgdb
        doc/WWW/removing_ghost_files.html
        etc/enstoredb.schema
    Cherrypick from master 2007-04-10 19:30:15 UTC Alexander Moibenko <moibenko> 'enstore configuration parameters':
        doc/config_params.html
        doc/config_params.txt
    Cherrypick from master 2002-04-03 20:18:43 UTC Chih Hao Huang <huangch> 'finish option coversion':
        src/restore.py
    Cherrypick from master 2007-07-31 21:10:17 UTC Alexander Moibenko <moibenko> 'install real-encp to run on a dcache pool node':
        external_distr/install_dcache_deploy.sh
    Cherrypick from master 2007-08-17 20:50:27 UTC Alexander Moibenko <moibenko> 'complete server install is added':
        sbin/complete_server_install.sh
    Cherrypick from master 2009-01-12 22:27:41 UTC Dmitry Litvinsev <litvinse> 'make sure we specify option type of port number':
        sbin/restore_enstore_database.py
    Cherrypick from master 2010-03-03 23:06:30 UTC Alexander Moibenko <moibenko> 'Fixed bugs per bz 619':
        external_distr/setup-enstore
    Cherrypick from production 2008-08-12 18:26:51 UTC Alexander Moibenko <moibenko> 'run read test in batch. Still needs work':
        test/torture/read_batch_setup.sh
    Delete:
        tools/recover_volume.py

--- databases/schemas/ddl/enstoredb/enstoredb_constraints.sql
+++ databases/schemas/ddl/enstoredb/enstoredb_constraints.sql
@@ -2,7 +2,7 @@ ALTER TABLE ONLY volume
    ADD  CONSTRAINT volume_write_protected_check CHECK ((((write_protected = 'u'::bpchar) OR (write_protected = 'y'::bpchar)) OR (write_protected = 'n'::bpchar)));
 
 --
--- Name: active_file_copying_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: active_file_copying_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY active_file_copying
@@ -10,7 +10,7 @@ ALTER TABLE ONLY active_file_copying
 
 
 --
--- Name: file_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY file
@@ -18,28 +18,38 @@ ALTER TABLE ONLY file
 
 
 --
--- Name: media_capacity_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: media_capacity_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY media_capacity
     ADD CONSTRAINT media_capacity_pkey PRIMARY KEY ("type");
 
+
 --
--- Name: migration_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: migration_dst_bfid_key; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY migration
-    ADD CONSTRAINT migration_pkey PRIMARY KEY  (src_bfid,dst_bfid);
+    ADD CONSTRAINT migration_dst_bfid_key UNIQUE (dst_bfid);
 
 --
--- Name: migration_history_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: migration_history_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY migration_history
     ADD CONSTRAINT migration_history_pkey PRIMARY KEY (src_vol_id, dst_vol_id);
 
+
 --
--- Name: quota_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: migration_src_bfid_key; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
+--
+
+ALTER TABLE ONLY migration
+    ADD CONSTRAINT migration_src_bfid_key UNIQUE (src_bfid);
+
+
+--
+-- Name: quota_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY quota
@@ -47,7 +57,7 @@ ALTER TABLE ONLY quota
 
 
 --
--- Name: quotas_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: quotas_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY quotas
@@ -55,7 +65,7 @@ ALTER TABLE ONLY quotas
 
 
 --
--- Name: sg_count_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: sg_count_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY sg_count
@@ -63,7 +73,7 @@ ALTER TABLE ONLY sg_count
 
 
 --
--- Name: state_type_name_key; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: state_type_name_key; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY state_type
@@ -71,7 +81,7 @@ ALTER TABLE ONLY state_type
 
 
 --
--- Name: state_type_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: state_type_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY state_type
@@ -79,7 +89,7 @@ ALTER TABLE ONLY state_type
 
 
 --
--- Name: volume_label_key; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_label_key; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY volume
@@ -87,7 +97,7 @@ ALTER TABLE ONLY volume
 
 
 --
--- Name: volume_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_pkey; Type: CONSTRAINT; Schema: public; Owner: enstore; Tablespace: 
 --
 
 ALTER TABLE ONLY volume
--- databases/schemas/ddl/enstoredb/enstoredb_indexes.sql
+++ databases/schemas/ddl/enstoredb/enstoredb_indexes.sql
@@ -1,262 +1,251 @@
 
 --
--- Name: bad_file_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: bad_file_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX bad_file_bfid_idx ON bad_file USING btree (bfid);
 
 
 --
--- Name: file_copies_map_alt_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_copies_map_alt_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_copies_map_alt_bfid_idx ON file_copies_map USING btree (alt_bfid);
 
 
 --
--- Name: file_copies_map_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_copies_map_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_copies_map_bfid_idx ON file_copies_map USING btree (bfid);
 
 
 --
--- Name: file_crc_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_crc_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_crc_idx ON file USING btree (crc);
 
 
 --
--- Name: file_deleted_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_deleted_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_deleted_idx ON file USING btree (deleted);
 
 
 --
--- Name: file_location_cookie_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_location_cookie_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_location_cookie_idx ON file USING btree (location_cookie);
 
 
 --
--- Name: file_pnfs_id_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_pnfs_id_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_pnfs_id_idx ON file USING btree (pnfs_id);
 
 
 --
--- Name: file_pnfs_path_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_pnfs_path_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_pnfs_path_idx ON file USING btree (pnfs_path);
 
 
 --
--- Name: file_sanity_crc_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_sanity_crc_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_sanity_crc_idx ON file USING btree (sanity_crc);
 
 
 --
--- Name: file_sanity_size_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_sanity_size_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_sanity_size_idx ON file USING btree (sanity_size);
 
 
 --
--- Name: file_size_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_size_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_size_idx ON file USING btree (size);
 
 
 --
--- Name: file_volume_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: file_volume_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX file_volume_idx ON file USING btree (volume);
 
---
--- Name: migration_dst_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
---
-
-CREATE INDEX migration_dst_bfid_idx ON migration USING btree (dst_bfid);
 
 --
--- Name: migration_src_bfid_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
---
-
-CREATE INDEX migration_src_bfid_idx ON migration USING btree (src_bfid);
-
---
--- Name: migration_history_dst_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: migration_history_dst_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX migration_history_dst_idx ON migration_history USING btree (dst);
 
 
 --
--- Name: migration_history_src_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: migration_history_src_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX migration_history_src_idx ON migration_history USING btree (src);
 
 
 --
--- Name: quota_library_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: quota_library_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX quota_library_idx ON quota USING btree (library);
 
 
 --
--- Name: quota_storage_group_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: quota_storage_group_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX quota_storage_group_idx ON quota USING btree (storage_group);
 
 
 --
--- Name: state_time_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: state_time_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX state_time_idx ON state USING btree ("time");
 
 
 --
--- Name: state_type_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: state_type_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX state_type_idx ON state USING btree ("type");
 
 
 --
--- Name: state_value_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: state_value_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX state_value_idx ON state USING btree (value);
 
 
 --
--- Name: state_volume_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: state_volume_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX state_volume_idx ON state USING btree (volume);
 
 
 --
--- Name: volume_declared_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_declared_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_declared_idx ON volume USING btree (declared);
 
 
 --
--- Name: volume_eod_cookie_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_eod_cookie_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_eod_cookie_idx ON volume USING btree (eod_cookie);
 
 
 --
--- Name: volume_file_family_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_file_family_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_file_family_idx ON volume USING btree (file_family);
 
 
 --
--- Name: volume_label_key_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_label_key_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_label_key_idx ON volume USING btree (label);
 
 
 --
--- Name: volume_last_access_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_last_access_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_last_access_idx ON volume USING btree (last_access);
 
 
 --
--- Name: volume_library_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_library_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_library_idx ON volume USING btree (library);
 
 
 --
--- Name: volume_media_type_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_media_type_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_media_type_idx ON volume USING btree (media_type);
 
 
 --
--- Name: volume_modification_time_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_modification_time_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_modification_time_idx ON volume USING btree (modification_time);
 
 
 --
--- Name: volume_remaining_bytes_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_remaining_bytes_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_remaining_bytes_idx ON volume USING btree (remaining_bytes);
 
 
 --
--- Name: volume_storage_group_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_storage_group_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_storage_group_idx ON volume USING btree (storage_group);
 
 
 --
--- Name: volume_system_inhibit_0_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_system_inhibit_0_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_system_inhibit_0_idx ON volume USING btree (system_inhibit_0);
 
 
 --
--- Name: volume_system_inhibit_1_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_system_inhibit_1_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_system_inhibit_1_idx ON volume USING btree (system_inhibit_1);
 
 
 --
--- Name: volume_user_inhibit_0_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_user_inhibit_0_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_user_inhibit_0_idx ON volume USING btree (user_inhibit_0);
 
 
 --
--- Name: volume_user_inhibit_1_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_user_inhibit_1_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_user_inhibit_1_idx ON volume USING btree (user_inhibit_1);
 
 
 --
--- Name: volume_wrapper_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_wrapper_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_wrapper_idx ON volume USING btree (wrapper);
 
 
 --
--- Name: volume_write_protected_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace:
+-- Name: volume_write_protected_idx; Type: INDEX; Schema: public; Owner: enstore; Tablespace: 
 --
 
 CREATE INDEX volume_write_protected_idx ON volume USING btree (write_protected);
