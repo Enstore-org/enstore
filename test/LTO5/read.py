19ab05a8d882a45c52a9ff83b57e5de51c5460f6
Dmitry Litvinsev <litvinse>
2010-11-13 01:37:33 +0000

    corrected counter print

--- test/LTO5/read.py
+++ test/LTO5/read.py
@@ -10,7 +10,9 @@ import time
 #Q="select f.pnfs_path  from file f where f.volume in (select id from volume v where v.file_family in ('%s') and v.system_inhibit_1='full' and v.label not in ('%s') order by v.label) and f.deleted='n' order by f.location_cookie"
 #Q1="select v.label from volume v where v.system_inhibit_1='full' and v.file_family='%s' order by v.label limit 1"
 
-Q="select f.pnfs_path  from file f where f.volume in (select id from volume v where v.library='%s' and v.file_family='%s' order by v.label limit 1) and f.deleted='n' order by f.location_cookie"
+#Q="select f.pnfs_path  from file f where f.volume in (select id from volume v where v.library='%s' and v.file_family='%s' order by v.label limit 1) and f.deleted='n' order by f.location_cookie"
+
+Q="select f.pnfs_path  from file f where f.volume in (select id from volume v where v.library='%s' order by v.label limit 1) and f.deleted='n' order by f.location_cookie"
 
 t0=time.time()
 
@@ -20,6 +22,7 @@ def random_loop(n,input_list,done_list):
     delta = int((time.time()-t0)/3600./24.)
     n = n - delta
     counter=0
+    input_list_length=len(input_list)
     while len(input_list)>0:
         if os.path.exists(STOP_FILE):
             return 0
@@ -37,8 +40,8 @@ def random_loop(n,input_list,done_list):
             os.system("touch %s"%(STOP_FILE))
             return rc
         counter = counter + 1
-        if counter % 100 :
-            print_message("Completed %4.2f%%"%(float(counter)/float(number_of_files)*100.))
+        if not counter % 100 :
+            print_message("Completed 100 transfers (%4.2f%%)"%(float(counter)/float(input_list_length)*100.))
     time.sleep(120)
     random_loop(n,done_list,input_list)
 
@@ -52,7 +55,8 @@ def read(i,job_config):
                port  = enstoredb.get('db_port', 5432),
                user  = enstoredb.get('dbuser_reader', "enstore_reader"))
 
-    res=db.query(Q%(job_config.get('library'),job_config.get('hostname')))
+    #res=db.query(Q%(job_config.get('library'),job_config.get('hostname')))
+    res=db.query(Q%(job_config.get('library'),))
     if res.ntuples() == 0 :
         print_error("library %s, file_family %s, There are no files to read"%(job_config.get('library'),
                                                                               job_config.get('hostname')))
