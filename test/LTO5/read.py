37a578f4842358d679eab3675eb65d92179401ce
Dmitry Litvinsev <litvinse>
2010-11-15 18:38:28 +0000

    imporve printing

--- test/LTO5/read.py
+++ test/LTO5/read.py
@@ -17,12 +17,12 @@ Q="select f.pnfs_path  from file f where f.volume in (select id from volume v wh
 t0=time.time()
 
 def random_loop(n,input_list,done_list):
-    if n<=0 : return
-    print_message("Doing %d"%(n,))
     delta = int((time.time()-t0)/3600./24.)
-    n = n - delta
+    print_message("Doing %d"%(delta,))
+    if delta > n : return
     counter=0
     input_list_length=len(input_list)
+    start=time.time()
     while len(input_list)>0:
         if os.path.exists(STOP_FILE):
             return 0
@@ -42,6 +42,7 @@ def random_loop(n,input_list,done_list):
         counter = counter + 1
         if not counter % 100 :
             print_message("Completed %d transfers (%4.2f%%)"%(counter, float(counter)/float(input_list_length)*100.))
+        print_message("Completed full pass on the tape %d transfers (100%%), took %d seconds"%(input_list_length,int(time.time()-start)))
     time.sleep(120)
     random_loop(n,done_list,input_list)
 
