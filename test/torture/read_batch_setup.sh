406e5c0e09d936b16ed69afbcb09fc6e7c616ebd
cvs2svn <UNKNOWN@fnal.gov>
2011-01-11 20:49:10 +0000

    This commit was manufactured by cvs2svn to create tag 'ENSTORE_RPM_2_1_1_1'.
    Sprout from master 2011-01-03 18:04:51 UTC Alexander Moibenko <moibenko> 'Version="2.1.1"'
    Cherrypick from master 2008-06-02 14:53:03 UTC Dmitry Litvinsev <litvinse> 'add stken systems to pg_hba.conf of d0 and cdf systems (enstoredb) for migration to work':
        databases/control_files/pg_hba.conf-cdfensrv0
        databases/control_files/pg_hba.conf-cdfensrv0-accounting
        databases/control_files/pg_hba.conf-cdfensrv0-drivestat
        databases/control_files/pg_hba.conf-cdfensrv0-enstoredb
        databases/control_files/pg_hba.conf-cdfensrv6-accounting
        databases/control_files/pg_hba.conf-cdfensrv6-drivestat
        databases/control_files/pg_hba.conf-d0ensrv0
        databases/control_files/pg_hba.conf-d0ensrv0-accounting
        databases/control_files/pg_hba.conf-d0ensrv0-drivestat
        databases/control_files/pg_hba.conf-d0ensrv0-enstoredb
        databases/control_files/pg_hba.conf-d0ensrv6-accounting
        databases/control_files/pg_hba.conf-d0ensrv6-drivestat
        databases/control_files/pg_hba.conf-stkensrv0
        databases/control_files/pg_hba.conf-stkensrv0-accounting
        databases/control_files/pg_hba.conf-stkensrv0-drivestat
        databases/control_files/pg_hba.conf-stkensrv0-enstoredb
        databases/control_files/pg_hba.conf-stkensrv6-accounting
        databases/control_files/pg_hba.conf-stkensrv6-drivestat
        release-notes/ENSTORE_RPM_1_0_1_10.notes
        release-notes/ENSTORE_RPM_1_0_1_11.notes
    Cherrypick from master 2006-09-08 20:17:09 UTC Michael Zalokar <zalokar> 'Added documentation for alarm descriptions and for removing ghost files.':
        databases/scripts/enstore-drivestat
        databases/scripts/enstore-pgdb
        doc/WWW/removing_ghost_files.html
        etc/enstoredb.schema
    Cherrypick from master 2007-04-10 19:30:15 UTC Alexander Moibenko <moibenko> 'enstore configuration parameters':
        doc/config_params.html
        doc/config_params.txt
    Cherrypick from master 2011-01-11 20:49:09 UTC Alexander Moibenko <moibenko> 'Added volume clerk':
        release-notes/ENSTORE_RPM_2_1_1_1.txt
        src/volume_clerk.py
    Cherrypick from master 2002-04-03 20:18:43 UTC Chih Hao Huang <huangch> 'finish option coversion':
        src/restore.py
    Cherrypick from master 2007-07-31 21:10:17 UTC Alexander Moibenko <moibenko> 'install real-encp to run on a dcache pool node':
        external_distr/install_dcache_deploy.sh
    Cherrypick from master 2007-08-17 20:50:27 UTC Alexander Moibenko <moibenko> 'complete server install is added':
        sbin/complete_server_install.sh
    Cherrypick from master 2009-01-12 22:27:41 UTC Dmitry Litvinsev <litvinse> 'make sure we specify option type of port number':
        sbin/restore_enstore_database.py
    Cherrypick from master 2010-07-23 22:30:25 UTC Alexander Moibenko <moibenko> 'Check in local changes':
        etc/gccen.conf
    Cherrypick from production 2008-08-12 18:26:51 UTC Alexander Moibenko <moibenko> 'run read test in batch. Still needs work':
        test/torture/read_batch_setup.sh
    Delete:
        etc/dmsen.conf
        test/LTO5/full_pass.py
        test/LTO5/full_pass.sh
        test/LTO5/lto5.cf
        test/LTO5/mount_dismount.py
        test/LTO5/mount_dismount.sh
        test/LTO5/read.py
        test/LTO5/read.sh
        test/LTO5/utils.py
        test/LTO5/write.py
        test/LTO5/write.sh

--- test/torture/read_batch_setup.sh
+++ test/torture/read_batch_setup.sh
@@ -12,15 +12,17 @@ if [ "${1:-}" = "-p" ] ; then shift; pnfs_path=$1; shift; fi
 if [ "${1:-}" = "-u" ] ; then shift; user=$1; shift; else user=`whoami`;fi
 if [ "${1:-}" = "-d" ] ; then shift; data=$1; shift; fi
 if [ "${1:-}" = "-c" ] ; then shift; node_list=$1; shfit;else node_list=/tmp/`whoami`/node_list;fi
-if [ -z $data ]; then data="/scratch_dcache/cdfcaf";fi
+#if [ -z $data ]; then data="/scratch_dcache/cdfcaf";fi
+if [ -z $data ]; then data=`dirname ${node_list}`/data;fi
 
 if [ ! -f $node_list ]; then echo "no node_list"; exit 1;fi
 cat $node_list | while read node;do echo $node;
-    scp ~/.bashrc  ${user}@${node}:~/
+    scp ~/.bashrc  ${user}@${node}:
     echo AAAA
-    scp read.sh ${user}@${node}:~/bin
-    echo BBB
-    ssh ${user}@${node} "cd $data; rm -f read_$$.out; read.sh $pnfs_path > read_$$.out 2>&1&"
+    ssh ${user}@${node} "mkdir -p ~/bin" 
+    scp -rp read.sh ${user}@${node}:~/bin/read.sh
+    echo BBB 'ssh ${user}@${node} "if [ ! -x ${data} ]; then mkdir -p ${data}; fi; cd ${data}; rm -f read_$$.out; read.sh $pnfs_path > read_$$.out 2>&1&"'
+    ssh ${user}@${node} "if [ ! -x ${data} ]; then mkdir -p ${data}; fi; . /usr/local/etc/setups.sh; setup enstore; cd ${data}; rm -f read_$$.out; ~${user}/bin/read.sh $pnfs_path > read_$$.out 2>&1&"
     echo CCC
 done
 
