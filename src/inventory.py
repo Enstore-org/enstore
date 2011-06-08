e83cf8b681267be98897d5cf922657b294b05d93
Dmitry Litvinsev <litvinse>
2011-06-08 15:13:43 +0000

    typo : replace "Reqested" w/ "Requested" http://uqbar.fnal.gov/reviews/r/329/ http://www-ccf.fnal.gov/Bugzilla/show_bug.cgi?id=979

--- src/inventory.py
+++ src/inventory.py
@@ -365,7 +365,7 @@ def print_volume_quotas_status(volume_quotas, authorized_tapes, output_file, quo
     vq_file.write("Date this listing was generated: %s\n\n" % \
                   time.asctime(time.localtime(time.time())))
 
-    fields = ("    ", "Library", "Storage Group", "Reqested",
+    fields = ("    ", "Library", "Storage Group", "Requested",
               "Authorized", "Quota", "Allocated",
               "Blank", "Used", "Deleted", "Space Used",
               "Active", "Deleted", "Unknown",
