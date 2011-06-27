46fa9327fa1a93694b9618e69e798c2674a9bae3
Alex Kulyavtsev <aik>
2011-06-27 21:59:24 +0000

    Sasha's version

--- src/cache/messaging/file_list.py
+++ src/cache/messaging/file_list.py
@@ -5,41 +5,55 @@
 # $Id$
 #
 ###############################################################################
-
-import uuid
+import types
 
 # enstore imports
 import e_errors
 
-from cache.messaging.messages import MSG_TYPES as mt
-
-class FileListItem(dict):
+#from cache.messaging.messages import MSG_TYPES as mt
+#valid_operations = [mt.ARCHIVE, mt.STAGE, mt.PURGE]
+class FileListItem:
     def __init__(self, bfid = None, nsid = None, path  = None, libraries = None ):
 
         if [bfid, nsid, path, libraries].count(None) != 0:
             raise e_errors.EnstoreError(None, "need bfid, nsid, path, libraries", e_errors.WRONGPARAMETER)
 
-        self["bfid"] = bfid
-        self["nsid"] = nsid
-        self["path"] = path
-        # @todo: check type, libraries is a list
-        self["libraries"] = libraries
-
-# @todo: - implementation
-
-class FileListOp():
-    """ File List Operations 
+        if type(libraries) != types.ListType:
+            raise e_errors.EnstoreError(None, "libraries must be a list", e_errors.WRONGPARAMETER)
+        self.d = {"bfid" : bfid,
+                  "nsid" : nsid,
+                  "path" : path,
+                  "libraries" : libraries,
+                  }
+       
+class FileList:
     """
-    valid_ops = [mt.ARCHIVE, mt.STAGE, mt.PURGE]
-
-class FileList(list):
-    """ File List 
+    File List.
+    This is a wrapper class which checks if the appending item has a correct format.
     """
+    def __init__(self):
+        self.file_list = []
+        
+    def append(self, item):
+        if isinstance(item, FileListItem):
+           self.file_list.append(item.d)
+        else:
+            raise e_errors.EnstoreError(None, "item must be FileListItem", e_errors.WRONGPARAMETER)
 
-    def __init__(self, id=None, *options ):
-        # set random UUID if not given in opts
-        if id is None:
-            id = uuid.uuid4()
-        self.id = id
+    def remove(self, item):
+        self.file_list.remove(item.d)
+        
+    def __str__(self):
+        return "%s"%(self.file_list)
 
-    # @todo enforce list item types as FileListItem
+if __name__ == "__main__":
+    l =  FileList()
+    i = FileListItem(bfid = "CDMS0001", nsid = "0DAF0001", path = "/diska/aaa/f1", libraries= ["lib1", "lib2"])
+    l.append(i)
+    print "L",l
+    
+             
+
+    l.remove(i)
+    print l
+    
