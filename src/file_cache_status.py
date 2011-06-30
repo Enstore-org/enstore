1f878f2c10b36d969d8925a203457e9cf6c789e3
Alexander Moibenko <moibenko>
2011-06-30 21:25:48 +0000

    added archive status

--- src/file_cache_status.py
+++ src/file_cache_status.py
@@ -4,7 +4,14 @@ class CacheStatus:
     CREATED="CREATED"
     PURGING="PURGING"
     PURGED="PURGED"
+    STAGING="STAGING"
     CACHED="CACHED"
+
+class ArchiveStatus:
+    ARCHIVING="ARCHIVING"
+    ARCHIVED="ARCHIVED"
 
 if __name__ == "__main__":
     print CacheStatus.CREATED
+    print ArchiveStatus.ARCHIVED
+    
