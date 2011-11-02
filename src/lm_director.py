d04e06f051e46b5f54db3b9342729d8378ef6656
Alexander Moibenko <moibenko>
2011-11-02 20:04:15 +0000

    Python version of library manager director

---
+++ src/lm_director.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+
+##############################################################################
+#
+# $Id$
+# Library Mnager Director
+##############################################################################
+
+
+'''
+    LMD - python prototype for Enstore File Cache Library Manager Dispatcher core functionality implementation
+'''
+
+# system imports
+import sys
+import time
+import types
+import string
+import multiprocessing
+
+# qpid / amqp
+import qpid.messaging
+import Queue
+
+# enstore imports
+import e_errors
+import enstore_constants
+import generic_server
+import dispatching_worker
+import event_relay_client
+import Trace
+import string
+import lmd_policy_selector
+
+# enstore cache imports
+#from cache.messaging.client import EnQpidClient
+import cache.messaging.client as cmc
+
+MY_NAME = enstore_constants.LM_DIRECTOR
+QPID_BROKER_NAME = "amqp_broker" # this must be in enstore_constants
+
+class LMD(dispatching_worker.DispatchingWorker,
+          generic_server.GenericServer):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, csc, auto_ack=True):
+        '''
+        Constructor
+        '''
+        generic_server.GenericServer.__init__(self, csc, MY_NAME,
+                                              function = self.handle_er_msg)
+        self.shutdown = False
+        self.finished = False
+
+        # get all necessary information from configuration
+        self.lmd_config = self.csc.get(MY_NAME)
+        broker_config = self.csc.get(QPID_BROKER_NAME)
+        proxy_server_config = self.csc.get(self.lmd_config['udp_proxy_server'])
+        
+        self.queue_in = self.lmd_config['queue_in'] # this must be the same as target_addr for LMD.udp_proxy_server
+        self.queue_out = "udp2amq_%s_%s"%(proxy_server_config['hostip'], proxy_server_config['udp_port']) 
+        self.policy_file = self.lmd_config['policy_file']
+        try:
+            self.policy_selector = lmd_policy_selector.Selector(self.policy_file)
+        except Exception, detail:
+            Trace.log(e_errors.ALARM, "Can not create policy selector: %s" %(detail,))
+            sys.exit(-1)
+        
+        Trace.log(e_errors.INFO, "lmd _init in %s out %s"%(self.queue_in, self.queue_out))
+        #self.qpid_client = cmc.EnQpidClient(amq_broker=(broker_config['host'], broker_config['port']), self.queue_in, self.queue_out)
+        
+        queue_in = "%s; {create: always}"%(self.queue_in,)
+        queue_out = "%s; {create: always}"%(self.queue_out,)
+        self.qpid_client = cmc.EnQpidClient((broker_config['host'], broker_config['port']), queue_in, queue_out)
+        self.auto_ack = auto_ack 
+
+
+        dispatching_worker.DispatchingWorker.__init__(self, (self.lmd_config['hostip'],
+	                                              self.lmd_config['port']))
+
+	self.erc = event_relay_client.EventRelayClient(self)
+	self.erc.start_heartbeat(enstore_constants.CONFIG_SERVER, 
+				 enstore_constants.CONFIG_SERVER_ALIVE_INTERVAL)
+
+    ##############################################
+    #### Configuration related methods
+    ##############################################
+    # reload policy when this method is called
+    # by the request from the client
+    def reload_policy(self, ticket):
+        try:
+            self.policy_selector.read_config()
+            ticket['status'] = (e_errors.OK, None)
+            self.send_reply_with_long_answer(reply)
+        except Exception, detail:
+            ticket['status'] = (e_errors.ERROR, "Error loading policy for LMD: %s"%(detail,))
+            self.reply_to_caller(ticket)
+            
+
+    # send current policy to client
+    def show_policy(self, ticket):
+        try:
+            ticket['dump'] = self.policy_selector.policydict
+            ticket['status'] = (e_errors.OK, None)
+            self.send_reply_with_long_answer(ticket)
+        except Exception, detail:
+            ticket['status'] = (e_errors.ERROR, "Error %s"%(detail,))
+            self.reply_to_caller(ticket)
+
+    def get_library_manager(self, ticket):
+        result = self.lmd_decision(ticket)
+        self.reply_to_caller(result)
+
+    def _fetch_enstore_ticket(self):
+        try:
+            return self.qpid_client.rcv.fetch()
+        except Queue.Empty:
+            return None
+        except qpid.messaging.ReceiverError, e:
+            Trace.trace(10, "_fetch_enstore_ticket exception %s"%(e,))
+            return None
+
+    def _ack_enstore_ticket(self, msg):
+        try:
+            Trace.trace(10, "lmd _ack_enstore_ticket(): sending acknowledge")
+            self.qpid_client.ssn.acknowledge(msg)         
+        except:
+            exc, emsg = sys.exc_info()[:2]
+            Trace.trace(10, "lmd _ack_enstore_ticket(): Can not send auto acknowledge for the message. Exception e=%s msg=%s" % (str(exc), str(emsg)))    
+            pass
+
+    ##############################################################################
+    # Ticket Processing logic
+    def lmd_decision(self, ticket):
+        Trace.trace(10, "lmd_decision")         
+        if type(ticket) != types.DictType:
+            Trace.trace(10, "lmd serve_qpid()  - ticket is not dictionary type, ticket %s." % (ticket,))
+            result['status'] = (e_errors.LMD_WRONG_TICKET_FORMAT, 'LMD: ticket is not dictionary type')
+            return result
+        result = ticket
+        # create a copy of the original library
+        result['original_library'] = result['vc']['library']
+        Trace.trace(10, "lmd_decision1 %s"%(result,))         
+
+        try:
+            rc, new_library = self.policy_selector.match_found(ticket)
+        except:
+            exc, msg, tb = sys.exc_info()
+            Trace.handle_error(exc, msg, tb)
+            del(tb)
+            
+        Trace.trace(10, "lmd_decision2 rc=%s lm=%s"%(rc, new_library,))         
+        if rc:
+            result['vc']['library'] = new_library
+        result['status'] = (e_errors.OK, None)    
+        return result
+
+##############################################################################
+        
+    def serve_qpid(self):
+        """
+        read qpid messages from queue
+        """
+        print "SERVE_QPID"
+        try:
+            self.qpid_client.start()
+        except Exception, detail:
+            Trace.log(e_errors.ERROR, "Can not start qpid server: %s"%(detail,))
+            sys.exit(1)
+        
+        print "SERVE_QPID1"
+        try:
+            while not self.shutdown:
+                # Fetch message from qpid queue
+                message =  self._fetch_enstore_ticket()
+                if not message:
+                    continue
+                Trace.trace(10, "lmd serve_qpid()  - got encp message=%s" %(message,))
+
+                ticket = message.content
+
+                ##################
+                # Process ticket #
+                ##################
+                reply = None
+                try:
+                    Trace.trace(10, "serve_qpid: ticket %s"%(ticket,))
+                    result = self.lmd_decision(ticket)
+                    Trace.trace(10, "serve_qpid: decision %s"%(result,))
+
+                    reply = qpid.messaging.Message(result, correlation_id=message.correlation_id )      
+                except Exception, detail:
+                    Trace.log(e_errors.ERROR, "Exception processing encp request %s: %s"%(detail, message))
+
+                # send reply to encp
+                try:
+                    if reply :
+                        self.qpid_client.send(reply)
+                        Trace.trace(10, "serve_qpid: reply sent %s"%(reply,))
+                except qpid.messaging.SendError, e:
+                    Trace.trace(10, "serve_qpid: exception sending reply  %s"%(e,))
+                    continue
+                    
+                # Acknowledge ORIGINAL ticket so we will not get it again
+                self._ack_enstore_ticket(message)
+
+        # try / while 
+        finally:
+            self.qpid_client.stop()
+
+
+
+    def start(self):
+        #dispatching_worker.run_in_thread(thread_name="start_qpid_server",
+        #                                 function=self.start_qpid_server)
+        self.start_qpid_server()
+        
+
+    def start_qpid_server(self):
+        # start qpid server in separate process (we may add more processes reading the same queue)
+        print "START QPID SERVER"
+        self.qpid_proc = multiprocessing.Process(target=self.serve_qpid) 
+        self.qpid_proc.start()
+
+    def stop_qpid_server(self):
+        # tell serving thread to stop and wait until it finish    
+        print "STOP QPID SERVER"
+        self.shutdown = True
+        
+        self.qpid_client.stop()
+        self.qpid_proc.join()
+        sys.exit(0)
+        
+
+class LMDInterface(generic_server.GenericServerInterface):
+    pass
+
+if __name__ == "__main__":    
+    Trace.init(string.upper(MY_NAME))
+    # get the interface
+    intf = LMDInterface()
+    lmd = LMD((intf.config_host, intf.config_port))
+    lmd._do_print({'levels':[10]})
+    # start qpid server
+    lmd.start_qpid_server()
+    
+    lmd.handle_generic_commands(intf)
+
+    while 1:
+        try:
+            Trace.log(e_errors.INFO, "Library Manager Diriector (re)starting")
+            lmd.serve_forever()
+        except SystemExit, exit_code:
+            lmd.stop_qpid_server()
+            Trace.log(e_errors.INFO, "Library Manager Diriector Exiting %s"%(exit_code,))
+            
+    Trace.trace(e_errors.ERROR,"Library Manager Diriector finished (impossible)")
+
---
+++ src/lmd_policy_selector.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+
+###############################################################################
+# $Id$
+# Library Manager Directior policy selector. 
+# This module is based on the approach in discipline.py
+# Lots of coding was copied from there
+# The purpose of this module is to return a library manager name based
+# on a search in the policy described in some python dictionary
+###############################################################################
+# system imports
+import os
+import re
+import copy
+import string
+import types
+# enstore imports
+import e_errors
+import Trace
+
+# Style of policy entry
+# Dictinary of rules and resulting library managers
+# Each value can be a regular expression
+#'LTO3.library_manager':{1: {'rule': {'storage_group': 'G1',
+#                                          'file_family': 'F1',
+#                                          'wrapper':'cpio_odc'
+#                                     }
+#                            'minimal_file_size': 2000000000L
+#                                          }
+#                            'resulting_library': 'new_library'
+#                            }
+#                        2: .....
+#
+#                         }   
+                            
+class Selector:
+
+    # Read policy file
+    # @return - (e_errors_OK, None) or (e_errors.ERROR, "description)
+    def read_config(self):
+        Trace.log(e_errors.INFO, "(Re)loading LMD Policy")
+        self.policydict = {}
+        f = open(self.policy_file,'r')
+        code = string.join(f.readlines(),'')
+
+        # Lint hack, otherwise lint can't see where configdict is defined.
+        policydict = {}
+        del policydict 
+        policydict = {}
+
+        exec(code)
+        # ok, we read entire file - now set it to real dictionary
+        self.policydict = policydict
+
+        # tickets to lmd_policy engine come via qpid
+        # so, all strings are in UTF-8
+    
+    def __init__(self, policy_file):
+        self.policy_file = policy_file
+        self.read_config()
+        
+
+
+    # match value from key, value pair with valule from conf_key, value pair
+    # @param - ticket providing key, value pair
+    # @param - policy entry in the the policy configuration
+    # @param - key to match
+    def ticket_match(self, ticket, policy, key):
+        try:
+            pattern = "^%s" % (policy[key])
+            item='%s'%(ticket.get(key, 'Unknown'),)
+            return re.search(pattern, item)
+        except Exception, detail :
+            Trace.log(e_errors.ERROR,"parse errorr: %s"%(detail,))
+            return False
+
+
+    def make_flat_ticket(self, ticket):
+        flat_ticket=copy.deepcopy(ticket)
+        if ticket.has_key('wrapper'):
+            # ticket has a wrapper dictionary in vc subditionary
+            # so, copy this one separately.
+            for key in flat_ticket['wrapper'].keys():
+                 flat_ticket[key] = flat_ticket['wrapper'][key]
+            del(flat_ticket['wrapper'])
+        rticket = {}
+        for key in flat_ticket.keys():
+            if type(flat_ticket[key]) is type({}):
+                for k in flat_ticket[key].keys():
+                    if k == 'machine': flat_ticket['host'] = flat_ticket[key][k][1]
+                    else: flat_ticket[k] = flat_ticket[key][k]
+                del(flat_ticket[key])
+
+        # tickets to lmd_policy engine come via qpid
+        # so, all strings are in UTF-8
+        # convert them into regular strings
+
+        for key in flat_ticket.keys():
+            k = key
+            if type(key) == types.UnicodeType:
+                k = key.encode("utf-8")
+            v = flat_ticket[k]
+            if type(flat_ticket[k]) == types.UnicodeType:
+                v = flat_ticket[k].encode("utf-8")
+            rticket[k] = v
+        return rticket
+
+    # @param - ticket to match
+    # @return - (True/False, Library/None)
+    def match_found(self, ticket):
+        # returns a tuple:
+        # True if match was found, False - if not
+        # library manager name, None othrewise
+        
+        # default match settings
+        match = False, None
+        failed = False
+
+        if not self.policydict:  # no policy configuration info
+            return match
+
+        # make a "flat" copy of ticket
+        flat_ticket = self.make_flat_ticket(ticket)
+        Trace.trace(10, "FLAT TICKET:%s"%(flat_ticket,))
+        library = flat_ticket['library']
+        library_manager = library + ".library_manager"
+        if self.policydict.has_key(library_manager):
+            rules = self.policydict[library_manager].keys() # these are policy numberc
+            for rule in rules:
+                # start matching
+                policy_keys = self.policydict[library_manager][rule]['rule'] # these are plicy numbers
+                rule_dict = self.policydict[library_manager][rule]['rule']
+                nkeys = len(policy_keys)
+                nmatches = 0
+                for policy in policy_keys:
+                    if not self.ticket_match(flat_ticket, rule_dict, policy): break
+                    nmatches = nmatches + 1
+                if nmatches == nkeys:
+                    # match found
+                    # check the file size:
+                    if ticket['file_size'] < self.policydict[library_manager][rule]['minimal_file_size']:
+                        match = (True, self.policydict[library_manager][rule]['resulting_library'])
+                        return match
+        return match
+
+if __name__ == "__main__":
+    import socket
+    import configuration_client
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    r = Selector('/home/enstore/policy_files/lmd_policy.py')
+    print "DICT", r.policydict
+    ticket={'lm': {'address': (ip, 7520)}, 'unique_id': '%s-1005321365-0-28872'%(host,), 'infile': '/pnfs/rip6/happy/mam/aci.py',
+            'bfid': 'HAMS100471636100000', 'mover': 'MAM01.mover', 'at_the_top': 3, 'client_crc': 1, 'encp_daq': None,
+            'encp': {'delayed_dismount': None, 'basepri': 1, 'adminpri': -1, 'curpri': 1, 'agetime': 0, 'delpri':0},
+            'fc': {'size': 1434L, 'sanity_cookie': (1434L, 657638438L), 'bfid': 'HAMS100471636100000', 'location_cookie':
+                   '0000_000000000_0000001', 'address': ('131.225.84.122', 7501), 'pnfsid': '00040000000000000040F2F8',
+                   'pnfs_mapname': '/pnfs/rip6/volmap/alex/MM0001/0000_000000000_0000001', 'drive':
+                   'happy:/dev/rmt/tps0d4n:0060112307', 'external_label': 'MM0001', 'deleted': 'no', 'pnfs_name0':
+                   '/pnfs/rip6/happy/mam/aci.py', 'pnfsvid': '00040000000000000040F360', 'complete_crc': 657638438L,
+                   'status': ('ok', None)},
+            'file_size': 1434, 'outfile': '/dev/null', 'volume': 'MM0001',
+            'times': {'t0': 1005321364.951048,'in_queue': 14.586493015289307, 'job_queued': 1005321365.7764519, 'lm_dequeued': 1005321380.363162},
+            'version': 'v2_14  CVS $Revision$ ', 'retry': 0, 'work': 'read_from_hsm', 'callback_addr': ('131.225.13.132', 1463),
+            'wrapper': {'minor': 5, 'inode': 0, 'fullname': '/dev/null', 'size_bytes': 1434, 'rmajor': 0, 'mode': 33268,
+                        'pstat': (33204, 71365368, 5L, 1, 6849, 5440, 1434, 1004716362, 1004716362, 1004716329), 'gname': 'hppc',
+                        'sanity_size': 65536, 'machine': ('Linux', 'gccensrv2.fnal.gov', '2.2.17-14', '#1 Mon Feb 5 18:48:50 EST 2001', 'i686'),
+                        'uname': 'moibenko', 'pnfsFilename': '/pnfs/rip6/happy/mam/aci.py', 'uid': 6849, 'gid': 5440, 'rminor': 0, 'major': 0},
+            'vc': {'first_access': 1004716170.54972, 'sum_rd_err': 0, 'last_access': 1004741744.274856, 'media_type': '8MM',
+                   'capacity_bytes': 5368709120L, 'declared': 1004474612.7774431, 'remaining_bytes': 20105625600L,
+                   'wrapper': 'cpio_odc', 'external_label': 'MM0001', 'system_inhibit': ['none', 'none'],
+                   'user_inhibit': ['none', 'none'], 'current_location': '0000_000000000_0000001', 'sum_rd_access': 7,
+                   'volume_family': 'D0.alex.cpio_odc', 'address': ('131.225.84.122', 7502), 'file_family': 'alex',
+                   'sum_wr_access': 2, 'library': 'mam', 'sum_wr_err': 1, 'non_del_files': 1, 'blocksize': 131072,
+                   'eod_cookie': '0000_000000000_0000002', 'storage_group': 'D0', 'status': ('ok', None)},
+            'status': ('ok', None)
+            }
+    ticket1 = {'vc':
+               {'storage_group': 'ANM', 'library': 'LTO3', 'file_family': 'gcc1', 'wrapper': 'cpio_odc', 'address': ('131.225.13.187', 7502), 'file_family_width': '1'},
+               'outfilepath': '/pnfs/data2/test/moibenko/LTO3/regression_test/encp_test/D3',
+               'encp':
+               {'delpri': 0, 'basepri': 1, 'adminpri': -1, 'delayed_dismount': None, 'agetime': 0},
+               'file_size': 4084853254L, 'ignore_fair_share': None, 'retry': None,
+               'wrapper':
+               {'major': 0, 'rminor': 0, 'pnfsFilename': '/pnfs/data2/test/moibenko/LTO3/regression_test/encp_test/D3', 'uid': 5744, 'uname': 'enstore', 'type': 'cpio_odc', 'mtime': 1319666280, 'rmajor': 0, 'machine': ('Linux', 'enmvr050.fnal.gov', '2.6.18-238.5.1.el5', '#1 SMP Tue Mar 1 18:58:43 EST 2011', 'x86_64'), 'sanity_size': 65536, 'gid': 6209, 'pstat': (16893, 36872184, 22L, 1, 5744, 6209, 512, 1319494584, 1319494584, 1300483574), 'mode_octal': '0100036', 'mode': 33188, 'gname': 'enstore', 'size_bytes': 4084853254L, 'fullname': '/opt/scratch/DEBUGLOG-2011-07-06', 'inode': 38134800, 'minor': 22},
+               'version': 'v3_10c CVS $Revision$ encp',
+               'encp_daq': None, 'client_crc': 1,
+               'r_a': (('131.225.204.151', 40811), 1L, '131.225.204.151-40811-1319666285.627585-1783-46976464740336'),
+               'crc_seed': 0,
+               'infile': '/opt/scratch/DEBUGLOG-2011-07-06',
+               'outfile': '/pnfs/data2/test/moibenko/LTO3/regression_test/encp_test/.(access)(00020000000000000045E410)',
+               'fc': {'pnfsid': '000200', 'unique_id': 'enmvr050.fnal.gov-1319666280-1783-0'}}
+    
+    ticket2 = ticket1
+    ret = r.match_found(ticket)
+    print "Match result", ret
+    print "========================================"
+    ret = r.match_found(ticket1)
+    
+    print "Match result", ret
+
+    ticket2['file_size'] = 1000000
+    print "========================================"
+    ret = r.match_found(ticket2)
+    
+    print "Match result", ret
+    
