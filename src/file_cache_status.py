b30bbabc87d8f6cadd11c9b9865c300bb60b715f
Alexander Moibenko <moibenko>
2011-11-10 23:26:26 +0000

    Added state FAILED

--- src/file_cache_status.py
+++ src/file_cache_status.py
@@ -7,10 +7,12 @@ class CacheStatus:
     STAGING="STAGING"
     CACHED="CACHED"
     STAGING_REQUESTED="STAGING_REQUESTED"
+    FAILED="FAILED"
 
 class ArchiveStatus:
     ARCHIVING="ARCHIVING"
     ARCHIVED="ARCHIVED"
+    FAILED="FAILED"
 
 if __name__ == "__main__":
     print CacheStatus.CREATED
