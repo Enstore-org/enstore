5224decada60ea6d34a782e2af02179c0e62b63e
Alexander Moibenko <moibenko>
2011-12-02 21:24:24 +0000

    Merged with production (1.13) and resolved conflicts

--- src/enstore_functions3.py
+++ src/enstore_functions3.py
@@ -249,21 +249,26 @@ def extract_file_number(location_cookie):
         
     return None
 
-
-# convert file id (pnfs id)
-# to local data file path
+# generate file path from string of hexadecimal characters representing file ID
 def file_id2path(root, file_id):
     # based on Alex suggestion
     # for even distrbution of files in directories
+    # The file_id is presented by a string of hexadecimal characters
     # example:
-    #root = "/data_files"
-    #file_id = "00001E9281CFB7054652B62737ED1ED3B3F6"
-    #return value:
-    # "/data_files/3816/3387/00001E9281CFB7054652B62737ED1ED3B3F6"
+    # 1. chimera ID
+    # root = "/data_files"
+    # file_id = "00001E9281CFB7054652B62737ED1ED3B3F6"
+    # return value:
+    # "/data_files/ee8/d3b/00001E9281CFB7054652B62737ED1ED3B3F6"
+    # 2. pnfs ID
+    # root = "/data_files"
+    # file_id = "00020000000000001141B638"
+    # return value:
+    # " /data_files/629/41b/00020000000000001141B638"
 
     file_id_hex = int("0x"+file_id, 16)
-    first = "%s"%((file_id_hex & 0xFFF) ^ ( (file_id_hex >> 24) & 0xFFF),)
-    second = "%s"%((file_id_hex>>12) & 0xFFF,)
+    first = "%03x"%((file_id_hex & 0xFFF) ^ ( (file_id_hex >> 24) & 0xFFF),)
+    second = "%03x"%((file_id_hex>>12) & 0xFFF,)
     path = os.path.join(root, first, second, file_id) 
     
     return path
