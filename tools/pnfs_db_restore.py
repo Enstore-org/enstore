9c582a827e436e0bab036460790765715b87be39
Alexander Moibenko <moibenko>
2011-12-05 16:50:28 +0000

    merged with production (1.34)

--- tools/pnfs_db_restore.py
+++ tools/pnfs_db_restore.py
@@ -1,37 +1,127 @@
 #!/usr/bin/env python
+###############################################################################
+#
+# This script is to be used to restore and start pnfs from live backup
+#
+#
 # $Id$
+###############################################################################
 import os
 import sys
-import popen2
 import string
 import pwd
 import getopt
 import time
 import types
 
+import re
+
 import configuration_client
 import enstore_constants
+import e_errors
+import file_utils
 
-sys2host={'cms': ('cmspnfs1', 'psql-data'),
-          'eag':('eagpnfs1','psql-data'),
-          'sdss': ('?????', '?????'),
+sys2host={'cms':  ('cmspnfs1', 'psql-data'),
+          'd0en': ('d0ensrv1n', 'psql-data'),
+          'stken': ('stkensrv1n', 'psql-data'),
+          'cdfen': ('cdfensrv1n', 'psql-data'),
           }
 
-version2version={'v8_0_7' : '8.0',
-                 'v8_1_3' : '8.1',
-                 'v8_2_6' : '8.2',
-                 'v8_3_0' : '8.3',
-                 }
+PSQL_COMMAND = "psql -U enstore postgres -c 'select datname from pg_database' 2> /dev/null"
 
-postgres_version = 'v8_1_3'
+def copy(source,destination):
+    try:
+        s = open(source,"r")
+        d = open(destination,"w")
+        for l in s:
+            d.write(l)
+        return 0
+    except Exception, msg:
+        sys.stderr.write("Got exception %s copying %s to %s"%(str(msg),source,destination,))
+        sys.stderr.flush()
+        return 1
+
+
+class PnfsSetup:
+    # find uncommented lines that look like
+    # " var = /value "
+    # there could be arbitraty number of white spaces between words
+    #
+    MATCH=re.compile("^[\s]*[^#]+[\s]*[\w]+[\s]*=[\s]*[\w\-/~]+")
+    # default pnfsSetup file name
+    NAME="pnfsSetup"
+    # default destination directory
+    DEFAULT_LOCATION="/usr/etc"
+    # mandatory keys:
+    REMOTE_BACKUP="remotebackup"
+    TRASH="trash"
+    DATABASE_POSTGRES="database_postgres"
+    DATABASE="database"
+    PNFS="pnfs"
+    def __init__(self,fname):
+        f=open(fname,"r")
+        self.contents={}
+        self.pnfs_dir = "/opt/pnfs"
+        for line in f:
+            if not line : continue
+            if PnfsSetup.MATCH.match(line):
+                data = line.split("=")
+                try:
+                    ls=data[0].strip()
+                    rs=string.join(data[1:],"=").strip()
+                    if ls==PnfsSetup.REMOTE_BACKUP:
+                        ls="#"+ls
+                        self.remote_backup_host, self.remote_backup_dir = rs.split("#")[0].strip().split(":")
+                    self.contents[ls]=rs
+                except Exception, msg:
+                    sys.stderr.write("Got exception %s parsing line %s"%(str(msg),line))
+		    sys.stderr.flush()
+		    pass
+        f.close()
+        pgdb = self.contents[PnfsSetup.DATABASE_POSTGRES].split("#")[0].strip().rstrip("/")
+        self.backup_name = os.path.basename(pgdb.rstrip("/"))
+
+    def __repr__(self):
+        s=""
+        for k, v in self.contents.iteritems():
+            s+="%s=%s\n"%(k,v)
+        return s
+
+    def __str__(self):
+        return self.__repr__()
+
+    def write(self,directory=None):
+        if not directory :
+            directory = PnfsSetup.DEFAULT_LOCATION
+        f=open(os.path.join(directory,PnfsSetup.NAME),"w")
+        f.write(str(self))
+        f.close()
+
+    def __setitem__(self,key,value):
+        if self.contents.has_key(key):
+            self.contents[key]=value
+        else:
+            raise KeyError, "No such key %s"%(key,)
+
+    def __getitem__(self,key):
+        return self.contents[key]
+
+def checkPostgres():
+	psqlFile = os.popen(PSQL_COMMAND)
+	psqlOutput = psqlFile.readlines()
+	psqlFile.close()
+	if not psqlOutput:
+		#sys.stderr.write('Postgres server is not running properly yet\n')
+		return False
+	return True
 
 class PnfsDbRestore:
 
     def __init__(self):
         self.config   = configuration_client.get_config_dict()
-        self.systems = self.config.get('known_config_servers')
+        self.systems  = self.config.get('known_config_servers',{})
         self.pnfs_host = None
-        
+
     def get_configuration_client(self):
         return self.config
 
@@ -45,13 +135,9 @@ class PnfsDbRestore:
         return list
 
     def usage(self,cmd):
-        print "Usage: %s -s [--system=] -t [backup_time=] -p [--postgres_version=]"%(cmd,)
+        print "Usage: %s -s [--system=] -t [backup_time=] "%(cmd,)
         print "\t allowed systems: ", self.get_enstore_systems()
-        print "\t specify timestamp YYYY-MM-DD to get backup up to certain date" 
-        versions=""
-        for p in version2version.keys():
-            versions=versions+p+","
-        print "\t allowed postgres versions: "+versions[:-1]
+        print "\t specify timestamp YYYY-MM-DD to get backup up to certain date"
 
     def recover(self,name,backup_time=None):
         pnfsSetupFile=os.path.join(os.getenv("ENSTORE_DIR"),"etc/%s-pnfsSetup"%(name))
@@ -61,52 +147,72 @@ class PnfsDbRestore:
             if s != name : continue
             server_name,server_port = self.systems.get(s)
             config_server_client   = configuration_client.ConfigurationClient((server_name, server_port))
-            if config_server_client.get('pnfs_server',None) != None:
+            pnfs_server_dict=config_server_client.get('pnfs_server')
+            if pnfs_server_dict["status"][0] == e_errors.KEYERROR:
+                self.pnfs_host = sys2host[name][0]
+            elif  pnfs_server_dict["status"][0] ==e_errors.OK:
                 self.pnfs_host=config_server_client.get('pnfs_server').get('host')
-        cmd = "cp %s /tmp/pnfsSetup.%s"%(pnfsSetupFile, self.pnfs_host)
-        os.system(cmd)
-        pnfs_db, pgdb, trash, backup_host, backup_dir, pnfs_dir, backup_name = get_config(self.pnfs_host)
-
-        cmd='umount /pnfs/fs'
-        print 'Unmounting pnfs: %s'% (cmd,)
-        os.system(cmd)
-
-        cmd='/etc/init.d/pnfs stop'
-        print "Stopping pnfs: %s"% (cmd,)
-        os.system(cmd)
-
-        cmd='/etc/init.d/postgres-boot stop'
-        print "Stopping DB server: %s"% (cmd,)
-        os.system(cmd)
-
-        backup_file = get_backup(backup_host, backup_dir,  backup_name)
-        # go to a proper directory
+            else:
+                print "Failed to communicate with config server ",pnfs_server_dict["status"]
+                return 1
+	#
+	# read configuration file
+	#
+	pnfsSetup = PnfsSetup(pnfsSetupFile)
+	#
+	# write modified configuration file in place (/usr/etc/pnfsSetup)
+	#
+	pnfsSetup.write()
+        #
+        # copy pnfs_wrapper and postgres_check in place
+        #
+        pnfs_wrapper   = os.path.join(os.getenv("ENSTORE_DIR"),"tools/pnfs_wrapper")
+        pnfs_wrapper_destination = "/etc/init.d/pnfs_wrapper"
+        if not os.path.exists(pnfs_wrapper_destination):
+            rc=copy(pnfs_wrapper,pnfs_wrapper_destination)
+            if rc != 0 :
+                sys.exit(1)
+        postgres_check = os.path.join(os.getenv("ENSTORE_DIR"),"tools/postgres_check")
+        postgres_check_destination = "%s/tools/postgres_check"%(pnfsSetup["pnfs"])
+        if not os.path.exists(postgres_check_destination):
+            rc=copy(postgres_check,postgres_check_destination)
+            if rc != 0 :
+                sys.exit(1)
+        for cmd in ["/sbin/service pnfs_wrapper stop",\
+                    "umount -f /pnfs/fs", \
+                    "/sbin/service postgresql stop"]:
+            print "Executing command ",cmd
+            rc=os.system(cmd)
+            if rc != 0 :
+                sys.stderr.write("Failed to execute command '%s' ignoring\n"%(cmd,))
+                sys.stderr.flush()
+        backup_file = get_backup(pnfsSetup)
+	pnfs_db = pnfsSetup[pnfsSetup.DATABASE]
         if not os.path.exists(pnfs_db):
             os.makedirs(pnfs_db)
         else:
-           os.rename(pnfs_db,"%s-%s.%d"%(pnfs_db,
+            parent = pnfs_db
+            for i in range(2):
+                parent = os.path.dirname(parent)
+            os.rename(parent,"%s-%s.%d"%(parent,
                                          time.strftime("%b-%d-%Y",time.localtime()),
                                          os.getpid()))
-           os.makedirs(pnfs_db)
+            os.makedirs(pnfs_db)
 
         os.chdir(os.path.dirname(pnfs_db))
         cwd=os.getcwd()
 
-        # clean directories if exist
+	for d in ['%s/log'%(os.path.dirname(cwd),),\
+		  pnfsSetup[pnfsSetup.TRASH]]:
+            if os.path.exists(d):
+                file_utils.rmdir(d)
+            print "Creating directory ",d
+            os.makedirs(d)
 
-        print 'Cleaning %s'% (cwd,)
-        os.system('rm -rf *')
-        d='%s/log'%(os.path.dirname(cwd),)
-        print 'Recreating %s'% (d,)
-        os.system('rm -rf %s'% (d,))
-        os.system('mkdir -p %s'% (d,))
-        d='%s/trash'%(os.path.dirname(cwd),)
-        print 'Recreating %s'% (d,)
-        os.system('rm -rf %s'% (d,))
-        os.system('mkdir -p %s' %(d,)) 
+        # copy backup file over
 
-        #copy a backup file
-        cmd = "/usr/bin/rsync -e rsh  %s:%s . "%(backup_host, backup_file)
+        cmd = "/usr/bin/rsync -e rsh  %s:%s . "%(pnfsSetup.remote_backup_host,
+						 backup_file)
         print 'copying: %s'% (cmd,)
         err=os.system(cmd)
         if err:
@@ -116,65 +222,61 @@ class PnfsDbRestore:
         print 'Extracting %s with: %s'% (os.path.basename(backup_file), cmd)
         os.system(cmd)
 
+        # fill in the database location in sysconfig file
+
+        psql_data=pnfsSetup[PnfsSetup.DATABASE_POSTGRES]
+        f=open("/etc/sysconfig/pgsql/postgresql","w")
+        f.write("PGDATA=%s\n"%(psql_data))
+        f.close()
+
+
         # create recovery.conf
-        rdir = '%s:%s.xlogs'% (backup_host, os.path.dirname(backup_file))
-        cmd = "restore_command = '/home/enstore/enstore/sbin/enrcp %s/"% (rdir) + "%f.Z %p.Z" + " && uncompress %p.Z'"
+        rdir = '%s:%s.xlogs'% (pnfsSetup.remote_backup_host,
+			       os.path.dirname(backup_file))
+        cmd = "restore_command = '%s/sbin/gettkt && %s/sbin/enrcp %s/"% (os.getenv("ENSTORE_DIR"),
+									 os.getenv("ENSTORE_DIR"),
+									 rdir) + "%f.Z %p.Z" + " && uncompress %p.Z'"
+	pgdb = pnfsSetup[PnfsSetup.DATABASE_POSTGRES]
         print 'Creating recovery.conf: %s'% (cmd, )
         f=open('%s/recovery.conf'%(pgdb,), 'w')
         f.write('%s\n'%(cmd,))
         if (backup_time != None) :
             f.write("recovery_target_time='%s'\n"%(backup_time,))
-            f.write("recovery_target_inclusive='true'\n")        
+            f.write("recovery_target_inclusive='true'\n")
         f.close()
         os.system("cat %s/recovery.conf"%(pgdb))
-        cmd = 'sed -i "s/archive_command/#archive_command/g" %s/postgresql.conf '% (pgdb,)
-        os.system(cmd)
-#        cmd = 'sed -i "s# *shared_buffers*=*.*#shared_buffers = 126976 #" %s/postgresql.conf'% (pgdb,)
-#        os.system(cmd)
 
-        #create xlog dirs
-        cmd = 'mkdir -p %s/pg_xlog/archive_status'% (pgdb,)
-        print 'Creating xlog dirs: %s'% (cmd, )
-        os.system(cmd)
-        cmd='chown -R enstore.enstore %s'% (pgdb,)
-        print 'Changing Ownership: %s'% (cmd,)
-        os.system(cmd)
+	for cmd in ['sed -i "s/archive_command/#archive_command/g" %s/postgresql.conf '% (pgdb,), \
+                    'sed -i "s/^[ \t\r]*archive_mode[ \t\r]*=[ \t\r]*on/archive_mode = off/g" %s/postgresql.conf '% (pgdb,),\
+		    "mkdir -p %s/pg_xlog/archive_status"% (pgdb,),\
+		    "chown -R enstore.enstore %s"% (pgdb,),\
+                    "rm -f %s/postmaster.pid"%(pgdb),\
+                    "/sbin/service postgresql start"]:
+            rc = os.system(cmd)
+            if rc != 0 :
+                sys.stderr.write("Command %s failed, bailing out "%(cmd,))
+                sys.stderr.flush()
+                return rc
 
-        ## print "the following steps need to be made manually to ensure that all is rigth" 
-        ## print "Now start postgreSQL and pnfs. Mount /pnfs/fs"
-        ## print "postgreSQL can be started by /etc/init.d/postgres-boot start"
-        ## print "pnfs can be started by /etc/init.d/pnfs start"
+        rc = checkPostgres()
+        print "Starting DB server"
 
-        os.system("rm -f %s/postmaster.pid"%(pgdb))
-
-        # start postgres
-        cmd='/etc/init.d/postgres-boot start %s'%(postgres_version,)
-        print "Starting DB server: %s"% (cmd,)
-        time.sleep(360)
-        os.system(cmd)
-        # this is sloppy but how else can I check that postmasted is ready
-        # recovery may take a very long time
-
-        while 1:
+        while not rc:
             print "Waiting for DB server"
-            rc = os.system('%s/tools/tstpostgres > /dev/null 2>&1'%(pnfs_dir,)) >> 8
-            print 
-            print "Waiting for DB server", rc
-            if rc == 0:
-                print "DB server is ready"
-                break
+            time.sleep(60)
+            rc = checkPostgres()
 
+        print "DB server is ready"
 
-        cmd='. /usr/local/etc/setups.sh; setup pnfs; export LD_LIBRARY_PATH=$PNFS_DIR/tools/linux/pgsql/lib; /etc/init.d/pnfs start'
+        cmd='/sbin/service pnfs_wrapper start'
         print "Starting pnfs: %s"% (cmd,)
-        os.system(cmd)
-
-
-        cmd='mount /pnfs/fs'
-        print 'Mounting pnfs: %s'% (cmd,)
-        os.system(cmd)
+        rc=os.system(cmd)
+	if rc != 0 :
+		sys.stderr.write("Command %s failed, bailing out "%(cmd,))
+		sys.stderr.flush()
+		return rc
+        print "DONE"
         return 0
-
 
 
 def get_command_output(command):
@@ -184,83 +286,30 @@ def get_command_output(command):
     if err:
 	raise RuntimeError, '%s failed w/ exit code %d' % (command, err)
     return data[:-1] # (skip '\n' at the end)
-
-    
-    
-def get_config(host):
-    #    print "get_config PNFS HOST ",host
-    #    cmd = "rcp %s:/usr/etc/pnfsSetup pnfsSetup.%s"%(host, host)
-    #    pipeObj = popen2.Popen3(cmd, 0, 0)
-    #    if pipeObj is None:
-    #        print "%s failed"%(cmd, )
-    #        sys.exit(1) 
-    #    stat = pipeObj.wait()
-    #    result = pipeObj.fromchild.readlines()  # result has returned string
-    #
-    f = open("/tmp/pnfsSetup.%s"%(host,), 'r')
-    of = open("/tmp/pnfsSetup.%s.tmp"%(host,), 'w')
-    while 1:
-        l = f.readline()
-        
-        if not l:
-            break
-        if l[0] == '#':
-            # skip comment line
-            continue
-        a=l[:-1].split('=')
-        if a[0] == 'database':
-            pnfs_db = a[1]
-        if a[0] == 'database_postgres':
-            pgdb = a[1]
-            backup_name=pgdb.split('/')[-1]
-        if a[0] == 'trash':
-            trash = a[1]
-        if a[0] == 'remotebackup':
-            backup_host, backup_dir = a[1].split(':')
-            l1 = string.join(('#', l))
-            l = l1
-        of.write(l)
-    of.close()
-    f.close()
-    os.system("mv /tmp/pnfsSetup.%s.tmp /tmp/pnfsSetup.%s"%(host, host))
-    os.system("cp -f /tmp/pnfsSetup.%s /usr/etc/pnfsSetup"%(host, ))
-    # get local pnfs directory
-    pnfs_dir=os.popen('. /usr/local/etc/setups.sh; setup pnfs; echo $PNFS_DIR').readlines()[0][:-1]
-    #
-    # kludge
-    #
-    # cmd = "rsh %s:/usr/etc/pnfsSetup pnfsSetup.%s"%(host, host)
-    os.system('. /usr/local/etc/setups.sh; unsetup postgres')
-    cmd = ". /usr/local/etc/setups.sh; setup postgres %s"%(postgres_version,)
-    print "setting postgres ",postgres_version
-    if ( os.system(cmd) ):
-        print "failed to ",cmd
-        sys.exit(1)
-    return pnfs_db, pgdb, trash, backup_host, backup_dir,pnfs_dir, backup_name
 #
 # get next to the last backup
 #
-def get_backup(backup_host, backup_dir,  backup_name):
-    cmd='rsh %s  "ls -t %s/%s.*|head -n 2|tail -1"'%(backup_host, backup_dir,  backup_name)
-#    cmd='rsh %s  "ls -t %s/%s.*|head -n 1"'%(backup_host, backup_dir,  backup_name)
-    return get_command_output(cmd)
+def get_backup(pnfsSetup):
+	cmd='rsh %s  "ls -t %s/%s.*|head -n 2|tail -1"'%(pnfsSetup.remote_backup_host,
+							 pnfsSetup.remote_backup_dir,
+							 pnfsSetup.backup_name)
+	return get_command_output(cmd)
 
-    
+
 if __name__ == "__main__" :
     r = PnfsDbRestore();
-    
+
     uname = pwd.getpwuid(os.getuid())[0]
     backup_time      = None
     sysname          = None
     emergency        = False
     ourhost          = string.split(os.uname()[1],'.')[0]
-    
-    
+
     if uname != 'root':
         print "Must be 'root' to run this program"
         sys.exit(1)
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hs:p:t:es:", ["help","system=","postgres_version=","backup_time=","emergency"])
+        opts, args = getopt.getopt(sys.argv[1:], "hs:t:es:", ["help","system=","backup_time=","emergency"])
     except getopt.GetoptError:
         print "Failed to process arguments"
         r.usage(sys.argv[0])
@@ -273,8 +322,6 @@ if __name__ == "__main__" :
             sys.exit(1)
         if o in ("-s", "--system"):
             sysname = a
-        if o in ("-p", "--postgres_version"):
-            postgres_version = a
         if o in ("-t", "--backup_time"):
             backup_time = a
     for value in sys2host.values():
@@ -296,18 +343,6 @@ if __name__ == "__main__" :
     if (backup_time == None or backup_time=="" ) :
         backup_time=None
 
-#    if sys2host.has_key(sysname):
-#        pnfs_host   = sys2host[sysname][0]
-#        backup_name = sys2host[sysname][1]
-#    else:
-#        print "Error: no such system ",sysname
-#        r.usage(sys.argv[0])
-#        sys.exit(1)
-
-    if not version2version.has_key(postgres_version):
-        print "Error: no such postgres version", postgres_version
-        r.usage(sys.argv[0])
-        sys.exit(1)
     rc = r.recover(sysname,backup_time)
     sys.exit(rc)
 
