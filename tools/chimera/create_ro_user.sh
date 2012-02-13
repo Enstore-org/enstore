6edac86a1a34c5ea9862bf41bd94321eb86939c0
Dmitry Litvinsev <litvinse>
2012-02-13 02:45:25 +0000

    looks like we need UPDATE

--- tools/chimera/create_ro_user.sh
+++ tools/chimera/create_ro_user.sh
@@ -12,5 +12,5 @@ psql -U postgres -c "GRANT USAGE ON SCHEMA public TO enstore_reader"
 
 dbs=`psql  -U enstore -l  -t | sed "/^\s*\:/d;/^\s*$/d" | cut -d"|" -f1`
 for db in ${dbs}; do
-    psql -U enstore -qAt -c "SELECT 'GRANT SELECT ON ' || relname || ' TO enstore_reader;' FROM pg_class JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace WHERE nspname = 'public' AND relkind IN ('r', 'v');"  $db | grep "^GRANT" | psql -U enstore $db
+    psql -U enstore -qAt -c "SELECT 'GRANT SELECT,UPDATE ON ' || relname || ' TO enstore_reader;' FROM pg_class JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace WHERE nspname = 'public' AND relkind IN ('r', 'v');"  $db | grep "^GRANT" | psql -U enstore $db
 done
