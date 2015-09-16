4867faeda7ddff84521f24f9c3bc86e851dfdb34
Alexander Moibenko <moibenko@fnal.gov>
2015-09-16 17:03:12 -0500

    Disk movers went into error state because the "address" in "vc" subticket     was the address of some other disk mover instead of volume clerk on encp retry.     The ticket is coming from encp and it is not clear where the wrong address     is coming from initially: encp or disk mover. This patch fixes the ticket in the library manager.     http://srm.fnal.gov/reviews/r/299/     http://www-ccf.fnal.gov/Bugzilla/show_bug.cgi?id=1678

--- src/library_manager.py
+++ src/library_manager.py
@@ -1512,7 +1512,10 @@ class LibraryManagerMethods:
         Trace.trace(self.trace_level+2, 'inquire_vol label %s req %s addr=%s'%
                     (external_label, requestor, vol_server_address))
         if requestor and requestor.get('mover_type') == 'DiskMover':
-            vol_info = requestor # return information from mover
+            vol_info = {}
+            vol_info.update(requestor) # return information from mover
+            if "address" in vol_info:
+                del(vol_info['address'])
             vol_info['system_inhibit'] = vol_info['volume_status'][0]
             vol_info['user_inhibit'] = vol_info['volume_status'][1]
         elif not external_label:
@@ -4550,6 +4553,8 @@ class LibraryManager(dispatching_worker.DispatchingWorker,
                 # update volume info
                 vol_info = self.inquire_vol(w["fc"]["external_label"], mticket, w['vc']['address'])
                 if vol_info['status'][0] == e_errors.OK:
+                    if self.mover_type(mticket) == 'DiskMover':
+                       vol_info["external_label"] = w["vc"]["external_label"]
                     w['vc'].update(vol_info)
 	    log_add_to_wam_queue(w['vc'])
             #self.work_at_movers.append(w)
