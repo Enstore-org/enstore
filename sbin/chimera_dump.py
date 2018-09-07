2e7e23f8488b5a3a09be0871fcf34ec0f3e04e79
Dmitry Litvintsev <litvinse@fnal.gov>
2018-09-06 19:40:35 -0500

    chimera: use replica DB to produce chimera dump
    RB : http://srm.fnal.gov/reviews/r/515/
    BZ : http://www-ccf.fnal.gov/Bugzilla/show_bug.cgi?id=1937

--- sbin/chimera_dump.py
+++ sbin/chimera_dump.py
@@ -68,46 +68,81 @@ def help():
     """
     return HELP
 
-def dump_directory(path,pnfsid):
+def dump_directory(path,pnfsid,dbhost,dbname,dbuser,dbport):
     q=DUMP_DIRECTORY.format(pnfsid,path)
     outfile=os.path.join("/tmp","CHIMERA_DUMP_%s"%(os.path.basename(path)))
-    cmd = "psql -U enstore_reader  -A -F ' ' -c \"%s\" -o %s -h stkensrv1n chimera"%(q,outfile,)
+    cmd = "psql -U  {}   -A -F ' ' -c \"{}\" -o {} -h {} -p {} {}".format(dbuser,q,outfile,dbhost,dbport,dbname)
     os.system(cmd)
 
 if __name__ == "__main__":
     parser = OptionParser(usage=help())
     (options, args) = parser.parse_args()
-
-    db  = dbaccess.DatabaseAccess(maxconnections=1,
-                                  host     = "stkensrv1n",
-                                  database = "chimera",
-                                  user     = "enstore_reader")
-    storage_groups = []
-    if len(args) > 0:
-        for arg in set(args):
-            q=QUERY_TOP_DIRECTORY.format(arg)
-            res=db.query(q)
-            for row in res:
-                path=row[0]
-                storage_groups.append(os.path.basename(path))
-                dump_directory(row[0],row[1])
-
-    else:
-        q=QUERY_TOP_DIRECTORIES
-        res=db.query(q)
-        for row in res:
-            path=row[0]
-            storage_groups.append(os.path.basename(path))
-            dump_directory(row[0],row[1])
-
-    db.close()
-
     csc = configuration_client.ConfigurationClient((enstore_functions2.default_host(),
                                                     enstore_functions2.default_port()))
     inventory = csc.get('inventory')
     dest_path = inventory.get('inventory_rcp_dir')
+    namespaceDictionary = csc.get('namespace')
 
-    for sg in storage_groups:
-        cmd = "enrcp %s %s"%(os.path.join("/tmp","CHIMERA_DUMP_%s"%(sg,)),dest_path)
-        os.system(cmd)
+    if not namespaceDictionary:
+        sys.stderr.write("No namespace dictionary in configuration root.\n")
+        sys.exit(1)
+
+    """
+    Kludge to skip cms namespace which we do not want to query
+    """
+    for key in ("cms","status",) :
+        try:
+            del namespaceDictionary[key]
+        except:
+            pass
+
+    for key, value in namespaceDictionary.iteritems():
+        dbcon = value
+        if "replica" in value:
+            dbcon = value.get("replica")
+        if not dbcon  and "master" in value:
+            dbcon = value.get("master")
+        db = None
+        try:
+            db  = dbaccess.DatabaseAccess(maxconnections=1,
+                                          host     = dbcon.get("dbhost","localhost"),
+                                          database = dbcon.get("dbname","chimera"),
+                                          user     = dbcon.get("dbuser","enstore_reader"),
+                                          port     = dbcon.get("dbport",5432))
+            storage_groups = []
+            if len(args) > 0:
+                for arg in set(args):
+                    q=QUERY_TOP_DIRECTORY.format(arg)
+                    res=db.query(q)
+                    for row in res:
+                        path=row[0]
+                        storage_groups.append(os.path.basename(path))
+                        dump_directory(row[0],
+                                       row[1],
+                                       dbcon.get("dbhost","localhost"),
+                                       dbcon.get("dbname","chimera"),
+                                       dbcon.get("dbuser","enstore_reader"),
+                                       dbcon.get("dbport",5432))
+            else:
+                q=QUERY_TOP_DIRECTORIES
+                res=db.query(q)
+
+                for row in res:
+                    path=row[0]
+                    storage_groups.append(os.path.basename(path))
+                    dump_directory(row[0],
+                                   row[1],
+                                   dbcon.get("dbhost","localhost"),
+                                   dbcon.get("dbname","chimera"),
+                                   dbcon.get("dbuser","enstore_reader"),
+                                   dbcon.get("dbport",5432))
+            for sg in storage_groups:
+                cmd = "enrcp %s %s"%(os.path.join("/tmp","CHIMERA_DUMP_%s"%(sg,)),dest_path)
+                os.system(cmd)
+        except Exception as e:
+            sys.stderr.write("Failed to query database {} \n".format(str(e)))
+            sys.exit(1)
+        finally:
+            if db :
+                db.close()
 
