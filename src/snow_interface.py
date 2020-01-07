82318996b615027f523429248e6a14174eef1baa
Dmitry Litvintsev <litvinse@fnal.gov>
2020-01-07 14:41:57 -0600

    snow interface: correct fields so that reported source and caller id are filled

--- src/snow_interface.py
+++ src/snow_interface.py
@@ -8,20 +8,21 @@
 
 import json
 import os
+import socket
 import sys
 import urllib2
 
 from ConfigParser import ConfigParser
 
-CONFIG_FILE=os.path.join(os.environ["ENSTORE_DIR"],"etc/servicenow_create_entry.cf")
+CONFIG_FILE = os.path.join(os.environ["ENSTORE_DIR"], "etc/servicenow_create_entry.cf")
+
 
 def submit_ticket(**kwargs):
-
     config_parser = ConfigParser()
     config_parser.read(CONFIG_FILE)
     url = config_parser.get("HelpDesk", "incidentRestUrl")
 
-    if not url :
+    if not url:
         raise Exception("service now URL is not defined")
 
     password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
@@ -35,23 +36,24 @@ def submit_ticket(**kwargs):
     urllib2.install_opener(opener)
 
     data = json.dumps({
-        "impact" :  kwargs.get("Impact_Type","3-Moderate/Limited"),
-        "u_monitored_ci_name" : kwargs.get("CiName").upper(),
-        "short_description" : kwargs.get("Summary", None),
-        "description" : kwargs.get("Notes", None),
-        "u_reported_source value" : kwargs.get("Reported_Source_Type","Event Monitoring"),
-        "u_service value" : kwargs.get("Service_Type","Storage"),
-        "urgency" :  kwargs.get("Urgency_Type","3-Medium"),
-        "u_monitored_categorization" : kwargs.get("Product_Categorization_Tier_1",
-                                                  config_parser.get("create_entry","categorization")),
-        "caller_id" : config_parser.get("create_entry","user_first"),
-        "u_categorization" : "1128451829dd90408638a6dc41528b56",
-        "u_virtual_organization" : "69f4000e6f4c9600c6df5d412e3ee43c",
+        "impact":  kwargs.get("Impact_Type","3-Moderate/Limited"),
+        "u_monitored_ci_name": kwargs.get("CiName").upper(),
+        "short_description": kwargs.get("Summary", None),
+        "description": kwargs.get("Notes", None),
+        "u_reported_source": kwargs.get("Reported_Source_Type", "Event Monitoring"),
+        "u_service": kwargs.get("Service_Type", "Storage"),
+        "urgency":  kwargs.get("Urgency_Type", "3-Medium"),
+        "u_monitored_categorization": kwargs.get("Product_Categorization_Tier_1",
+                                                  config_parser.get("create_entry", "categorization")),
+        "caller_id": config_parser.get("create_entry", "user_first") + " " +
+                     config_parser.get("create_entry", "user_last"),
+        "u_categorization": "1128451829dd90408638a6dc41528b56",
+        "u_virtual_organization": "69f4000e6f4c9600c6df5d412e3ee43c",
         })
 
-    request = urllib2.Request(url,data)
-    request.add_header("Content-Type","application/json")
-    request.add_header("Accept","application/json")
+    request = urllib2.Request(url, data)
+    request.add_header("Content-Type", "application/json")
+    request.add_header("Accept", "application/json")
 
     try:
         response = urllib2.urlopen(request)
@@ -60,16 +62,18 @@ def submit_ticket(**kwargs):
             ticket = data["result"]["number"]
             return ticket
         else:
-            raise Exception("Failed to create incident ticket HTTP %s"%(response.getcode()))
+            raise Exception("Failed to create incident ticket HTTP %s" % (response.getcode()))
     except urllib2.HTTPError as e:
         raise Exception(str(e))
 
 if __name__ == "__main__":
     try:
-        ticket=submit_ticket()
-        sys.stdout.write("Entry created with id= %s\n"%(ticket,))
+        ticket = submit_ticket(CiName=socket.gethostname().split(".")[0],
+                               Summary="test summary",
+                               Notes="test description")
+        sys.stdout.write("Entry created with id= %s\n" % (ticket,))
         sys.exit(0)
     except Exception, msg:
-        sys.stderr.write("%s\n"%(str(msg)))
+        sys.stderr.write("%s\n" % (str(msg)))
         sys.exit(1)
 
