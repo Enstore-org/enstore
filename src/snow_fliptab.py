a5b9051e5e653733542362d2fb1c581eafac41bf
Dmitry Litvintsev <litvinse@fnal.gov>
2020-01-15 10:38:36 -0600

    snow_interface: use RESTFul SNOW API to create requests
           - get rid of dependency on suds
           - unified apporach to incidents and requests creation
    
    	BZ : https://www-enstore.fnal.gov/Bugzilla/show_bug.cgi?id=2104
    	RB : http://srm/reviews/r/629/

--- src/snow_fliptab.py
+++ src/snow_fliptab.py
@@ -5,99 +5,24 @@ This script creates requests tickets in ServiceNow
 to be used to create tab flip tickets
 """
 
-import os
 import socket
-import time
-from ConfigParser import ConfigParser
-from optparse import OptionParser
+import snow_interface
 
-import suds.client
 
-CONFIG_FILE=os.path.join(os.environ["ENSTORE_DIR"],"etc/servicenow_create_entry.cf")
-
-""" ServiceNow development URL used to insert requests """
-DEV_SNOW_PUT_URL="https://fermidev.service-now.com/CreateRequestedItem.do?WSDL"
-""" ServiceNow production URL used to insert requests """
-PRD_SNOW_PUT_URL="https://fermi.service-now.com/CreateRequestedItem.do?WSDL"
-""" ServiceNow development URL used to retrieve created ticket """
-DEV_SNOW_GET_URL="https://fermidev.service-now.com/sc_req_item.do?WSDL"
-""" ServiceNow production URL used to retrieve created ticket """
-PRD_SNOW_GET_URL="https://fermi.service-now.com/sc_req_item.do?WSDL"
-""" Assignment group """
-ASSIGNMENT_GROUP="Logistics/PREP Support"
-""" Magic categorization item that initiates the workflow """
-CAT_ITEM="18c27227042950008638553dd6544037"
-CALLER_ID="Dms-enstore System"
-AFFILIATION="Fermilab"
-E_MAIL="ssa-auto@fnal.gov"
-DESCRIPTION="Please run lock on stkensrv4n.fnal.gov to write protect 10 tapes (2 caps)"
-IMPACT = "3 - Moderate/Limited"
-INCIDENT_STATE="New"
-OPENED_BY="Enstore System"
-PRIORITY="3-Medium"
-SEVERITY=""
-SHORT_DESCRIPTION="write protect 10 tapes (flip tabs) in STKEN 8500GS tape library"
-COMMENTS="Please run lock on stkensrv4n.fnal.gov to write protect 10 tapes (2 caps)"
-
-MONITORED_CATEGORIZATION="Scientific Data Storage and Access -- Enstore Tape Storage"
-REPORTED_SOURCE="Event Monitoring"
-SERVICE="Infrastructure Event"
-CI_NAME=socket.gethostname().split('.')[0].upper()
-URGENCY="3 - Medium"
+SHORT_DESCRIPTION = "write protect 10 tapes (flip tabs) in STKEN 8500GS tape library"
+COMMENTS = "Please run lock on stkensrv4n.fnal.gov to write protect 10 tapes (2 caps)"
+DESCRIPTION = "Please run lock on stkensrv4n.fnal.gov to write protect 10 tapes (2 caps)"
+CI_NAME = socket.gethostname().split('.')[0].upper()
 
 
 def submit_ticket(**kwargs):
-    config_parser = ConfigParser()
-    config_parser.read(CONFIG_FILE)
-    url = DEV_SNOW_PUT_URL if kwargs.get("Dev") else PRD_SNOW_PUT_URL
-    client = suds.client.Client(url,
-                                username=config_parser.get("HelpDesk","acct","cd-srv-dms-enstore"),
-                                password=config_parser.get("HelpDesk","passwd"))
-    method = client.service.execute
-    result = method(assignment_group           = kwargs.get("Assignment_Group", ASSIGNMENT_GROUP),
-                    cat_item                   = kwargs.get("Cat_Item",CAT_ITEM),
-                    caller_id                  = kwargs.get("Caller_Id",CALLER_ID),
-                    affiliation                = kwargs.get("Affiliation",AFFILIATION),
-                    email                      = kwargs.get("E_mail",E_MAIL),
-                    description                = kwargs.get("Description",DESCRIPTION),
-                    impact                     = kwargs.get("Impact",IMPACT),
-                    incident_state             = kwargs.get("Incident_State",INCIDENT_STATE),
-                    opened_by                  = kwargs.get("Opened_By",OPENED_BY),
-                    opened_at                  = time.strftime("%Y-%m-%d %H:%M:%S",time.gmtime()),
-                    priority                   = kwargs.get("Priority",PRIORITY),
-                    severity                   = kwargs.get("Severity",SEVERITY),
-                    short_description          = kwargs.get("Summary",SHORT_DESCRIPTION),
-                    comments                   = kwargs.get("Comments",COMMENTS),
-                    u_monitored_categorization = kwargs.get("Monitored_Categorization",MONITORED_CATEGORIZATION),
-                    u_reported_source          = kwargs.get("Reported_Source",REPORTED_SOURCE),
-                    u_service                  = kwargs.get("Service",SERVICE),
-                    u_monitored_ci_name        = kwargs.get("CiName",CI_NAME).upper(),
-                    urgency                    = kwargs.get("Urgency",URGENCY),
-                    u_categorization           = "1128451829dd90408638a6dc41528b56",
-                    u_virtual_organization     = "69f4000e6f4c9600c6df5d412e3ee43c",
-                    )
-    url = DEV_SNOW_GET_URL if kwargs.get("Dev") else PRD_SNOW_GET_URL
-    client = suds.client.Client(url,
-                                username=config_parser.get("HelpDesk","acct","cd-srv-dms-enstore"),
-                                password=config_parser.get("HelpDesk","passwd"))
-    result = client.service.get(sys_id=result)
-    return result.number
-
-def help():
-    return "usage %prog [options]"
+    intf = snow_interface.SnowInterface()
+    ticket = intf.create_request(CiName=kwargs.get("CiName", CI_NAME).upper(),
+                                 Summary=kwargs.get("Summary", SHORT_DESCRIPTION),
+                                 Notes=kwargs.get("Description", DESCRIPTION),
+                                 Comments=kwargs.get("Comments", COMMENTS),)
+    return ticket
 
 if __name__ == "__main__":
-
-    parser = OptionParser(usage=help())
-    parser.add_option("-p", "--prd",action="store_true",
-                      dest="prd",default=False,
-                      help="Create ticket in production system [default: %default] ")
-
-    (options, args) = parser.parse_args()
-
-    if options.prd:
-        result=submit_ticket()
-    else:
-        result=submit_ticket(Dev=True)
-    print result
-
+    result = submit_ticket()
+    print(result)
--- src/snow_interface.py
+++ src/snow_interface.py
@@ -12,68 +12,157 @@ import socket
 import sys
 import urllib2
 
-from ConfigParser import ConfigParser
+import ConfigParser
 
 CONFIG_FILE = os.path.join(os.environ["ENSTORE_DIR"], "etc/servicenow_create_entry.cf")
+HTTP_SUCCESS_CODES = (200, 201)
+
+CI_NAME = socket.gethostname().split('.')[0].upper()
+DESCRIPTION = "Please run lock on stkensrv4n.fnal.gov to write protect 10 tapes (2 caps)"
+SHORT_DESCRIPTION = "write protect 10 tapes (flip tabs) in STKEN 8500GS tape library"
+
+
+class SnowInterface(object):
+
+    def __init__(self, config_file=None):
+        """
+        :param config_file: string, path to config file
+        """
+        self.config_file = config_file if config_file is None else CONFIG_FILE
+        self.config_parser = ConfigParser.ConfigParser()
+        self.config_parser.read(self.config_file)
+
+        self.password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        auth_handler = urllib2.HTTPBasicAuthHandler(self.password_manager)
+        opener = urllib2.build_opener(auth_handler)
+        urllib2.install_opener(opener)
+
+    def post(self, url, payload):
+        """
+        Creates/modifies entries in SNOW
+
+        :param url: string, SNOW URL
+        :param payload: dictionary of fields to be inserted
+        :return: ticket number, string
+        """
+        self.password_manager.add_password(None,
+                                           url,
+                                           self.config_parser.get("HelpDesk", "acct"),
+                                           self.config_parser.get("HelpDesk", "passwd"))
+        request = urllib2.Request(url, json.dumps(payload))
+        request.add_header("Content-Type", "application/json")
+        request.add_header("Accept", "application/json")
+        response = urllib2.urlopen(request)
+        if response.getcode() not in HTTP_SUCCESS_CODES:
+            raise Exception("HTTP code %s" % (response.getcode()))
+        return json.load(response)
+
+    def create_incident(self, **kwargs):
+        """
+        Creates incidents in SNOW
+
+        :param kwargs: keyworded function arguments
+        :return: ticket number, string
+        """
+        url = self.config_parser.get("HelpDesk", "incidentRestUrl")
+        if not url:
+            raise Exception("service now URL is not defined")
+
+        data = {
+            "impact":  kwargs.get("Impact_Type", "3-Moderate/Limited"),
+            "u_monitored_ci_name": kwargs.get("CiName").upper(),
+            "short_description": kwargs.get("Summary", None),
+            "description": kwargs.get("Notes", None),
+            "comments": kwargs.get("Comments", None),
+            "u_reported_source": kwargs.get("Reported_Source_Type", "Event Monitoring"),
+            "u_service": kwargs.get("Service_Type", "Storage"),
+            "urgency":  kwargs.get("Urgency_Type", "3-Medium"),
+            "u_monitored_categorization": kwargs.get("Monitored_Categorization",
+                                                     self.config_parser.get("create_entry", "categorization")),
+            "caller_id": self.config_parser.get("create_entry", "user_first") + " " +
+                         self.config_parser.get("create_entry", "user_last"),
+            "u_categorization": kwargs.get("Categorization",
+                                           self.config_parser.get("create_entry", "u_categorization")),
+            "u_virtual_organization": kwargs.get("Virtual_Organization",
+                                                 self.config_parser.get("create_entry", "u_virtual_organization")),
+            }
+        response = self.post(url, data)
+        return response["result"]["number"]
+
+    def create_request(self, **kwargs):
+        """
+        Creates requests (RITMs) in SNOW
+
+        :param kwargs: keyworded function arguments
+        :return: ticket number, string
+        """
+        url = self.config_parser.get("HelpDesk", "requestRestUrl")
+        if not url:
+            raise Exception("service now URL is not defined")
+
+        """
+        First we create the request ticket
+        """
+        data = {
+                "catalog_item": {
+                    "sys_id": kwargs.get("Sys_Id",
+                                         self.config_parser.get("create_entry", "sys_id")),
+                    "vars": {
+                        "u_monitored_ci_name": kwargs.get("CiName", CI_NAME).upper(),
+                        "short_description": kwargs.get("Summary", SHORT_DESCRIPTION),
+                        "description": kwargs.get("Description", DESCRIPTION),
+                        "watch_list": kwargs.get("Watch_List",
+                                                 self.config_parser.get("create_entry", "watch_list")),
+                        "u_requestor_email": kwargs.get("E_Mail",
+                                                        self.config_parser.get("create_entry", "u_requestor_email")),
+                        }
+                    }
+                }
+        response = self.post(url, data)
+        ticket_number = response["items"][0]["number"]
+        url = response["items"][0]["link"]
+
+        """
+        Then we update the ticket
+        """
+        update_data = {"u_categorization": self.config_parser.get("create_entry",
+                                                                  "u_categorization"),
+                       "priority": "3",
+                       "u_virtual_organization": self.config_parser.get("create_entry",
+                                                                        "u_virtual_organization"),
+                       "urgency": "3",
+                       "u_reported_source": self.config_parser.get("create_entry",
+                                                                   "reported_source"),
+                       "assignment_group": self.config_parser.get("create_entry",
+                                                                  "assignment_group"),
+                       }
+        self.post(url, update_data)
+        return ticket_number
 
 
 def submit_ticket(**kwargs):
-    config_parser = ConfigParser()
-    config_parser.read(CONFIG_FILE)
-    url = config_parser.get("HelpDesk", "incidentRestUrl")
+    """
+    Creates incidents. For backward compatibility.
+    :param kwargs: keyworded arguments
+    :return: ticket number, string
+    """
+    snow = SnowInterface()
+    return snow.create_incident(**kwargs)
 
-    if not url:
-        raise Exception("service now URL is not defined")
-
-    password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    password_manager.add_password(None,
-                                  url,
-                                  config_parser.get("HelpDesk","acct"),
-                                  config_parser.get("HelpDesk","passwd"))
-
-    auth_handler = urllib2.HTTPBasicAuthHandler(password_manager)
-    opener = urllib2.build_opener(auth_handler)
-    urllib2.install_opener(opener)
-
-    data = json.dumps({
-        "impact":  kwargs.get("Impact_Type","3-Moderate/Limited"),
-        "u_monitored_ci_name": kwargs.get("CiName").upper(),
-        "short_description": kwargs.get("Summary", None),
-        "description": kwargs.get("Notes", None),
-        "u_reported_source": kwargs.get("Reported_Source_Type", "Event Monitoring"),
-        "u_service": kwargs.get("Service_Type", "Storage"),
-        "urgency":  kwargs.get("Urgency_Type", "3-Medium"),
-        "u_monitored_categorization": kwargs.get("Product_Categorization_Tier_1",
-                                                  config_parser.get("create_entry", "categorization")),
-        "caller_id": config_parser.get("create_entry", "user_first") + " " +
-                     config_parser.get("create_entry", "user_last"),
-        "u_categorization": "1128451829dd90408638a6dc41528b56",
-        "u_virtual_organization": "69f4000e6f4c9600c6df5d412e3ee43c",
-        })
-
-    request = urllib2.Request(url, data)
-    request.add_header("Content-Type", "application/json")
-    request.add_header("Accept", "application/json")
-
-    try:
-        response = urllib2.urlopen(request)
-        if response.getcode() == 201:
-            data = json.load(response)
-            ticket = data["result"]["number"]
-            return ticket
-        else:
-            raise Exception("Failed to create incident ticket HTTP %s" % (response.getcode()))
-    except urllib2.HTTPError as e:
-        raise Exception(str(e))
 
 if __name__ == "__main__":
     try:
-        ticket = submit_ticket(CiName=socket.gethostname().split(".")[0],
-                               Summary="test summary",
-                               Notes="test description")
-        sys.stdout.write("Entry created with id= %s\n" % (ticket,))
-        sys.exit(0)
-    except Exception, msg:
-        sys.stderr.write("%s\n" % (str(msg)))
-        sys.exit(1)
+        intf = SnowInterface()
+        ticket = intf.create_incident(CiName=socket.gethostname().split(".")[0],
+                                      Summary="test summary",
+                                      Notes="test description")
+        sys.stdout.write("Created incident with id= %s\n" % (ticket,))
 
+        ticket = intf.create_request(CiName=socket.gethostname().split(".")[0],
+                                     Summary="test summary",
+                                     Notes="test description")
+        sys.stdout.write("Created request with id= %s\n" % (ticket,))
+        sys.exit(0)
+    except Exception as e:
+        sys.stderr.write("%s\n" % (str(e)))
+        sys.exit(1)
