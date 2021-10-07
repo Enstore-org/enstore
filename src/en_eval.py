58f755e9c98e2c52427e7d70f38a942a5509d835
Alexander Moibenko <moibenko@fnal.gov>
2021-10-07 13:04:28 -0500

    Fix code injection hole.
    Use ast.literal_eval istead of eval.

--- src/en_eval.py
+++ src/en_eval.py
@@ -5,24 +5,11 @@
 # Wrapper code for builtin eval() to run in restricted mode/environment.
 
 # system imports
-import re
-import compiler
 import sys
-import types
 import time
+import ast
+
 import Trace
-
-# looking for "CallFunc("
-# to be used with compiler
-re_CallFunc = re.compile("CallFunc\(")
-
-# any alphanum character, or _
-# followed by 0 or more of "_",
-# followed by 0 or more whitespaces,
-# followed by "("
-# to be used without complier
-# this takes abut 20 times less time for executon
-re_func=re.compile("[A-Za-z0-9_] *\(")
 
 # en_eval(expr) -- safer eval
 #
@@ -30,42 +17,14 @@ re_func=re.compile("[A-Za-z0-9_] *\(")
 #            (2) function invocation
 # and it does not allow access to any local or global symbols
 
-def en_eval(expr, debug=False, check=True, compile=False):
-	# check - check message
-	# if checked once there is no need to check again
-	# compile - use complier - compiler is deprecated anyway
-	
+def en_eval(expr, debug=False):
+
 	Trace.trace(5,"en_eval %s"%(expr,))
 	t0=time.time()
-	# reject anything that is NOT a string
-	if type(expr) != types.StringType:
-		if debug:
-			sys.stderr.write("en_eval Error: not a string type\n")
-		#return None
-		raise TypeError("expected string not %s" % (type(expr),))
-
-	# reject function invocation
-	if check:
-		t02=t01=time.time()
-		if compile:
-			fun = str(compiler.parse(expr).node.nodes)
-			t02=time.time()
-			rc = re_CallFunc.search(fun)
-		else:
-			rc = re_func.search(expr)	
-		if rc != None:
-
-			if debug:
-				sys.stderr.write("en_eval Error: function invocation\n")
-				#return None
-				raise SyntaxError("functions not allowed")
-
-		t03=time.time()
-		Trace.trace(5,"en_eval %s %s %s"%(t01-t0, t02-t01,t03-t02))
 
 	# reject empty UDP datagrams.
 	#
-	## On Wednesday, January 30th 2008, it was discovered that 
+	## On Wednesday, January 30th 2008, it was discovered that
 	## "intrusion detection" software called samhain would send an
 	## empty UDP datagram to open UDP sockets.  The revalation that
 	## that any hacker could initiate a denial of service attack
@@ -75,25 +34,19 @@ def en_eval(expr, debug=False, check=True, compile=False):
 	if expr == "":
 		if debug:
 			sys.stderr.write("en_eval Error: empty UDP datagram ignored\n")
-		#return None
 		raise SyntaxError("empty string not expected")
- 
 
-	# no access to globals nor locals
+
 	try:
 		t=time.time()
-		val = eval(expr, {}, {})
+		val = ast.literal_eval(expr)
 		t1=time.time()
 		Trace.trace(5,"en_eval:eval %s"%(t1-t,))
 	except SyntaxError, msg:
 		if debug:
 			sys.stderr.write("en_eval Error: %s parsing string: %s\n" % (str(msg), expr))
-		#return None
 		raise sys.exc_info()
 	except NameError, detail:
 		Trace.trace(5, "NameError %s %s"%(detail, expr))
 		val = expr
-		#raise NameError, expr
-
 	return val
-
