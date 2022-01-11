5d739f643d2c2379607152f1c161748738bc2a0a
Ren Bauer <renbauer@fnal.gov>
2022-01-11 19:51:34 +0000

    Add comment to give tip on import failures

--- src/tests/test_movcmd_mc.py
+++ src/tests/test_movcmd_mc.py
@@ -1,3 +1,6 @@
+# movcmd_mc includes C module imports.
+# If this import fails, please read enstore-pytest-c-module.md,
+# and remember to use `python -m pytest`.
 import movcmd_mc
 import os
 import pytest
