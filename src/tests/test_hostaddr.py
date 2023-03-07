4c407ae41a973bc1e059e8b45fba27ea8966c869
Dennis Box <dbox@fnal.gov>
2023-03-07 17:54:43 -0600

    improved coverage

--- src/tests/test_hostaddr.py
+++ src/tests/test_hostaddr.py
@@ -6,6 +6,7 @@ import types
 import errno
 import time
 import mock
+import StringIO
 try:
     import enroute
 except ImportError:
@@ -49,26 +50,30 @@ class TestHostaddr(unittest.TestCase):
            hostinfo = hostaddr.gethostinfo("www.fnal.gov") 
            self.assertTrue(isinstance(hostinfo, types.ListType))
            self.assertTrue(len(hostinfo) > 0)
+           with mock.patch('os.uname', new_callable=mock.MagicMock): 
+               with mock.patch('sys.stderr', new_callable=StringIO.StringIO) as stderr_mock:
+                   hostaddr.hostinfo = None
+                   hostaddr.gethostinfo("www.fnal.gov")
+                   self.assertTrue("Warning:  gethostname returns" in stderr_mock.getvalue(), stderr_mock.getvalue())
     
     def test_getdomainname(self):
             dm = hostaddr.getdomainname()
             self.assertTrue(isinstance(dm, types.StringType))
             self.assertEqual(dm, 'fnal.gov')
 
-    @unittest.skip('private, cannot test') 
     def test__getdomainaddr(self):
-            dadr = hostaddr._getdomainaddr('www.fnal.gov')
-            self.assertTrue(isinstance(dadr, types.ListType))
-            self.assertTrue(len(dadr) > 0)
+        dadr = hostaddr._getdomainaddr((10, 1, 6, '', ('2620:6a:0:8421::96', 0, 0, 0)))
+        self.assertTrue(isinstance(dadr, types.StringType),dadr)
+        self.assertTrue(len(dadr) > 0)
     
     def test_getdomainaddr(self):
             dadr = hostaddr.getdomainaddr()
             self.assertTrue(isinstance(dadr, types.ListType))
             self.assertTrue(len(dadr) > 0)
 
-    @unittest.skip('private, cannot test') 
+    @unittest.skip('private, cannot test directly') 
     def test___my_gethostbyaddr(self):
-            hba = hostaddr._my_gethostbyaddr("www.fnal.gov")
+            hba = hostaddr.__my_gethostbyaddr("www.fnal.gov")
             self.assertTrue(isinstance(hba, types.ListType))
             self.assertTrue(len(hba) > 0)
             self.assertTrue(isinstance(hba[0], types.StringType))
@@ -76,31 +81,53 @@ class TestHostaddr(unittest.TestCase):
             self.assertTrue(isinstance(hba[2], types.ListType))
             self.assertEqual(hba[2][0],"131.225.105.239")
     
-    @unittest.skip('private, cannot test') 
+    @unittest.skip('private, cannot test directly') 
     def test___my_gethostbyname(self):
-            hbn = hostaddr._my_gethostbyname("www.fnal.gov")
+            hbn = hostaddr.__my_gethostbyname("www.fnal.gov")
             self.assertTrue(isinstance(hbn, types.StringType))
             # return value set by mock of socket.getaddrinfo
             self.assertEqual(hbn, "131.225.105.239")
     
     def test_address_to_name(self):
-            atn = hostaddr.address_to_name("127.0.0.1")
-            self.assertTrue(isinstance(atn, types.StringType))
-            self.assertTrue(len(atn) > 0)
-            self.assertEqual(atn, "localhost")
+        atn = hostaddr.address_to_name("127.0.0.1")
+        self.assertTrue(isinstance(atn, types.StringType))
+        self.assertTrue(len(atn) > 0)
+        self.assertEqual(atn, "localhost")
+        atn = hostaddr.address_to_name(":::1")
+        self.assertEqual(atn, ":::1")
+        
     
     
     def test_name_to_address(self):
-            nta = hostaddr.name_to_address("www.fnal.gov")
-            self.assertTrue(isinstance(nta, types.StringType))
-            self.assertTrue(len(nta) > 0)
-            self.assertEqual(nta, '131.225.105.239')
+        nta = hostaddr.name_to_address("www.fnal.gov")
+        self.assertTrue(isinstance(nta, types.StringType))
+        self.assertTrue(len(nta) > 0)
+        self.assertEqual(nta, '131.225.105.239')
+        nta2 = hostaddr.name_to_address(None)
+        nta3 = hostaddr.name_to_address('foo,bar.baz')
     
     def test_update_domains(self):
            d = {'invalid_domains':['999.999']} 
            hostaddr.update_domains(d)
            self.assertEqual(hostaddr.known_domains['invalid_domains']['default2'], ['999.999'])
-    
+   
+    def test__allow(self):
+        host_list = [('2620:6a:0:8421::96', 0, 0, 0), 
+                    ('131.225.191.96', 0),
+                    ('abcdefg',0),
+                    ('127.0.0.1',0),
+                    ('127.0.0.1',0,0)
+                    ]
+        for hi in host_list:
+            hostaddr._allow(hi[0])
+        with mock.patch('socket.getfqdn', new_callable=Exception):
+            ret = hostaddr._allow('131.225.191.96')
+            self.assertEqual(ret,0)
+        with mock.patch('socket.getaddrinfo', new_callable=Exception):
+            ret = hostaddr._allow('131.225.191.96')
+            self.assertEqual(ret,0)
+            
+
     def test_allow(self):
         host_list = [('2620:6a:0:8421::96', 0, 0, 0), 
                     ('131.225.191.96', 0),
@@ -108,7 +135,6 @@ class TestHostaddr(unittest.TestCase):
                     ('127.0.0.1',0),
                     ('127.0.0.1',0,0)
                     ]
-        #import pdb; pdb.set_trace()
         for hi in host_list:
             try:
                 val = hostaddr.allow(hi)
@@ -129,6 +155,11 @@ class TestHostaddr(unittest.TestCase):
             hostaddr.ifconfig_command = self.ifconfig_command
         ifn = hostaddr.interface_name("127.0.0.1")
         self.assertTrue("lo" in ifn)
+        ifn = hostaddr.interface_name(None)
+        self.assertTrue(ifn is None)
+        ifn = hostaddr.interface_name(":::1")
+        self.assertTrue(ifn is None)
+
 
 if __name__ == "__main__":
     unittest.main()
