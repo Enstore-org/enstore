af41591db27fba88ac0a4cbe82a2c5c0e6f86d1c
Ren Bauer <renbauer@fnal.gov>
2023-06-26 16:14:45 -0400

    syntax fixes

--- src/delete_at_exit.py
+++ src/delete_at_exit.py
@@ -13,13 +13,14 @@ import signal
 import Trace
 import errno
 import types
+
 try:
     import threading
-    #import thread
-    thread_support=1
-except ImportError:
-    thread_support=0
 
+    # import thread
+    thread_support = 1
+except ImportError:
+    thread_support = 0
 
 # enstore modules
 import configuration_client
@@ -29,17 +30,18 @@ import e_errors
 import file_utils
 import namespace
 
-#global value with each item for a different thread
+# global value with each item for a different thread
 thread_specific_data = threading.local()
-#global locks for the thread_specific_data
+# global locks for the thread_specific_data
 deletion_list_lock = threading.Lock()
 
-#boolean to catch a signal handler called while running another signal handler.
+# boolean to catch a signal handler called while running another signal handler.
 signal_in_progress = False
-#lock to protect kill_in_progress
+# lock to protect kill_in_progress
 sip_lock = threading.Lock()
 
-#Build thread specific data.  get_deletion_lists() should only be called
+
+# Build thread specific data.  get_deletion_lists() should only be called
 # from functions that have acquired the deletion_list_lock lock.
 def get_deletion_lists():
     global thread_specific_data
@@ -51,6 +53,7 @@ def get_deletion_lists():
 
     return thread_specific_data
 
+
 # get_deletion_lists() should only be called from functions that have
 # acquired the deletion_list_lock lock.
 def clear_deletion_lists():
@@ -58,6 +61,7 @@ def clear_deletion_lists():
 
     thread_specific_data.bfids = []
     thread_specific_data.files = []
+
 
 def register(filename):
     if filename == '/dev/null':
@@ -71,6 +75,7 @@ def register(filename):
 
     deletion_list_lock.release()
 
+
 def register_bfid(bfid):
     deletion_list_lock.acquire()
 
@@ -79,6 +84,7 @@ def register_bfid(bfid):
         _deletion_list_bfids.append(bfid)
 
     deletion_list_lock.release()
+
 
 def unregister(filename):
     if filename == '/dev/null':
@@ -92,6 +98,7 @@ def unregister(filename):
 
     deletion_list_lock.release()
 
+
 def unregister_bfid(bfid):
     deletion_list_lock.acquire()
 
@@ -103,16 +110,15 @@ def unregister_bfid(bfid):
 
 
 def delete():
-
     deletion_list_lock.acquire()
 
-    #Acquire the list of things to delete.
+    # Acquire the list of things to delete.
     del_list = get_deletion_lists()
     _deletion_list = del_list.files
     _deletion_list_bfids = del_list.bfids
 
     if not _deletion_list and not _deletion_list_bfids:
-        deletion_list_lock.release() # Avoid deadlocks.
+        deletion_list_lock.release()  # Avoid deadlocks.
         return
 
     # get a configuration server
@@ -125,20 +131,17 @@ def delete():
         Trace.log(e_errors.INFO, "Performing file cleanup for file: %s" % (f,))
 
         if os.path.exists(f):
-            #We need to obtain the parent directory for .(access)() file names.
+            # We need to obtain the parent directory for .(access)() file names.
             # The unlink() fails (without an error) if the parent directory is
             # not included in the path.  This also requires that the basename
             # not be a .(access)() name, so convert its real name.
             fs = namespace.StorageFS(f)
             try:
                 fs.rm(f)
-                _deletion_list.remove(f) #Remove from the list.
+                _deletion_list.remove(f)  # Remove from the list.
             except:
                 Trace.log(e_errors.ERROR,
-                      "Can not delete file %s from fs.\n" % (f,))
-
-
-
+                          "Can not delete file %s from fs.\n" % (f,))
 
     # Delete registered bfids.
     for b in _deletion_list_bfids:
@@ -153,7 +156,7 @@ def delete():
                 sys.stderr.write("Can not delete bfid %s from database.\n" % (b,))
                 sys.stderr.flush()
             except IOError:
-                    pass
+                pass
 
     deletion_list_lock.release()
 
@@ -163,15 +166,15 @@ def signal_handler(sig, frame):
 
     if signal_in_progress:
         try:
-            sys.stderr.write("Ignoring signal %s, previous signal handler " \
+            sys.stderr.write("Ignoring signal %s, previous signal handler "
                              "still running.\n" % (sig,))
             sys.stderr.flush()
         except IOError:
             pass
-        #Go back to the first signal handler.
+        # Go back to the first signal handler.
         return
 
-    #Try and solve the problem where a second signal can be received while
+    # Try and solve the problem where a second signal can be received while
     # the signal handler from the first signal is still running.
     signal_in_progress = True
 
@@ -199,17 +202,17 @@ def signal_handler(sig, frame):
     except (OSError, IOError):
         pass
 
-    #A lot of utilities return different values when a signal terminates them.
+    # A lot of utilities return different values when a signal terminates them.
     quit(128 + sig)
 
-def setup_signal_handling():
 
-    #This block of code is necessary on systems that use signals internally
+def setup_signal_handling():
+    # This block of code is necessary on systems that use signals internally
     # within the C libraries.  It finds the highest user defined signal.
     # Then it creates a list of all signals between the highest signal
     # and the smallest realtime signal.
     #
-    #This is important on Linux, which uses signals 32 (SIGCANCEL & SIGTIMER)
+    # This is important on Linux, which uses signals 32 (SIGCANCEL & SIGTIMER)
     # and 33 (SIGSETXID) to handle inter-thread processing of per-process
     # resources.  SIGSETXID is used to coordinate all threads in a process
     # changing thier UID and GID (and other things), which is a process
@@ -221,8 +224,8 @@ def setup_signal_handling():
     max_regular_signal = 0
     for key in dir(signal):
         value = getattr(signal, key)
-        if type(value) == types.IntType and \
-           key[0:3] == "SIG" and key not in ("SIGRTMAX", "SIGRTMIN"):
+        if isinstance(value, types.IntType) and \
+                key[0:3] == "SIG" and key not in ("SIGRTMAX", "SIGRTMIN"):
             if value > max_regular_signal:
                 max_regular_signal = value
     # Create the list from the range.
@@ -230,17 +233,17 @@ def setup_signal_handling():
     if SIGRTMIN:
         sig_leave_alone_list = range(max_regular_signal + 1, SIGRTMIN)
     else:
-        #We need this for non-Linux systems that don't define SIGRTMIN.
+        # We need this for non-Linux systems that don't define SIGRTMIN.
         sig_leave_alone_list = []
 
-    #This is a known list of signals to leave thier default handler in place.
+    # This is a known list of signals to leave their default handler in place.
     sig_leave_alone_list.append(signal.SIGTSTP)
     sig_leave_alone_list.append(signal.SIGCONT)
     sig_leave_alone_list.append(signal.SIGCHLD)
     sig_leave_alone_list.append(signal.SIGWINCH)
-    sig_leave_alone_list.append(signal.SIGPIPE)  #Use python's default.
+    sig_leave_alone_list.append(signal.SIGPIPE)  # Use python's default.
 
-    #Handle all signals not in the known skip list.
+    # Handle all signals not in the known skip list.
     for sig in range(1, signal.NSIG):
         if sig not in sig_leave_alone_list:
             try:
@@ -255,16 +258,18 @@ def setup_signal_handling():
                 except IOError:
                     pass
 
-#Simply ignoring the pychecker warning really isn't a great solution.  A
+
+# Simply ignoring the pychecker warning really isn't a great solution.  A
 # different name other than "quit" sould really be used.  It is just used
 # in so many places.
 __pychecker__ = "no-shadowbuiltin"
-def quit(exit_code=1):
 
-    #Perform cleanup.
+
+def quit(exit_code=1):
+    # Perform cleanup.
     delete()
 
-    #The os._exit() call below does not flush out the contents in file
+    # The os._exit() call below does not flush out the contents in file
     # descriptor buffers.  To get stdout and stderr to do this, we must do so
     # explicitly, before calling os._exit().
     try:
@@ -273,7 +278,7 @@ def quit(exit_code=1):
     except IOError:
         pass
 
-    #Exit in a unclean way.
+    # Exit in a unclean way.
     ### Note MWZ 2-26-2004: There is (likely) a reason why this has always
     ### been done with os._exit(), but I don't know what it is...
     os._exit(exit_code)
